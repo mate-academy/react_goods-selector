7367cc6438283ba1e339688993830b6ece8861dd
ArtemiiKab <artemiikab@gmail.com>
2020-08-31 14:41:44 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +14,56 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+function App() {
+  const [selectedGood, setSelectedGood] = useState('');
+  const goods = [...document.querySelectorAll('.good')];
+
+  function selectGood(target, el) {
+    setSelectedGood(el);
+    cleanGoodClass();
+    target.classList.add('selected');
+  }
+
+  function removeSelectedGood() {
+    setSelectedGood('');
+    cleanGoodClass();
+  }
+
+  function cleanGoodClass() {
+    for (let i = 0; i < goods.length; i += 1) {
+      goods[i].className = 'good';
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="selectedGood">
+        <h1>
+          Selected good:
+          {selectedGood}
+        </h1>
+        <button
+          className="selectedGood__btnX"
+          onClick={removeSelectedGood}
+          type="button"
+        >
+          X
+        </button>
+      </div>
+      {
+        goodsFromServer.map(el => (
+          <button
+            onClick={event => selectGood(event.target, el)}
+            className="good"
+            key={Math.random()}
+            type="button"
+          >
+            {el}
+          </button>
+        ))
+      }
+    </div>
+  );
+}
 
 export default App;
