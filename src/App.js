e16ec04dbf835c8b83db0f2e031fcdc72ed24fe9
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-31 16:00:51 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,63 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+// document.addEventListener('keydown', function (event) {
+//   if (event.target.tagName == "BUTTON") {
+//     console.log(56456)
+//     const arr =[];
+//     arr.push(event.target)
+//   }
+// })
+class App extends React.Component {
+  state = {
+    selectedGoods: '-',
+    elem: '',
+  }
+
+  selectedElement;
+
+  onSelect = (event) => {
+    this.setState({
+      selectedGoods: event.target.textContent,
+      elem: event.target.textContent,
+    });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {selectedGoods}
+        </h1>
+        {goodsFromServer.map(item => (
+          <button
+            type="button"
+            className={this.state.elem === item ? 'selected' : ''}
+            onClick={this.onSelect}
+          >
+            {item}
+          </button>
+        ))}
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              selectedGoods: '-',
+            });
+            const cleared = document.querySelector('.selected');
+
+            cleared.className = '';
+          }
+          }
+        >
+          X
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
