e6dc917861e2b6cd6a832fd589a85b8422e44cc1
Maxym Chernyavskiy <masyatya@gmail.com>
2020-09-02 18:06:57 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,74 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    select: '',
+    selectText: '-',
+  }
+
+  clickHandler = (even) => {
+    if (this.state.select !== '') {
+      this.state.select.className = 'button';
+    }
+
+    this.setState({ selectText: even.target.textContent });
+    this.setState({ select: even.target });
+    this.state.select.className = 'selected';
+  };
+
+  handlerClose = () => {
+    const { selectText, select } = this.state;
+
+    if (selectText === '-') {
+      return;
+    }
+
+    select.className = 'button';
+
+    this.setState({
+      selectText: '-',
+      select: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="heading">
+          {'Selected good: '}
+          {this.state.selectText}
+          {this.state.selectText === '-' ? '' : (
+            <button
+              type="button"
+              onClick={this.handlerClose}
+              className="close"
+            >
+              X
+            </button>
+          )}
+
+        </h1>
+        {goodsFromServer.length}
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+            >
+              <button
+                onClick={this.clickHandler}
+                type="button"
+                className="button"
+              >
+                {good}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
