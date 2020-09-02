a6e8777b4af3d1e0939bc99c61696a0b3f3605ab
Anatasia <krajnova.stasy@gmail.com>
2020-09-02 19:29:29 +0300

    develop

--- src/App.js
+++ src/App.js
@@ -14,11 +14,73 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preparedGoods = goodsFromServer.map((good, id) => ({
+  good,
+  id,
+}));
 
-export default App;
+export class App extends React.Component {
+  state = {
+    selectedGood: 'no item selected',
+  }
+
+  toRemoveSelection = () => {
+    const selected = document.querySelector('.selected');
+
+    if (!selected) {
+      return;
+    }
+
+    selected.classList.remove('selected');
+    this.setState({ selectedGood: 'no item selected' });
+  }
+
+  toSetSelection = (element) => {
+    this.toRemoveSelection();
+
+    if (!element.classList.contains('good')) {
+      return;
+    }
+
+    element.classList.add('selected');
+    this.setState({ selectedGood: element.textContent });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <section className="heading">
+          <h1>
+            {'Selected good: - '}
+            {this.state.selectedGood}
+          </h1>
+
+          <button
+            className="heading__button"
+            type="button"
+            onClick={() => this.toRemoveSelection()}
+          >
+            X
+          </button>
+        </section>
+
+        <ul
+          className="goods"
+        >
+          {preparedGoods.map(item => (
+            <li>
+              <button
+                key={item.id}
+                type="button"
+                className="good"
+                onClick={event => this.toSetSelection(event.target)}
+              >
+                {item.good}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
