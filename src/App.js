e537ff43bbc763e92e164895eba5e5c54a2790d2
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-09-02 16:31:33 +0300

    goods selection solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,67 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    activeElement: null,
+    selectedGood: '-',
+  };
+
+  removeActiveClass = () => {
+    const { activeElement } = this.state;
+
+    if (activeElement) {
+      activeElement.classList.remove('active');
+    }
+  };
+
+  selectedGoodHandler = (e, good) => {
+    this.removeActiveClass();
+
+    e.target.classList.add('active');
+
+    this.setState({
+      activeElement: e.target,
+      selectedGood: good,
+    });
+  };
+
+  clearGoodsListHandler = () => {
+    this.setState({
+      selectedGood: '-',
+    });
+
+    this.removeActiveClass();
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          onClick={this.clearGoodsListHandler}
+          className="clear"
+          type="button"
+        >
+          Clear
+        </button>
+        <h1>{`Selected good: ${selectedGood}`}</h1>
+
+        {goodsFromServer.map(good => (
+          <button
+            key={good}
+            onClick={e => this.selectedGoodHandler(e, good)}
+            type="button"
+            className="button"
+          >
+            {good}
+          </button>
+        ))}
+
+      </div>
+    );
+  }
+}
 
 export default App;
