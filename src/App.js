3c587d396ca78a78a04f2cc3825f120c6bde1d64
Kostya Sabada <kostyasabada@gmail.com>
2020-09-03 12:55:16 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import './App.scss';
 
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+class App extends React.Component {
+  state = {
+    goodsFromServer: [
+      'Dumplings',
+      'Carrot',
+      'Eggs',
+      'Ice cream',
+      'Apple',
+      'Bread',
+      'Fish',
+      'Honey',
+      'Jam',
+      'Garlic',
+    ],
+    word: '',
+  }
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+  changeWord = (message) => {
+    this.setState({ word: message });
+  };
+
+  clearAll = () => {
+    this.setState({ word: '' });
+  }
+
+  render() {
+    const { goodsFromServer, word } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{`Selected good: - ${word}`}</h1>
+        <button
+          type="button"
+          className="clear"
+          onClick={this.clearAll}
+        >
+          X
+        </button>
+        {goodsFromServer.map(goods => (
+          <li
+            key={goods}
+            className="item"
+          >
+            <button
+              className={`button ${word === goods && 'selected'}`}
+              type="button"
+              onClick={(event) => {
+                this.changeWord(goods);
+              }}
+            >
+              {goods}
+            </button>
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
