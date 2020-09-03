94e4ffb99b598dd1c7d7e15f80f4efb5fb37382d
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-09-03 15:08:40 +0300

    basic solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,63 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    current: '___',
+    previous: null,
+  };
+
+  selectHandler = (event) => {
+    const li = event.target;
+    const { state } = this;
+
+    if (!li.classList.contains('good--selected')) {
+      li.classList.add('good--selected');
+      state.previous && state.previous.classList.remove('good--selected');
+      state.previous = li;
+      this.setState({ current: li.innerText });
+    } else {
+      li.classList.remove('good--selected');
+      state.previous = null;
+      this.setState({ current: '___' });
+    }
+  }
+
+  resetHandler = (event) => {
+    if (this.state.previous) {
+      this.state.previous.classList.remove('good--selected');
+      this.state.previous = null;
+      this.setState({ current: '___' });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {this.state.current}
+        </h1>
+
+        <button type="button" onClick={this.resetHandler}>Clear</button>
+
+        <div>
+          {goodsFromServer.map(good => (
+            <button
+              type="button"
+              key={good}
+              className="good"
+              onClick={this.selectHandler}
+            >
+              {good}
+            </button>
+          ))}
+        </div>
+
+        {goodsFromServer.length}
+      </div>
+    );
+  }
+}
 
 export default App;
