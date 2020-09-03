354d266248f4523ccb27fb64dca342e01f20f9d3
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-09-03 12:41:22 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-// import cn from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -18,25 +17,32 @@ const goodsFromServer = [
 class App extends React.Component {
   state = {
     status: 'Your bag is empty',
+    selected: '',
   }
 
   savedGoods = (event) => {
-    this.setState({ status: `${event.target.textContent} is in basket` });
+    this.setState({
+      status: `${event.target.textContent} is in basket`,
+      selected: event.target.textContent,
+    });
   };
 
   render() {
-    const { status } = this.state;
+    const { status, selected } = this.state;
 
     return (
       <div className="App">
-        <h1 className="selected">{status}</h1>
+        <h1 className=" status">{status}</h1>
         <ul className="goods">
           {goodsFromServer.map(goods => (
             <li key={goods}>
               <button
                 type="button"
                 onClick={this.savedGoods}
-                className="goods__item"
+                className={goods === selected
+                  ? 'selected goods__item'
+                  : 'goods__item'
+                }
               >
                 {goods}
               </button>
@@ -47,7 +53,10 @@ class App extends React.Component {
           type="button"
           className="remove"
           onClick={() => {
-            this.setState({ status: 'Your bag is empty' });
+            this.setState({
+              status: 'Your bag is empty',
+              selected: '',
+            });
           }}
         >
           Remove
