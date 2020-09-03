e3fbc2b01a278c3dd6baf646a4effea150af7903
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-09-03 09:09:49 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+// import cn from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,46 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    status: 'Your bag is empty',
+  }
+
+  savedGoods = (event) => {
+    this.setState({ status: `${event.target.textContent} is in basket` });
+  };
+
+  render() {
+    const { status } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="selected">{status}</h1>
+        <ul className="goods">
+          {goodsFromServer.map(goods => (
+            <li key={goods}>
+              <button
+                type="button"
+                onClick={this.savedGoods}
+                className="goods__item"
+              >
+                {goods}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className="remove"
+          onClick={() => {
+            this.setState({ status: 'Your bag is empty' });
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
