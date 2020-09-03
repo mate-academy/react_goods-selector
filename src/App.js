78b8d7229af8aec4a9dff48413dc553da5e8eafa
Alisa <cheshyr.alisa@gmail.com>
2020-09-03 16:46:45 +0300

    sol

--- src/App.js
+++ src/App.js
@@ -14,11 +14,55 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: '-',
+  }
+
+  select = (event) => {
+    this.setState({
+      selectedGood: event.target.textContent,
+    });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {' '}
+          {selectedGood}
+        </h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              selectedGood: '-',
+            });
+            const toClear = document.querySelector('.selected');
+
+            if (toClear) {
+              toClear.className = '';
+            }
+          }
+          }
+        >
+          X
+        </button>
+        {goodsFromServer.map(good => (
+          <button
+            type="button"
+            className={selectedGood === good ? `selected` : ''}
+            onClick={this.select}
+          >
+            {good}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
