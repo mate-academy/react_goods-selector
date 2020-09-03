633e46a695ee7453d3cd8a6f1ab8c0f2e99dfe9a
martakupka <marta-k2008@ukr.net>
2020-09-03 21:23:07 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,65 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: '-',
+  }
+
+  selectingClickHandler = (good) => {
+    this.setState({
+      selectedGood: `${good}`,
+    });
+  }
+
+  removingClickHandler = () => {
+    this.setState({
+      selectedGood: '-',
+    });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <div className="heading">
+          <h1 className="heading__title">
+            {`Selected good: ${selectedGood}`}
+          </h1>
+          <button
+            type="button"
+            className="heading__button"
+            onClick={() => (
+              this.removingClickHandler()
+            )}
+          >
+            X
+          </button>
+        </div>
+
+        <ul className="goods">
+          {goodsFromServer.map(good => (
+            <li key={good}>
+              <button
+                type="button"
+                className={
+                  `goods__button ${selectedGood === `${good}`
+                    ? 'goods__button--active'
+                    : ''}`
+                }
+                onClick={() => (
+                  this.selectingClickHandler(good)
+                )}
+              >
+                {good}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
