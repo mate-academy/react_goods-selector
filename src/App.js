06be524322aa39f2175bc2e3454913b8805c7f47
artsashko <artyomsashko@gmail.com>
2020-09-04 00:11:58 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,52 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    selectedGood: '',
+  }
+
+  clickHandler = (good) => {
+    this.setState({
+      selectedGood: good || '',
+    });
+  }
+
+  render() {
+    const { goods, selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <div className="goods">
+          <h1 className="goods__title">
+            {`Selected good: - ${selectedGood}`}
+          </h1>
+          <button
+            onClick={() => this.clickHandler()}
+            type="submit"
+            className={classNames('goods__clear', {
+              invisible: !selectedGood,
+            })}
+          >
+            X
+          </button>
+          {goods.map(good => (
+            <button
+              key={good}
+              onClick={() => this.clickHandler(good)}
+              type="submit"
+              className={classNames('goods__button', {
+                goods__button_selected: selectedGood === good,
+              })}
+            >
+              {good}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
