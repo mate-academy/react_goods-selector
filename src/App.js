770ca005e76ef018b823905d21b462ea5d16dedd
ChrisZinch <chrispakulyak@gmail.com>
2020-09-04 12:22:45 +0300

    first solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,53 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: '',
+  };
+
+  saveHandler = (event) => {
+    this.setState({ selectedGood: event.target.textContent });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="app">
+        <h1>
+          Selected good: -
+          {selectedGood}
+          {selectedGood
+            ? (
+              <button
+                type="button"
+                onClick={this.saveHandler}
+                className="smallButton"
+              />
+            )
+            : ''
+          }
+        </h1>
+        {goodsFromServer.map(product => (
+          <ul>
+            <button
+              type="button"
+              onClick={this.saveHandler}
+              className={selectedGood === product
+                ? 'clicked'
+                : null
+              }
+            >
+              <li key={product}>
+                {product}
+              </li>
+            </button>
+          </ul>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
