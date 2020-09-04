233f7f88aad0fb3e320f406c8bf569c81f1ec7e4
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-09-04 11:03:32 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
 import './App.scss';
 
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+class App extends React.Component {
+  state = {
+    goodsFromServer: [
+      'Dumplings',
+      'Carrot',
+      'Eggs',
+      'Ice cream',
+      'Apple',
+      'Bread',
+      'Fish',
+      'Honey',
+      'Jam',
+      'Garlic',
+    ],
+    clicked: '',
+  };
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+  clickWorker = (item) => {
+    this.setState({ clicked: item });
+  }
+
+  render() {
+    const { goodsFromServer, clicked } = this.state;
+
+    return (
+
+      <div className="App">
+        <h1>
+          {`Selected good: - ${clicked}`}
+
+          {clicked ? (
+            <button
+              type="button"
+              onClick={() => {
+                this.clickWorker('');
+              }}
+            >
+              X
+            </button>
+          )
+            : null
+          }
+
+        </h1>
+        <ul>
+          {goodsFromServer.map(item => (
+            <li>
+              <button
+                type="button"
+                className={clicked === item ? 'selected' : null}
+                key={item}
+                onClick={() => {
+                  this.clickWorker(item);
+                }}
+              >
+                {item}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
