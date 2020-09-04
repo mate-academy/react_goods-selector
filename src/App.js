cae464d1e9318d3b59cfd16dc0842640dd30ae77
sobchuksasha <sobchuksasha1995@gmail.com>
2020-09-04 11:34:42 +0300

    version1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,50 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: '-',
+  };
+
+  handleClick = (event) => {
+    this.setState({
+      selectedGood: event.target.textContent,
+    });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{`Selected good: ${this.state.selectedGood}`}</h1>
+        <div className="products">
+          {goodsFromServer.map(elem => (
+            <button
+              key={elem}
+              type="button"
+              className={classNames({
+                products__product: true,
+                products__product_active: elem === selectedGood,
+              })}
+              onClick={this.handleClick}
+            >
+              {elem}
+            </button>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="product-clear"
+          onClick={() => this.setState({
+            selectedGood: '-',
+          })}
+        >
+          x
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
