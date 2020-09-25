15378af7aa236b2062dec0776c6212378f226802
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-09-25 18:10:56 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +17,77 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export default class App extends React.Component {
+  state = {
+    countGoods: 0,
+    nameGood: '',
+    arrSelectedItem: [],
+  }
 
-export default App;
+  electNextGoods = (e) => {
+    const button = document.querySelector('.btn');
+
+    if (e.ctrlKey) {
+      e.target.classList.toggle('electGood');
+      this.state.arrSelectedItem.push(e.target);
+      button.classList.add('active');
+    } else {
+      this.state.arrSelectedItem
+        .forEach(item => item.classList.remove('electGood'));
+      this.state.arrSelectedItem = [];
+      e.target.classList.add('electGood');
+      this.state.arrSelectedItem.push(e.target);
+      this.setState(state => (state.arrSelectedItem));
+    }
+  }
+
+  cleanSelectedItems = (button) => {
+    this.state.arrSelectedItem
+      .forEach(item => item.classList.remove('electGood'));
+    button.classList.remove('active');
+    this.state.nameGood = '';
+    this.state.arrSelectedItem = [];
+    this.setState(state => (state.nameGood));
+  }
+
+  render() {
+    return (
+      (
+        <div className="App">
+          <div className="title">
+            <h1 className="titleH1">
+              Selected good: -
+              {' '}
+              {this.state.nameGood}
+            </h1>
+            <button
+              type="button"
+              className="btn"
+              onClick={(event) => {
+                this.cleanSelectedItems(event.target);
+              }}
+            >
+              Clear selected
+            </button>
+
+          </div>
+          <ul>
+            {goodsFromServer.map(good => (
+              <li
+                key={good}
+                className="itemInList"
+                tabIndex={this.state.countGoods++}
+                onClick={(event) => {
+                  this.state.nameGood = event.target.textContent;
+                  this.electNextGoods(event);
+                }}
+              >
+                {good}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    );
+  }
+}
