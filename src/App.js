91e2196ea1232b08e3f3d73b1467397900492693
Dima Diachenko <zarg129@gmail.com>
2020-10-16 17:59:14 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { element } from 'prop-types';
 import React from 'react';
 import './App.scss';
+
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +16,59 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selected: [],
+  };
+
+  handleSelect = (e, good) => {
+    let { selected } = this.state
+
+    if (e.ctrlKey && !selected.includes(e.target.innerText)) {
+      this.setState({ selected: [...selected, good]})
+    } else {
+      this.setState( {selected: [good]})
+    }
+  }
+
+  clearButton = () => {
+    this.setState( { selected: []} );
+  }
+
+  render() {
+    const { selected } = this.state
+  return (
+    <div className="App">
+      <h1>Selected good: 
+        {' '}
+      {selected.join(', ')}
+      <button type='button' className='clear' onClick={this.clearButton}>
+        {' '}
+        X
+      </button>
+      </h1>
+      <div className='select-list'>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li>
+              <button
+            key={good}
+            style={{cursor: 'pointer'}}
+            className={selected.includes(good) ? 'active':''}
+            onClick={(event)=>  this.handleSelect(event,good)}
+          >
+            {good}
+          </button>
+            </li>
+          
+        ))}
+        </ul>
+        
+      </div>
+      
+    </div>
+  );
+  }
+};
 
 export default App;
