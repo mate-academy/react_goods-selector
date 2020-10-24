fcf70e02702c5409a6ba839e68d11e164920978f
VoduDay <andriy.maniak@gmail.com>
2020-10-24 17:07:43 +0300

    class for selected items fixed

--- src/components/SelectButton/SelectButton.js
+++ src/components/SelectButton/SelectButton.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './SelectButton.scss';
 
-export const SelectButton = ({ handleClick, goodName, isSelected }) => {
-  const classes = isSelected
-    ? 'SelectButton SelectButton--active'
-    : 'SelectButton';
-
-  return (
-    <button
-      className={classes}
-      type="button"
-      onClick={handleClick}
-    >
-      {goodName}
-    </button>
-  );
-};
+export const SelectButton = ({ handleClick, goodName, isSelected }) => (
+  <button
+    className={
+      classNames(
+        'SelectButton',
+        { 'SelectButton--active': isSelected },
+      )
+    }
+    type="button"
+    onClick={handleClick}
+  >
+    {goodName}
+  </button>
+);
 
 SelectButton.propTypes = {
   handleClick: PropTypes.func.isRequired,
