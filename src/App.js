ae3e2f11f0a6be594e787fd198e78d0e24c890da
Evgeniy <more.inspirationn@gmail.com>
2020-10-27 19:20:46 +0200

    Solution Goods selector, part 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +14,82 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
+const goods = goodsFromServer.map(
+  (good, index) => ({
+    name: good,
+    id: index,
+  }),
 );
+
+class App extends Component {
+  state = {
+    selected: '',
+  }
+
+  targetGood = (good) => {
+    this.setState({
+      selected: good.name,
+    });
+  }
+
+  clear = () => {
+    this.setState({
+      selected: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          Selected good:
+          {' '}
+          <span className="App__selected-good">
+            {this.state.selected}
+          </span>
+        </h1>
+
+        <button
+          className="App__btn App__btn--clear"
+          type="button"
+          onClick={this.clear}
+        >
+          Clear
+        </button>
+
+        <div className="App__products">
+          {goods.map((good) => {
+            let isActive;
+
+            good.name === this.state.selected
+              ? isActive = 'App--active'
+              : isActive = '';
+
+            return (
+              <div
+                key={good.id}
+                className="App__product"
+              >
+                <p className={`${isActive}`}>
+                  {good.name}
+                </p>
+
+                <button
+                  className="App__btn"
+                  type="submit"
+                  onClick={() => {
+                    this.targetGood(good);
+                  }}
+                >
+                  Submit
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
