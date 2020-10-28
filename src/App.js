6319234ae5fffab8c1684bc00051f7b610290680
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-28 17:05:54 +0200

    react goods selector

--- src/App.js
+++ src/App.js
@@ -14,11 +14,75 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  selectCancelGood = (item) => {
+    this.setState((state) => {
+      if (!state.selectedGoods.includes(item)) {
+        return {
+          selectedGoods: [...state.selectedGoods, item],
+        };
+      }
+
+      const itemIndex = state.selectedGoods.indexOf(item);
+
+      state.selectedGoods.splice(itemIndex, 1);
+
+      return {
+        selectedGoods: state.selectedGoods,
+      };
+    });
+  }
+
+  clearSelectedGood = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__heading">
+          {`Selected good: ${selectedGoods.join(', ')}`}
+        </h1>
+        <button
+          type="button"
+          className="button App__button-clear"
+          onClick={this.clearSelectedGood}
+        >
+          X
+        </button>
+
+        <ul className="App__list">
+          {goodsFromServer.map(item => (
+            <li
+              key={item}
+              className={selectedGoods.indexOf(item) !== -1
+                ? 'App__selected'
+                : 'App__list-item'
+              }
+            >
+              {item}
+
+              <button
+                type="button"
+                className="button App__button-select"
+                onClick={() => this.selectCancelGood(item)}
+              >
+                {selectedGoods.indexOf(item) === -1 ? 'Add' : 'Remove'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
