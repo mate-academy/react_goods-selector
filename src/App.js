2b23536c9093392a3b58b653366320513738c222
Evgenii <voznukzhenia@gmail.com>
2020-10-28 10:56:20 +0200

    goods selector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Good } from './components/Good/Good';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,79 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  toogleGoods = (good) => {
+    this.setState((prevState) => {
+      const temp = [...prevState.selectedGoods];
+
+      if (temp.includes(good)) {
+        return ({
+          selectedGoods: temp.filter(item => item !== good),
+        });
+      }
+
+      temp.push(good);
+
+      return {
+        selectedGoods: temp,
+      };
+    });
+  }
+
+  clearSelected = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__selected">
+          Selected Goods:
+          {' '}
+          {selectedGoods.join(', ')}
+        </h1>
+        <ul className="App__list">
+          {goodsFromServer.map((good) => {
+            let nameClass = 'good__button';
+
+            if (this.state.selectedGoods.includes(good)) {
+              nameClass = 'good__button--active';
+            }
+
+            return (
+              <Good
+                key={good}
+                nameOfClass={nameClass}
+                nameOfGood={good}
+                click={
+                  () => {
+                    this.toogleGoods(good);
+                  }
+                }
+              />
+            );
+          })}
+          <button
+            type="button"
+            className="App__clear"
+            onClick={
+              () => {
+                this.clearSelected();
+              }
+            }
+          >
+            Clear
+          </button>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
