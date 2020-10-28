27c7fc91a4530fc9fdd008ee2414398230cea0d5
Olena kuzii <okuziy@gmail.com>
2020-10-28 17:13:07 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Product } from './components/Product';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,54 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const goodsItems = goodsFromServer.map((good, index) => ({
+  id: index,
+  name: good,
+}));
+
+class App extends React.Component {
+  state = {
+    selectedGood: [],
+  };
+
+  getProduct = (event) => {
+    this.setState({ selectedGood: [event.target.textContent] });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="app">
+        <div className="header">
+          <h1 className="heading">
+            {`Selected good: ${selectedGood}`}
+          </h1>
+
+          <button
+            className="clear-button"
+            type="button"
+            onClick={() => {
+              this.setState({ selectedGood: [] });
+            }}
+          >
+            X
+          </button>
+        </div>
+        <ul className="list">
+          {goodsItems.map(good => (
+            <li key={good.id} className="list__item">
+              <Product
+                selectedGoods={selectedGood}
+                onClick={this.getProduct}
+                good={good}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Product.scss';
+
+export const Product = ({ selectedGoods, onClick, good }) => (
+  <button
+    className={selectedGoods.includes(good.name)
+      ? 'button button__active'
+      : 'button'}
+    onClick={event => onClick(event)}
+    type="button"
+  >
+    {good.name}
+  </button>
+);
+
+Product.propTypes = {
+  selectedGoods: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onClick: PropTypes.func.isRequired,
+  good: PropTypes.string.isRequired,
+};
