201848f0b4284173ebd9fe41fb5811036e64d8e4
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-10-28 11:38:41 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,56 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: '-',
+  };
+
+  resetSelectedGood = () => {
+    this.setState({ selectedGood: '-' });
+  };
+
+  selectGood = (good) => {
+    this.setState({ selectedGood: good });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+        </h1>
+        <h2>{selectedGood}</h2>
+        {selectedGood !== '-' && (
+          <button
+            type="button"
+            className="buttonReset"
+            onClick={this.resetSelectedGood}
+          >
+            X
+          </button>
+        )}
+        <div>
+          {goodsFromServer.map(good => (
+            <button
+              type="button"
+              key={good}
+              className={classNames('good', {
+                'good--selected': good === selectedGood,
+              })}
+              onClick={() => {
+                this.selectGood(good);
+              }}
+            >
+              {good}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
