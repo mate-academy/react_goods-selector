6ecc22433cd4b944b9a0e67d113dae6faa846d81
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-28 17:00:01 +0200

    optinal task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './components/Button/Button';
 import './App.scss';
 
 const goodsFromServer = [
@@ -16,65 +17,77 @@ const goodsFromServer = [
 
 class App extends React.Component {
   state = {
-    selectedGood: null,
-  }
-
-  static getGoodClassName(selectedGood, good) {
-    return (
-      `app__good 
-      ${selectedGood === good
-        ? 'app__good--is-active'
-        : ''}`
-    );
+    selectedGoods: [],
   }
 
   chooseGood(good) {
-    this.setState({ selectedGood: good });
+    this.setState(
+      prevState => ({ selectedGoods: [...prevState.selectedGoods, good] }),
+    );
   }
 
-  clearGood() {
-    this.setState({ selectedGood: null });
+  removeGood(good) {
+    this.setState((prevState) => {
+      const newList = prevState.selectedGoods.filter(item => item !== good);
+
+      return { selectedGoods: newList };
+    });
+  }
+
+  clearGoods() {
+    this.setState({ selectedGoods: [] });
   }
 
   render() {
-    const { selectedGood } = this.state;
+    const { selectedGoods } = this.state;
 
     return (
       <div className="app">
         <h1 className="app__header">
-          Selected good:
+          Selected goods:
           <span className="app__header-list">
             {' '}
-            {selectedGood}
+            {selectedGoods.join(', ')}
           </span>
         </h1>
         <button
           className="app__button-clear"
           type="button"
-          onClick={() => this.clearGood()}
+          onClick={() => this.clearGoods()}
         >
           X
         </button>
         <ul className="app__list">
           {goodsFromServer.map(
-            good => (
-              <li
-                className={App.getGoodClassName(selectedGood, good)}
-                key={good}
-              >
-                <button
-                  className="app__button-add"
-                  type="button"
-                  onClick={() => {
-                    this.chooseGood(good);
-                  }}
+            (good) => {
+              const isSelected = selectedGoods.some(item => item === good);
+              const className
+                = `app__good ${isSelected ? 'app__good--is-active' : ''}`;
+
+              return (
+                <li
+                  className={className}
+                  key={good}
                 >
-                  Select
-                </button>
-                {' '}
-                {good}
-              </li>
-            ),
+                  {isSelected
+                    ? (
+                      <Button
+                        text="Remove"
+                        onClick={() => this.removeGood(good)}
+                      />
+                    )
+                    : (
+                      <Button
+                        text="Add"
+                        onClick={() => this.chooseGood(good)}
+                      />
+                    )
+                  }
+                  {' '}
+                  {good}
+                </li>
+              );
+            },
           )}
         </ul>
       </div>
