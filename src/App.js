4e98a65dd5df7b4d96938c0e3b58212291f60884
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-28 18:56:47 +0200

    add goods selector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.scss';
+import 'semantic-ui-css/semantic.min.css';
+import { GoodsList } from './components/GoodsList';
+import { Header } from './components/Header';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +17,43 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const transformedGoods = goodsFromServer.map((good, index) => (
+  {
+    name: good,
+    id: index,
+  }
+));
+
+class App extends React.Component {
+  state = {
+    selectedGoods: '',
+  };
+
+  saveGood = (good) => {
+    this.setState({ selectedGoods: good });
+  }
+
+  removeGood = () => {
+    this.setState({ selectedGoods: '' });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <Header
+          selectedGoods={selectedGoods}
+          onClick={this.removeGood}
+        />
+        <GoodsList
+          selectedGoods={selectedGoods}
+          data={transformedGoods}
+          onClick={this.saveGood}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
