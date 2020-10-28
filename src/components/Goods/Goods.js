6052555b9a2000ece1f4987ae0b5e28de4848ee2
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-28 17:59:03 +0300

    fixed classNames and functional component

--- src/components/Goods/Goods.js
+++ src/components/Goods/Goods.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import cn from 'classnames';
 import './Goods.scss';
 
 import PropTypes from 'prop-types';
 
-export class Goods extends React.PureComponent {
-  render() {
-    const { good, selectedGoods, toggleSelectedGoods } = this.props;
+export const Goods = ({ good, selectedGoods, toggleSelectedGoods }) => {
+  const buttonClasses = cn(
+    'goods__button',
+    {
+      'goods__button--selected': selectedGoods.includes(good),
+    },
+  );
 
-    return (
-      <li className="goods">
-        <div className="goods__text">
-          {good}
-        </div>
-        <button
-          type="button"
-          className={selectedGoods.includes(good)
-            ? 'goods__button goods__button--selected'
-            : 'goods__button'}
-          onClick={() => {
-            toggleSelectedGoods(good);
-          }}
-        >
-          {selectedGoods.includes(good)
-            ? 'Remove'
-            : 'Add'}
-        </button>
-      </li>
-    );
-  }
-}
+  return (
+    <li className="goods">
+      <div className="goods__text">
+        {good}
+      </div>
+      <button
+        type="button"
+        className={buttonClasses}
+        onClick={() => {
+          toggleSelectedGoods(good);
+        }}
+      >
+        {selectedGoods.includes(good)
+          ? 'Remove'
+          : 'Add'}
+      </button>
+    </li>
+  );
+};
 
 Goods.propTypes = {
   good: PropTypes.string.isRequired,
