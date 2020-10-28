4dd40f68d92b1a8f5ef8802f664e563719f0a13f
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-28 14:43:42 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,72 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: null,
+  }
+
+  static getGoodClassName(selectedGood, good) {
+    return (
+      `app__good 
+      ${selectedGood === good
+        ? 'app__good--is-active'
+        : ''}`
+    );
+  }
+
+  chooseGood(good) {
+    this.setState({ selectedGood: good });
+  }
+
+  clearGood() {
+    this.setState({ selectedGood: null });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="app__header">
+          Selected good:
+          <span className="app__header-list">
+            {' '}
+            {selectedGood}
+          </span>
+        </h1>
+        <button
+          className="app__button-clear"
+          type="button"
+          onClick={() => this.clearGood()}
+        >
+          X
+        </button>
+        <ul className="app__list">
+          {goodsFromServer.map(
+            good => (
+              <li
+                className={App.getGoodClassName(selectedGood, good)}
+                key={good}
+              >
+                <button
+                  className="app__button-add"
+                  type="button"
+                  onClick={() => {
+                    this.chooseGood(good);
+                  }}
+                >
+                  Select
+                </button>
+                {' '}
+                {good}
+              </li>
+            ),
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
