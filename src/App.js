385198a58043c6e88bfb991613d54ef0199e07ce
Lancaelot <Lancaelot@gmail.com>
2020-10-28 17:36:03 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+
+import React, { PureComponent } from 'react';
 import './App.scss';
+import 'semantic-ui-css/semantic.min.css';
+import GoodsList from './Components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +17,80 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preparedGoods = goodsFromServer.map((item, index) => (
+  {
+    name: item,
+    id: index,
+  }
+));
+
+class App extends PureComponent {
+  state = {
+    selectedGoods: [],
+  }
+
+  SelectGoodsHandler = ({ name, id }) => {
+    this.setState((state) => {
+      if (!state.selectedGoods.includes(` ${name}`)) {
+        return {
+          selectedGoods: [...state.selectedGoods, ` ${name}`],
+        };
+      }
+
+      const goodIndex = state.selectedGoods.indexOf(` ${name}`);
+
+      state.selectedGoods.splice(goodIndex, 1);
+
+      return {
+        selectedGoods: state.selectedGoods,
+      };
+    });
+  }
+
+  ClearGoodsHandler = () => {
+    this.setState(
+      {
+        selectedGoods: [],
+      },
+    );
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>
+          Selected goods:
+          {this.state.selectedGoods}
+        </h1>
+        <GoodsList
+          preparedGoods={preparedGoods}
+          clickHandler={this.SelectGoodsHandler}
+        />
+        <button
+          className="fluid ui red button"
+          onClick={this.ClearGoodsHandler}
+          type="button"
+        >
+          Clear All
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
+
+/* this.setState(prevState => {
+  if (!prevState.selectedGoods.includes(` ${name}`)) {
+    return ({
+      selectedGoods: [...prevState.selectedGoods, ` ${name}`],
+    });
+  }
+
+  const currIndex = this.state.selectedGoods.indexOf(` ${name}`);
+
+  return ({
+    selectedGoods: this.state.selectedGoods.splice(currIndex, 1),
+  });
+});
+ */
---
+++ src/Components/GoodsList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Good from './Good';
+
+const GoodsList = ({ preparedGoods, clickHandler }) => (
+  <>
+    {preparedGoods.map(good => (
+      <button
+        type="button"
+        className="ui animated button"
+        onClick={() => clickHandler(good)}
+        key={good.id}
+      >
+        <Good item={good.name} />
+      </button>
+    ))
+    }
+  </>
+);
+
+GoodsList.propTypes = {
+  preparedGoods: PropTypes.string.isRequired,
+  clickHandler: PropTypes.element.isRequired,
+};
+
+export default GoodsList;
