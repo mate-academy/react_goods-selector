3e89c0ca0e931efc425e631331ae206c50fd1ba7
Atteroot <yuora556@gmail.com>
2020-10-28 16:40:23 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +14,65 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preparedGoods = goodsFromServer.map((good, index) => ({
+  name: good,
+  id: index,
+}));
+
+export class App extends Component {
+  state = {
+    selectedGood: '-',
+  };
+
+  addGood = (good) => {
+    this.setState({ selectedGood: good });
+  }
+
+  clearGood = () => {
+    this.setState({ selectedGood: '-' });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {selectedGood}
+          <button
+            className="button"
+            type="button"
+            onClick={this.clearGood}
+          >
+            Clear
+          </button>
+        </h1>
+
+        <ul className="good-list">
+          {preparedGoods.map(good => (
+            <li
+              key={good.id}
+              className="good-list__item"
+            >
+              {good.name}
+
+              <button
+                className="button"
+                type="button"
+                onClick={() => {
+                  this.addGood(good.name);
+                }}
+              >
+                Add
+              </button>
+
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
