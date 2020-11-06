cc038edaac8babc759dd887d983e1eaa89e782fa
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-06 14:24:24 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-parens */
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +16,46 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = { selected: 'Apple' };
+
+  render() {
+    const { selected } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{`Selected good: ${selected}`}</h1>
+        <ul>
+          {goodsFromServer.map((item) => (
+            <div
+              key={item}
+              style={{
+                display: 'flex',
+                margin: '10px 0',
+              }}
+            >
+              <li className={selected === item ? 'selected' : ''}>{item}</li>
+              <button
+                type="button"
+                style={{ margin: '0 5px' }}
+                onClick={() => this.setState({ selected: item })}
+              >
+                Select
+              </button>
+              <button
+                type="button"
+                onClick={() =>
+                  item === selected && this.setState({ selected: '' })
+                }
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
