a23cf7af7e162bc447ec9f64e7c47797c1c5a5ea
Olga Rubalska <olga.anikeieva@gmail.com>
2021-01-14 20:23:32 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,95 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+// eslint-disable-next-line react/prefer-stateless-function
+export class App extends React.Component {
+  state = {
+    selectedGood: [],
+    domElem: [],
+  }
 
-export default App;
+  clearSelection = () => {
+    if (this.state.selectedGood.length !== 0) {
+      this.setState((state) => {
+        state.domElem.map(elem => elem.classList.remove('selected'));
+
+        return {
+          domElem: state.domElem,
+        };
+      });
+      this.setState({
+        selectedGood: [],
+        domElem: [],
+      });
+    }
+  }
+
+  clickHandler({ target }, good) {
+    const targetElem = target.closest('li');
+
+    if (this.state.selectedGood.includes(good)) {
+      this.setState((state) => {
+        const index = state.selectedGood.indexOf(good);
+
+        state.selectedGood.splice(index, 1);
+        state.domElem[index].classList.remove('selected');
+        state.domElem.splice(index, 1);
+
+        return {
+          selectedGood: state.selectedGood,
+          domElem: state.domElem,
+        };
+      });
+    } else {
+      this.setState((state) => {
+        state.selectedGood.push(good);
+        const length = state.domElem.push(targetElem);
+
+        state.domElem[length - 1].classList.add('selected');
+
+        return {
+          selectedGood: state.selectedGood,
+          domElem: state.domElem,
+        };
+      });
+    }
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          { selectedGood.join(', ')}
+          {' '}
+          <button
+            type="button"
+            onClick={this.clearSelection}
+          >
+            X
+          </button>
+        </h1>
+        {goodsFromServer.length}
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+            >
+              {`${good}  - `}
+              <button
+                type="button"
+                onClick={e => this.clickHandler(e, good)}
+              >
+                Add/Remove
+              </button>
+            </li>
+          ))
+          }
+        </ul>
+
+      </div>
+    );
+  }
+}
