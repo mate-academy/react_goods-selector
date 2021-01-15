b6f844d33f227e9eb0752867e5cac101df2b2d73
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-15 21:14:01 +0200

    React good selector

--- src/App.js
+++ src/App.js
@@ -14,11 +14,110 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    allGoods: goodsFromServer,
+    selectedGoods: [],
+  }
+
+  toggleClass = (e) => {
+    const listItem = e.target.parentNode;
+
+    listItem.classList.toggle('App__list-item--active');
+  }
+
+  clearAll = () => {
+    // eslint-disable-next-line
+    const selectedElements = [...document.querySelectorAll('.App__list-item--active')];
+
+    // eslint-disable-next-line
+    selectedElements.map(element => element.classList.toggle('App__list-item--active'));
+
+    this.setState({
+      selectedGoods: [],
+    });
+  }
+
+  addSelected = (e, good) => {
+    this.toggleClass(e);
+
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, good],
+    }));
+  }
+
+  removeSelected = (e, good) => {
+    this.toggleClass(e);
+    // eslint-disable-next-line
+    const newState = [...this.state.selectedGoods];
+
+    newState.splice(newState.indexOf(good), 1);
+
+    this.setState({
+      selectedGoods: newState,
+    });
+  }
+
+  renderSelected() {
+    const selected = document.querySelectorAll('.App__list-item--active');
+
+    this.setState({ selectedGoods: selected });
+  }
+
+  render() {
+    const { allGoods, selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__header">
+          Selected good:
+          {' '}
+          {selectedGoods.length}
+        </h1>
+
+        {selectedGoods.map(good => (
+          <span
+            key={good}
+            className="App__selected"
+          >
+            {`${good}, `}
+          </span>
+        ))}
+        <br />
+        <button
+          type="button"
+          className="App__clear-button"
+          onClick={this.clearAll}
+        >
+          Clear all
+        </button>
+
+        <ul className="App__list">
+          {allGoods.map(good => (
+            <li
+              className="App__list-item"
+              key={good}
+            >
+              {good}
+
+              <button
+                className="App__list-item-button"
+                type="button"
+                onClick={e => (selectedGoods.includes(good)
+                  ? this.removeSelected(e, good)
+                  : this.addSelected(e, good))}
+              >
+
+                Select / Remove
+              </button>
+
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    );
+  }
+}
 
 export default App;
