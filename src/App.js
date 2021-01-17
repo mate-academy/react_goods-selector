25334cfe164d60f81341c7d71ffab32fd9a4e225
MarchD <dubchmar@gmail.com>
2021-01-17 18:25:56 +0200

    added solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,89 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    basket: [],
+  }
+
+  toSelect = ({ target }) => {
+    target.parentElement.classList.add('selected');
+
+    const targetName = goodsFromServer.find((good) => {
+      const titleOfGood = target.parentElement.firstChild.textContent;
+
+      return good === titleOfGood;
+    });
+    const prevStateOfBasket = this.state.basket;
+
+    this.setState({ basket: [...prevStateOfBasket, targetName] });
+  };
+
+  toCancel = ({ target }) => {
+    const titleOfGood = target.parentElement.firstChild.textContent;
+    const index = this.state.basket.indexOf(titleOfGood);
+
+    this.state.basket.splice(index, 1);
+    const itemsOfBasket = this.state.basket;
+
+    itemsOfBasket.splice(1, index);
+    this.setState({ basket: [...itemsOfBasket] });
+
+    if (!this.state.basket.includes(titleOfGood)) {
+      target.parentElement.classList.remove('selected');
+    }
+  }
+
+  toReset = () => {
+    const goods = [...document.querySelectorAll('.good')];
+
+    goods.map(good => good.classList.remove('selected'));
+    this.setState({ basket: [] });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="basket">
+          <button
+            type="button"
+            className="basket__button"
+            onClick={this.toReset}
+          >
+            X
+          </button>
+
+          <h1>
+            Basket:
+            {this.state.basket.join(', ')}
+          </h1>
+        </div>
+
+        <div className="goods">
+          {goodsFromServer.map(good => (
+            <div className="good">
+              <p>{good}</p>
+              <button
+                className="button"
+                type="button"
+                onClick={this.toSelect}
+              >
+                Select
+              </button>
+
+              <button
+                className="button"
+                type="button"
+                onClick={this.toCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
