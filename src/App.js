86ac486e3983d2af8cd05d609aee835a51b9d5d7
PaulT <ptimofejev@gmail.com>
2021-01-17 13:47:35 +0200

    Selected Goods - basic version solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,85 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: null,
+    header: null,
+  }
+
+  markSelectedGood = (event, product) => {
+    const { selectedGood } = this.state;
+    const item = event.target.closest('.list__item');
+
+    if (selectedGood) {
+      selectedGood.classList.remove('list__item--selected');
+    }
+
+    item.classList.add('list__item--selected');
+
+    this.setState({
+      selectedGood: item,
+      header: product,
+    });
+  }
+
+  clearSelectedGood = (event) => {
+    const { selectedGood } = this.state;
+
+    if (selectedGood) {
+      selectedGood.classList.remove('list__item--selected');
+    }
+
+    this.setState({
+      selectedGood: null,
+      header: null,
+    });
+  }
+
+  render() {
+    const { header } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {` ${header || ''}`}
+
+          <button
+            className="button--clear"
+            type="button"
+            onClick={this.clearSelectedGood}
+          >
+            x
+          </button>
+        </h1>
+
+        <ul>
+          {goodsFromServer.map(product => (
+            <li
+              className="list__item"
+              key={product}
+            >
+              <span>
+                {product}
+              </span>
+              <button
+                type="button"
+                className="list__button"
+                onClick={(event) => {
+                  this.markSelectedGood(event, product);
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {goodsFromServer.length}
+      </div>
+    );
+  }
+}
 
 export default App;
