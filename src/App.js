ddd448180e6337d5db2d80fba027b2b759fac756
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-18 18:23:32 +0200

    soli

--- src/App.js
+++ src/App.js
@@ -14,11 +14,87 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    currentTarget: [],
+  }
+
+toggleHandler = (good) => {
+  const copy = [...this.state.currentTarget];
+
+  if (!copy.includes(good)) {
+    this.setState({ currentTarget: [...copy, good] });
+  }
+}
+
+removeItem = (good) => {
+  this.setState((prevState) => {
+    if (prevState.currentTarget.includes(good)) {
+      prevState.currentTarget
+        .splice(prevState.currentTarget
+          .indexOf(good), 1);
+    }
+
+    return {
+      prevState: prevState.currentTarget,
+    };
+  });
+}
+
+clean = () => {
+  this.setState({ currentTarget: [] });
+}
+
+render() {
+  const { goods, currentTarget } = this.state;
+
+  return (
+    <div className="App">
+      <h1>
+        Selected good: -
+        {currentTarget.join(', ')}
+      </h1>
+      <button
+        type="button"
+        onClick={() => this.clean()}
+      >
+        clean table
+      </button>
+      <ul>
+        {goods.map(good => (
+          <li
+            className={currentTarget.includes(good)
+              ? 'seclected'
+              : ''
+            }
+            key={Math.random()}
+          >
+            {good}
+            <button
+              type="button"
+              onClick={() => {
+                this.toggleHandler(good);
+              }}
+            >
+              Add
+            </button>
+
+            <button
+              type="button"
+              className={currentTarget.includes(good) && ''}
+              onClick={() => {
+                this.removeItem(good);
+              }}
+            >
+              remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+}
 
 export default App;
