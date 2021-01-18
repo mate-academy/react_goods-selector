88afe6f4e2f6b63784329e41adbdbc99129e3597
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-18 15:20:55 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,72 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  select = (e) => {
+    const { selectedGoods } = this.state;
+    const selectedElement = e.target.parentElement;
+
+    selectedElement.classList.add('selected');
+    selectedGoods.push(selectedElement.innerText.split(' ')[0]);
+
+    this.setState({ selectedGoods });
+  }
+
+  clear = (e) => {
+    const { selectedGoods } = this.state;
+    const elementToRemove = e.target.parentElement;
+    const index = selectedGoods
+      .indexOf(elementToRemove.innerText.split(' ')[0]);
+
+    elementToRemove.classList.remove('selected');
+    selectedGoods.splice(index, 1);
+
+    this.setState({ selectedGoods });
+  }
+
+  clearAll = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {selectedGoods.join(', ')}
+        </h1>
+        <button type="button" onClick={this.clearAll}>Clear</button>
+        <ul className="list">
+          {goodsFromServer.map(good => (
+            <li className="list__item" key={good}>
+              {good}
+              {' '}
+              <button
+                className="button"
+                type="button"
+                onClick={this.select}
+              >
+                Add
+              </button>
+              <button
+                className="button"
+                type="button"
+                onClick={this.clear}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
