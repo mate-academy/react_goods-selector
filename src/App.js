7ec07a1f327ad672833f50dbc6ec3b973842bf69
Zhenya <e.mezhueva@gmail.com>
2021-01-18 16:10:14 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,77 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  handlerAdd = (item) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, item],
+    }));
+  }
+
+  handlerRemove = (item) => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods
+        .filter(selectedItem => selectedItem !== item),
+    }));
+  }
+
+  handlerClear = () => {
+    this.setState(() => ({
+      selectedGoods: [],
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App app__card">
+        <h1 className="goods__title">
+          Selected goods
+        </h1>
+        <p>
+          Your cart: &nbsp;
+          <span className="selected__goods">
+            {this.state.selectedGoods.length > 0
+              ? this.state.selectedGoods.join(', ')
+              : 'empty'}
+          </span>
+        </p>
+        <button
+          className="clear"
+          type="button"
+          onClick={this.handlerClear}
+        >
+          Clear
+        </button>
+        <ul className="goods__list">
+          {goodsFromServer.map(item => (
+            <div className="list__item" key={item}>
+              <li>{item}</li>
+              <div>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => (this.handlerAdd(item))}
+                >
+                  Add
+                </button>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => (this.handlerRemove(item))}
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
