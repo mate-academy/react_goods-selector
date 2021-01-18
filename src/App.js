f5ef35078f20ec5b5768d3080bf2dad3010b7d5f
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-18 20:33:12 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,78 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+  // selected: '',
+    selectedGoods: [],
+  };
+
+  selectItem = (item) => {
+    const { selectedGoods } = this.state;
+    //  this.setState({ selected: item });
+
+    if (selectedGoods.includes(item)) {
+      const removeItem = selectedGoods.filter(x => x !== item);
+
+      this.setState({ selectedGoods: removeItem });
+    } else {
+      this.setState({ selectedGoods: [...selectedGoods, item] });
+    }
+  };
+
+  clear = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {' '}
+          {/* {this.state.selected} */}
+          {selectedGoods.join(', ')}
+        </h1>
+
+        <button
+          type="button"
+          className={classNames('button',
+            selectedGoods.length === 0 ? 'hover' : '')}
+          onClick={this.clear}
+        >
+          X
+        </button>
+
+        <ul className="list">
+          {goodsFromServer.map(item => (
+            <>
+              <li
+                key={item}
+                className={classNames('list__item',
+                  selectedGoods.includes(item) ? 'selected' : '')}
+              >
+                {/* <li
+                  key={item}
+                  className={`list__item ${this.state.selected === item
+                    ? 'selected' : ''}`}
+                > */}
+                {item}
+              </li>
+              <button
+                type="button"
+                className="button"
+                onClick={() => this.selectItem(item)}
+              >
+                {!selectedGoods.includes(item) ? 'Add' : 'Remove'}
+              </button>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
