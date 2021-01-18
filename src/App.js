2c34bbe573dfdcd32e8cdd581a0821dded818ed3
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-18 19:38:52 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,83 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const goods = goodsFromServer.map((item, i) => (
+  {
+    name: item,
+    id: i,
+  }
+));
+
+class App extends React.Component {
+  state = {
+    selectedNames: [],
+    selectedId: [],
+  }
+
+  toggleSelection = (good) => {
+    const state = { ...this.state };
+
+    if (this.state.selectedId.includes(good.id)) {
+      const index = state.selectedId.indexOf(good.id);
+
+      state.selectedId.splice(index, 1);
+      state.selectedNames.splice(index, 1);
+      this.setState(state);
+    } else {
+      state.selectedNames.push(good.name);
+      state.selectedId.push(good.id);
+      this.setState(state);
+    }
+  }
+
+  clearSelection = () => {
+    this.setState({
+      selectedNames: [],
+      selectedId: [],
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {' '}
+          {this.state.selectedNames.join(', ')}
+        </h1>
+        {this.state.selectedId.length > 0
+        && (
+          <button
+            type="button"
+            onClick={this.clearSelection}
+          >
+            Clear
+          </button>
+        )
+        }
+        <ul className="list">
+          {goods.map(good => (
+            <li
+              key={good.id}
+              className={classNames('list__item', {
+                selected: this.state.selectedId.includes(good.id),
+              })}
+            >
+              <span>{good.name}</span>
+              <button
+                type="button"
+                onClick={() => {
+                  this.toggleSelection(good);
+                }}
+              >
+                Add/Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
