0b14bfba8766a43024fee0569515b7a3cfcf12c8
Viktor Morhun <victor.morgun16@gmail.com>
2021-01-21 11:55:06 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -2,23 +2,74 @@ import React from 'react';
 import './App.scss';
 
 const goodsFromServer = [
-  'Dumplings',
+  'Juice',
   'Carrot',
-  'Eggs',
+  'Soy sauce',
   'Ice cream',
   'Apple',
   'Bread',
-  'Fish',
-  'Honey',
+  'Tahini',
+  'Maple syrup',
   'Jam',
   'Garlic',
+  'Chewing gum',
+  'Soda',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: '',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <header className="header">
+          <h1 className="header__heading">
+            Selected good:
+            {' '}
+            {selectedGood}
+          </h1>
+          {selectedGood
+          && (
+            <button
+              onClick={() => {
+                this.setState({ selectedGood: '' });
+              }}
+              className="header__button"
+              type="button"
+            >
+              X
+            </button>
+          )}
+        </header>
+        <main className="main">
+          {goodsFromServer.map(product => (
+            <div
+              className={
+                (product === selectedGood)
+                  ? 'product class1'
+                  : 'product'
+              }
+            >
+              <h3>{product}</h3>
+              <button
+                onClick={() => {
+                  this.setState({ selectedGood: product });
+                }}
+                className="product__button"
+                type="button"
+              >
+                Buy
+              </button>
+            </div>
+          ))}
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
