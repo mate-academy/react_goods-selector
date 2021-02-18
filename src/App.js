40b3a7f37072a723179bb1ea2631c09c52e56139
Taras Sidak <tarasidak1990@gmail.com>
2021-02-18 02:23:38 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,79 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const products = [];
+
+class App extends React.Component {
+  state = {
+    productName: ' none',
+    goodsList: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      goodsList: goodsFromServer.map((good, index) => ({
+        id: index,
+        name: good,
+        isSelected: false,
+      })),
+    });
+  }
+
+  toggleSelectedGoods = (selectedId, name, isSelected) => {
+    if (!isSelected) {
+      products.push(name);
+    }
+
+    if (isSelected) {
+      const deleteIndex = products.indexOf(name);
+
+      products.splice(deleteIndex, 1);
+    }
+
+    this.setState(prevState => ({
+      productName: products.length === 0 ? ' none' : products.join(', '),
+      goodsList: prevState.goodsList.map((good) => {
+        if (good.id === selectedId) {
+          return {
+            ...good,
+            isSelected: !isSelected,
+          };
+        }
+
+        return good;
+      }),
+    }));
+  }
+
+  render() {
+    const { productName, goodsList } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {productName}
+        </h1>
+        <ul>
+          {goodsList.length && goodsList.map(({ id, name, isSelected }) => (
+            <li
+              key={id}
+              className={`${isSelected ? 'list-yellow' : 'list-white'}`}
+            >
+              {name}
+              <button
+                className={`btn ${isSelected ? 'btn-red' : 'btn-green'}`}
+                type="button"
+                onClick={() => this.toggleSelectedGoods(id, name, isSelected)}
+              >
+                { isSelected ? 'Remove' : 'Add' }
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
