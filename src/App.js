aecca603ce33ecd39c84e08dbb315ea3350e5fa9
Bohdan Dobak <chudolipton@gmail.com>
2021-03-08 16:34:19 +0200

    made class component App by which render list of goods and u can select goods by clicking on it name or on button Add, also add button clear X which clear all goods in basket and add button remove which appears after selecting the good

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +16,85 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  showSelectedGood = (event) => {
+    const { target } = event;
+
+    target.classList.toggle('selected');
+
+    this.setState({
+      selectedGoods: [target.textContent],
+    });
+  }
+
+  handleSelectedGood = (event) => {
+    const good = event.target.previousSibling;
+
+    const arrayOfGoods = this.state.selectedGoods;
+
+    if (arrayOfGoods.includes(good.textContent) && arrayOfGoods.length > 0) {
+      this.setState(prevState => ({
+        selectedGoods: prevState.selectedGoods.filter(item => (
+          item !== good.textContent
+        )),
+      }));
+    }
+
+    if (!arrayOfGoods.includes(good.textContent)) {
+      this.setState(prevState => ({
+        selectedGoods: prevState.selectedGoods.concat(good.textContent),
+      }));
+    }
+  }
+
+  handleClearSelected = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <>
+        <h1>
+          {!selectedGoods.length
+            ? 'No goods selected'
+            : `${selectedGoods} in basket`
+          }
+        </h1>
+        {selectedGoods.length > 0 && (
+        <button type="button" onClick={this.handleClearSelected}>
+          X
+        </button>
+        )}
+        <ul>
+          {goodsFromServer.map(goodFromServer => (
+            <>
+              <li
+                key={goodFromServer}
+                onClick={this.showSelectedGood}
+              >
+                {goodFromServer}
+              </li>
+              <button
+                type="submit"
+                key={goodFromServer}
+                onClick={this.handleSelectedGood}
+              >
+                {selectedGoods.includes(goodFromServer) ? 'Remove' : 'Add'}
+              </button>
+            </>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
 export default App;
