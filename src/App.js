7df23c16ddacbc1682cb3c58a511cdb3fc578bae
chri911 <delightsome@bigmir.net>
2021-03-09 15:06:43 +0200

    solution advanced

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,75 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  selectGood = (good) => {
+    if (this.state.selectedGoods.includes(good)) {
+      this.setState(state => (
+        { selectedGoods: state.selectedGoods.filter(el => el !== good) }
+      ));
+    } else {
+      this.setState(state => (
+        { selectedGoods: [...state.selectedGoods, good] }
+      ));
+    }
+  }
+
+  removeGoods = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+    let header;
+
+    if (selectedGoods.length === 0) {
+      header = 'No goods selected';
+    } else if (selectedGoods.length === 1) {
+      header = `${selectedGoods}  is selected`;
+    } else {
+      header = `${selectedGoods.slice(0, -1).join(', ')}
+       and ${selectedGoods.slice(-1)} are selected`;
+    }
+
+    return (
+      <div className="App">
+        <h1 className="goods__header">
+          {header}
+          {selectedGoods.length > 0
+            && (
+            <button type="button" onClick={this.removeGoods}>
+              X
+            </button>
+            )}
+        </h1>
+        <ul className="goods__list">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={classNames(
+                'goods__item',
+                { 'goods__item--selected': selectedGoods.includes(good) },
+              )}
+
+            >
+              {good}
+              <button
+                type="button"
+                className="goods__button"
+                onClick={() => this.selectGood(good)}
+              >
+                Add/Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
