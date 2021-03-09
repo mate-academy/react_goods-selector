f2f5b2ba4315dcbde3710ceee658cf16a15956bf
Taras <seman6745@gmail.com>
2021-03-09 20:37:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,85 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state ={
+    choosenGoods: [],
+    isButtonForDeleteAllVisible: false,
+  }
+
+  addGood = ({ target }, good) => {
+    const clickedButton = target;
+
+    if (this.state.choosenGoods.includes(good)) {
+      this.setState(prevState => (
+        { choosenGoods: prevState.choosenGoods.filter(el => el !== good) }
+      ));
+
+      clickedButton.innerText = 'ADD';
+    } else {
+      this.setState(prevState => (
+        {
+          choosenGoods: [...prevState.choosenGoods, good],
+        }));
+
+      clickedButton.innerText = 'DELETE';
+      this.setState({ isButtonForDeleteAllVisible: true });
+    }
+  }
+
+  deletingAllButton = () => {
+    this.setState(
+      {
+        choosenGoods: [],
+        isButtonForDeleteAllVisible: false,
+      },
+    );
+    const buttons = document.querySelectorAll('.App__button-select');
+    // eslint-disable-next-line
+    for (let button of buttons) {
+      button.innerText = 'ADD';
+    }
+  }
+
+  render() {
+    const { choosenGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          { (choosenGoods.length === 0)
+            ? 'No goods selected'
+            : `Selected: ${choosenGoods.join(', ')}`
+          }
+        </h1>
+        {this.state.isButtonForDeleteAllVisible && (
+          <button
+            type="button"
+            onClick={this.deletingAllButton}
+            className="App__button-delete-all"
+          >
+            X
+          </button>
+        )}
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+            <li key={`${good}`}>
+              {` ${good} `}
+              <button
+                type="button"
+                className="App__button-select"
+                onClick={(event) => {
+                  this.addGood(event, good);
+                }}
+              >
+                ADD
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
