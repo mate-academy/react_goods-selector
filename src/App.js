ae3f5090e6bea808b2909eb256d8f42eb140e097
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-10 15:21:08 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,63 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  toggleGoods = (good) => {
+    if (this.state.goods.includes(good)) {
+      this.setState(state => (
+        { goods: state.goods.filter(item => item !== good) }
+      ));
+    } else {
+      this.setState(state => (
+        { goods: [...state.goods, good] }
+      ));
+    }
+  }
+
+  clearGoods = () => {
+    this.setState({
+      goods: [],
+    });
+  }
+
+  render() {
+    let goodsText;
+
+    if (this.state.goods.length === 0) {
+      goodsText = 'No goods selected';
+    } else if (this.state.goods.length === 1) {
+      goodsText = `Selected ${this.state.goods}`;
+    } else {
+      goodsText = `${this.state.goods.slice(0, -1).join(', ')}
+      and
+      ${this.state.goods.slice(-1)} selected`;
+    }
+
+    return (
+      <div className="App">
+        <h1>
+          {goodsText}
+          {this.state.goods
+          && (<button type="button" onClick={this.clearGoods}>X</button>
+          )}
+        </h1>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li key={good}>
+              {good}
+              <button type="button" onClick={() => this.toggleGoods(good)}>
+                {this.state.goods.includes(good) ? 'Delete' : 'add'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
