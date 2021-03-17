c8a604de0a58c8cc16c342e818280eb83040e1d7
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-17 19:59:20 +0200

    main task done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import ClassNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,81 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const goodsWithId = goodsFromServer.map((name, index) => ({
+  name, id: index + 1,
+}));
+
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  }
+
+  setSelection = (good) => {
+    this.setState({
+      selectedGood: good,
+    });
+  }
+
+  resetSelection = () => {
+    this.setState({
+      selectedGood: null,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="ui middle aligned divided list">
+          <div className="list-header">
+            <h1>
+              {this.state.selectedGood ? (
+                <>
+                  <span className="selected">
+                    {this.state.selectedGood}
+                  </span>
+                  {' '}
+                  is selected
+                </>
+              ) : 'Nothing selected'}
+            </h1>
+            <button
+              type="button"
+              onClick={this.resetSelection}
+              className={ClassNames(
+                'ui icon button header-button',
+                { hidden: !this.state.selectedGood },
+              )}
+            >
+              <i className="close icon" />
+            </button>
+          </div>
+          {goodsWithId.map(good => (
+            <div key={good.id} className="item">
+              <div className="right floated content">
+                <button
+                  type="button"
+                  className={ClassNames(
+                    'ui',
+                    'button',
+                    { disabled: good.name === this.state.selectedGood },
+                  )}
+                  onClick={() => this.setSelection(good.name)}
+                >
+                  Select
+                </button>
+              </div>
+              <div className={ClassNames({
+                selected: good.name === this.state.selectedGood,
+              })}
+              >
+                {good.name}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
