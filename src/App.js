b59505410a8abfb8d380e3505adee19aaf39f207
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-18 19:05:22 +0200

    main task

--- src/App.js
+++ src/App.js
@@ -12,13 +12,71 @@ const goodsFromServer = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map((name, index) => ({
+  name,
+  id: index + 1,
+}));
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  }
+
+  addGoods = (good) => {
+    this.setState({ selectedGood: good.name });
+  }
+
+  removeGoods = () => {
+    this.setState({ selectedGood: 'No goods selected' });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <button
+            type="button"
+            onClick={this.removeGoods}
+            className={
+              this.state.selectedGood
+              === 'No goods selected' ? 'hiddenButton' : ''
+            }
+          >
+            X
+          </button>
+          {` Selected good: ${this.state.selectedGood}`}
+        </h1>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good.id}
+              className="list"
+            >
+              <span
+                className={
+                good.name === this.state.selectedGood ? 'selectedGood' : ''
+                }
+              >
+                {good.name}
+              </span>
+              {' '}
+              <button
+                type="button"
+                className={
+                  good.name === this.state.selectedGood ? 'hiddenButton' : ''
+                }
+                onClick={() => {
+                  this.addGoods(good);
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
