987978b5c99ad87c0fcab906ea74197b08c4d3f1
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-19 10:12:39 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,60 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: null,
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {' '}
+          {selectedGood || 'No goods selected'}
+        </h1>
+        <button
+          className={selectedGood
+            ? '' : 'Button'}
+          type="button"
+          onClick={() => {
+            this.setState({
+              selectedGood: null,
+            });
+          }}
+        >
+          X
+        </button>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={selectedGood === good
+                ? 'Good' : ''}
+            >
+              {good}
+              {' '}
+              <button
+                className={selectedGood === good
+                  ? 'Button' : ''}
+                type="button"
+                onClick={() => {
+                  this.setState({
+                    selectedGood: good,
+                  });
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
