d72a23b4acf013ba541b63f86aef9cbb5b864f70
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-22 01:20:48 +0200

    first solution goods-selector

--- src/App.js
+++ src/App.js
@@ -14,11 +14,131 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preparedListGoods = goodsFromServer.map((name, index) => (
+  {
+    name,
+    index: index + 1,
+  }
+));
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedGoods: [],
+    };
+  }
+
+  addGood = (goodName) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, goodName],
+    }));
+  }
+
+  deleteGood = (goodName) => {
+    this.setState((prevState) => {
+      const filteredGoods = prevState.selectedGoods.filter(
+        selectedGoodName => selectedGoodName !== goodName,
+      );
+
+      return {
+        selectedGoods: filteredGoods,
+      };
+    });
+  }
+
+  resetGoods = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+    const listLength = selectedGoods.length;
+
+    return (
+      <div className="App">
+        <div className="wrap">
+          {
+              listLength
+                ? (
+                  <>
+                    <h1>
+                      {
+                        listLength === 1
+                          ? (
+                            <>
+                              {selectedGoods[0]}
+                              {' '}
+                              is selected
+                            </>
+                          )
+                          : ''
+                      }
+                      {
+                        listLength > 1
+                          ? (
+                            <>
+                              {selectedGoods.join(', ')}
+                              {' '}
+                              are selected
+                            </>
+                          )
+                          : ''
+                      }
+                    </h1>
+                    <button
+                      type="button"
+                      onClick={this.resetGoods}
+                      className="button"
+                    >
+                      Reset
+                    </button>
+                  </>
+                )
+                : <h1>No goods selected</h1>
+            }
+        </div>
+
+        {
+          preparedListGoods.map(good => (
+            <div className="wrap" key={good.index}>
+              {
+                 selectedGoods.includes(good.name)
+                   ? (
+                     <>
+                       <span className="selected">
+                         {good.name}
+                       </span>
+                       <button
+                         type="button"
+                         onClick={this.deleteGood.bind(null, good.name)}
+                         className="button button--red"
+                       >
+                         Delete
+                       </button>
+                     </>
+                   )
+                   : (
+                     <>
+                       <span>
+                         {good.name}
+                       </span>
+                       <button
+                         type="button"
+                         onClick={this.addGood.bind(null, good.name)}
+                         className="button"
+                       >
+                         Select
+                       </button>
+                     </>
+                   )
+              }
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
