091b40a96cbbf4c165d7a47784637e7692182a7e
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-22 23:12:48 +0200

    best of the best solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,78 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+    previousButton: null,
+  }
+
+  addProduct = (product) => {
+    this.setState({ selectedGood: product });
+  };
+
+  removeProduct = () => {
+    this.setState({ selectedGood: null });
+  };
+
+  render() {
+    const { selectedGood, previousButton } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {selectedGood
+            ? (
+              <>
+                {`${selectedGood} is selected `}
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.removeProduct();
+
+                    if (previousButton) {
+                      previousButton.hidden = false;
+                    }
+                  }}
+                >
+                  X
+                </button>
+              </>
+            )
+            : `No goods selected`}
+        </h1>
+
+        <ul className="list">
+          {goodsFromServer.map(product => (
+            <li
+              key={product}
+              className={selectedGood === product ? 'selected' : 'item'}
+            >
+              {`${product} `}
+              <button
+                type="button"
+                onClick={(event) => {
+                  this.addProduct(product);
+
+                  const pressedButton = event.target;
+
+                  pressedButton.hidden = true;
+
+                  if (previousButton) {
+                    previousButton.hidden = false;
+                  }
+
+                  this.setState({ previousButton: event.target });
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
