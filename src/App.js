4016c08ca02146453fe97ab773cb2f1ea826f942
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-22 18:33:52 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,69 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: goodsFromServer[8],
+  };
+
+  selectGood = (product) => {
+    this.setState({ selectedGood: product });
+  }
+
+  removeSelected =() => {
+    this.setState({ selectedGood: null });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {' '}
+          {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
+        </h1>
+        {
+          selectedGood
+            ? (
+              <button
+                type="button"
+                onClick={this.removeSelected}
+              >
+                X
+              </button>
+            )
+            : ''
+        }
+        <ul className="goods">
+          {
+            goodsFromServer.map(product => (
+              <li
+                // eslint-disable-next-line max-len
+                className={`goods__item  ${selectedGood === product ? 'active' : ''}`}
+                key={product}
+              >
+                {product}
+                {
+                  selectedGood !== product
+                    ? (
+                      <button
+                        type="button"
+                        onClick={() => this.selectGood(product)}
+                      >
+                        Select
+                      </button>
+                    )
+                    : ''
+                }
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
