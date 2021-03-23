ff686cea1c3d1f4f8fa5f9335316e78943727365
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-23 12:58:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,64 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  }
+
+  addGood = (good) => {
+    this.setState({ selectedGood: good });
+  }
+
+  removeGood = () => {
+    this.setState({ selectedGood: null });
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {' '}
+          {
+            selectedGood
+              ? `${selectedGood}`
+              : `no goods`
+          }
+          {' '}
+          <button
+            type="button"
+            className="button__remove"
+            onClick={this.removeGood}
+          >
+            X
+          </button>
+        </h1>
+        <ul className="list">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={selectedGood === good ? 'product' : ''}
+            >
+              <button
+                type="button"
+                className={selectedGood !== good ? '' : 'button'}
+                onClick={() => {
+                  this.addGood(good);
+                }}
+              >
+                Select
+              </button>
+              {' '}
+              {good}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
