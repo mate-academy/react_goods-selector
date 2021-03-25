0ec79fcdc1223d9445b635b80d248e0f13d0504d
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-03-25 17:11:03 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -12,13 +12,80 @@ const goodsFromServer = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map((good, index) => ({
+  product: good,
+  id: index + 1,
+}));
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+let isResetted = false;
+
+class App extends React.Component {
+  state = {
+    selectedGood: ['Jam'],
+  }
+
+  addGoods = (good) => {
+    this.setState(prevState => ({
+      selectedGood: [...prevState.selectedGood, good],
+    }));
+
+    isResetted = false;
+  }
+
+  resetGoods = () => {
+    this.setState(() => ({
+      selectedGood: [],
+    }));
+
+    isResetted = true;
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {isResetted
+            ? 'No goods selected'
+            : `${selectedGood.join(', ')} is selected`
+          }
+        </h1>
+        {isResetted || (
+          <button
+            type="button"
+            onClick={this.resetGoods}
+          >
+            X
+          </button>
+        )}
+        <ul>
+          {goodsFromServer.map(({ product, id }) => (
+            <li
+              key={id}
+              className={`item ${selectedGood.includes(product)
+                ? 'item--selected'
+                : ''}`
+              }
+            >
+              {product}
+              {' '}
+              {selectedGood.includes(product) || (
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.addGoods(product);
+                  }}
+                >
+                  Select
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
