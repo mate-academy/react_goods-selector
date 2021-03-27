8b5bcfe5fe950eb426084069e869a4729ac16ac3
Smihotur Roman <smikhotur@gmail.com>
2021-03-27 19:19:55 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,102 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  addSelectedGoods = (goods) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({
+      selectedGoods: [...selectedGoods, goods],
+    });
+  }
+
+  removeSelectedGoods = (goods) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({
+      selectedGoods: selectedGoods.filter(el => el !== goods),
+    });
+  }
+
+  deleteSelectedGoods = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+    const {
+      addSelectedGoods,
+      removeSelectedGoods,
+      deleteSelectedGoods,
+    } = this;
+
+    return (
+      <div className="app">
+        <h1 className="title">Internet-shop</h1>
+        <ul className="list">
+          {goodsFromServer.map(product => (
+            <li key={product} className="list__item">
+              <span className={
+                classNames(`list__text`, {
+                  list__active: selectedGoods.includes(product),
+                })}
+              >
+                { product }
+              </span>
+              <section className="list__btn btn">
+                <button
+                  className="btn__select add-remove"
+                  type="button"
+                  onClick={() => {
+                    addSelectedGoods(product);
+                  }}
+                >
+                  Select
+                </button>
+                <button
+                  className="btn__remove add-remove"
+                  type="button"
+                  onClick={() => {
+                    removeSelectedGoods(product);
+                  }}
+                >
+                  Remove
+                </button>
+              </section>
+            </li>
+          ))}
+        </ul>
+
+        <section className="products">
+          <p className="products__count">
+            {`Selected:
+            ${selectedGoods.length}`}
+          </p>
+          {selectedGoods.length > 1 && (
+            <button
+              type="button"
+              className="products__delete"
+              onClick={deleteSelectedGoods}
+            >
+              Х
+            </button>
+          )}
+        </section>
+        <section className="text">
+          <div className="text__inner">
+            <strong className="text__title">Selected goods:</strong>
+            <span className="text__col">
+              {`${selectedGoods.join(', ')}`}
+            </span>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
