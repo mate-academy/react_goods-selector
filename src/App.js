f00c5be4a6359113175c01b4b269e0534c1e24ff
Inna Sorochynska <innasoro@gmail.com>
2021-04-02 23:23:13 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
 import React from 'react';
 import './App.scss';
 
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+class App extends React.Component {
+  state = {
+    goodsFromServer: [
+      'Dumplings',
+      'Carrot',
+      'Eggs',
+      'Ice cream',
+      'Apple',
+      'Bread',
+      'Fish',
+      'Honey',
+      'Jam',
+      'Garlic',
+    ],
+    selectedGoods: [],
+  }
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+  addItem = (item) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, item],
+    }));
+  }
+
+  removeItem = (item) => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods.filter(good => good !== item),
+    }));
+  }
+
+  clearSelections = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  selectTitle = () => {
+    const { selectedGoods } = this.state;
+
+    if (selectedGoods.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (selectedGoods.length === 1) {
+      return `${selectedGoods[0]} is selected`;
+    }
+
+    return `${selectedGoods.slice(0, -1).join(', ')}
+    and ${selectedGoods[selectedGoods.length - 1]} is selected`;
+  }
+
+  render() {
+    const { goodsFromServer, selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          <button
+            className="button"
+            hidden={selectedGoods.length === 0}
+            type="button"
+            onClick={this.clearSelections}
+          >
+            X
+          </button>
+          {this.selectTitle()}
+        </h1>
+        {goodsFromServer.length}
+        <div className="list">
+          {goodsFromServer.map(item => (
+            <div key={item} className="list__item">
+              <span className={selectedGoods.includes(item) && 'selected'}>
+                {item}
+              </span>
+              <button
+                className="button list__button"
+                hidden={selectedGoods.includes(item)}
+                type="button"
+                onClick={() => {
+                  this.addItem(item);
+                }}
+              >
+                Add
+              </button>
+              <button
+                className="button list__button"
+                type="button"
+                hidden={!selectedGoods.includes(item)}
+                onClick={() => {
+                  this.removeItem(item);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
