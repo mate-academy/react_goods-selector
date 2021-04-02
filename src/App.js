7d83df5cb5fc00044a091fadfa029e1d33f4fcf9
Julia <mjulia1794@gmail.com>
2021-04-02 23:46:03 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,86 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  addGoods = (good) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, good],
+    }));
+  }
+
+  removeGood = (good) => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods
+        .filter(product => product !== good),
+    }));
+  }
+
+  removeGoods = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          {this.state.selectedGoods.length !== 0
+            ? (
+              <h1>
+                {`Selected goods:
+                  ${this.state.selectedGoods.join(', ')}
+                  are selected`}
+              </h1>
+            )
+            : <h1>Selected goods: No goods selected</h1>}
+          {this.state.selectedGoods.length !== 0
+          && (
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => {
+              this.removeGoods();
+            }}
+          >
+            Remove All
+          </button>
+          )}
+        </div>
+        <div className="container shadow p-3 mb-5 bg-body rounded">
+          <ul className="list-group list-group-flush">
+            {goodsFromServer.map(good => (
+              <li className="list-group-item" key={good}>
+                {good}
+                <button
+                  type="button"
+                  className="btn btn-outline-danger"
+                  onClick={() => {
+                    this.addGoods(good);
+                  }}
+                >
+                  Select
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-success"
+                  onClick={() => {
+                    this.removeGood(good);
+                  }}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
