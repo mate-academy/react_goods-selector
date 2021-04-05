5c63833fd6d4ad04582e937a72cbad18287a4440
Iryna Haiduk <$ git config --global user.name Iryna>
2021-04-05 21:06:43 +0300

    Created React Goods list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,112 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preparedGoods = goodsFromServer.map(good => (
+  {
+    name: good,
+    selected: false,
+    id: uuidv4(),
+  }
+));
+
+class App extends React.Component {
+  state = {
+    goods: preparedGoods.map(good => (good.name === 'Jam' ? {
+      ...good, selected: true,
+    } : { ...good })),
+    selectedGoods: ['Jam'],
+  }
+
+  addGood(good) {
+    this.setState(prevState => ({
+      goods: prevState.goods.map(item => (
+        (item === good) ? {
+          ...item, selected: true,
+        } : { ...item })),
+      selectedGoods: [...prevState.selectedGoods, good.name],
+    }));
+  }
+
+  removeGood(good) {
+    this.setState(prevState => ({
+      goods: prevState.goods.map(item => (
+        (item === good) ? {
+          ...item, selected: false,
+        } : { ...item }
+      )),
+      selectedGoods: prevState.selectedGoods.filter(item => item !== good.name),
+    }));
+  }
+
+  removeSelectedGoods() {
+    this.setState(prevState => ({
+      goods: prevState.goods.map(item => ({
+        ...item,
+        selected: false,
+      })),
+      selectedGoods: [],
+    }));
+  }
+
+  render() {
+    const { selectedGoods, goods } = this.state;
+
+    return (
+      <div className="App d-flex flex-column align-items-center m-4">
+        <h1>
+          {selectedGoods.length > 0 ? 'Selected:' : 'No goods selected'}
+          {' '}
+          {selectedGoods.join(', ')}
+        </h1>
+        <button
+          type="button"
+          className="btn btn-danger m-3"
+          onClick={() => {
+            this.removeSelectedGoods();
+          }
+          }
+        >
+          Remove all
+        </button>
+        <ul className="list-group d-inline-flex">
+          {goods.map(good => (
+            <li
+              className={`list-group-item  d-flex justify-content-evenly
+                align-items-center p-4
+                ${good.selected ? 'list-group-item-danger'
+                : 'list-group-item-success'}`}
+              key={good.id}
+            >
+              {good.name}
+              {good.selected
+                ? (
+                  <button
+                    onClick={() => {
+                      this.removeGood(good);
+                    }}
+                    type="button"
+                    className="btn btn-warning m-3"
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="btn btn-success m-3"
+                    onClick={() => {
+                      this.addGood(good);
+                    }}
+                  >
+                    Add
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
