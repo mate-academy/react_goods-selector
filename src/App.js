6c6e70bfba7ac912f78d858f3d8a42d4584f2071
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-05 20:36:35 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './App.scss';
 
@@ -12,13 +13,110 @@ const goodsFromServer = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map((item, index) => (
+  // eslint-disable-next-line no-param-reassign
+  item = {
+    id: index,
+    nameGoods: item,
+  }
+));
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    listGoods: ['Jam'],
+  }
 
-export default App;
+  getSelectedGoodsList() {
+    const goods = this.state.listGoods;
+    const selectedCount = goods.length;
+
+    if (selectedCount === 1) {
+      return `${goods[0]} is selected`;
+    }
+
+    if (selectedCount > 1) {
+      const firstPart = goods.slice(0, -1).join(', ');
+
+      return `${firstPart} and ${goods[selectedCount - 1]} are selected`;
+    }
+
+    return 'No goods selected';
+  }
+
+  addGoods = goods => (
+    this.setState(prevState => ({
+      listGoods: [...prevState.listGoods, goods],
+    }))
+  )
+
+  removeGoods = (goods) => {
+    const { listGoods } = this.state;
+
+    if (listGoods.includes(goods)) {
+      const index = listGoods.lastIndexOf(goods);
+
+      if (index > -1) {
+        listGoods.splice(index, 1);
+        this.setState({
+          listGoods,
+        });
+      }
+    }
+  }
+
+  clearList = () => {
+    this.setState({
+      listGoods: [],
+    });
+  }
+
+  render() {
+    const { listGoods } = this.state;
+
+    return (
+      <>
+        <div className="alert alert-success">
+          {this.getSelectedGoodsList()}
+        </div>
+        <div className="box">
+          {goodsFromServer.map(goods => (
+            <div
+              key={goods.id}
+            >
+              <h1>{goods.nameGoods}</h1>
+              <button
+                type="button"
+                className="btn btn-warning margin"
+                onClick={() => this.addGoods(goods.nameGoods)}
+              >
+                Add
+              </button>
+              <button
+                type="button"
+                className="btn btn-success margin"
+                onClick={() => this.removeGoods(goods.nameGoods)}
+              >
+                Done
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary margin"
+                onClick={this.clearList}
+              >
+                Clear
+              </button>
+              <button
+                type="button"
+                className="btn btn-light margin"
+              >
+                {(listGoods.filter(product => (
+                  product === goods.nameGoods
+                )).length || null) + 0}
+              </button>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
