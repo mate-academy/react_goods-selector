d43beca026db80bb484daddc717ea2c3a6cff39a
Artem Maksimikhin <uanov86@gmail.com>
2021-04-06 12:46:37 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -25,50 +26,59 @@ export class App extends React.Component {
 
     return (
       <div className="App">
-        {!selectedGood && (
-          <div className="title-info">
-            <h1 className="goods-title">
-              No goods selected
-            </h1>
-          </div>
-        )}
-        {selectedGood && (
-          <div className="title-info">
-            <h1 className="goods-title">
-              {selectedGood}
-              {' '}
-              is selected
-            </h1>
-            <button
-              type="button"
-              className="btn-clear"
-              onClick={() => {
-                this.setState({
-                  selectedGood: null,
-                });
-              }}
-            >
-              +
-            </button>
-          </div>
-        )}
+        {selectedGood
+          ? (
+            <div className="title-info">
+              <h1 className="goods-title">
+                {selectedGood}
+                {' '}
+                is selected
+              </h1>
+              <button
+                type="button"
+                className="btn-clear"
+                onClick={() => {
+                  this.setState({
+                    selectedGood: null,
+                  });
+                }}
+              >
+                +
+              </button>
+            </div>
+          )
+          : (
+            <div className="title-info">
+              <h1 className="goods-title">
+                No goods selected
+              </h1>
+            </div>
+          )
+
+        }
         <ul className="goods-list">
           {goods.map(item => (
             <li
               key={item}
               className={
-                item === selectedGood
-                  ? 'goods-list__item  goods-list__item--active'
-                  : 'goods-list__item'
+                classNames(
+                  `goods-list__item`, {
+                    goodsListItemActive:
+                      item === selectedGood,
+                  },
+                )
               }
             >
               {item}
               <button
                 type="button"
                 className={
-                  item === selectedGood
-                    ? 'goods-list__button  goods-list__button--active'
-                    : 'goods-list__button'
+                  classNames(
+                    `goods-list__button`, {
+                      goodsListButtonActive:
+                        item === selectedGood,
+                    },
+                  )
                 }
                 onClick={() => {
                   this.setState({
