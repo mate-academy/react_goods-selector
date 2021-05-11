933b05495e92336e3c5270cea9e021c59854f295
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-05-11 14:10:44 +0300

    first try

--- src/App.js
+++ src/App.js
@@ -14,11 +14,82 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    selectedGoodsArr: ['No goods'],
+  };
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {' '}
+          {`${this.state.selectedGoodsArr.map(
+            good => ` ${good}`,
+          )} are selected`}
+          <button
+            type="button"
+            id="xButton"
+            style={{ visibility: 'hidden' }}
+            onClick={() => {
+              this.setState({ selectedGoodsArr: ['No goods'] });
+              document.getElementById('xButton').style.visibility = 'hidden';
+            }}
+          >
+            {' '}
+            X
+            {' '}
+          </button>
+        </h1>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li>
+              {' '}
+              {good}
+              <button
+                type="button"
+                id={good}
+                onClick={() => {
+                  if (this.state.selectedGoodsArr[0] === 'No goods') {
+                    this.setState(() => ({ selectedGoodsArr: [good] }));
+                  } else {
+                    this.setState(prevState => (
+                      prevState.selectedGoodsArr.push(good)));
+                  }
+
+                  const button = document.getElementById(good);
+
+                  button.style.visibility = 'hidden';
+                  document.getElementById('xButton').style.visibility
+                    = 'visible';
+                }}
+              >
+                {' '}
+                Add
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  this.setState(prev => (
+                    { selectedGoodsArr: prev.selectedGoodsArr.filter(
+                      arrGood => arrGood !== good,
+                    ) }));
+                  if (
+                    this.state.selectedGoodsArr.length === 1
+                    && this.state.selectedGoodsArr[0] === good
+                  ) {
+                    this.setState({ selectedGoodsArr: ['No goods'] });
+                  }
+
+                  document.getElementById(good).style.visibility = 'visible';
+                }}
+              >
+                remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
