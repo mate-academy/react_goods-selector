63e562cb228bbfbbd67b7cd10c9452207a36b3f4
Georgy Dzumenko <erterj05@gmail.com>
2021-05-15 23:49:19 +0300

    After fixes

--- src/App.js
+++ src/App.js
@@ -16,44 +16,57 @@ const goodsFromServer = [
 
 class App extends React.Component {
   state = {
-    goodsStatus: goodsFromServer.map(prod => ({
+    goodsStatus: goodsFromServer.map((prod, id) => ({
       value: prod,
+      id,
       isSelected: false,
     })),
 
     selected: [],
   }
 
+  changeProdStat = (prodId) => {
+    this.setState(state => (
+      {
+        goodsStatus: state.goodsStatus.map(product => (product.id === prodId ? {
+          value: product.value,
+          id: product.id,
+          isSelected: !product.isSelected,
+        }
+          : product
+        )),
+      }));
+
+    this.setState(state => ({
+      selected: state.goodsStatus.filter(
+        prod => prod.isSelected,
+      ).map(
+        ({ value }) => value,
+      ),
+    }));
+  };
+
+  getTitle = () => {
+    const { selected } = this.state;
+
+    switch (selected.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selected[0]} is selected`;
+      default:
+        return `${selected.join(', ')} are selected`;
+    }
+  };
+
   render() {
-    const { goodsStatus, selected } = this.state;
-
-    const changeProdStat = (product, stat) => {
-      // eslint-disable-next-line no-param-reassign
-      product.isSelected = stat;
-      this.setState({
-        selected: goodsStatus.filter(
-          prod => prod.isSelected,
-        ).map(
-          ({ value }) => value,
-        ),
-      });
-    };
-
-    const chooseNeme = () => {
-      switch (selected.length) {
-        case 0:
-          return 'No goods selected';
-        case 1:
-          return `${selected[0]} is selected`;
-        default:
-          return `${selected.join(', ')} are selected`;
-      }
-    };
+    const { goodsStatus } = this.state;
+    const { changeProdStat, getTitle } = this;
 
     return (
       <div className="App">
         <h1>
-          { chooseNeme() }
+          { getTitle() }
         </h1>
         {goodsFromServer.length}
 
@@ -61,22 +74,13 @@ class App extends React.Component {
           {goodsStatus.map(product => (
             <li>
               { product.value }
-              { product.isSelected ? (
-                <button
-                  type="button"
-                  onClick={() => changeProdStat(product, false)}
-                >
-                  Remove
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={() => changeProdStat(product, true)}
-                >
-                  Add
-                </button>
+              <button
+                type="button"
+                onClick={() => changeProdStat(product.id)}
+              >
+                { product.isSelected ? 'Remove' : 'Add' }
+              </button>
               )
-              }
             </li>
           ))}
         </ul>
