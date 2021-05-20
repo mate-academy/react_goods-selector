7703ae5b4d09eea0e2e8356d35c1dfd71414753f
Ivan Grekov <grekovv76@gmail.com>
2021-05-20 12:21:42 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +16,120 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  clearSelected = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  renderTitle(array) {
+    switch (array.length) {
+      case 0:
+        return (
+          'No goods selected'
+        );
+
+      case 1:
+        return (
+          <>
+            {`${array[0]} `}
+            is selected
+
+            <button
+              type="button"
+              className="title__clear"
+              title="Clear selected"
+              onClick={this.clearSelected}
+            >
+              x
+            </button>
+          </>
+        );
+
+      default:
+        return (
+          <>
+            {`${array.join(', ')} `}
+            are selected
+
+            <button
+              type="button"
+              className="title__clear"
+              title="Clear selected"
+              onClick={this.clearSelected}
+            >
+              x
+            </button>
+          </>
+        );
+    }
+  }
+
+  render() {
+    let { selectedGoods: curentProductList } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {this.renderTitle(curentProductList)}
+        </h1>
+
+        <ul className="product-list">
+          {goodsFromServer.map(product => (
+            <li
+              key={product}
+              className="product-list__item"
+            >
+              <span
+                className={classNames({
+                  hightlighted: curentProductList.includes(product),
+                })}
+              >
+                {product}
+              </span>
+
+              <button
+                className={
+                  classNames({
+                    hidden: curentProductList.includes(product),
+                    'product-list__add': true,
+                  })
+                }
+                type="button"
+                onClick={() => {
+                  curentProductList.push(product);
+                  this.setState({ selectedGoods: curentProductList });
+                }}
+              >
+                Add
+              </button>
+
+              <button
+                className={
+                  classNames({
+                    hidden: !curentProductList.includes(product),
+                    'product-list__remove': true,
+                  })
+                }
+                type="button"
+                onClick={() => {
+                  curentProductList = curentProductList.filter(
+                    element => element !== product,
+                  );
+                  this.setState({ selectedGoods: curentProductList });
+                }}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
