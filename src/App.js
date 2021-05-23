e927847b5a6ff86d8f12abde44fbbc8ec84c1f02
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-05-23 14:17:28 +0300

    goods-selector

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +16,99 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  goodsSelector = (goods) => {
+    if (goods.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (goods.length === 1) {
+      return `${goods[0]} is selected`;
+    }
+
+    return `${goods.slice(0, goods.length - 1).join(', ')}`
+      + ` and ${goods[goods.length - 1]} are selected`;
+  }
+
+  removeGood = (good) => {
+    this.setState(state => ({
+      selectedGoods: state.selectedGoods.filter(item => item !== good),
+    }));
+  }
+
+  addGood = (good) => {
+    this.setState(state => ({
+      selectedGoods: [...state.selectedGoods, good],
+    }));
+  }
+
+  clearGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="Title">
+          <h1 className="Title-text">
+            {this.goodsSelector(selectedGoods)}
+          </h1>
+          {selectedGoods.length > 0 && (
+            <button
+              type="button"
+              className="Title-button"
+              onClick={this.clearGoods}
+            >
+              X
+            </button>
+          )
+          }
+        </div>
+
+        <ul className="List">
+          {goodsFromServer.map(good => (
+            <li key={good} className="List-item">
+              <span>{good}</span>
+              <button
+                type="button"
+                className={
+                  classNames('List-button', 'List-button--add', {
+                    hidden: selectedGoods.includes(good),
+                  })
+                }
+                onClick={() => (
+                  this.addGood(good))}
+              >
+                Add
+              </button>
+
+              <button
+                type="button"
+                className={
+                  classNames('List-button', 'List-button--remove',
+                    {
+                      hidden: !selectedGoods.includes(good),
+                    })
+                }
+                onClick={() => (
+                  this.removeGood(good)
+                )}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    );
+  }
+}
 
 export default App;
