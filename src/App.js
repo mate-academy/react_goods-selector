6d0544265eb71af20450a6554cfc2f98eb180881
amithos <alex.theutos@gmail.com>
2021-05-24 21:41:09 +0300

    goods selector

--- src/App.js
+++ src/App.js
@@ -14,11 +14,78 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  }
+
+  reset = () => {
+    this.setState({
+      selectedGood: null,
+    });
+  }
+
+  makeSelect = (good) => {
+    this.setState({
+      selectedGood: good,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="out">
+          { !this.state.selectedGood
+            ? 'No goods selected'
+            : `${this.state.selectedGood} is selected`
+          }
+          {this.state.selectedGood && (
+            <button
+              className="reset"
+              onClick={this.reset}
+              type="button"
+            >
+              X
+            </button>
+          )
+          }
+        </h1>
+
+        <div className="list-wrapper">
+          <ul className="list">
+            {goodsFromServer.map(good => (
+              <li
+                key={good}
+                className={
+                this.state.selectedGood === good
+                  ? 'selected item'
+                  : 'item'
+              }
+              >
+                <span className="text">
+                  {good}
+                </span>
+                <button
+                  className={
+                  this.state.selectedGood === good
+                    ? 'button buttonRem'
+                    : 'button buttonSel'
+                  }
+                  onClick={() => {
+                    this.makeSelect(good);
+                  }}
+                  type="button"
+                >
+                  select
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+      </div>
+    );
+  }
+}
 
 export default App;
