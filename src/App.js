a91009676a67a4e0fec486aca2bf6c1ac90ce7ed
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-05-24 10:30:44 +0300

    Add solution react_goods_selector

--- src/App.js
+++ src/App.js
@@ -14,11 +14,113 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goodsCounter: 1,
+    selectedGoods: ['Jam'],
+  }
+
+  showGoodsList = (() => {
+    const goods = [...this.state.selectedGoods];
+
+    if (goods.length === 0) {
+      return ' No goods selected.';
+    }
+
+    if (goods.length === 1) {
+      return ` ${goods[0]} is selected.`;
+    }
+
+    const lastGood = goods.pop();
+
+    return ` ${goods.join(', ')} and ${lastGood} are selected.`;
+  })
+
+  goodsHandler = (product) => {
+    if (!this.state.selectedGoods.includes(product)) {
+      this.setState(state => ({
+        selectedGoods: [...state.selectedGoods, product],
+        goodsCounter: state.goodsCounter + 1,
+      }));
+    } else {
+      this.setState(state => ({
+        selectedGoods: state.selectedGoods.filter(good => good !== product),
+        goodsCounter: state.goodsCounter - 1,
+      }));
+    }
+  }
+
+  clearGoodsList = () => {
+    this.setState({
+      goodsCounter: 0,
+      selectedGoods: [],
+    });
+  }
+
+  render() {
+    const { goodsCounter, selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="header">
+          <h1 className="header__title">
+            Selected good: -
+            {this.showGoodsList()}
+          </h1>
+        </div>
+
+        <div className="main-content">
+          <ul className="goods-list">
+            {goodsFromServer.map(
+              (product) => {
+                const isProductSelected = selectedGoods.includes(product);
+
+                return (
+                  <li key={product}>
+                    <div className="button-wrapper">
+                      <button
+                        type="button"
+                        className={
+                          `button ${isProductSelected
+                            ? 'button__active'
+                            : 'button__noneactive'}`
+                        }
+                        onClick={() => {
+                          this.goodsHandler(product);
+                        }}
+                      >
+                        {product}
+                      </button>
+                    </div>
+                  </li>
+                );
+              },
+            )}
+            <li>
+              <div className="button-wrapper">
+                <button
+                  type="button"
+                  className="clear-button"
+                  onClick={this.clearGoodsList}
+                  hidden={!this.state.selectedGoods.length}
+                />
+              </div>
+            </li>
+            <li>
+              <div className="button-wrapper">
+                <button
+                  type="button"
+                  className="button"
+                >
+                  {` ${goodsCounter}`}
+                </button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
