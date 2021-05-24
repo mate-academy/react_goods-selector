4f1373f51043eae2c6f5e4dec4131228fe28a621
Vitalii Siedykh <siedykh.v@gmail.com>
2021-05-24 13:20:16 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,77 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
 
-export default App;
+  clearSelection = () => {
+    if (this.state.selectedGoods.length !== 0) {
+      this.setState({
+        selectedGoods: [],
+      });
+    }
+  }
+
+  clickHandler(good) {
+    if (this.state.selectedGoods.includes(good)) {
+      this.setState(state => ({
+        selectedGoods: state.selectedGoods.filter(elem => elem !== good),
+      }));
+    } else {
+      this.setState((state) => {
+        state.selectedGoods.push(good);
+
+        return {
+          selectedGoods: state.selectedGoods,
+
+        };
+      });
+    }
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        {selectedGoods.length > 0
+          ? (
+            <h1>
+              Selected good:
+              {' '}
+              { `${selectedGoods.join(', ')} `}
+              <button
+                type="button"
+                onClick={this.clearSelection}
+              >
+                Clear All
+              </button>
+            </h1>
+          )
+          : <h1>No Goods Selected</h1>
+        }
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={
+                classNames({ selected: selectedGoods.includes(good) })}
+            >
+              {`${good}`}
+              <button
+                type="button"
+                onClick={() => this.clickHandler(good)}
+              >
+                Add/Remove
+              </button>
+            </li>
+          ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
