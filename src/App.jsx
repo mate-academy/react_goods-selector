0c20a89a3e663c8062393bd0937e45e8d86bcad1
Andron <blackdog337.ab@gmail.com>
2021-05-24 16:25:34 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -16,40 +16,53 @@ const goodsFromServer = [
 
 class App extends React.Component {
   state = {
-    selectedGood: ['Jam'],
-  }
-
-  constructor() {
-    super();
-    this.goods = [...this.state.selectedGood];
+    selectedGoods: ['Jam'],
   }
 
   selectItem = (item) => {
-    this.goods.push(item);
-    this.setState({ selectedGood: this.goods });
+    this.setState(state => ({
+      selectedGoods: [...state.selectedGoods, item],
+    }));
   }
 
   removeItem = (item) => {
-    this.goods.splice(this.goods.indexOf(item), 1);
-    this.setState({ selectedGood: this.goods });
+    this.setState(state => ({
+      selectedGoods: state.selectedGoods
+        .filter(currentItem => currentItem !== item),
+    }));
   }
 
   clearItems = () => {
-    this.goods = [];
-    this.setState({ selectedGood: this.goods });
+    this.setState({ selectedGoods: [] });
   }
 
   render() {
-    const { selectedGood } = this.state;
-    const goodsIsSelected = selectedGood.length > 0;
+    const { selectedGoods } = this.state;
+    const goodsIsSelected = selectedGoods.length > 0;
+    const preparedTitleGoods = selectedGoods.map((item, index, array) => {
+      if (array.length > 1) {
+        if (index === array.length - 2) {
+          return `${item} and ${array[index + 1]}`;
+        }
+
+        return item;
+      }
+
+      return item;
+    });
 
     return (
       <div className="app">
         <h1>
           Selected good: -
           {goodsIsSelected
-            ? `${selectedGood.join(', ')} is selected`
-            : 'No goods selected'
+            ? ` ${preparedTitleGoods.length > 1
+              ? preparedTitleGoods.slice(0, selectedGoods.length - 1).join(', ')
+              : preparedTitleGoods.join(', ')} 
+              ${selectedGoods.length === 1
+              ? 'is'
+              : 'are'} selected`
+            : ' No goods selected'
           }
         </h1>
         {goodsIsSelected
@@ -58,18 +71,19 @@ class App extends React.Component {
         <ul className="app__list">
           {goodsFromServer.map(item => (
             <li
-              className={`app__item ${selectedGood.includes(item)
+              key={item}
+              className={`app__item ${selectedGoods.includes(item)
                 && 'app__item--active'
               }`}
             >
               <button
                 type="button"
                 onClick={() => (
-                  selectedGood.includes(item)
+                  selectedGoods.includes(item)
                     ? this.removeItem(item)
                     : this.selectItem(item))}
               >
-                select
+                {selectedGoods.includes(item) ? 'remove' : 'add'}
               </button>
               {' '}
               {item}
