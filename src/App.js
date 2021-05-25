7c41029987037ff44b8b0a38626747a0b0a6b9b3
Igor Lisogor <illisogori@gmail.com>
2021-05-25 09:54:10 +0300

    Goods selector implemented

--- src/App.js
+++ src/App.js
@@ -14,11 +14,69 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selected: null,
+    isGoodSelected: false,
+  };
+
+  selectGood = (good) => {
+    this.setState({
+      selected: good, isGoodSelected: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isGoodSelected
+          ? (
+            <>
+              <h1>
+                {this.state.selected}
+                {' is selected'}
+              </h1>
+              <button
+                type="button"
+                onClick={() => {
+                  this.setState({
+                    selected: null, isGoodSelected: false,
+                  });
+                }}
+              >
+                X
+              </button>
+            </>
+          ) : (
+            <h1>No goods selected</h1>
+          )}
+
+        <ul className="Goods">
+          {goodsFromServer.map(good => (
+            <li
+              className={good.localeCompare(this.state.selected) || 'active'}
+              key={good}
+            >
+              {good}
+              {good.localeCompare(this.state.selected)
+                ? (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.selectGood(good);
+                    }}
+                  >
+                    add
+                  </button>
+                ) : (
+                  ''
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
