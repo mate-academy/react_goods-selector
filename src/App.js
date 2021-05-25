22d9a98a9a8f3fc4f9bdc11d9214ec9ab10e4e11
Olga Tsarova <alita.jet@gmail.com>
2021-05-25 08:52:46 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,108 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    selectedGoods: ['Jam'],
+  }
+
+  addItem = (good) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, good],
+    }));
+  }
+
+  removeItem = (good) => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods.filter(el => el !== good),
+    }));
+  }
+
+  removeAll = () => {
+    this.setState(({
+      selectedGoods: [],
+    }));
+  }
+
+  selectedGoodssTitle = () => {
+    const { selectedGoods } = this.state;
+
+    if (selectedGoods.length === 1) {
+      return `${selectedGoods} is selected`;
+    }
+
+    if (selectedGoods.length === 2) {
+      return `${selectedGoods[0]} and ${selectedGoods[1]} is selected`;
+    }
+
+    if (selectedGoods.length > 2) {
+      const selected = [...selectedGoods];
+      const last = selected.splice(selected.length - 1, 1);
+
+      return `${selected.map(good => ` ${good}`)} and ${last} is selected`;
+    }
+
+    return 'No goods selected';
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__wrapper">
+          <h1 className="App__wrapper--title">
+            {this.selectedGoodssTitle()}
+          </h1>
+          {selectedGoods.length !== 0
+          && (
+          <button
+            type="button"
+            className="App__wrapper--button"
+            onClick={this.removeAll}
+          >
+            X
+          </button>
+          )}
+        </div>
+        <ul className="App__list">
+          {this.state.goods.map(good => (
+            <li
+              key={good}
+              className={classNames('App__list--item', {
+                selected: selectedGoods.includes(good),
+              })}
+            >
+              <span className="App__list--title">
+                {good}
+              </span>
+              {!selectedGoods.includes(good)
+                ? (
+                  <button
+                    type="button"
+                    className="App__list--button"
+                    onClick={() => this.addItem(good)}
+                  >
+                    Select
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="App__list--button remove"
+                    onClick={() => this.removeItem(good)}
+                  >
+                    Remove
+                  </button>
+                )
+              }
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
