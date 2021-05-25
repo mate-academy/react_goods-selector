0c05f51acbc22bbca787f041551adad5e2ada513
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-25 11:49:46 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,83 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: ['Jam'],
+  }
+
+  title = (param) => {
+    switch (param.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${param[0]} is selected`;
+      default:
+        return `${param.slice(0, -1).join(', ')}`
+        + ` and ${param[param.length - 1]} is selected `;
+    }
+  }
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {this.title(selectedGood)}
+          <button
+            type="button"
+            className={selectedGood.length === 0
+              ? 'hide'
+              : ''}
+            onClick={() => {
+              this.setState({ selectedGood: [] });
+            }}
+          >
+            X
+          </button>
+        </h1>
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <>
+              <li
+                key={good}
+                className={selectedGood.includes(good)
+                  ? 'selected'
+                  : ''
+                }
+              >
+                {good}
+              </li>
+
+              <button
+                type="button"
+                className={selectedGood.includes(good) ? 'hide' : ''}
+                onClick={() => {
+                  this.setState({ selectedGood: [...selectedGood, good] });
+                }}
+              >
+                Add
+              </button>
+
+              <button
+                type="button"
+                className={selectedGood.includes(good) ? '' : 'hide'}
+                onClick={() => {
+                  this.setState(selectedGood.splice(selectedGood.findIndex(
+                    element => element === good,
+                  ), 1));
+                }}
+              >
+                Remove
+              </button>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
