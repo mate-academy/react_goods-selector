5e5bf9b1a4cc166ed32ecdab9fb7cc1d69dc4351
Grygoriy Shytikov <gregorysgm@gmail.com>
2021-05-25 11:49:47 +0300

    solution - simple version without styses

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import className from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,57 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    value: 'Jam',
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className="app">
+        {value ? (
+          <h1 className="app__title">
+            <button
+              type="button"
+              className="app__clear-button"
+              onClick={() => {
+                this.setState({ value: null });
+              }}
+            >
+              X
+            </button>
+            {`${value} is selected`}
+          </h1>
+        ) : (
+          <h1>No goods selected</h1>
+        )}
+
+        <ul className="app__list">
+          {goodsFromServer.map(good => (
+            <li
+              className="app__item"
+            >
+              <button
+                type="button"
+                className={
+                  className(['app__button', { active: value === good }])
+                }
+                key={good}
+                onClick={() => {
+                  this.setState({ value: good });
+                }}
+              >
+                select
+              </button>
+              {good}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
