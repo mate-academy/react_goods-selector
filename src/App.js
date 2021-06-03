554a8cc1331b872dc9b00057e8024f07454843c5
romandubovskyi <rdubowskij@gmail.com>
2021-06-03 10:37:35 +0300

    implemented selection of items

--- src/App.js
+++ src/App.js
@@ -14,11 +14,59 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    selected: 'Jam',
+  }
 
-export default App;
+   selectItem = (event) => {
+     const itemContainer = event.target.closest('li');
+
+     this.setState({
+       selected: itemContainer.children[0].innerText,
+     });
+   }
+
+   clearSelected = () => {
+     this.setState({ selected: '' });
+   }
+
+   render() {
+     const { selected } = this.state;
+
+     return (
+       <div className="App">
+         <div className="title">
+           <h1>
+             {selected
+               ? `${selected} is selected `
+               : 'No goods selected'}
+           </h1>
+           <button type="button" onClick={() => this.clearSelected()}>
+             X
+           </button>
+         </div>
+         <ul className="list">
+           {goodsFromServer.map(item => (
+             <li
+               key={item}
+               className={selected === item ? 'selected' : ''}
+             >
+               <span>
+                 {item}
+               </span>
+               {selected !== item && (
+               <button
+                 type="button"
+                 onClick={event => (this.selectItem(event))}
+               >
+                 select
+               </button>
+               )}
+             </li>
+           ))}
+         </ul>
+       </div>
+     );
+   }
+}
