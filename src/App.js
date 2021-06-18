e1630b6ce4507277fbfdca8e03c66a66bef076da
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-06-18 11:46:57 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,143 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  makeUnselected = (item) => {
+    const itemsLi = [...document.querySelectorAll('.item')];
+
+    itemsLi.forEach((itemLi) => {
+      if (itemLi.textContent === item) {
+        itemLi.classList.remove('selected');
+
+        this.setState((prevState) => {
+          /* const index = prevState.selectedGoods.findIndex(item);
+
+          const newSelectedGoods = prevState.selectedGoods.splice(index, 1);
+
+          return {
+            selectedGoods: newSelectedGoods,
+          }; */
+        });
+      }
+    });
+  };
+
+  makeUnSelectedAll = () => {
+    const itemsLi = [...document.querySelectorAll('.item')];
+
+    this.setState({
+      selectedGoods: [],
+    });
+
+    itemsLi.forEach((itemLi) => {
+      itemLi.classList.remove('selected');
+    });
+  };
+
+  renderList = (array) => {
+    if (array.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (array.length === 1) {
+      return `${array[0]} is selected`;
+    }
+
+    const selectedItems = [];
+    let index = 0;
+
+    array.forEach((item) => {
+      if (index === array.length - 2) {
+        selectedItems.push(`${item}`);
+      } else if (index === array.length - 1) {
+        selectedItems.push(` and ${item} `);
+      } else {
+        selectedItems.push(`${item}, `);
+      }
+
+      index += 1;
+    });
+
+    return `${selectedItems.join('')} are selected`;
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          <span>
+            {' '}
+            {this.renderList(selectedGoods)}
+          </span>
+          {' '}
+          <button
+            type="button"
+            onClick={(e) => {
+              this.setState({
+                selectedGoods: [],
+              });
+            }}
+          >
+            X
+          </button>
+        </h1>
+        <h2>List of goods:</h2>
+        <ul>
+
+          {goodsFromServer.map(item => (
+            <>
+              <li
+                key={item}
+                className={`item ${selectedGoods.includes(item)
+                  ? 'selected'
+                  : ''}`}
+              >
+                {item}
+              </li>
+              <button
+                type="button"
+                onClick={(e) => {
+                  if (!selectedGoods.includes(item)) {
+                    this.setState(prevState => ({
+                      selectedGoods: [...prevState.selectedGoods, item],
+                    }));
+                  }
+                }}
+              >
+                Add
+              </button>
+              <button
+                type="button"
+                onClick={(e) => {
+                  if (selectedGoods.includes(item)) {
+                    this.setState((prevState) => {
+                      const index = prevState.selectedGoods.indexOf(item);
+
+                      prevState.selectedGoods.splice(index, 1);
+
+                      return {
+                        selectedGoods: prevState.selectedGoods,
+                      };
+                    });
+                  }
+                }}
+              >
+                Remove
+              </button>
+            </>
+          ))}
+
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
