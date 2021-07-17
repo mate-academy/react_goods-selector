dacb3799d2f76b2c9c86e65e9283381c087aff06
Oleksii Lymonov <alex25_92@ukr.net>
2021-07-17 14:18:34 +0300

    first_version

--- src/App.js
+++ src/App.js
@@ -14,11 +14,106 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
+const productNotSelected = (item, action) => (
+  <div key={item}>
+    <li className="noAdd">
+      { item }
+    </li>
+    <button
+      type="button"
+      className="addButton"
+      onClick={() => {
+        action(item);
+      }
+      }
+    >
+      Add
+    </button>
   </div>
 );
+
+const productSelected = (item, action) => (
+  <div key={item}>
+    <li className="added">
+      { item }
+    </li>
+    <button
+      type="button"
+      className="removeButton"
+      onClick={() => {
+        action(item);
+      }
+      }
+    >
+      Remove
+    </button>
+  </div>
+);
+
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
+
+  addProduct = (item) => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods.concat(item),
+    }));
+  }
+
+  removeProduct = (item) => {
+    this.setState(prevState => ({
+      selectedGoods:
+        prevState.selectedGoods.filter(product => product !== item),
+    }));
+  }
+
+  clearProductList = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  createProductList = () => {
+    if (this.state.selectedGoods.length < 3) {
+      return this.state.selectedGoods.join(' and ');
+    }
+
+    return `${this.state.selectedGoods.slice(0, -2).join(', ')
+    }, ${this.state.selectedGoods.slice(-2).join(' and ')}`;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {this.createProductList()}
+        </h1>
+        {this.state.selectedGoods.length === 0
+          ? null
+          : (
+            <button
+              type="button"
+              className="deleteButton"
+              onClick={() => this.clearProductList()}
+            >
+              Clear all
+            </button>
+          )}
+        <ul>
+          {goodsFromServer.map((item) => {
+            const isProductSelected = this.state.selectedGoods.includes(item);
+
+            return isProductSelected ? (
+              productSelected(item, this.removeProduct)
+            ) : (
+              productNotSelected(item, this.addProduct)
+            );
+          })
+        }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
