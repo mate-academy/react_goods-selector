e1e048e14eb8a1c3d1e79b9fdb0290b4e8fc5842
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-18 12:44:03 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,73 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    selectedGoods: ['Jam'],
 
+  }
+
+  addGoods = (value) => {
+    // this.setState({selectedGoods: [this.state.selectedGoods, ...value]})
+    if (!this.state.selectedGoods.includes(value)) {
+      this.setState(prevState => prevState.selectedGoods.push(value));
+    } else if (this.state.selectedGoods.includes(value)) {
+      this.setState(prevState => ({
+        selectedGoods: prevState.selectedGoods.filter(x => x !== value),
+      }));
+    }
+  }
+
+  cleanGoods = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <>
+        <h1>
+          {selectedGoods.length === 0
+            ? 'No goods selected'
+            : `${selectedGoods} is selected`
+          }
+          {' '}
+          {selectedGoods.length > 0
+            ? (
+              <button
+                type="button"
+                onClick={() => this.cleanGoods()}
+              >
+                clean
+              </button>
+            )
+            : null
+          }
+        </h1>
+        <ul>
+          {this.state.goods.map(good => (
+            <>
+              {' '}
+              <li className={selectedGoods.includes(good)
+                ? 'show'
+                : 'hide'}
+              >
+                {good}
+              </li>
+              <button
+                type="button"
+                onClick={() => this.addGoods(good)}
+              >
+                {selectedGoods.includes(good) ? 'remove' : 'add'}
+              </button>
+            </>
+          ))
+        }
+        </ul>
+      </>
+    );
+  }
+}
 export default App;
