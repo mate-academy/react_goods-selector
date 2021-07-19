d0a63f305a137446b897f3c06d249e2676f74127
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-19 17:41:13 +0300

    feat: completed task goods-selector

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +14,83 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
+
+  selectedProduct = (products) => {
+    switch (products.length) {
+      case 1:
+        return products[0];
+
+      case 0:
+        return 'No goods selected';
+
+      default:
+        return `${products.slice(0, -1)} and`
+          + ` ${products[products.length - 1]} are selected`;
+    }
+  }
+
+  addProduct = (product) => {
+    this.setState(state => ({
+      selectedGoods: [...state.selectedGoods, product],
+    }));
+  }
+
+  removeProduct = (product) => {
+    this.setState(state => ({
+      selectedGoods: state.selectedGoods.filter(item => item !== product),
+    }));
+  }
+
+  removeAllGoods = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="product">
+        <h1 className="product__title">
+          {`Selected good: ${this.selectedProduct(selectedGoods)}`}
+        </h1>
+        <button
+          type="button"
+          className="product__button"
+          onClick={this.removeAllGoods}
+        >
+          X
+        </button>
+        <ul className="product__list">
+          {goodsFromServer.map(product => (
+            <li
+              key={product}
+              className="product__item"
+            >
+              {product}
+              <button
+                type="button"
+                className="add"
+                onClick={() => this.addProduct(product)}
+              >
+                Add
+              </button>
+              <button
+                type="button"
+                className="remove"
+                onClick={() => this.removeProduct(product)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
