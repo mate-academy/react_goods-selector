bfe3f8f6f0141143e5b05fc9d0fa15ed188ac0ed
David Dohru <tronsmartmy@gmail.com>
2021-07-19 20:35:36 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 const goodsFromServer = [
@@ -12,13 +12,89 @@ const goodsFromServer = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(item => ({
+  item,
+}));
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
+
+  renderTitle = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return `No goods selected`;
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and
+          ${selectedGoods.slice(-1)} are selected`;
+    }
+  }
+
+  render() {
+    const addGoods = (value) => {
+      this.setState(prevState => ({
+        selectedGoods: [...prevState.selectedGoods, value],
+      }));
+    };
+
+    const removeGoods = (value) => {
+      this.setState(prevState => ({
+        selectedGoods: prevState.selectedGoods.filter(
+          selectedGood => selectedGood !== value,
+        ),
+      }));
+    };
+
+    const resetSelectedGoods = () => this.setState({ selectedGoods: [] });
+
+    return (
+      <div className="App">
+        <h1>{this.renderTitle()}</h1>
+
+        <button
+          type="button"
+          className="reset"
+          onClick={resetSelectedGoods}
+          disabled={!this.state.selectedGoods.length}
+        >
+          Reset selections
+        </button>
+
+        <ul className="App__list">
+          {goodsFromServer.map(value => (
+            <li
+              key={value.id}
+              className={this.state.selectedGoods.includes(value.item)
+                ? 'selected goods' : 'goods'}
+            >
+              <span>{value.item}</span>
+
+              <button
+                type="button"
+                className={'App__button'
+                  && this.state.selectedGoods.includes(value.item)
+                  ? 'active'
+                  : 'no_active'}
+                onClick={() => (
+                  this.state.selectedGoods.includes(value.item)
+                    ? removeGoods(value.item)
+                    : addGoods(value.item)
+                )}
+              >
+                {this.state.selectedGoods.includes(value.item)
+                  ? 'Unselect' : 'Select'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
