d4aee5ef157edd8a234ad4b23c2f8f27898024ab
Sviatoslav Kishka <sviatoslavkishka@gmail.com>
2021-07-19 16:56:02 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,76 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
 
-export default App;
+  selectedItemsList = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No Goods Selected';
+      case 1:
+        return `${selectedGoods[0]} is Selected`;
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and
+          ${selectedGoods.slice(-1)} are Selected`;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.selectedItemsList()}
+        </h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              selectedGoods: [],
+            });
+          }}
+          disabled={this.state.selectedGoods.length === 0}
+        >
+          Clear all Selections
+        </button>
+        <ul>
+          {goodsFromServer.map(item => (
+            <li
+              className={classNames('goods__item', {
+                selectedGood: this.state.selectedGoods.includes(item),
+              })}
+            >
+              {item}
+              <button
+                type="button"
+                onClick={() => {
+                  this.setState((prevState) => {
+                    if (prevState.selectedGoods.includes(item)) {
+                      const index = prevState.selectedGoods.indexOf(item);
+
+                      prevState.selectedGoods.splice(index, 1);
+                    } else {
+                      prevState.selectedGoods.push(item);
+                    }
+
+                    return {
+                      selectedGoods: prevState.selectedGoods,
+                    };
+                  });
+                }}
+              >
+                {this.state.selectedGoods.includes(item)
+                  ? 'Unselect'
+                  : 'Select'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
