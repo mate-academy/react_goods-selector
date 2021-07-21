1d0ad0c4e4db944d055c35bd02a52486452f4c73
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-21 17:10:04 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,75 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: ['Jam'],
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {(goods.length !== 0)
+            ? (
+              `Selected goods: ${goods.join(', ')}`
+            )
+            : (
+              'No goods selected'
+            )
+          }
+        </h1>
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+            <li key={good} className="App__list-item">
+              {good}
+              {goods.includes(good)
+                ? (
+                  <button
+                    className="App__btn-remove"
+                    type="button"
+                    onClick={() => {
+                      this.setState({
+                        goods: goods.filter(value => good !== value),
+                      });
+                    }}
+                  >
+                    remove
+                  </button>
+                )
+                : (
+                  <button
+                    className="App__btn-add"
+                    type="button"
+                    onClick={() => {
+                      this.setState({
+                        goods: [...goods, good],
+                      });
+                    }}
+                  >
+                    add
+                  </button>
+                )
+              }
+            </li>
+          ))}
+          <button
+            className="App__btn-clear"
+            type="button"
+            onClick={() => {
+              this.setState({
+                goods: [],
+              });
+            }}
+          >
+            CLEAR ALL
+          </button>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
