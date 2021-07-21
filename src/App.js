59f5b9dca81a888b6f46ab70bd940f7e3b0bace7
Dana Pryshelets <tanyarud53@gmail.com>
2021-07-21 18:15:36 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,63 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  }
+
+  render() {
+    return (
+      <div className="container border rounded-pill">
+        <div className="message">
+          <h1>
+            {
+            this.state.selectedGood === null
+              ? 'No goods selected'
+              : `${this.state.selectedGood} is selected`
+            }
+          </h1>
+          <button
+            type="button"
+            className={this.state.selectedGood === null
+              ? 'button--hidden'
+              : 'btn-close'
+            }
+            onClick={() => {
+              this.setState({ selectedGood: null });
+            }}
+          />
+        </div>
+        <ol className="goods">
+          {goodsFromServer.map(good => (
+            <div className="good">
+              <li
+                key={good}
+                className={good === this.state.selectedGood
+                  ? 'good-selected'
+                  : ''}
+              >
+                {good}
+              </li>
+              <button
+                type="button"
+                className={
+                  good === this.state.selectedGood
+                    ? 'button--hidden btn btn-outline-secondary btn-sm'
+                    : 'btn btn-outline-secondary btn-sm'
+                }
+                onClick={() => {
+                  this.setState({ selectedGood: good });
+                }}
+              >
+                Select
+              </button>
+            </div>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default App;
