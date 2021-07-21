d54ae85e572096b6a5d15cb470686e6e6ee55e75
korzh <korzh.alexey@yahoo.com>
2021-07-21 15:56:38 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,87 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    chosenElements: ['Jam'],
+  }
+
+  chooseElement = (ev, product) => {
+    const element = ev.nativeEvent.path[2];
+
+    if (!this.state.chosenElements.includes(product)) {
+      this.setState(state => ({
+        chosenElements: [...state.chosenElements, product],
+      }));
+    }
+
+    element.style.backgroundColor = '#ff6347';
+  }
+
+  removeElement = (ev, product) => {
+    const elemet = ev.nativeEvent.path[2];
+
+    this.setState(state => ({
+      chosenElements: state.chosenElements.filter(item => item !== product),
+    }));
+
+    elemet.style.backgroundColor = '';
+  }
+
+  selectedProducts = (products) => {
+    switch (products.length) {
+      case 0: return ' No products were selected';
+      case 1: return ` ${products} is selected`;
+      default: return ` ${products.join(', ')} are selected`;
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <h1>
+            Selected goods:
+            {this.selectedProducts(this.state.chosenElements)}
+          </h1>
+          <div className="goods">
+            {goodsFromServer.map(product => (
+              <div
+                className="goods__product"
+                key={product}
+                style={product === 'Jam'
+                  ? { backgroundColor: '#ff6347' }
+                  : { backgroundColor: 'transparent' }}
+              >
+                {product}
+                <div className="buttons">
+                  <button
+                    className="button"
+                    type="button"
+                    onClick={(ev) => {
+                      this.chooseElement(ev, product);
+                    }}
+
+                  >
+                    Choose
+                  </button>
+                  <button
+                    className="button"
+                    type="button"
+                    onClick={(ev) => {
+                      this.removeElement(ev, product);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
