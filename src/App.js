cdc2c808b7a7e791e2dc748a10f07d0e9c66fe6d
VadymMakevytss <vadymlat@mail.ru>
2021-07-23 13:10:31 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ListGroup, Button } from 'react-bootstrap';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +17,90 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends Component {
+  state = {
+    selectedGoods: ['Jam'],
+  };
 
-export default App;
+  setTitleList() {
+    const listLength = this.state.selectedGoods.length;
+
+    switch (listLength) {
+      case 0:
+        return 'NO GOODS SELECTED';
+      case 1:
+        return `${this.state.selectedGoods} is selected`;
+      default:
+        return `${this.state.selectedGoods.slice(0, -1).join(', ')} and
+          ${this.state.selectedGoods.slice(-1)} are selected`;
+    }
+  }
+
+  clearText = () => {
+    this.setState(
+      prevState => prevState.selectedGoods.splice(0),
+    );
+  }
+
+  goodIsSelected(good) {
+    const res = this.state.selectedGoods.includes(good);
+
+    return res;
+  }
+
+  addGood(good) {
+    this.setState(prevState => prevState.selectedGoods.push(good));
+  }
+
+  removeGood(good) {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods.filter(
+        selectedGood => selectedGood !== good,
+      ),
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App list">
+        <div>
+          <h1 className="list__title">
+            Selected good:
+            {' '}
+            {this.setTitleList()}
+            {' '}
+            <Button
+              onClick={this.clearText}
+            >
+              X
+            </Button>
+          </h1>
+        </div>
+
+        <ListGroup as="ul" className="list__group">
+          {goodsFromServer.map(good => (
+            <ListGroup.Item
+              key={uuidv4()}
+              active={this.goodIsSelected(good)}
+              as="li"
+              className="list__item"
+            >
+              { good }
+              <Button
+                key={uuidv4()}
+                variant={this.goodIsSelected(good) ? 'danger' : 'success'}
+                className="list__selector"
+                onClick={this.goodIsSelected(good)
+                  ? () => this.removeGood(good)
+                  : () => this.addGood(good)
+                }
+              >
+                {this.goodIsSelected(good) ? 'Remove' : 'Add'}
+              </Button>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
+}
