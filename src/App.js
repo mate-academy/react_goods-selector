85d8a3a7a859d2588a1fad3e6e177a3b7f7e1cc4
Liudmyla Yevchuck <lol22lil22@gmail.com>
2021-07-23 15:46:16 +0300

    List of goods

--- src/App.js
+++ src/App.js
@@ -14,11 +14,86 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
+
+  addProduct = (good) => {
+    this.setState(
+      prevState => ({ selectedGoods: [...prevState.selectedGoods, good] }),
+    );
+  }
+
+  removeProduct = ({ good }) => {
+    this.setState(
+      prevState => (
+        {
+          selectedGoods:
+          [prevState.selectedGoods.filter(item => item !== good)],
+        }
+      ),
+    );
+  }
+
+  cleanList = () => {
+    this.setState({ selectedGoods: [] });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="panel">
+          <div className="panel-heading">
+            <h1>
+              {this.state.selectedGoods.length
+                ? (`Selected good:
+                  ${this.state.selectedGoods.map(good => ` ${good} `)}`)
+                : ('No goods selected')
+            }
+            </h1>
+          </div>
+          <ul className="list">
+            {goodsFromServer.map(
+              good => (
+                <>
+                  <li key={good}>
+                    {good}
+                    {!this.state.selectedGoods.includes(good)
+                    && (
+                      <button
+                        type="submit"
+                        className="list__button
+                      button is-small is-light is-success is-rounded"
+                        onClick={() => (
+                          this.addProduct(good))
+                      }
+                      >
+                        Select
+                      </button>
+                    )}
+
+                  </li>
+                </>
+              ),
+            )}
+          </ul>
+          <div className="panel-block">
+            {this.state.selectedGoods.length > 0
+              && (
+              <button
+                type="submit"
+                className="button is-rounded is-black is-small"
+                onClick={this.cleanList}
+              >
+                Clean product list
+              </button>
+              )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
