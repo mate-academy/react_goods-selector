c267c6ce742f11bffb7c5453f58982f1bab76d87
Sasha Kozlovskyy <alex33507@gmail.com>
2021-07-24 18:23:07 +0300

    Goods selector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,74 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
+
+  selectGood = (good) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, good],
+    }));
+  }
+
+  clearingSelectedGoodslist = () => {
+    this.setState(() => ({
+      selectedGoods: [],
+    }));
+  }
+
+  makeSelectedGoodslist = () => {
+    switch (this.state.selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${this.state.selectedGoods} is selected`;
+      case 2:
+        return `${this.state.selectedGoods.join(' and ')} are selected`;
+      default: {
+        return `${this.state.selectedGoods.join(', ')} are selected`;
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>
+          Selected good:
+          {' '}
+          {this.makeSelectedGoodslist()}
+        </h1>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              className={classNames('app__list',
+                { activList: this.state.selectedGoods.includes(good) })}
+              key={goodsFromServer.indexOf(good)}
+            >
+              {good}
+              <button
+                type="button"
+                onClick={() => this.selectGood(good)}
+                className={classNames('list__button',
+                  { activ: this.state.selectedGoods.includes(good) })}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className="clear--button"
+          onClick={() => this.clearingSelectedGoodslist()}
+        >
+          Clear
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
