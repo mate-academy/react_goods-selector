9ca19b9a16aa5623c9f87a5ca71d20bd34937cc1
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-25 15:49:43 +0300

    sol1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,90 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [`Jam`],
+  }
 
+  clearSelection() {
+    this.setState({ selectedGoods: [] });
+  }
+
+  addGood(good) {
+    this.setState(state => (state.selectedGoods.push(good)));
+  }
+
+  removeGood(good) {
+    this.setState(
+      state => (
+        { selectedGoods: state.selectedGoods.filter(item => item !== good) }
+      ),
+    );
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App box">
+        <h1 className="row-with-items">
+          {!this.state.selectedGoods.length ? (
+            `No goods selected`
+          ) : (
+            <>
+              {`Selected products: ${selectedGoods}`}
+              <button
+                className="button is-small is-rounded"
+                type="button"
+                onClick={
+                  () => this.clearSelection()
+                }
+              >
+                X
+              </button>
+            </>
+          )}
+        </h1>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              className="item"
+              key={good}
+            >
+              {!this.state.selectedGoods.includes(good)
+                ? (
+                  <>
+                    {good}
+                    <button
+                      className="button is-small is-rounded"
+                      type="button"
+                      onClick={
+                        () => this.addGood(good)
+                      }
+                    >
+                      Select
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    {good}
+                    <button
+                      className="button is-small is-rounded"
+                      type="button"
+                      onClick={
+                        () => this.removeGood(good)
+                      }
+                    >
+                      Remove
+                    </button>
+                  </>
+                )
+              }
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 export default App;
