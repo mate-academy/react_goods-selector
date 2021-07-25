29624ec67522c2c99d2d9488789877c46d087061
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-25 12:34:08 +0300

    solution2

--- src/App.js
+++ src/App.js
@@ -19,20 +19,44 @@ class App extends React.Component {
     goods: ['Jam'],
   }
 
+  removeGood = (good) => {
+    this.setState(prevState => ({
+      goods: prevState.goods.filter(value => good !== value),
+    }));
+  }
+
+  addGood = (good) => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods, good],
+    }));
+  }
+
+  strOfGoods = () => {
+    const { goods } = this.state;
+
+    switch (goods.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `Selected goods: ${goods.join(', ')} is selected`;
+
+      case 2:
+        return `Selected goods: ${goods[0]} and ${goods[1]} are selected`;
+
+      default:
+        return `Selected goods: ${goods.slice(0, -1).join(', ')} `
+        + `and ${goods[goods.length - 1]} are selected`;
+    }
+  }
+
   render() {
     const { goods } = this.state;
 
     return (
       <div className="App">
         <h1>
-          {(goods.length !== 0)
-            ? (
-              `Selected goods: ${goods.join(', ')}`
-            )
-            : (
-              'No goods selected'
-            )
-          }
+          {this.strOfGoods()}
         </h1>
         <ul className="App__list">
           {goodsFromServer.map(good => (
@@ -44,9 +68,7 @@ class App extends React.Component {
                     className="App__btn-remove"
                     type="button"
                     onClick={() => {
-                      this.setState({
-                        goods: goods.filter(value => good !== value),
-                      });
+                      this.removeGood(good);
                     }}
                   >
                     remove
@@ -57,9 +79,7 @@ class App extends React.Component {
                     className="App__btn-add"
                     type="button"
                     onClick={() => {
-                      this.setState({
-                        goods: [...goods, good],
-                      });
+                      this.addGood(good);
                     }}
                   >
                     add
