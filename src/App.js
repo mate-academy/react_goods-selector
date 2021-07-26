e068b573669ac21be765967c5ecf8e33cd274bb1
Leonid <manu31051996@gmail.com>
2021-07-26 12:56:56 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,100 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  }
+
+  chooseProduct = (product) => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods.concat(product),
+    }));
+  }
+
+  deleteProduct = (product) => {
+    this.setState(prevState => ({
+      selectedGoods:
+      prevState.selectedGoods.filter(produce => produce !== product),
+    }));
+  }
+
+  clearProductList = () => {
+    this.setState(prevState => ({
+      selectedGoods: prevState.selectedGoods.splice(),
+    }));
+  }
+
+  makeProductList = () => {
+    if (this.state.selectedGoods.length === 0) {
+      return 'No goods selected';
+    }
+
+    return this.state.selectedGoods.join(' , ');
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="title">
+          Selected good:
+          {this.makeProductList()}
+        </h1>
+        {!!this.state.selectedGoods.length
+          && (
+            <button
+              type="button"
+              className="deleteButton"
+              onClick={() => this.clearProductList()}
+            >
+              Clear product list  X
+            </button>
+          )}
+        <ul className="list">
+          {goodsFromServer.map((product) => {
+            const productList = this.state.selectedGoods.includes(product);
+
+            return (
+              <React.Fragment key={product}>
+                <li className={
+                  productList
+                    ? 'added'
+                    : 'noAdd'
+                  }
+                >
+                  { product }
+                </li>
+                <button
+                  type="button"
+                  className={
+                    productList
+                      ? 'removeButton'
+                      : 'addButton'
+                  }
+                  onClick={
+                    productList
+                      ? () => {
+                        this.deleteProduct(product);
+                      }
+                      : () => {
+                        this.chooseProduct(product);
+                      }
+                  }
+                >
+                  {
+                    productList
+                      ? 'delete'
+                      : 'choose'
+                  }
+                </button>
+              </React.Fragment>
+            );
+          })
+        }
+        </ul>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
