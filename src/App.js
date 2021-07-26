59dfeef157219c35c99b42008d1079565a7d9da8
Pavlo Starynskyi <starinskiy@gmail.com>
2021-07-26 09:31:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,111 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [goodsFromServer[8]],
+  }
+
+  addGood = (good) => {
+    this.setState(state => ({
+      selectedGoods: [...state.selectedGoods, good],
+    }));
+  }
+
+  removeGood = (good) => {
+    this.setState(state => ({
+      selectedGoods: state.selectedGoods.filter(item => item !== good),
+    }));
+  }
+
+  removeAllGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  getListOfOrder = () => {
+    const { selectedGoods } = this.state;
+
+    let message = '';
+
+    if (selectedGoods.length === 0) {
+      message = 'No goods selected';
+    }
+
+    if (selectedGoods.length === 1) {
+      message = `${selectedGoods.join('')} is selected`;
+    }
+
+    if (selectedGoods.length === 2) {
+      message = `${selectedGoods.join(' and ')} are selected`;
+    }
+
+    if (selectedGoods.length > 2) {
+      const lastItem = selectedGoods.slice(-1);
+      const restOfItems = selectedGoods.slice(0, selectedGoods.length - 1);
+
+      message = `${restOfItems.join(`, `)} and ${lastItem.join('')} 
+      are selected`;
+    }
+
+    return message;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.getListOfOrder()}
+        </h1>
+
+        <button
+          type="button"
+          className={this.state.selectedGoods.length > 0
+            ? 'showButton'
+            : 'hideButton'
+          }
+          onClick={this.removeAllGoods}
+        >
+          X
+        </button>
+
+        <ul>
+          {goodsFromServer.map((item) => {
+            const isProductSelected = this.state.selectedGoods.includes(item);
+
+            return (
+              <li
+                key={item}
+                className={isProductSelected
+                  ? 'added'
+                  : 'notAdded'
+                }
+              >
+                <div className="item">
+                  {item}
+                </div>
+
+                <button
+                  type="button"
+                  className="buttonToAdd"
+                  onClick={() => this.addGood(item)}
+                >
+                  Add
+                </button>
+
+                <button
+                  type="button"
+                  className="buttonToRemove"
+                  onClick={() => this.removeGood(item)}
+                >
+                  Remove
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
