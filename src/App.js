9886115b9e74c26ae31c37f15968d7946bea4586
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-07-26 13:48:45 +0300

    sol

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-const goodsFromServer = [
+const productsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,11 +14,92 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedProducts: ['Jam'],
+  }
+
+  addProduct(product) {
+    this.setState(state => (state.selectedProducts.push(product)));
+  }
+
+  deleteProduct(product) {
+    this.setState(state => (
+      { selectedProducts: state.selectedProducts.filter(
+        value => value !== product,
+      ) }
+    ));
+  }
+
+  clearProduct() {
+    this.setState({ selectedProducts: [] });
+  }
+
+  render() {
+    const { selectedProducts } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          {!this.state.selectedProducts.length ? (
+            'No Products selected'
+          ) : (
+            `Selected products: ${selectedProducts}`
+          )}
+        </h1>
+        <button
+          className="App__btn-close"
+          type="button"
+          onClick={
+            () => this.clearProduct()
+          }
+        >
+          Remove selected products
+        </button>
+        <ul className="List">
+          {productsFromServer.map(
+            product => (!this.state.selectedProducts.includes(product)
+              ? (
+                <li
+                  className="List__product"
+                  key={product}
+                >
+                  {product}
+                  :
+                  <button
+                    className="List__btn"
+                    type="button"
+                    onClick={
+                      () => this.addProduct(product)
+                    }
+                  >
+                    Add product
+                  </button>
+                </li>
+              ) : (
+                <li
+                  className="List__product"
+                  key={product}
+                >
+                  {product}
+                  :
+                  <button
+                    className="List__btn"
+                    type="button"
+                    onClick={
+                      () => this.deleteProduct(product)
+                    }
+                  >
+                    Delete Product
+                  </button>
+                </li>
+              )
+            ),
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
