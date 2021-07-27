69f953b57ba4cdb0e9f8064905709b3b1cea44af
Михаил <mihay0113@gmail.com>
2021-07-27 21:07:53 +0300

    no styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.scss';
 
 const goodsFromServer = [
@@ -14,11 +15,63 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preparedGoods = goodsFromServer.map(good => ({
+  name: good,
+  id: uuidv4(),
+}));
+
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  }
+
+  selectGood = ({ name, id }) => {
+    this.setState({
+      selectedGood: name,
+      id,
+    });
+  };
+
+  removeGood = () => {
+    this.setState({
+      selectedGood: null,
+      id: null,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.state.selectedGood
+            ? `${this.state.selectedGood} is selected`
+            : `No goods selected`
+          }
+        </h1>
+        <button type="button" onClick={this.removeGood}>
+          X
+        </button>
+        <ul>
+          {preparedGoods.map(good => (
+            <li key={good.id}>
+              {good.name}
+              {(this.state.id !== good.id)
+                && (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.selectGood(good);
+                    }}
+                  >
+                    Select
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
