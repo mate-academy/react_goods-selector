e1b395382db15a151ac3b4ec362e6e87f56a9c90
NYaremchuk <nazarmails@gmail.com>
2021-07-27 15:52:26 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,70 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: 'Jam',
+  };
+
+  saveHandler = (goods) => {
+    this.setState({ selectedGoods: goods });
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {selectedGoods
+            ? (` ${selectedGoods} is selected `)
+            : (` No goods selected `)
+          }
+          <button
+            className={
+              selectedGoods
+                ? 'enable btn btn-info'
+                : 'disable btn'
+            }
+            type="button"
+            onClick={() => {
+              this.saveHandler('');
+            }}
+          >
+            X
+          </button>
+        </h1>
+
+        <ul>
+          {goodsFromServer.map(goods => (
+            <li
+              className={
+                    goods === selectedGoods
+                      ? 'alert alert-danger'
+                      : 'alert alert-success'
+                  }
+            >
+              {`${goods} `}
+              <button
+                className={
+                        goods !== selectedGoods
+                          ? 'enable btn btn-info'
+                          : 'disable btn'
+                      }
+                type="button"
+                onClick={() => {
+                  this.saveHandler(goods);
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    );
+  }
+}
 
 export default App;
