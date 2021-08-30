80179d4c89cc5ff2b455ab60ba2660c020eb916e
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-08-30 20:39:31 +0300

    Resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +15,88 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface Option {
+  [key: string]: boolean;
+}
+
+type State = {
+  goods?: string;
+  visited: Option;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    visited: { Jam: true },
+  };
+
+  render() {
+    const keys = Object.keys(this.state.visited);
+
+    const performGoods = () => {
+      const finn = keys.filter(key => this.state.visited[key]);
+
+      if (finn.length === 1) {
+        return `${finn} is selected`;
+      }
+
+      if (finn.length === 2) {
+        return `${finn.join(' and ')} are selected`;
+      }
+
+      if (finn.length > 2) {
+        finn[finn.length - 1] = `and ${finn[finn.length - 1]}`;
+
+        return `${finn.join(', ')} are selected`;
+      }
+
+      return 'No goods selected';
+    };
+
+    const showButton = () => keys.some(key => this.state.visited[key]);
+
+    return (
+      <div className="App">
+        <h1>
+          {performGoods()}
+        </h1>
+        <button
+          type="button"
+          className={`button ${(showButton()) ? 'button__visible' : 'button__hide'}`}
+          onClick={() => {
+            if (Object.keys(this.state.visited).length > 0) {
+              this.setState({ visited: {} });
+            }
+          }}
+        >
+          X CLEAR X
+        </button>
+        <ol>
+          {goodsFromServer.map(product => {
+            const { visited } = this.state;
+
+            return (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+            // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+              <li
+                key={product}
+                onClick={() => {
+                  this.setState((prevState) => ({
+                    visited: {
+                      ...prevState.visited,
+                      [product]: !prevState.visited[product],
+                    },
+                  }));
+                }}
+                className={`goods ${(visited[product]) ? 'goods__unvisited' : 'goods__visited'}`}
+              >
+                {product}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default App;
