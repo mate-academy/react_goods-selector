4e15b7ea3ecacd8480846640a23537548c0cb1d7
VeronikaLushchik <loaaary212@gmail.com>
2021-09-01 16:18:20 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,98 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string[],
+};
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: [],
+  };
+
+  resetGoods = () => {
+    this.setState({ selectedGood: [] });
+  };
+
+  addGood = (newGood:string) => {
+    this.setState((prevGoods) => ({
+      selectedGood: [...prevGoods.selectedGood, newGood],
+    }));
+  };
+
+  removeGood = (deleteGood:string) => {
+    const arrayOfGoods = this.state.selectedGood;
+
+    if (arrayOfGoods.includes(deleteGood)) {
+      arrayOfGoods.splice(arrayOfGoods.lastIndexOf(deleteGood), 1);
+    }
+
+    this.setState((newGoods) => ({
+      selectedGood: [...newGoods.selectedGood],
+    }));
+  };
+
+  render() {
+    let message = '';
+
+    if (this.state.selectedGood.length === 0) {
+      message = 'No goods selected';
+    }
+
+    if (this.state.selectedGood.length === 1) {
+      message = `${this.state.selectedGood.join(', ')} is selected`;
+    }
+
+    if (this.state.selectedGood.length > 1) {
+      const lastElem = this.state.selectedGood[this.state.selectedGood.length - 1];
+      const firstPart = this.state.selectedGood.slice(0, -1).join(', ');
+
+      message = `${firstPart} and ${lastElem} are selected`;
+    }
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {message}
+        </h1>
+        <button
+          type="button"
+          className="button"
+          onClick={this.resetGoods}
+        >
+          X
+        </button>
+
+        <section className="cards">
+          {goodsFromServer.map(good => (
+            <div className={this.state.selectedGood.includes(good) ? 'card__active' : 'card'}>
+              <h2 className="card__title">
+                {good}
+              </h2>
+
+              <button
+                type="button"
+                className="button button__add"
+                onClick={() => {
+                  this.addGood(good);
+                }}
+              >
+                Add
+              </button>
+              <button
+                type="button"
+                className="button"
+                onClick={() => {
+                  this.removeGood(good);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
