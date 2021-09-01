05652841e3dd4fbd11aa4688ec3150ac14fde5de
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-09-02 00:11:52 +0300

    goods list was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,117 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+enum Operation {
+  Add,
+  Remove,
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  goodsOperationsHandler = (
+    event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLButtonElement>,
+    operation: Operation,
+    good?: string,
+  ) => {
+    event.preventDefault();
+
+    switch (operation) {
+      case Operation.Add:
+        if (good) {
+          this.setState((state) => ({ selectedGoods: [...state.selectedGoods, good] }));
+        }
+
+        break;
+      case Operation.Remove:
+        if (good) {
+          this.setState((state) => (
+            { selectedGoods: state.selectedGoods.filter(goodItem => goodItem !== good) }
+          ));
+        }
+
+        break;
+      default:
+    }
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+    const goodsLengthCheck = selectedGoods.length > 1
+      ? `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods[selectedGoods.length - 1]} are selected`
+      : `${selectedGoods[0]} is selected`;
+
+    return (
+      <div className="App">
+        <div className="App__top">
+          <h1>
+            {selectedGoods.length ? goodsLengthCheck : 'No goods selected'}
+          </h1>
+          {!!selectedGoods.length && (
+            <button
+              type="button"
+              onClick={() => {
+                this.setState({ selectedGoods: [] });
+              }}
+              onKeyDown={() => {
+                this.setState({ selectedGoods: [] });
+              }}
+            >
+              X
+            </button>
+          )}
+        </div>
+        <ul>
+          {goodsFromServer.map((good) => (
+            <li key={good}>
+              <div className="App__good">
+                <p
+                  className={classNames('App__good-text', { 'App__good-text-selected': selectedGoods.includes(good) })}
+                >
+                  {good}
+                </p>
+                {
+                  selectedGoods.includes(good)
+                    ? (
+                      <button
+                        type="button"
+                        onClick={(event) => {
+                          this.goodsOperationsHandler(event, Operation.Remove, good);
+                        }}
+                        onKeyDown={(event) => {
+                          this.goodsOperationsHandler(event, Operation.Remove, good);
+                        }}
+                      >
+                        Remove
+                      </button>
+                    )
+                    : (
+                      <button
+                        type="button"
+                        onClick={(event) => {
+                          this.goodsOperationsHandler(event, Operation.Add, good);
+                        }}
+                        onKeyDown={(event) => {
+                          this.goodsOperationsHandler(event, Operation.Add, good);
+                        }}
+                      >
+                        Add
+                      </button>
+                    )
+                }
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
