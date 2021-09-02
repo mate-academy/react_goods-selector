f9f672cf3b10d9415b66858dbe2a3007a1a4d923
NastyaVovk <winc4ester@gmail.com>
2021-09-02 19:01:46 +0300

    the very first

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,66 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  addProduct = (product : string) => {
+    this.setState({ selectedGood: product });
+  };
+
+  clearSelection = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.state.selectedGood || 'Nothing'}
+          {' is selected'}
+        </h1>
+
+        {this.state.selectedGood && (
+          <button
+            type="button"
+            onClick={this.clearSelection}
+          >
+            Clear selection
+          </button>
+        )}
+
+        <ul>
+          { goodsFromServer.map(product => (
+            <li
+              key={product}
+              className={classNames('list__item', {
+                selected: this.state.selectedGood === product,
+              })}
+            >
+              {product}
+              <button
+                className={classNames('goods__button', {
+                  added: this.state.selectedGood === product,
+                })}
+                type="button"
+                onClick={() => {
+                  this.addProduct(product);
+                }}
+              >
+                Select
+              </button>
+            </li>
+          )) }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
