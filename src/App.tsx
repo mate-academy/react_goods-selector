427b83b6d704d0f1e902c829f1056e1624c8b879
Ihor Nahnybida <igor653279@gmail.com>
2021-09-03 00:52:38 +0300

    app component created. scss files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,84 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: [],
+  };
+
+  addItem = (item: string) => {
+    const { selectedGoods } = this.state;
+
+    if (!selectedGoods.includes(item)) {
+      this.setState({ selectedGoods: [...selectedGoods, item] });
+    }
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+    const normalizedGoods = [
+      selectedGoods.slice(0, -1).join(', '),
+      selectedGoods.slice(-1),
+    ].join(
+      selectedGoods.length > 1
+        ? ' and '
+        : '',
+    );
+
+    return (
+      <div className="App">
+
+        <div className="App__container">
+          <div className="App__header">
+            <div className="App__title">
+              <h1>
+                {selectedGoods.length
+                  ? 'Selected:'
+                  : 'No goods selected'}
+              </h1>
+              {selectedGoods.length !== 0 && (
+                <button
+                  className="App__clear-button"
+                  type="button"
+                  onClick={() => {
+                    this.setState({ selectedGoods: [] });
+                  }}
+                >
+                  +
+                </button>
+              )}
+            </div>
+            <p className="App__selected-items">
+              {normalizedGoods}
+            </p>
+          </div>
+
+          <ul className="App__items">
+            {goodsFromServer.map(item => (
+              <li className="App__item-wrapper" key={item}>
+                <button
+                  className={classNames('App__button', {
+                    'App__button--pressed': selectedGoods.includes(item),
+                  })}
+                  type="button"
+                  onClick={() => {
+                    this.addItem(item);
+                  }}
+                >
+                  +
+                </button>
+                <p className="App__item">
+                  {item}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
