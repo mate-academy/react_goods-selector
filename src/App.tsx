ce11a9074dacfd60cb578fe88b07cc2771d4f950
SashaRudenko <for_london@ukr.net>
2021-09-02 13:52:48 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,99 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  goods: string[];
+  addedToCart: string[];
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    addedToCart: [],
+  };
+
+  array: string[] = [];
+
+  addGood = (good: string) => {
+    this.array.push(good);
+    this.setState(() => ({
+      addedToCart: this.array,
+    }));
+  };
+
+  removeGood = (good: string) => {
+    this.array.splice(this.array.lastIndexOf(good), 1);
+    this.setState(() => ({
+      addedToCart: this.array,
+    }));
+  };
+
+  reset = () => {
+    this.array = [];
+    this.setState(() => ({
+      addedToCart: this.array,
+    }));
+  };
+
+  render() {
+    const { goods, addedToCart } = this.state;
+
+    return (
+      <div className="App">
+        <div className="container">
+          <h1 className="h1. Bootstrap heading">Selected good: </h1>
+          {addedToCart.length === 1 && `${addedToCart[0]} is selected`}
+          {addedToCart.length > 1 && (
+            <>
+              {addedToCart.map((item: string) => (
+                <div key={item} className="item">{item}</div>))}
+              are selected
+            </>
+          )}
+          {!addedToCart.length && 'No goods selected'}
+        </div>
+        <button
+          type="submit"
+          onClick={() => {
+            this.reset();
+          }}
+          className="btn btn-warning"
+        >
+          Reset
+        </button>
+        <div className="Goods">
+          {goods.length && (
+            goods.map((good: string) => (
+              <div className="GoodItem">
+                <h3 key={good} className={!addedToCart.includes(good) ? ('text-primary') : ('text-light bg-dark')}>{good}</h3>
+                {!addedToCart.includes(good) && (
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      this.addGood(good);
+                    }}
+                    className="btn btn-primary"
+                  >
+                    Add
+                  </button>
+                )}
+                {addedToCart.includes(good) && (
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      this.removeGood(good);
+                    }}
+                    className="btn btn-danger"
+                  >
+                    X
+                  </button>
+                )}
+              </div>
+            )))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
