6eef3ff2ec4b2c08783ff055066a0f20dddd9590
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-09-05 16:40:12 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,57 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string | null;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1 className="app__title">
+          {this.state.selectedGood
+            ? (`${this.state.selectedGood} is selected`)
+            : ('No goods selected')}
+        </h1>
+        <button
+          type="button"
+          className="app__button"
+          onClick={() => this.setState({ selectedGood: null })}
+          disabled={!this.state.selectedGood}
+        >
+          X
+        </button>
+        <ul>
+          {goodsFromServer.map(good => {
+            const selected = good === this.state.selectedGood;
+
+            return (
+              <div>
+                <li key={good} className={classNames('app__item', { app__selected: selected })}>{good}</li>
+                {!selected
+                  && (
+                    <button
+                      type="button"
+                      className="app__button"
+                      onClick={() => {
+                        this.setState({ selectedGood: good });
+                      }}
+                    >
+                      Select
+                    </button>
+                  )}
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
