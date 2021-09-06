5e657e5b4b2b01ee848563f4e0ca2c980e2d9549
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-06 22:24:25 +0300

    new goods selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-const goodsFromServer: string[] = [
+const goodsArrayFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -25,68 +24,96 @@ export class App extends React.Component<{}, State> {
     selectedGood: ['Jam'],
   };
 
-  getSelect = (item: string) => {
+  addSelect = (item: string) => {
     this.setState((state) => ({
       selectedGood: [...state.selectedGood, item],
     }));
   };
 
+  removeSelect = (item: string) => {
+    const index = this.state.selectedGood.indexOf(item);
+
+    this.state.selectedGood.splice(index, 1);
+    this.setState((state) => ({
+      selectedGood: [...state.selectedGood],
+    }));
+  };
+
   render() {
+    const goodsArray = this.state.selectedGood;
+    const titleOneGoods = `${goodsArray[0]} is selected`;
+    const titleEmpty = 'No goods selected';
+    const titleFull = `${goodsArray.slice(0, goodsArray.length - 1).join(', ')}`
+    + ` and ${goodsArray[goodsArray.length - 1]} is selected`;
+
+    const titleInner = goodsArray.length > 1
+      ? titleFull
+      : titleOneGoods;
+
     return (
       <div className="App">
-        <ul className="list-group">
-          {goodsFromServer.map(item => (
+        <div className="titleContainer">
+          <h1 className="title">
+            {goodsArray.length === 0 ? titleEmpty : titleInner}
+          </h1>
+          <button
+            className={classNames(
+              'title-button',
+              {
+                'title-button--visible': this.state.selectedGood.length === 0,
+              },
+            )}
+            type="button"
+            onClick={() => (
+              this.setState({ selectedGood: [] })
+            )}
+          >
+            X
+          </button>
+        </div>
+        <ul className="goodsList">
+          {goodsArrayFromServer.map(item => (
             <li
               key={item}
               className={classNames(
-                'list-group-item list-group-item-info',
+                'goodsList__item',
                 {
-                  'list-group-item-success': this.state.selectedGood.includes(item),
+                  'goodsList__item--active': this.state.selectedGood.includes(item),
                 },
               )}
             >
               <div>{item}</div>
               <button
                 className={classNames(
-                  'btn btn-success',
+                  'goodsList__button',
                   {
-                    visible: this.state.selectedGood.includes(item),
+                    'goodsList__button--visible': this.state.selectedGood.includes(item),
                   },
                 )}
                 type="button"
                 onClick={() => {
-                  this.getSelect(item);
+                  this.addSelect(item);
                 }}
               >
-                Select
+                Add
+              </button>
+              <button
+                className={classNames(
+                  'goodsList__button goodsList__button--remove',
+                  {
+                    'goodsList__button--visible': !this.state.selectedGood.includes(item),
+                  },
+                )}
+                type="button"
+                onClick={() => {
+                  this.removeSelect(item);
+                }}
+              >
+                Remove
               </button>
             </li>
           ))}
         </ul>
-        <h1
-          className={classNames(
-            'title',
-            {
-              visible: this.state.selectedGood.length === 0,
-            },
-          )}
-        >
-          {`${this.state.selectedGood.join(', ')} is selected`}
-        </h1>
-        <button
-          className={classNames(
-            'btn btn-warning btn-center',
-            {
-              visible: this.state.selectedGood.length === 0,
-            },
-          )}
-          type="button"
-          onClick={() => (
-            this.setState({ selectedGood: [] })
-          )}
-        >
-          X
-        </button>
       </div>
     );
   }
