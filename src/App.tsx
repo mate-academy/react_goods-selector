44b822821d431832c8ab2b449a9f02c7924a0add
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-06 18:11:19 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,78 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: ['Jam'],
+  };
+
+  getSelect = (item: string) => {
+    this.setState((state) => ({
+      selectedGood: [...state.selectedGood, item],
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ul className="list-group">
+          {goodsFromServer.map(item => (
+            <li
+              key={item}
+              className={classNames(
+                'list-group-item list-group-item-info',
+                {
+                  'list-group-item-success': this.state.selectedGood.includes(item),
+                },
+              )}
+            >
+              <div>{item}</div>
+              <button
+                className={classNames(
+                  'btn btn-success',
+                  {
+                    visible: this.state.selectedGood.includes(item),
+                  },
+                )}
+                type="button"
+                onClick={() => {
+                  this.getSelect(item);
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+        <h1
+          className={classNames(
+            'title',
+            {
+              visible: this.state.selectedGood.length === 0,
+            },
+          )}
+        >
+          {`${this.state.selectedGood.join(', ')} is selected`}
+        </h1>
+        <button
+          className={classNames(
+            'btn btn-warning btn-center',
+            {
+              visible: this.state.selectedGood.length === 0,
+            },
+          )}
+          type="button"
+          onClick={() => (
+            this.setState({ selectedGood: [] })
+          )}
+        >
+          X
+        </button>
+      </div>
+    );
+  }
+}
