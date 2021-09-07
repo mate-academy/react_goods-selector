f33010d65116841c5530aff7d1b265ff7addffb1
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-09-07 13:58:51 +0300

    React_goods-selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,74 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: 'Jam',
+  };
+
+  selectedGood = (item: string) => {
+    this.setState({ selectedGoods: item });
+  };
+
+  clearItem = () => {
+    this.setState({ selectedGoods: '' });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__top">
+          {selectedGoods
+            ? <h1>{`Selected good: - ${selectedGoods}`}</h1>
+            : <h1>No goods selected!</h1>}
+
+          {selectedGoods && (
+            <button
+              type="button"
+              className="App__button--clear button"
+              onClick={() => this.clearItem()}
+            >
+              Clear
+            </button>
+          )}
+        </div>
+
+        <ul className="App__list-of-goods">
+          {goodsFromServer.map(good => (
+            <li
+              className={classNames(
+                { 'App__good-item': good === selectedGoods },
+              )}
+              key={good}
+            >
+              {good}
+              {!(selectedGoods.includes(good)) && (
+                <button
+                  type="button"
+                  className="App__goods-button button"
+                  onClick={() => this.selectedGood(good)}
+                >
+                  Select
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+// const App: React.FC = () => (
+//   <div className="App">
+//     <h1>Selected good: -</h1>
+//     {goodsFromServer.length}
+//   </div>
+// );
 
 export default App;
