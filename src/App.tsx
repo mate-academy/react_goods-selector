541b145c83683f3a678ef8cac77488cef2878f0e
unknown <hado.roman.ihorovych@gmail.com>
2021-09-07 15:59:41 +0300

    implemented react_goods-selector

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,92 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  addProduct = (product: string) => {
+    this.setState((prevState) => ({
+      selectedGoods: [...prevState.selectedGoods, product],
+    }));
+  };
+
+  removeProduct = (product: string) => {
+    this.state.selectedGoods.splice(this.state.selectedGoods.indexOf(product), 1);
+
+    this.setState((prevState) => ({
+      selectedGoods: [...prevState.selectedGoods],
+    }));
+  };
+
+  clearTheGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    selectedGoods.join(' ,');
+
+    return (
+      <div className="app">
+        <h1>
+          {selectedGoods.length === 0 && 'No goods selected'}
+          {selectedGoods.length === 1 && `${selectedGoods} is selected`}
+          {selectedGoods.length >= 2 && `${selectedGoods.join(', ')} are selected`}
+        </h1>
+        <ul className="app__list">
+          {goodsFromServer.map(product => {
+            if (this.state.selectedGoods.includes(product)) {
+              return (
+                <li
+                  className="list-group-item selected app__item"
+                >
+                  {product}
+                  <button
+                    type="button"
+                    className="btn btn-outline-danger"
+                    onClick={() => {
+                      this.removeProduct(product);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </li>
+              );
+            }
+
+            return (
+              <li className="list-group-item app__item">
+                {product}
+                <button
+                  type="button"
+                  className="btn btn-outline-success"
+                  onClick={() => {
+                    this.addProduct(product);
+                  }}
+                >
+                  Select
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          className="btn btn-outline-danger button__clear"
+          type="button"
+          onClick={() => this.clearTheGoods()}
+        >
+          Clear
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
