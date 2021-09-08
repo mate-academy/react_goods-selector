ae12c858fe09b7801a841350c9a0844cc465ca45
maximbalyk <maximbalyk@gmail.com>
2021-09-08 09:20:38 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,91 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  addGood = (good: string) => {
+    this.setState(prevState => ({
+      selectedGoods: [...prevState.selectedGoods, good],
+    }));
+  };
+
+  resetGood = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  message = () => {
+    const { selectedGoods } = this.state;
+
+    let message = '';
+
+    if (selectedGoods.length === 0) {
+      message = 'No selected goods';
+    }
+
+    if (selectedGoods.length === 1) {
+      message = `${selectedGoods} is selected`;
+    }
+
+    if (selectedGoods.length > 1) {
+      const firstPartOfGoods = selectedGoods.slice(0, -1).join(', ');
+      const lastGood = selectedGoods[selectedGoods.length - 1];
+
+      message = `${firstPartOfGoods} and ${lastGood} are selected`;
+    }
+
+    return message;
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {this.message()}
+          {selectedGoods.length !== 0 && (
+            <button
+              type="button"
+              onClick={this.resetGood}
+            >
+              X
+            </button>
+          )}
+        </h1>
+
+        <ul>
+          {goodsFromServer.map(good => {
+            return (
+              <li
+                key={good}
+                className={classNames(selectedGoods.includes(good) ? 'App__good--active' : 'App__good')}
+              >
+                {good}
+                {!selectedGoods.includes(good) && (
+                  <button
+                    type="button"
+                    className="App__button"
+                    onClick={() => {
+                      this.addGood(good);
+                    }}
+                  >
+                    add
+                  </button>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
