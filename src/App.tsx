40246dc3c3c0207eeeac44c585b3c341d3257908
Oleksandra-Pasenchenko <pasenchenko.av97@gmail.com>
2021-09-23 15:53:37 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,91 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type Good = string;
+
+type State = {
+  selectedGoods: Good[],
+};
+
+const createList = (goods: Good[]) => {
+  const listOfGoods = [...goods];
+  const lastGood = listOfGoods.pop();
+
+  switch (goods.length) {
+    case 0:
+      return 'No goods selected';
+    case 1:
+      return `${goods[0]} is selected`;
+    default:
+      return `${listOfGoods.join(', ')} and ${lastGood} are selected`;
+  }
+};
+
+class App extends React.Component<Props, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  removeGoods = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  };
+
+  toggleGood = (good: Good) => {
+    this.setState((state: State) => {
+      if (state.selectedGoods.includes(good)) {
+        return {
+          selectedGoods: state.selectedGoods.filter(selectedGood => selectedGood !== good),
+        };
+      }
+
+      return {
+        selectedGoods: [good, ...state.selectedGoods],
+      };
+    });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {createList(selectedGoods)}
+          {' '}
+          {selectedGoods.length > 0 && (
+            <button
+              type="button"
+              onClick={this.removeGoods}
+            >
+              X
+            </button>
+          )}
+        </h1>
+        <ul>
+          {goodsFromServer.map((good) => (
+            <li key={good}>
+              {good}
+              {' '}
+              <button
+                type="button"
+                onClick={() => {
+                  this.toggleGood(good);
+                }}
+              >
+                {selectedGoods.includes(good)
+                  ? 'Remove'
+                  : 'Select'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
