1be025dd81ae98d4f688bd7105d4a8fe9741c624
Bohdan Hnatuk <hnatuk996@gmail.com>
2021-09-30 19:03:21 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,90 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  title: [string],
+};
+
+class App extends React.Component {
+  state: State = {
+    title: ['Jam'],
+  };
+
+  selectedGoods = (good: string) => {
+    this.setState((state: State) => ({
+      title: [...state.title, good],
+    }));
+  };
+
+  removeGoods = (good: string) => {
+    const goodToRemove = this.state.title.indexOf(good);
+
+    this.setState((state:State) => {
+      const newGoods = [...state.title];
+
+      newGoods.splice(goodToRemove, 1);
+
+      return {
+        title: newGoods,
+      };
+    });
+  };
+
+  removeAllGoods = () => {
+    this.setState({
+      title: [],
+    });
+  };
+
+  render() {
+    const { title } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {
+            this.state.title.length > 0
+              ? ` ${title.join(', ')} ${this.state.title.length > 1 ? 'are ' : 'is '} selected`
+              : 'No goods selected'
+          }
+          <button
+            type="button"
+            onClick={this.removeAllGoods}
+            className="title__button"
+          >
+            Clear
+          </button>
+        </h1>
+        <ul className="list">
+          {goodsFromServer.map(good => (
+            <li className="list__item">
+              <b>
+                {good}
+              </b>
+              <button
+                type="button"
+                key={good}
+                className="list__button"
+                onClick={() => {
+                  return !this.state.title.includes(good)
+                    ? this.selectedGoods(good)
+                    : this.removeGoods(good);
+                }}
+              >
+                <b>
+                  {
+                    this.state.title.includes(good)
+                      ? 'remove'
+                      : 'select'
+                  }
+                </b>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
