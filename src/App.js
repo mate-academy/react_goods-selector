36adf977d96a717b120b2d7363944dd18499ebb5
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-10-04 12:19:16 +0300

    some message

--- src/App.js
+++ src/App.js
@@ -14,11 +14,74 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: [],
+  }
+
+  selectBtnClick = (arr, good) => {
+    arr.push(good);
+    this.setState({ selectedGoods: arr });
+  }
+
+  removeBtnClick = (arr, good) => {
+    arr.splice(arr.indexOf(good), 1);
+    this.setState({ selectedGoods: arr });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {this.state.selectedGoods.length}
+        </h1>
+        <h2>
+          {this.state.selectedGoods.length !== 0
+            ? (
+              `${this.state.selectedGoods.join(', ')
+                  + (this.state.selectedGoods.length === 1
+                    ? ' is'
+                    : ' are')} selected`
+            )
+            : 'No goods selected'
+          }
+        </h2>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li key={good}>
+              <span>{good}</span>
+              {
+                  this.state.selectedGoods.indexOf(good) === -1
+                    ? (
+                      <button
+                        type="button"
+                        className="select"
+                        onClick={() => {
+                          this.selectBtnClick(this.state.selectedGoods, good);
+                        }}
+                      >
+                        Select
+                      </button>
+                    )
+                    : (
+                      <button
+                        type="button"
+                        className="remove"
+                        onClick={() => {
+                          this.removeBtnClick(this.state.selectedGoods, good);
+                        }}
+                      >
+                        X
+                      </button>
+                    )
+                }
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
