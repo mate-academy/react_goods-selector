c7e79075da012a91d0fa59b166304bae408b0508
Denis Demyanok <DenisRusis@ex.ua>
2021-10-13 18:22:32 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,96 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedNames: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedNames: ['Jam'],
+  };
+
+  toggleListItem = (selectedName: string, selected: boolean) => {
+    this.setState(({ selectedNames }) => (
+      selected
+        ? { selectedNames: selectedNames.filter(good => good !== selectedName) }
+        : { selectedNames: [...selectedNames, selectedName] }
+    ));
+  };
+
+  getTitle = () => {
+    const { selectedNames } = this.state;
+
+    if (selectedNames.length === 0) {
+      return 'No goods';
+    }
+
+    if (selectedNames.length === 1) {
+      return `${selectedNames} is selected`;
+    }
+
+    const firstNames = selectedNames.slice(0, -1);
+
+    return `${firstNames.join(', ')} and ${selectedNames[selectedNames.length - 1]} are selected`;
+  };
+
+  clearList = () => {
+    this.setState({ selectedNames: [] });
+  };
+
+  render() {
+    const { selectedNames } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="title">
+          {this.getTitle()}
+          {' '}
+          {selectedNames.length !== 0 && (
+            <button
+              type="button"
+              className="button__title"
+              onClick={() => {
+                this.clearList();
+              }}
+            >
+              x
+            </button>
+          )}
+        </h1>
+        <ul className="goods__list">
+          {goodsFromServer.map(item => {
+            const selected = selectedNames.includes(item);
+
+            return (
+              <div
+                key="item"
+                className={classNames(
+                  'goods__field',
+                  { 'goods__field--selected': selected },
+                )}
+              >
+                <li className="goods__item">
+                  {item}
+                </li>
+                <div className="button__block">
+                  <button
+                    type="button"
+                    className="button"
+                    onClick={() => {
+                      return this.toggleListItem(item, selected);
+                    }}
+                  >
+                    {selected ? 'Remove' : 'Select'}
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
