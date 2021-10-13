ae2a40ba4a0b53a1cdcdc66e1598c9a1b77e8f4c
Oksana Onopa <onopa.kseni@gmail.com>
2021-10-13 15:51:45 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './App.scss';
 
-const goodsFromServer: string[] = [
+export const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,11 +16,63 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string,
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          {selectedGood === ' ' ? 'No goods selected' : `${selectedGood} is selected`}
+
+          {selectedGood === ' ' ? null
+            : (
+              <>
+                <button
+                  className="App__button"
+                  type="button"
+                  onClick={() => {
+                    this.setState({ selectedGood: ' ' });
+                  }}
+                >
+                  X
+                </button>
+              </>
+            )}
+        </h1>
+        <ul className="GoodsList">
+          {goodsFromServer.map(good => (
+            <>
+              <li
+                className={classNames(
+                  'GoodsList__item',
+                  { active: selectedGood === good },
+                )}
+                key={good}
+              >
+                {good}
+              </li>
+              <button
+                className="GoodsList__button"
+                type="button"
+                onClick={() => {
+                  this.setState({ selectedGood: good });
+                }}
+              >
+                Select
+              </button>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
