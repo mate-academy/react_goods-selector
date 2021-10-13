04fc5c8b0ccc47ccaab86e9d83721c3412f2c048
Danil Nikolaichuk <s.person2734@gmail.com>
2021-10-13 10:56:47 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +16,95 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  changeGoodsSelection = (selectedGood: string, isAlreadySelected: boolean) => {
+    this.setState(({ selectedGoods }) => (
+      isAlreadySelected
+        ? { selectedGoods: selectedGoods.filter(good => good !== selectedGood) }
+        : { selectedGoods: [...selectedGoods, selectedGood] }
+    ));
+  };
+
+  changeGoodsTitle = (selectedGoodsCount: number) => {
+    const { selectedGoods } = this.state;
+    let goodsTitle = '';
+    const selectedGoodsWithoutLast = selectedGoods
+      .filter((good, index) => {
+        if (index < selectedGoodsCount - 1) {
+          return good;
+        }
+
+        return false;
+      });
+
+    switch (selectedGoodsCount) {
+      case 0:
+        goodsTitle = 'No goods selected';
+        break;
+      case 1:
+        goodsTitle = `${selectedGoods[0]} is selected`;
+        break;
+      case 2:
+        goodsTitle = `${selectedGoods.join(' and ')} are selected`;
+        break;
+      default:
+        goodsTitle = `${selectedGoodsWithoutLast.join(', ')}`
+        + ` and ${selectedGoods[selectedGoodsCount - 1]} are selected`;
+    }
+
+    return goodsTitle;
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+    const selectedGoodsCount = selectedGoods.length;
+
+    return (
+      <div className="App">
+        <div className="goods">
+          <h1 className="goods__title">
+            {this.changeGoodsTitle(selectedGoodsCount)}
+          </h1>
+          <ul className="goods__list">
+            {goodsFromServer.map(good => {
+              const isAlreadySelected = selectedGoods.includes(good);
+
+              return (
+                <li
+                  key={good}
+                  className={classNames(
+                    'goods__item',
+                    { 'goods__item--selected': isAlreadySelected },
+                  )}
+                >
+                  <span>
+                    {good}
+                  </span>
+                  <button
+                    type="button"
+                    className="goods__btn"
+                    onClick={() => {
+                      return this.changeGoodsSelection(good, isAlreadySelected);
+                    }}
+                  >
+                    {isAlreadySelected ? 'Remove' : 'Add'}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
