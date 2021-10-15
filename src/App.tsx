3cf8cf22f2b11e765dc50c906ff0f79f90c39953
anastasiia <theyashkindzhus@gmail.com>
2021-10-15 16:11:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,63 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type States = {
+  selectedGood: string,
+};
+
+class App extends React.PureComponent<{}, States> {
+  state: States = {
+    selectedGood: 'Jam',
+  };
+
+  getTitle = () => {
+    return this.state.selectedGood === '' ? 'No goods selected' : `${this.state.selectedGood} is selected`;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>{this.getTitle()}</h1>
+          {this.state.selectedGood !== ''
+          && (
+            <button
+              type="button"
+              onClick={() => this.setState({ selectedGood: '' })}
+            >
+              X
+            </button>
+          )}
+        </div>
+        <ul className="list">
+          {goodsFromServer.map((good: string) => {
+            return (
+              <>
+                <li
+                  key={goodsFromServer.indexOf(good)}
+                  className={classNames('listItem', { selected: this.state.selectedGood === good })}
+                >
+                  {good}
+                </li>
+                {this.state.selectedGood !== good && (
+                  <button
+                    type="button"
+                    onClick={
+                      () => {
+                        this.setState({ selectedGood: good });
+                      }
+                    }
+                  >
+                    Select
+                  </button>
+                )}
+              </>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
