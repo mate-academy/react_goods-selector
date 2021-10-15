cec4a502dd158eddb3c90d18103c1c79908b3044
Kate Kuzmenko <rina.punina@gmail.com>
2021-10-15 12:41:33 +0300

    styles added

--- src/App.tsx
+++ src/App.tsx
@@ -38,14 +38,9 @@ class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        <h1>
-          {selectedGoods.length
-            ? `Selected goods: ${selectedGoods.join(', ')}`
-            : 'No goods selected'}
-        </h1>
-
         <button
           type="button"
+          className="button--close"
           onClick={() => {
             this.setState({ selectedGoods: [] });
           }}
@@ -53,13 +48,20 @@ class App extends React.Component<{}, State> {
           X
         </button>
 
-        <ul>
+        <h1 className="app__title">
+          {selectedGoods.length
+            ? `${selectedGoods.join(', ')} selected`
+            : 'No goods selected'}
+        </h1>
+
+        <ul className="goods-list">
           {goodsFromServer.map((good) => {
             const isSelected = selectedGoods.includes(good);
 
             return (
               <li
                 key={good}
+                className="goods-list__item"
               >
                 {good}
 
