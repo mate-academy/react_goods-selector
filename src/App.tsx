bb8f7043336ed79bfed1830020ba817989a8373e
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-11-01 19:01:38 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,70 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type State = {
+  selectedGood: string;
+};
+
+class App extends React.Component<Props, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  clickHandler = (good: string) => {
+    this.setState({
+      selectedGood: good,
+    });
+  };
+
+  clickClear = () => {
+    this.setState({
+      selectedGood: '',
+    });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
+          {' '}
+          <button type="button" onClick={this.clickClear}>
+            X
+          </button>
+        </h1>
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <div className="list">
+              <li
+                key={good}
+                className={classNames('good', {
+                  good__selected: good === selectedGood,
+                })}
+              >
+                {good}
+              </li>
+              <button
+                type="button"
+                onClick={() => {
+                  this.clickHandler(good);
+                }}
+                className={classNames({
+                  button_none: good === selectedGood,
+                })}
+              >
+                Selected
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
