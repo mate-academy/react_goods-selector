24ad6f926013affd8e32db7a1931a38084268772
MaxStets <weawer458@gmail.com>
2021-11-02 16:50:11 +0200

    Solution -- react_goods-selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,101 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  handlerSelect = (method: string, data: string) => {
+    if (method === 'Select') {
+      this.setState(state => ({
+        selectedGoods: [...state.selectedGoods, data],
+      }));
+    }
+
+    if (method === 'Remove') {
+      this.setState(state => ({
+        selectedGoods: state.selectedGoods.filter(good => good !== data),
+      }));
+    }
+  };
+
+  clearSelectedGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="selector">
+          <div className="selector__header">
+            <h1 className="selector__header-title">
+              {
+                selectedGoods.length
+                  ? <span>{selectedGoods.join(', ')}</span>
+                  : 'No goods selected'
+              }
+            </h1>
+            {
+              Boolean(selectedGoods.length) && (
+                <button
+                  type="button"
+                  className="selector__header-clear"
+                  onClick={this.clearSelectedGoods}
+                >
+                  Clear
+                </button>
+              )
+            }
+
+          </div>
+          <ul className="selector__list">
+            {goodsFromServer.map(good => {
+              const isSelected = selectedGoods.includes(good);
+
+              return (
+                <li
+                  key={good}
+                  className={
+                    classNames(
+                      'selector__list-item',
+                      {
+                        'selector__list-item--selected': isSelected,
+                      },
+                    )
+                  }
+                >
+                  <div>{good}</div>
+                  <button
+                    type="button"
+                    className="selector__list-item-btn"
+                    onClick={() => this.handlerSelect(
+                      isSelected
+                        ? 'Remove'
+                        : 'Select',
+                      good,
+                    )}
+                  >
+                    {
+                      isSelected
+                        ? 'Remove'
+                        : 'Select'
+                    }
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
