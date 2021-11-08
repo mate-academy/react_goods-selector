42ee39eefc51836ac27de35744875bf039dd2c07
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-11-08 14:22:02 +0200

    Solution (with one good selection)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,60 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  selectGood = (good: string) => {
+    this.setState(
+      { selectedGood: good },
+    );
+  };
+
+  clearSelection = () => {
+    this.setState(
+      { selectedGood: '' },
+    );
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{`Selected good: ${selectedGood}`}</h1>
+        <ul>
+          {goodsFromServer.map((good) => (
+            <li
+              key={good}
+              className={classNames('App__good', { 'App__good--selected': good === selectedGood })}
+            >
+              {good}
+              {good !== selectedGood && (
+                <button
+                  type="button"
+                  className="App__button App__button--select"
+                  onClick={() => this.selectGood(good)}
+                >
+                  Select
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+        {selectedGood && (
+          <button
+            type="button"
+            className="App__button App__button--clear"
+            onClick={this.clearSelection}
+          >
+            Clear selection
+          </button>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
