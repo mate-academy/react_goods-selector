584ca88a88d4b79a44134ea4fc90d9324cafcc34
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-09 21:36:19 +0300

    Task was solved

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,85 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type GoodItem = typeof goodsFromServer[number];
+interface State {
+  [key: GoodItem]: boolean;
+}
+
+class App extends React.Component<Props, State> {
+  state = goodsFromServer.reduce((stateObj: State, good: GoodItem) => {
+    return { ...stateObj, [good]: false };
+  }, {});
+
+  initialState = { ...this.state };
+
+  getActiveGoods() {
+    const activeGoods = Object.entries(this.state).reduce((goods: string[], goodState) => {
+      if (goodState[1]) {
+        goods.push(goodState[0]);
+      }
+
+      return goods;
+    }, []);
+
+    if (activeGoods.length) {
+      const joinedByAnd = activeGoods.splice(-2).join(' and ');
+
+      return activeGoods.concat(joinedByAnd).join(', ').concat(' are selected');
+    }
+
+    return 'No goods selected';
+  }
+
+  changeGoodState = (good: GoodItem) => {
+    this.setState(prevState => ({
+      ...prevState,
+      [good]: !prevState[good],
+    }));
+  };
+
+  clearSelection = () => {
+    this.setState({
+      ...this.initialState,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.getActiveGoods()}</h1>
+        <ul className="GoodsList">
+          {goodsFromServer.map(good => {
+            return (
+              <li
+                className={
+                  `GoodsItem ${this.state[good] ? 'GoodsItem--active' : ''}`
+                }
+              >
+                <span>{good}</span>
+                {' '}
+                <button
+                  type="button"
+                  onClick={
+                    () => {
+                      this.changeGoodState(good);
+                    }
+                  }
+                >
+                  {!(this.state[good]) ? 'Add' : 'Remove'}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <button type="button" onClick={this.clearSelection}>X</button>
+      </div>
+    );
+  }
+  // return (
+  // );
+};
 
 export default App;
