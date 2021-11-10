ff1642790db23676c8aa63be1e4dd06970d7e1db
Olexandr Yatsykovych <sashayatsikovic@gmail.com>
2021-11-10 17:39:02 +0200

    solution without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// import classNames from 'classnames';
+
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +16,71 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  selectedGoods: string[]
+}
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: [],
+  };
+
+  addGood = (good: string) => {
+    this.setState((state) => {
+      return {
+        selectedGoods: [...state.selectedGoods, good],
+      };
+    });
+  };
+
+  removeGood = (good: string) => {
+    this.setState((state) => {
+      return {
+        selectedGoods: state.selectedGoods.filter(elem => elem !== good),
+      };
+    });
+  };
+
+  displaySelectedGoods() {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods.slice(-1)} are selected`;
+    }
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{this.displaySelectedGoods()}</h1>
+        <ul>
+          {goodsFromServer.map(good => {
+            const isGoodSelected = selectedGoods.find((elem => elem === good));
+
+            return (
+              <li key={good}>
+                {good}
+
+                <button
+                  type="button"
+                  onClick={isGoodSelected
+                    ? () => this.removeGood(good)
+                    : () => this.addGood(good)}
+                >
+                  {isGoodSelected ? 'Remove' : 'Add'}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
