48b574eb826535b8abcc26e7493d5f1ad088ddb9
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-11-10 12:13:38 +0200

    sf-4

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ export class App extends React.Component<{}, State> {
               type="button"
               onClick={this.clearBasket}
             >
-              X
+              Clear
             </button>
           </div>
         )}
