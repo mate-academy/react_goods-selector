0577dcf537700f84ab33589ed4b9ec1c60718e4e
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-11-10 19:49:43 +0200

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,100 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export interface State {
+  selectedGoods: string[];
+}
+
+export class App extends React.Component {
+  state: State = {
+    selectedGoods: [],
+  };
+
+  add = (item: string) => {
+    this.setState((state: State) => ({
+      selectedGoods: [...state.selectedGoods, item],
+    }));
+  };
+
+  remove = (item: string) => {
+    this.setState((state: State) => ({
+      selectedGoods: [...state.selectedGoods].filter(selectedItem => selectedItem !== item),
+    }));
+  };
+
+  message = () => {
+    const { selectedGoods } = this.state;
+
+    if (selectedGoods.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (selectedGoods.length === 1) {
+      return `${selectedGoods} selected`;
+    }
+
+    return `${(selectedGoods.slice(0, selectedGoods.length - 1)).join(', ')} and ${selectedGoods[selectedGoods.length - 1]} are selected`;
+  };
+
+  removeSelections = () => {
+    this.setState({ selectedGoods: [] });
+
+    return 'No goods selected';
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="selectedItemsList">
+          {this.message()}
+          {selectedGoods.length !== 0 && (
+            <button
+              type="button"
+              className="button button--top"
+              onClick={() => {
+                this.removeSelections();
+              }}
+            >
+              Remove all
+            </button>
+          )}
+        </h1>
+        <div>
+          {goodsFromServer.map(item => (
+            <div className="itemAndButton" key={item}>
+              <li className="item">{item}</li>
+
+              {selectedGoods.includes(item)
+                ? (
+                  <button
+                    type="button"
+                    className="button"
+                    onClick={() => {
+                      this.remove(item);
+                    }}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="button"
+                    onClick={() => {
+                      this.add(item);
+                    }}
+                  >
+                    Add
+                  </button>
+                )}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
