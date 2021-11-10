3c18f8f039624e1685bff1cab3c45575bc69600a
Denys Anosov <anosovdenys@gmail.com>
2021-11-10 23:54:22 +0200

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,58 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+interface State {
+  selectedGood: string,
+}
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.selectedGood
+          ? (
+            <h1>
+              {`${this.state.selectedGood} is selected`}
+              <button
+                type="button"
+                onClick={() => {
+                  this.setState({ selectedGood: '' });
+                }}
+              >
+                Clear selected
+              </button>
+            </h1>
+          )
+          : <h1>No goods selected</h1>}
+
+        <ol>
+          {goodsFromServer.map(item => (
+            <div>
+              <li key={item}>
+                {item}
+                {' '}
+                {(this.state.selectedGood !== item) && (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.setState({ selectedGood: item });
+                    }}
+                  >
+                    Select
+                  </button>
+                )}
+              </li>
+            </div>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default App;
