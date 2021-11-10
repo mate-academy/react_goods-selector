84c8609d0ef2eb17c98ae9c53f38cfd1137c4f25
Darina Savytska <coldshine99@gmail.com>
2021-11-10 22:23:47 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,93 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  addGood = (good: string) => (
+    this.setState(prevGood => ({
+      selectedGoods: [...prevGood.selectedGoods, good],
+    }))
+  );
+
+  deleteGood = (good: string) => (
+    this.setState(prevGood => ({
+      selectedGoods: prevGood.selectedGoods.filter(delGood => delGood !== good),
+    }))
+  );
+
+  clearSelection = () => (
+    this.setState(
+      { selectedGoods: [] },
+    )
+  );
+
+  selectedGoods = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `${selectedGoods} is selected`;
+
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods.slice(-1)} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__notes">
+          My notes
+        </p>
+        <h1 className="App__title-goods">
+          {this.selectedGoods()}
+        </h1>
+
+        {goodsFromServer.map((good) => {
+          return (
+            <div>
+              <li
+                key={good}
+                className="App__good"
+              >
+                {good}
+                <button
+                  type="button"
+                  className={classNames('App__button',
+                    { 'App__button--selected': selectedGoods.includes(good) })}
+                  onClick={selectedGoods.includes(good)
+                    ? () => this.deleteGood(good)
+                    : () => this.addGood(good)}
+                >
+                  {selectedGoods.includes(good) ? 'Remove' : 'Selected'}
+                </button>
+              </li>
+            </div>
+          );
+        })}
+
+        <button
+          type="button"
+          className="App__clear-button"
+          onClick={this.clearSelection}
+        >
+          Clear All
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
