36cd3cc1934ddc6be14b261c915a79d4ee8b7677
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-11-10 20:34:27 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,86 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
 
-export default App;
+interface State {
+  selectedGood: string[],
+}
+
+export class App extends React.Component<Props, State> {
+  state: State = {
+    selectedGood: ['Jam'],
+  };
+
+  selectGood = (good: string) => {
+    this.setState((previousState) => {
+      return {
+        selectedGood: [...previousState.selectedGood, good],
+      };
+    });
+  };
+
+  clearCart = () => {
+    this.setState(
+      { selectedGood: [] },
+    );
+  };
+
+  goodsInCart = () => {
+    const { selectedGood } = this.state;
+
+    return (
+      (selectedGood.length > 1)
+        ? `${selectedGood.slice(0, -1).join(', ')} and ${selectedGood.slice(-1)} are selected`
+        : `${selectedGood} is selected`
+    );
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+    const listOfGoods = goodsFromServer.map((good) => (
+      <li
+        key={good}
+        className={`good__item ${selectedGood.includes(good) ? 'good--active' : ''}`}
+      >
+        {good}
+
+        { selectedGood.includes(good)
+          ? null
+          : (
+            <button
+              type="button"
+              className="good__add"
+              onClick={() => {
+                this.selectGood(good);
+              }}
+            >
+              Add in Cart
+            </button>
+          )}
+      </li>
+    ));
+
+    return (
+      <div className="App">
+        <h1>{ `${selectedGood.length === 0 ? 'No goods selected' : this.goodsInCart()}` }</h1>
+        <ul className="good__list">
+          {listOfGoods}
+        </ul>
+        {(selectedGood.length > 0)
+          ? (
+            <button
+              type="button"
+              className="good__clear"
+              onClick={() => {
+                this.clearCart();
+              }}
+            >
+              Clear cart
+            </button>
+          )
+          : null}
+      </div>
+    );
+  }
+}
