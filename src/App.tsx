ede5ac49bc82e18df098fa7348a8f053bbee3dec
Anton Karpena <antoniokarpena@gmail.com>
2021-11-11 13:15:19 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './App.scss';
+import { v4 as uuidv4 } from 'uuid';
+import classNames from 'classnames';
 
-const goodsFromServer: string[] = [
+interface Good {
+  id: string;
+  value: string;
+}
+
+const goodsFromServer: Good[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -12,13 +19,113 @@ const goodsFromServer: string[] = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(good => ({
+  id: uuidv4(),
+  value: good,
+}));
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  selectedGoods: string[],
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  clearSelection = () => this.setState({ selectedGoods: [] });
+
+  addGood = (good: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({ selectedGoods: [...selectedGoods, good] });
+  };
+
+  removeGood = (selectedGood: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({
+      selectedGoods: selectedGoods.filter(
+        good => good !== selectedGood,
+      ),
+    });
+  };
+
+  formatTitle = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods[selectedGoods.length - 1]}`;
+    }
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          Selected good:
+          {this.formatTitle()}
+        </h1>
+        <button
+          className="App__clear"
+          type="button"
+          onClick={this.clearSelection}
+        >
+          Clear
+        </button>
+        <ul className="App__list">
+          {
+            goodsFromServer.map(
+              good => {
+                const isGoodSelected = selectedGoods.includes(good.value);
+
+                const buttonCallback = isGoodSelected
+                  ? () => this.removeGood(good.value)
+                  : () => this.addGood(good.value);
+
+                const buttonTitle = isGoodSelected
+                  ? 'Remove good'
+                  : 'Add good';
+
+                const buttonToRender = (
+                  <button
+                    className={classNames(
+                      'App__item-button--add',
+                      { 'App__item-button--remove': isGoodSelected },
+                    )}
+                    type="button"
+                    onClick={buttonCallback}
+                  >
+                    {buttonTitle}
+                  </button>
+                );
+
+                return (
+                  <li
+                    className={classNames(
+                      'App__item--unactive',
+                      { 'App__item--active': isGoodSelected },
+                    )}
+                    key={good.id}
+                  >
+                    {good.value}
+                    {buttonToRender}
+                  </li>
+                );
+              },
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
