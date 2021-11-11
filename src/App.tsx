913c43fbf1ccc15db83d1e0f658cb1e2e775beeb
Denys Anosov <anosovdenys@gmail.com>
2021-11-11 18:05:25 +0200

    reworked solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import classnames from 'classnames';
 import './App.scss';
 
-const goodsFromServer: string[] = [
+interface Good {
+  id: string,
+  value: string,
+}
+
+const goodsFromServer: Good[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -12,57 +19,117 @@ const goodsFromServer: string[] = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(good => ({
+  id: uuidv4(),
+  value: good,
+}));
 
 type Props = {};
+
 interface State {
-  selectedGood: string,
+  selectedGoods: string[],
 }
 
 class App extends React.Component<Props, State> {
   state: State = {
-    selectedGood: 'Jam',
+    selectedGoods: ['Jam'],
+  };
+
+  clearSelection = () => this.setState({ selectedGoods: [] });
+
+  addGood = (goodToAdd: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({ selectedGoods: [...selectedGoods, goodToAdd] });
+  };
+
+  removeGood = (goodToRemove: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({
+      selectedGoods: selectedGoods.filter(
+        good => good !== goodToRemove,
+      ),
+    });
+  };
+
+  formatHeading = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')}
+         and ${selectedGoods[selectedGoods.length - 1]}
+         are selected`;
+    }
   };
 
   render() {
+    const { selectedGoods } = this.state;
+    const hasGoodsAdded = selectedGoods.length > 0;
+
     return (
       <div className="App">
-        {this.state.selectedGood
-          ? (
-            <h1>
-              {`${this.state.selectedGood} is selected`}
-              <button
-                type="button"
-                onClick={() => {
-                  this.setState({ selectedGood: '' });
-                }}
-              >
-                Clear selected
-              </button>
-            </h1>
-          )
-          : <h1>No goods selected</h1>}
+        <h1 className="heading">
+          Selected goods:
+          {' '}
+          {this.formatHeading()}
+        </h1>
+        {hasGoodsAdded && (
+          <button
+            className="button button--clear"
+            onClick={this.clearSelection}
+            type="button"
+          >
+            Clear selection
+          </button>
+        )}
+        <ul className="list">
+          {
+            goodsFromServer.map(
+              good => {
+                const isGoodSelected = selectedGoods.includes(good.value);
 
-        <ol>
-          {goodsFromServer.map(item => (
-            <div>
-              <li key={item}>
-                {item}
-                {' '}
-                {(this.state.selectedGood !== item) && (
+                const buttonCallback = isGoodSelected
+                  ? () => this.removeGood(good.value)
+                  : () => this.addGood(good.value);
+
+                const buttonText = isGoodSelected
+                  ? 'Remove good'
+                  : 'Add good';
+
+                const buttonToRender = (
                   <button
+                    className="button"
                     type="button"
-                    onClick={() => {
-                      this.setState({ selectedGood: item });
-                    }}
+                    onClick={buttonCallback}
                   >
-                    Select
+                    {buttonText}
                   </button>
-                )}
-              </li>
-            </div>
-          ))}
-        </ol>
+                );
+
+                return (
+                  <li
+                    className={classnames({
+                      list__item: true,
+                      active: isGoodSelected,
+                    })}
+                    key={good.id}
+                  >
+                    {good.value}
+                    {buttonToRender}
+                  </li>
+                );
+              },
+            )
+          }
+        </ul>
       </div>
     );
   }
