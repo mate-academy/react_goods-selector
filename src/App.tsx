28d3bcf2fe5d27b30f32fe4a8c4179a28ed2111c
Ilona Shulha <ilon.sh.ilo@gmail.com>
2021-11-11 14:18:26 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// import { type } from 'os';
+import classNames from 'classnames';
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +16,107 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  getHeadline = () => {
+    const { selectedGoods } = this.state;
+
+    if (selectedGoods.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (selectedGoods.length === 1) {
+      return `${selectedGoods[0]} is selected`;
+    }
+
+    const lastEl = selectedGoods[selectedGoods.length - 1];
+    const firstElements = [];
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < selectedGoods.length - 1; i++) {
+      firstElements.push(selectedGoods[i]);
+    }
+
+    return `${firstElements.join(', ')} and ${lastEl} are selected`;
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="app-header">
+          <h1>
+            { `${this.getHeadline()}` }
+          </h1>
+          <button
+            type="button"
+            className={classNames(
+              'removeAll',
+              'button',
+              { removeAll_invisible: selectedGoods.length === 0 },
+            )}
+            onClick={() => {
+              this.setState({
+                selectedGoods: [],
+              });
+            }}
+          >
+            X
+          </button>
+        </div>
+
+        <ul className="goods-list">
+          {goodsFromServer.map(item => {
+            return (
+              <li
+                key={item}
+                className={classNames(
+                  'goods-list__item',
+                  { 'goods-list__item_active': selectedGoods.includes(item) },
+                )}
+              >
+                <span className="goods-list__good">{item}</span>
+                <button
+                  className={classNames(
+                    'goods-list__button',
+                    'button',
+                    { 'goods-list__button_add': !selectedGoods.includes(item) },
+                  )}
+                  type="button"
+                  onClick={() => {
+                    type Prev = {
+                      selectedGoods: []
+                    };
+                    this.setState((prevState: Prev) => {
+                      if (selectedGoods.includes(item)) {
+                        prevState.selectedGoods.splice(
+                          (selectedGoods.indexOf(item)), 1,
+                        );
+
+                        return prevState;
+                      }
+
+                      return {
+                        selectedGoods: [...prevState.selectedGoods, item],
+                      };
+                    });
+                  }}
+                >
+                  {
+                    (selectedGoods.includes(item)) ? 'Remove' : 'Add'
+                  }
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
