51aeb1eaf33f24cc83dbde54916df1fabc70fdaf
Taras Pyda <tapac1991@icloud.com>
2021-11-21 14:09:47 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,84 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+type State = {
+  selectedGood: string;
+};
+
+class App extends React.Component<Props, State> {
+  state = {
+    selectedGood: '',
+  };
+
+  spans = document.getElementsByTagName('span');
+
+  buttons = document.getElementsByTagName('button');
+
+  render() {
+    const { selectedGood } = this.state;
+    const phrase = (selectedGood === 'Dumplings' || selectedGood === 'Eggs')
+      ? `${selectedGood} are selected`
+      : `${selectedGood} is selected`;
+
+    return (
+      <div className="app">
+        <h1>
+          {(selectedGood)
+            ? `Selected good: - ${phrase}`
+            : 'Selected good: - No goods selected'}
+          {' '}
+          {(this.state.selectedGood === '')
+            ? ''
+            : (
+              <button
+                type="button"
+                onClick={() => {
+                  for (let i = 0; i < this.spans.length; i += 1) {
+                    this.spans[i].classList.remove('selected');
+                    this.buttons[i + 1].hidden = false;
+                  }
+
+                  this.setState({ selectedGood: '' });
+                }}
+              >
+                X
+              </button>
+            )}
+        </h1>
+        <ul>
+          {goodsFromServer.map((product) => {
+            return (
+              <li
+                key={product}
+              >
+                <span>{product}</span>
+                {' '}
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    const button = e.target;
+
+                    this.setState({
+                      selectedGood: product,
+                    });
+                    for (let i = 0; i < this.spans.length; i += 1) {
+                      if (this.spans[i].textContent === product) {
+                        this.spans[i].classList.add('selected');
+                        (button as HTMLButtonElement).hidden = true;
+                      }
+                    }
+                  }}
+                >
+                  Select
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
