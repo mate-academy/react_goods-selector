20601997fa34b0bdd6461905b41c63dadddbfcfd
dinexpod <work.toxa@gmail.com>
2021-11-28 20:06:34 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,71 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[]
+};
+
+class App extends React.Component<{}, State> {
+  state:State = {
+    selectedGoods: [],
+  };
+
+  selectGood = (good: string) => {
+    this.setState((prevState) => {
+      this.setState({ selectedGoods: [...prevState.selectedGoods, good] });
+    });
+  };
+
+  unselectGood = (deletedGood: string) => {
+    this.setState((prevState) => {
+      this.setState(
+        { selectedGoods: [...prevState.selectedGoods.filter(good => good !== deletedGood)] },
+      );
+    });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{selectedGoods.length > 0 ? 'Selected good: -' : 'No goods selected'}</h1>
+
+        {selectedGoods
+            && selectedGoods.map(good => (
+              <h1 key={good}>
+                {`${good} is selected`}
+                <button
+                  type="button"
+                  onClick={() => this.unselectGood(good)}
+                >
+                  X
+                </button>
+              </h1>
+            ))}
+
+        <ul>
+          { goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={selectedGoods.includes(good) ? 'selectedGood' : ''}
+            >
+              {`${good} `}
+              {!selectedGoods.includes(good)
+                && (
+                  <button
+                    type="button"
+                    onClick={() => this.selectGood(good)}
+                  >
+                    Select
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
