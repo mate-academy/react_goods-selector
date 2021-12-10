e82eec36323c8c34b069c55f6a06e8456c374215
Ivan Meleganych <imi92@ukr.net>
2021-12-10 16:46:06 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 
+import classNames from 'classnames';
+
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -14,11 +16,84 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  clear = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  addGood = (good: string) => {
+    this.setState((state) => ({ selectedGoods: [...state.selectedGoods, good] }));
+  };
+
+  dispalySelected = () => {
+    const { selectedGoods: selected } = this.state;
+
+    switch (selected.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selected} is selected`;
+      case 2:
+        return `${selected.join(' and ')} are selected`;
+      default:
+        return `${selected.slice(0, -1).join(', ')} and ${selected.slice(-1)} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGoods: selected } = this.state;
+
+    return (
+
+      <div className="app">
+        <section className="products">
+          <h1 className="products__selected">
+            <button
+              type="button"
+              className={classNames('products__close', { active__close: selected.length === 0 })}
+              disabled={selected.length === 0}
+              onClick={this.clear}
+            >
+              X
+            </button>
+            <>
+              {this.dispalySelected()}
+            </>
+          </h1>
+          <ul className="products__items">
+            {goodsFromServer.map(item => (
+              <>
+                <li
+                  key={item}
+                  className={classNames('products__item', { active: selected.includes(item) })}
+                >
+                  {item}
+                </li>
+                <button
+                  type="button"
+                  className={classNames('products__button', { active__button: selected.includes(item) })}
+                  disabled={selected.includes(item)}
+                  onClick={() => (
+                    this.addGood(item)
+                  )}
+                >
+                  {selected.includes(item) ? 'Added' : 'Add'}
+                </button>
+              </>
+            ))}
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
