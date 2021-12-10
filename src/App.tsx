670aa037a7aee5f50a104ac2a978211e20a835d1
unknown <skirik0117@gmail.com>
2021-12-10 22:25:22 +0200

    event-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.scss';
+import './reset.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,98 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: [],
+  };
+
+  alreadyHasGoods = (good: string) => {
+    return this.state.selectedGoods.find(item => item === good);
+  };
+
+  productAdd = (product: string) => {
+    if (product) {
+      this.setState((state) => {
+        return !this.alreadyHasGoods(product)
+          ? {
+            selectedGoods: [...state.selectedGoods, product],
+          }
+          : {
+            selectedGoods: state.selectedGoods.filter(item => item !== product),
+          };
+      });
+    }
+  };
+
+  selectedGoodsInfo = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      case 2:
+        return `${selectedGoods[0]} and ${selectedGoods[1]} are selected`;
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods.slice(-1)} are selected`;
+    }
+  };
+
+  clearAllGoods = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App App__wrapper">
+        <div className="App__content">
+          <h1 className="App__title">
+            {this.selectedGoodsInfo()}
+          </h1>
+          {this.state.selectedGoods.length > 0 && (
+            <button
+              type="button"
+              className="App__button App__button-clear"
+              onClick={this.clearAllGoods}
+            >
+              X
+            </button>
+          )}
+        </div>
+        <section className="goods__wrapper">
+          {goodsFromServer.map(good => (
+            <div className="goods" key={good}>
+              <div className={(classNames(
+                'goods__content',
+                {
+                  goods__active: selectedGoods.includes(good),
+                },
+              ))}
+              >
+                <div className="goods__item">{good}</div>
+                <button
+                  type="button"
+                  className="goods__button"
+                  onClick={() => this.productAdd(good)}
+                >
+                  {!this.alreadyHasGoods(good) ? 'Add' : 'Remove'}
+                </button>
+              </div>
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
