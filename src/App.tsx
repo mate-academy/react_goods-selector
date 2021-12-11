8c29fc341dc609a5d731e4d30663aaf9501d1449
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-12-11 19:41:30 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,89 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type State = {
+  selectedGoods: string[],
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    selectedGoods: [],
+  };
+
+  remover = (index: number, array: string[]) => {
+    array.splice(index, 1);
+
+    return array;
+  };
+
+  makephrase = (array: string[]) => {
+    if (!array.length) {
+      return 'No goods selected';
+    }
+
+    if (array.length === 1) {
+      return `${array[0]} is selected`;
+    }
+
+    let phrase = '';
+
+    for (let i = 0; i < array.length - 1; i += 1) {
+      phrase += `${array[i]}, `;
+    }
+
+    phrase = phrase.slice(0, -2);
+
+    return `${phrase} and ${array[array.length - 1]} are selected`;
+  };
+
+  render() {
+    const {
+      selectedGoods,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {this.makephrase(this.state.selectedGoods)}
+          {' '}
+        </h1>
+        {
+          goodsFromServer.map((item) => (
+            <p>
+              <li
+                key={item}
+                className={selectedGoods.includes(item) ? 'active' : ''}
+              >
+                <button
+                  className={selectedGoods.includes(item) ? 'active-button' : 'non-active-button'}
+                  type="button"
+                  onClick={() => {
+                    if (!selectedGoods.includes(item)) {
+                      this.setState((prevState: State) => ({
+                        selectedGoods: [...prevState.selectedGoods, item],
+                      }));
+                    } else {
+                      const index = selectedGoods.indexOf(item);
+
+                      this.setState((prevState: State) => ({
+                        selectedGoods: this.remover(index, prevState.selectedGoods),
+                      }));
+                    }
+                  }}
+                >
+                  {selectedGoods.includes(item) ? 'Remove' : 'Add'}
+                </button>
+                {' '}
+                {item}
+              </li>
+            </p>
+          ))
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
