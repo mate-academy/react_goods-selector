a6a0af90f3d6ee4f932c8f87e371f0a1badf6ddb
Mariia Bila <freiheit.maria@gmail.com>
2021-12-11 17:32:25 +0200

    grocery

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import classNames from 'classnames';
 import React from 'react';
 import './App.scss';
+import CloseButton from 'react-bootstrap/CloseButton';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +18,105 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const delicious:string[] = ['Appetizing', 'Flavorful', 'Fresh', 'Delish', 'Delectable', 'Tasteful', 'Tasty', 'Toothsome', 'Yummy', 'Flavorful'];
+
+type State = {
+  selectedGoods: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="productCards">
+          {selectedGoods.length
+            ? (
+              <h1 className="productCards__cartContent">
+                {selectedGoods.length > 1
+                  ? `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods[selectedGoods.length - 1]} are `
+                  : (`${selectedGoods.join(', ')} is `)}
+
+                selected
+
+              </h1>
+            )
+            : <h1 className="productCards__cartContent">No goods selected</h1>}
+
+          {selectedGoods.length > 0 && (
+            <CloseButton
+              type="button"
+              className={classNames('button', 'button-close')}
+              onClick={() => {
+                this.setState({ selectedGoods: [] });
+              }}
+            />
+          )}
+
+          <p className="productCards__amount">
+            Goods amount:
+            {' '}
+            {selectedGoods.length}
+          </p>
+
+          <div className="productCards__products">
+            {goodsFromServer.map((el, i) => (
+              <Card
+                bg={el}
+                key={el}
+                style={{ width: '18rem' }}
+                className={classNames(
+                  'product',
+                  'mb-2',
+                  { product_selected: selectedGoods.includes(el) },
+                )}
+              >
+                <Card.Body>
+                  <Card.Title>{el}</Card.Title>
+
+                  <Card.Text>
+                    {delicious[i]}
+                    {' '}
+                    {el}
+                  </Card.Text>
+
+                  <Button
+                    variant="outline-success"
+                    className={classNames(
+                      `${el.toLowerCase()}-button`,
+                      'button',
+                      { selected: selectedGoods.includes(el) },
+                    )}
+                    onClick={() => {
+                      if (!selectedGoods.includes(el)) {
+                        this.setState(prevState => (
+                          { selectedGoods: [...prevState.selectedGoods, el] }));
+                      } else {
+                        this.setState(prevState => ({
+                          selectedGoods: [...prevState.selectedGoods].filter(
+                            product => product !== el,
+                          ),
+                        }));
+                      }
+                    }}
+                  >
+                    {!selectedGoods.includes(el)
+                      ? 'Select'
+                      : 'Remove'}
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
