6006ce7c9d1b974812b68aaa640cc8bff1e1d69c
VladDyrenkov <59515329+VladDyrenkov@users.noreply.github.com>
2021-12-13 16:43:18 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/sort-comp */
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,91 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  clear = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  addGoods = (good:string) => {
+    this.setState((state) => ({ selectedGoods: [...state.selectedGoods, good] }));
+  };
+
+  removeGood = (goodToRemove: string) => {
+    this.setState((state) => {
+      return {
+        selectedGoods: state.selectedGoods.filter(good => good !== goodToRemove),
+      };
+    });
+  };
+
+  displaySelected = () => {
+    const { selectedGoods: selected } = this.state;
+
+    switch (selected.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selected} is selected`;
+      case 2:
+        return `${selected.join(' and ')} are selected`;
+      default:
+        return `${selected.slice(0, -1).join(', ')} and ${selected.slice(-1)} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGoods: selected } = this.state;
+
+    return (
+      <div className="App">
+        <section className="goods">
+          <h1 className="goods__selected">
+            <button
+              type="button"
+              className={classNames('goods__close', { active__close: selected.length === 0 })}
+              disabled={selected.length === 0}
+              onClick={this.clear}
+            >
+              X
+            </button>
+            <>
+              {this.displaySelected()}
+            </>
+          </h1>
+
+          <ul className="goods__items">
+            {goodsFromServer.map(item => (
+              <>
+                <li
+                  key={item}
+                  className={classNames('goods__item', { active: selected.includes(item) })}
+                >
+                  {item}
+                </li>
+                <button
+                  type="button"
+                  className={classNames('products__button', { active__button: selected.includes(item) })}
+                  onClick={() => (
+                    selected.includes(item) ? this.removeGood(item) : this.addGoods(item)
+                  )}
+                >
+                  {selected.includes(item) ? 'Remove' : 'Add'}
+                </button>
+              </>
+            ))}
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
