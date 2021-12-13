ab8134cccb8996b982078a80fe33d9f9f316be92
nadiaH <nadiahrb79@gmailcom>
2021-12-13 15:07:08 +0200

    solution with multiple goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import cn from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -15,16 +16,60 @@ const goodsFromServer: string[] = [
 ];
 
 type State = {
-  selected: string;
+  selected: string[];
 };
 
 class App extends Component<{}, State> {
   state = {
-    selected: 'Jam',
+    selected: ['Jam'],
   };
 
   handleSelect = (good: string) => {
-    this.setState({ selected: good });
+    this.setState((prevState) => {
+      if (prevState.selected.includes(good)) {
+        return prevState;
+      }
+
+      return {
+        selected: [...prevState.selected, good],
+      };
+    });
+  };
+
+  cancelSelect = (good: string) => {
+    this.setState((prevState) => {
+      prevState.selected.splice(prevState.selected.indexOf(good), 1);
+
+      return {
+        selected: prevState.selected,
+      };
+    });
+  };
+
+  addItemsToTitle = (selected: string[]) => {
+    let title = '';
+
+    switch (true) {
+      case (selected.length === 1):
+        title += `${selected} is selected`;
+        break;
+
+      case (selected.length > 1):
+        title += `${selected.slice(0, -1).join(', ')} and ${selected.slice().pop()} is selected`;
+        break;
+
+      default:
+        title += 'No goods selected';
+        break;
+    }
+
+    return title;
+  };
+
+  clearSelect = () => {
+    this.setState(() => ({
+      selected: [],
+    }));
   };
 
   render() {
@@ -34,34 +79,37 @@ class App extends Component<{}, State> {
       <div className="App">
         <button
           type="button"
-          className={selected ? '' : 'App__hiden'}
-          onClick={() => {
-            this.setState({ selected: '' });
-          }}
+          className={cn({
+            App__hiden: selected.length === 0,
+          })}
+          onClick={this.clearSelect}
         >
           X
         </button>
         <h1 className="App__title">
-          {selected
-            ? `${selected} is selected`
-            : 'No goods selected'}
+          {this.addItemsToTitle(selected)}
         </h1>
         <ul>
           {goodsFromServer.map(good => (
             <li
               key={good}
-              className={`App__item ${selected === good && 'App__item--active'}`}
+              className="App__item"
             >
-              <div className="App__span">{good}</div>
-              <button
-                className="App__btn"
-                type="button"
-                onClick={() => {
-                  this.handleSelect(good);
-                }}
-              >
-                Select
-              </button>
+              <span className={`App__span ${selected.includes(good) && 'App__span--active'}`}>
+                {good}
+              </span>
+              <div className="App__btn">
+                <button
+                  type="button"
+                  onClick={() => {
+                    return selected.includes(good)
+                      ? this.cancelSelect(good)
+                      : this.handleSelect(good);
+                  }}
+                >
+                  {selected.includes(good) ? 'Cancel' : 'Select'}
+                </button>
+              </div>
             </li>
           ))}
         </ul>
