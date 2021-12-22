42a7dfd94b63121d549a39a87158918262f394a1
Oleh Khashchevskyi <hashchevskyj@gmail.com>
2021-12-22 13:44:32 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,88 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: ['Jam'],
+  };
+
+  addGood = (good:string) => {
+    this.setState(state => (
+      { selectedGood: [...state.selectedGood, good] }
+    ));
+  };
+
+  removeGood = (good:string) => {
+    this.setState(state => (
+      { selectedGood: state.selectedGood.filter(g => g !== good) }
+    ));
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        {selectedGood.length
+          ? (
+            <h1>
+              {selectedGood.length > 1
+                ? `${selectedGood.slice(0, -1).join(', ')} and ${selectedGood[selectedGood.length - 1]} are `
+                : `${selectedGood.join('')} is `}
+              selected
+              <button
+                className="App__clear-btn"
+                type="button"
+                onClick={() => {
+                  this.setState({ selectedGood: [] });
+                }}
+              >
+                X
+              </button>
+            </h1>
+          )
+          : <h1>No goods selected</h1>}
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className="App__item"
+            >
+              {good}
+              &nbsp;
+              {selectedGood.includes(good)
+                ? (
+                  <button
+                    type="button"
+                    className="App__remove-btn"
+                    onClick={() => {
+                      this.removeGood(good);
+                    }}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="App__add-btn"
+                    onClick={() => {
+                      this.addGood(good);
+                    }}
+                  >
+                    Add
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
