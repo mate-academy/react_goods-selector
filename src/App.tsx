ebde561eebd3730adbeffb1270bbddb06a13a1ef
Andrey Grigoriev <nkgrand25@gmail.com>
2021-12-27 13:11:43 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import './App.scss';
 
@@ -12,13 +13,93 @@ const goodsFromServer: string[] = [
   'Honey',
   'Jam',
   'Garlic',
+  'Banana',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  addGood = (good: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({ selectedGoods: [...selectedGoods, good] });
+  };
+
+  removeGood = (good: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({ selectedGoods: selectedGoods.filter(item => item !== good) });
+  };
+
+  showSelectedGoods = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+        break;
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      case 2:
+        return `${selectedGoods[0]} and ${selectedGoods[1]} are selected`;
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods[selectedGoods.length - 1]} are selected`;
+    }
+  };
+
+  clearBusket = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    console.log(selectedGoods.length);
+
+    return (
+      <div className="App">
+        <h1 className="title">
+          {this.showSelectedGoods()}
+        </h1>
+        {selectedGoods.length > 0
+          ? (
+            <button
+              type="button"
+              className="btn btn__clear"
+              onClick={this.clearBusket}
+            >
+              Clear all
+            </button>
+          )
+          : ''}
+        <ul className="list">
+          {goodsFromServer.map(good => (
+            <li key={good} className="list__item">
+              {good}
+              <button
+                key={good}
+                type="button"
+                className="btn"
+                onClick={
+                  selectedGoods.includes(good)
+                    ? () => this.removeGood(good)
+                    : () => this.addGood(good)
+                }
+              >
+                {selectedGoods.includes(good)
+                  ? 'Remove'
+                  : 'Add'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
