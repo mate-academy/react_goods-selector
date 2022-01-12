0f8186244fd0f8a7b025f481ba9e78383dee2e11
Oleksandr Leshchenko <bulochkin72@gmail.com>
2022-01-12 11:07:30 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -32,90 +32,94 @@ class App extends React.Component<{}, State> {
   };
 
   unselect = (good: string) => {
-    this.setState(prev => {
-      const arr = prev.selectedGoods;
-      const index = arr.findIndex(a => a === good);
+    this.setState(current => {
+      const index = current.selectedGoods.indexOf(good);
 
-      arr.splice(index, 1);
+      current.selectedGoods.splice(index, 1);
 
       const result = {
-        selectedGoods: arr,
+        selectedGoods: current.selectedGoods,
       };
 
       return result;
     });
   };
 
+  prettyString = (strings: string[]) => {
+    const result = [];
+
+    for (let i = 0; i < strings.length; i += 1) {
+      if (i === strings.length - 1) {
+        result[i] = `and ${strings[i]}`;
+      } else if (i === strings.length - 2) {
+        result[i] = `${strings[i]} `;
+      } else {
+        result[i] = `${strings[i]}, `;
+      }
+    }
+
+    return result.map(item => (<span key={item}>{item}</span>));
+  };
+
+  title = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return (
+          <h1>No goods selected</h1>
+        );
+      case 1:
+        return (
+          <h1>
+            {selectedGoods}
+            {' is selected'}
+          </h1>
+        );
+      default:
+        return (
+          <h1>
+            {this.prettyString(selectedGoods)}
+            {' are selected'}
+          </h1>
+        );
+    }
+  };
+
+  buttonSelector = (element: string) => {
+    const { selectedGoods } = this.state;
+
+    return (
+      !selectedGoods.includes(element)
+        ? (
+          <button
+            type="button"
+            onClick={() => {
+              this.select(element);
+            }}
+          >
+            Add
+          </button>
+        )
+        : (
+          <button
+            type="button"
+            onClick={() => {
+              this.unselect(element);
+            }}
+          >
+            remove
+          </button>
+        )
+    );
+  };
+
   render() {
     const { goods, selectedGoods } = this.state;
-    const prettyString = (strings: string[]) => {
-      const result = [];
-
-      for (let i = 0; i < strings.length; i += 1) {
-        if (i === strings.length - 1) {
-          result[i] = `and ${strings[i]}`;
-        } else if (i === strings.length - 2) {
-          result[i] = `${strings[i]} `;
-        } else {
-          result[i] = `${strings[i]}, `;
-        }
-      }
-
-      return result.map(item => (<span key={item}>{item}</span>));
-    };
-
-    const title = () => {
-      switch (selectedGoods.length) {
-        case 0:
-          return (
-            <h1>No goods selected</h1>
-          );
-        case 1:
-          return (
-            <h1>
-              {selectedGoods}
-              {' is selected'}
-            </h1>
-          );
-        default:
-          return (
-            <h1>
-              {prettyString(selectedGoods)}
-              {' are selected'}
-            </h1>
-          );
-      }
-    };
-
-    const buttonSelector = (element: string) => {
-      return (
-        !selectedGoods.includes(element)
-          ? (
-            <button
-              type="button"
-              onClick={() => {
-                this.select(element);
-              }}
-            >
-              Add
-            </button>
-          )
-          : (
-            <button
-              type="button"
-              onClick={() => {
-                this.unselect(element);
-              }}
-            >
-              remove
-            </button>
-          )
-      );
-    };
 
     return (
       <div className="App">
-        {title()}
+        {this.title()}
         <ul>
           {goods.map((good: string) => {
             const isValid = selectedGoods.includes(good);
@@ -123,12 +127,11 @@ class App extends React.Component<{}, State> {
             return (
               <li key={good} className={isValid ? 'selectedGood' : ''}>
                 {good}
-                {buttonSelector(good)}
+                {this.buttonSelector(good)}
               </li>
             );
           })}
         </ul>
-        {goodsFromServer.length}
       </div>
     );
   }
