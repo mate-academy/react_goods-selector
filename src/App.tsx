36fb9d07d6a6d335bd74f81a4d54e5b81c9e213b
Nikolas <tes.kolia@gmail.com>
2022-01-19 01:22:27 +0200

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import className from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,71 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    selectedGoods: ['Jam'],
+  };
+
+  render(): React.ReactNode {
+    const { selectedGoods, goods } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="title">
+          <button
+            className={className('button', 'button--close', { buttonVisibility: selectedGoods.length === 0 })}
+            type="button"
+            onClick={() => {
+              this.setState({ selectedGoods: [] });
+            }}
+          >
+            Clear all
+          </button>
+          {selectedGoods.length > 0
+            ? `${selectedGoods[selectedGoods.length - 1]} selected`
+            : 'No goods selected'}
+        </h1>
+        <ul className="app__list">
+          {selectedGoods.map(good => (
+            <li className="app__item" key={good}>
+              <div className="sheet">
+                <div className="sheet__good">{good}</div>
+                <button
+                  className={className('button', { buttonVisibility: !selectedGoods.includes(good) })}
+                  type="button"
+                  onClick={() => {
+                    this.setState({ selectedGoods: selectedGoods.filter(el => el !== good) });
+                  }}
+                >
+                  X
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <ul className="app__list">
+          {goods.map((good) => (
+            <li key={good} className="app__item">
+              <div className="sheet">
+                <div className="sheet__good">{good}</div>
+                <button
+                  className={className('button', { buttonVisibility: selectedGoods.includes(good) })}
+                  type="button"
+                  onClick={() => {
+                    this.setState({ selectedGoods: [...selectedGoods, good] });
+                  }}
+                >
+                  select
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
