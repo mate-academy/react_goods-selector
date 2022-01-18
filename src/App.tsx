62688e892f5f6277b8b64a0425af5dfb52c277df
Bohdan Maksymiuk <bohdan.max@ukr.net>
2022-01-18 15:29:56 +0200

    required tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.scss';
 
-const goodsFromServer: string[] = [
+type Good = string;
+
+const goodsFromServer: Good[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,11 +16,64 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: Good
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  goodSelector = (selectedGood: Good) => {
+    this.setState({ selectedGood });
+  };
+
+  render(): React.ReactNode {
+    const { selectedGood } = this.state;
+    const resetButton = (
+      <button
+        type="button"
+        onClick={() => this.setState({ selectedGood: '' })}
+      >
+        X
+      </button>
+    );
+
+    return (
+      <div className="App">
+        {selectedGood
+          ? (
+            <>
+              <h1>
+                {`${selectedGood} is selected`}
+                {resetButton}
+              </h1>
+            </>
+          )
+          : <h1>No goods selected</h1>}
+        {goodsFromServer.length}
+        <ul>
+          {goodsFromServer.map(good => (
+            <>
+              <li key={good}>
+                {good}
+                {selectedGood !== good
+                  && (
+                    <button
+                      type="button"
+                      onClick={() => this.goodSelector(good)}
+                    >
+                      Select
+                    </button>
+                  )}
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
