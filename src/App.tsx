68ab264d5cf67e1f45177abf13bf9bf4796344c2
Serhii Haidukov <sergeyglazgo@gmail.com>
2022-01-18 16:52:21 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,59 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {selectedGood}
+          {' '}
+          is selected
+        </h1>
+
+        <button
+          type="button"
+          className={classNames({
+            buttonVisibility: selectedGood === 'No goods',
+          })}
+          onClick={() => {
+            this.setState({ selectedGood: 'No goods' });
+          }}
+        >
+          X
+        </button>
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <li>
+              <div
+                className={classNames({
+                  selectedGood: selectedGood === good,
+                })}
+              >
+                {good}
+              </div>
+
+              <button
+                type="button"
+                onClick={() => {
+                  this.setState({ selectedGood: good });
+                }}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
