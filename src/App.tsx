6b11ce5e06119584c47ed41b7e3748a4e7dadd1d
Nikolas <tes.kolia@gmail.com>
2022-01-21 19:36:54 +0200

    fixed onClick method

--- src/App.tsx
+++ src/App.tsx
@@ -26,14 +26,28 @@ export class App extends React.Component<{}, State> {
     selectedGoods: ['Jam'],
   };
 
-  getButtonAction = (action: string, good?: string) => {
-    if (good && action === 'select') {
-      this.setState((state) => ({ selectedGoods: [...state.selectedGoods, good] }));
-    } else if (good && action === 'remove') {
-      this.setState((state) => ({ selectedGoods: state.selectedGoods.filter(el => el !== good) }));
-    } else {
-      this.setState({ selectedGoods: [] });
+  selectGood = (good: string) => {
+    this.setState((state) => ({ selectedGoods: [...state.selectedGoods, good] }));
+  };
+
+  removeGood = (good: string) => {
+    this.setState((state) => (
+      { selectedGoods: state.selectedGoods.filter(el => el !== good) }
+    ));
+  };
+
+  removeGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  getGoods = () => {
+    const { selectedGoods } = this.state;
+
+    if (selectedGoods.length > 1) {
+      return `${selectedGoods.join(', ')} are selected`;
     }
+
+    return `${selectedGoods.join(', ')} is selected`;
   };
 
   render() {
@@ -43,23 +57,19 @@ export class App extends React.Component<{}, State> {
       <div className="app">
         <h1 className="title">
           {selectedGoods.length > 0
-            ? `${selectedGoods.join(', ')} ${selectedGoods.length > 1 ? 'are' : 'is'}`
-              + ' selected'
+            ? this.getGoods()
             : 'No goods selected'}
         </h1>
 
-        {selectedGoods.length !== 0
-          && (
-            <button
-              type="button"
-              className="button button--big"
-              onClick={() => {
-                this.getButtonAction('ClearAll');
-              }}
-            >
-              Clear All
-            </button>
-          )}
+        {selectedGoods.length !== 0 && (
+          <button
+            type="button"
+            className="button button--big"
+            onClick={this.removeGoods}
+          >
+            Clear All
+          </button>
+        )}
 
         <div className="wrapper">
           <ul className="app__list">
@@ -67,18 +77,17 @@ export class App extends React.Component<{}, State> {
               <li className="app__item" key={good}>
                 <div className="sheet">
                   <div className="sheet__good">{good}</div>
-                  {selectedGoods.includes(good)
-                    && (
-                      <button
-                        type="button"
-                        className="button"
-                        onClick={() => {
-                          this.getButtonAction('remove', good);
-                        }}
-                      >
-                        remove
-                      </button>
-                    )}
+                  {selectedGoods.includes(good) && (
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={() => {
+                        this.removeGood(good);
+                      }}
+                    >
+                      remove
+                    </button>
+                  )}
                 </div>
               </li>
             ))}
@@ -95,7 +104,7 @@ export class App extends React.Component<{}, State> {
                         type="button"
                         className="button"
                         onClick={() => {
-                          this.getButtonAction('select', good);
+                          this.selectGood(good);
                         }}
                       >
                         select
