c6e0564b5801b123d72dec5d9647b9d8ca2c6257
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-01-21 17:02:23 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,57 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    goods: goodsFromServer,
+    selectedGood: 'Jam',
+  };
 
-export default App;
+  selectedHandler = (good: string) => {
+    this.setState({ selectedGood: `${good} is selected` });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -
+          {' '}
+          {!selectedGood ? 'No goods selected' : selectedGood}
+        </h1>
+        <button
+          className="removeGood"
+          type="button"
+          onClick={() => {
+            this.setState({ selectedGood: '' });
+          }}
+        >
+          X
+        </button>
+        <ul className="App__list">
+          {this.state.goods.map((good) => (
+            <>
+              <li key={good} className={classNames({ App__item: selectedGood.includes(good) })}>
+                {good}
+                {' '}
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.selectedHandler(good);
+                  }}
+                  className={classNames({
+                    buttonVisible: selectedGood.includes(good),
+                  })}
+                >
+                  Select
+                </button>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
