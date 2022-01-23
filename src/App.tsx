8474fd1c627119d5238899712f86da21a148792a
Iryna Zolko <koko_irina@ukr.net>
2022-01-24 01:05:31 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
+// import { render } from 'react-dom';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,79 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isGoodSelected: boolean;
+  infoString: string;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isGoodSelected: false,
+    infoString: 'No goods selected',
+  };
+
+  render() {
+    const { isGoodSelected } = this.state;
+    const { infoString } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="info">{infoString}</h1>
+        <button
+          type="button"
+          className={
+            classNames(
+              'button-hidden',
+              { 'button-visible': isGoodSelected },
+            )
+          }
+          onClick={() => {
+            this.setState({
+              isGoodSelected: !isGoodSelected,
+              infoString: '',
+            });
+          }}
+        >
+          X
+        </button>
+        <ul>
+          {goodsFromServer.map(good => (
+            <>
+              <li
+                key={good}
+                className={
+                  classNames(
+                    'good',
+                    { 'good-bg': isGoodSelected },
+                  )
+                }
+              >
+                {good}
+              </li>
+              <button
+                type="button"
+                className={
+                  classNames(
+                    'button',
+                    { 'button-hidden': isGoodSelected },
+                  )
+                }
+                onClick={() => {
+                  this.setState({
+                    isGoodSelected: !isGoodSelected,
+                    infoString: `${good} is selected`,
+                  });
+                }}
+              >
+                Select
+              </button>
+              <br />
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
