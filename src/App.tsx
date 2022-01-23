38d2792924f890a70c41abfed21bc8e07fcc06a6
Aleksandra Mileant <aleksandramileant1@gmail.com>
2022-01-23 13:00:34 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +15,109 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[],
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  addGood = (good: string) => {
+    this.setState(state => ({
+      selectedGoods: [...state.selectedGoods, good],
+    }));
+  };
+
+  removeGood = (selectedGood: string) => {
+    this.setState(state => {
+      const selectedGoods = state.selectedGoods
+        .filter(good => good !== selectedGood);
+
+      return {
+        selectedGoods,
+      };
+    });
+  };
+
+  deleteGoods = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  };
+
+  finalGood = (selectedGoods: string[]) => {
+    const copyGoods = [...selectedGoods];
+
+    const lastGood = copyGoods.pop();
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      case 2:
+        return `${selectedGoods.join(' and ')} are selected`;
+      default:
+        return `${selectedGoods.join(', ')} and ${lastGood} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App card has-background-grey-lighter">
+        <h1 className="title has-background-white-ter">
+          {this.finalGood(selectedGoods)}
+        </h1>
+
+        {selectedGoods.length > 0 && (
+          <button
+            className="card-button delete is-large"
+            type="button"
+            onClick={() => this.deleteGoods()}
+          >
+            X
+          </button>
+        )}
+
+        <ul className="card-content">
+          {goodsFromServer.map((good) => (
+            <li
+              key={good}
+              className={classNames(
+                'card-text',
+                { selected: selectedGoods.includes(good) },
+              )}
+            >
+              {good}
+              {selectedGoods.includes(good)
+                ? (
+                  <button
+                    className="button is-danger is-rounded"
+                    type="button"
+                    onClick={() => this.removeGood(good)}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    className="button is-success is-rounded"
+                    type="button"
+                    onClick={() => this.addGood(good)}
+                  >
+                    Add
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
