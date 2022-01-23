e28568aa393b8be127bad418babb320b7ea7f363
Nataliia Hrynyk <nataliiahrynyk2001@gmail.com>
2022-01-23 16:33:09 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,90 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[],
+};
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  getListInformation = () => {
+    const { selectedGoods } = this.state;
+    const listCopy = [...selectedGoods];
+
+    const lastGood = listCopy.pop();
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+      case 2:
+        return `${selectedGoods.join(' and ')} are selected`;
+      default:
+        return `${listCopy.join(', ')} and ${lastGood} are selected`;
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App_title box title">{this.getListInformation()}</h1>
+
+        <div className="App_box box">
+          {!!this.state.selectedGoods.length && (
+            <button
+              type="button"
+              className="button is-danger is-outlined"
+              onClick={() => this.setState({ selectedGoods: [] })}
+            >
+              X
+            </button>
+          )}
+          <ul className="App_list">
+            {goodsFromServer.map(good => (
+              <li
+                key={good}
+                className={classNames(
+                  'App_list-item box',
+                  {
+                    'has-background-primary-light': this.state.selectedGoods.includes(good),
+                  },
+                )}
+              >
+                {good}
+                {' '}
+                {this.state.selectedGoods.includes(good) ? (
+                  <button
+                    type="button"
+                    className="button is-danger is-outlined"
+                    onClick={() => this.setState(({ selectedGoods }) => ({
+                      selectedGoods: selectedGoods.filter(el => el !== good),
+                    }))}
+                  >
+                    Remove
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="button is-primary is-outlined"
+                    onClick={() => this.setState(({ selectedGoods }) => ({
+                      selectedGoods: [...selectedGoods, good],
+                    }))}
+                  >
+                    Add
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
