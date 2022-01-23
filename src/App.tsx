838b32d7e3a0a3a24d5c8ba01a621d697fe578a5
Sesile26 <37846431+Sesile26@users.noreply.github.com>
2022-01-23 23:30:03 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +15,76 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  selectGood = (good: string) => {
+    this.setState({ selectedGood: good });
+  };
+
+  clearSelect = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {selectedGood
+            ? `${selectedGood} selected`
+            : 'No goods selected'}
+        </h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.clearSelect();
+          }}
+          className={classNames(
+            {
+              hidden: selectedGood === '',
+            },
+          )}
+        >
+          Clear
+        </button>
+        <ul className="list">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={classNames(
+                {
+                  selected: selectedGood === good,
+                },
+              )}
+            >
+              {good}
+              <button
+                type="button"
+                onClick={() => {
+                  this.selectGood(good);
+                }}
+                className={classNames(
+                  {
+                    hidden: selectedGood === good,
+                  },
+                )}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
