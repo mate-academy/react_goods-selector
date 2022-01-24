f1cb9050e586943846a5171d450d7df0956db6da
Roman Shevchuk <14rom.shevchuk@gmail.com>
2022-01-24 15:13:17 +0200

    demo react goods-selector

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,100 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  selectedGood: string[],
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: [goodsFromServer[8]],
+  };
+
+  addStyleSelect = (element: string) => {
+    this.setState(
+      (prevState) => ({
+        selectedGood: [...prevState.selectedGood, element],
+      }),
+    );
+  };
+
+  resetSelected = () => {
+    this.setState(
+      () => ({
+        selectedGood: [],
+      }),
+    );
+  };
+
+  myTitle = (selectedGood: string[]) => {
+    const copyselectedGood = [...selectedGood];
+
+    if (selectedGood.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (selectedGood.length >= 3) {
+      const lastItem = copyselectedGood[copyselectedGood.length - 1];
+
+      copyselectedGood.pop();
+
+      return `${copyselectedGood.join(', ')} and ${lastItem} are selected`;
+    }
+
+    return `${copyselectedGood.join(' and ')} is selected`;
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {this.myTitle(selectedGood)}
+        </h1>
+        <div>
+          Reset your selections
+          <button
+            type="button"
+            onClick={() => this.resetSelected()}
+          >
+            x
+          </button>
+        </div>
+
+        <ul>
+          {goodsFromServer.map(item => (
+            ((selectedGood.includes(item)
+              ? (
+                <li key={item} className="active">
+                  {item}
+                  <button
+                    type="button"
+                    onClick={() => this.addStyleSelect(item)}
+                  >
+                    selected
+                  </button>
+                </li>
+              )
+              : (
+                <li key={item}>
+                  {item}
+                  <button
+                    type="button"
+                    onClick={() => (
+                      this.addStyleSelect(item)
+                    )}
+                  >
+                    selected
+                  </button>
+                </li>
+              )
+            )
+            )))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
