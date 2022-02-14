3f2cc9fc97757da641baa9b84b0b50efe21511d1
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-02-14 16:39:23 +0200

    oysters clams and cockles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+import { formatGoods } from './utils';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +16,104 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<Props, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+    const title = selectedGoods.length ? formatGoods(selectedGoods) : 'No goods selected';
+
+    return (
+      <div className="App">
+        <div className="title-container">
+          <h1>
+            {title}
+          </h1>
+
+          {Boolean(selectedGoods.length) && (
+            <button
+              type="button"
+              className="title-button"
+              onClick={() => {
+                this.setState({ selectedGoods: [] });
+              }}
+            >
+              x
+            </button>
+          )}
+        </div>
+
+        <ul className="goods">
+          {goodsFromServer.map(good => {
+            const goodIsSelected = selectedGoods.includes(good);
+
+            const selectHandler = () => {
+              selectedGoods.push(good);
+              this.setState({ selectedGoods });
+            };
+
+            const removeHandler = () => {
+              const index = selectedGoods.indexOf(good);
+
+              selectedGoods.splice(index, 1);
+              this.setState({ selectedGoods });
+            };
+
+            return (
+              <div className="good-container">
+                <li
+                  key={good}
+                  className={classNames(
+                    'good',
+                    { 'good--selected': goodIsSelected },
+                  )}
+                >
+                  {good}
+                </li>
+
+                {goodIsSelected
+                  ? (
+                    <div>
+                      <button
+                        type="button"
+                        className="button"
+                        onClick={selectHandler}
+                      >
+                        Add
+                      </button>
+                      <button
+                        type="button"
+                        className="button"
+                        onClick={removeHandler}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  )
+                  : (
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={selectHandler}
+                    >
+                      Select
+                    </button>
+                  )}
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+export function formatGoods(goods: string[]): string {
+  switch (goods.length) {
+    case 1:
+      return `${goods.join(', ')} is selected`;
+
+    case 2:
+      return `${goods[0]} and ${goods[1]} are selected`;
+
+    default:
+      return `${goods
+        .slice(0, goods.length - 1)
+        .join(', ')} and ${goods[goods.length - 1]} are selected`;
+  }
+}
