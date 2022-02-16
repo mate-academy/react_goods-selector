8c902a63df23465b15a210b2df33b5fa7f99f3da
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-02-16 20:48:31 +0200

    list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,83 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string;
+  goods: string[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+    goods: goodsFromServer,
+  };
+
+  addGood = (good: string) => {
+    this.setState({ selectedGood: good });
+  };
+
+  removeGood = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  selectGood = (good: string) => {
+    return this.state.selectedGood === good;
+  };
+
+  selectGoods(good: string) {
+    return this.state.selectedGood === good;
+  }
+
+  render() {
+    const { selectedGood, goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="goods__title">
+          {selectedGood
+            ? `${selectedGood} is selected`
+            : 'No goods selected'}
+          <button
+            type="button"
+            onClick={() => this.removeGood()}
+          >
+            X
+          </button>
+        </h1>
+
+        <ul>
+          {goods.map(good => (
+            <div className="goods__list">
+              <li
+                className={classNames('goods__item', {
+                  'goods__item--selected': this.selectGoods(good),
+                })}
+              >
+                {good}
+              </li>
+              {this.selectGood(good)
+                ? (
+                  <button
+                    type="button"
+                    className="goods__button"
+                    onClick={() => this.removeGood()}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="goods__button"
+                    onClick={() => this.addGood(good)}
+                  >
+                    Add
+                  </button>
+                )}
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
