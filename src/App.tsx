514513904578fd3089301728d0d3389e3052e619
Mykhailo Ianush <yanushmisha@gmail.com>
2022-02-16 13:12:26 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,110 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    data: goodsFromServer,
+    selected: [],
+  };
+
+  createSelected() {
+    const arr: string[] = this.state.selected;
+    const { length } = this.state.selected;
+
+    if (length === 0) {
+      return 'No Selected';
+    }
+
+    if (length === 1) {
+      return `${arr[0]} is selected`;
+    }
+
+    let output = '';
+
+    for (let i = 0; i < length - 1; i += 1) {
+      output += `${arr[i]} , `;
+    }
+
+    return `${output.substring(0, output.length - 2)} and ${arr[arr.length - 1]} are selected`;
+  }
+
+  addWord(word: string) {
+    const arr: string[] = this.state.selected;
+
+    arr.push(word);
+
+    return arr;
+  }
+
+  removeWord(word: string) {
+    const arr: string[] = this.state.selected;
+    const index: number = arr.indexOf(word);
+
+    if (index >= 0) {
+      arr.splice(index, 1);
+    }
+
+    return arr;
+  }
+
+  check(word: string) {
+    const arr: string[] = this.state.selected;
+    const index: number = arr.indexOf(word);
+
+    if (index >= 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>
+          {`Selected good: -- ${this.createSelected()}  -- `}
+          <button
+            type="button"
+            className="app__clear"
+            onClick={() => this.setState({ selected: [] })}
+          >
+            Clear
+          </button>
+        </h1>
+
+        <ul>
+          {this.state.data.map(good => (
+            <li>
+              {`${good} - `}
+              {!this.check(good) && (
+                <button
+                  type="button"
+                  className="app__add"
+                  onClick={
+                    () => this.setState({ selected: this.addWord(good) })
+                  }
+                >
+                  Add
+                </button>
+              )}
+              {this.check(good) && (
+                <button
+                  type="button"
+                  className="app__remove"
+                  onClick={
+                    () => this.setState({ selected: this.removeWord(good) })
+                  }
+                >
+                  Remove
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    );
+  }
+}
 
 export default App;
