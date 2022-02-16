3219815e9989997dd3b3ed4aa9c8278ee08ff700
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-02-17 01:40:28 +0200

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,94 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  select = (newGood: string) => {
+    this.setState(({ selectedGoods }) => {
+      return { selectedGoods: [...selectedGoods, newGood] };
+    });
+  };
+
+  remove = (goodToRemove: string) => {
+    this.setState(({ selectedGoods }) => {
+      return ({
+        selectedGoods: selectedGoods.filter(good => good !== goodToRemove),
+      });
+    });
+  };
+
+  deleteAll = () => {
+    this.setState({
+      selectedGoods: [],
+    });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          {`Selected goods: ${selectedGoods.length > 0
+            ? (selectedGoods.join(', '))
+            : ('No selected goods')
+          }`}
+        </h1>
+        <ul className="App__list">
+          {goodsFromServer.map((good) => {
+            return (
+              <li
+                key={good}
+                className="App__item"
+              >
+                <span
+                  className={
+                    selectedGoods.includes(good)
+                      ? 'App__good App__good--active'
+                      : 'App__good'
+                  }
+                >
+                  {good}
+                </span>
+                <button
+                  type="button"
+                  className="App__button"
+                  onClick={
+                    selectedGoods.includes(good)
+                      ? () => this.remove(good)
+                      : () => this.select(good)
+                  }
+                >
+                  {
+                    selectedGoods.includes(good)
+                      ? 'Delete'
+                      : 'Add'
+                  }
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        {selectedGoods.length > 0
+          && (
+            <button
+              type="button"
+              className="App__delete"
+              onClick={this.deleteAll}
+            >
+              X
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
