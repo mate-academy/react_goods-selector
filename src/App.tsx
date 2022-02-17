a30cd4a80565d64731bd9db704ae430e4beabac3
Maksym Mishchanchuk <yssam.nn@gmail.com>
2022-02-17 19:32:02 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,90 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: start -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  addGoods:string[]
+  dataGoods: string[]
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    addGoods: ['Jam'],
+    dataGoods: goodsFromServer,
+  };
+
+  create(good: string) {
+    this.setState((state) => (
+      { addGoods: [...state.addGoods, good] }
+    ));
+  }
+
+  deleteGoods() {
+    this.setState(() => (
+      { addGoods: [] }
+    ));
+  }
+
+  deleteGood(good: string) {
+    this.setState(({ addGoods }) => ({
+      addGoods: addGoods.filter(item => item !== good),
+    }));
+  }
+
+  render() {
+    const { addGoods, dataGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="check_goods">
+          <h1>
+            {(addGoods.length === 0)
+              ? 'No selected goods'
+              // eslint-disable-next-line no-useless-concat
+              : `${addGoods}` + ' is selected' }
+          </h1>
+          <button
+            onClick={() => this.deleteGoods()}
+            type="button"
+            className="button button_delete"
+          >
+            Delete goods
+          </button>
+        </div>
+        {/* eslint-disable-next-line array-callback-return */}
+        {dataGoods.map((good) => {
+          const isSelected = addGoods.includes(good);
+          const buttonText = isSelected
+            ? 'Delete'
+            : 'Add';
+
+          return (
+            <div
+              className={classNames('container__item',
+                { 'container__item--active': addGoods.includes(good) })}
+            >
+              <li
+                key={good}
+                className="container__item_good"
+              >
+                {good}
+              </li>
+              <button
+                onClick={() => (
+                  isSelected
+                    ? this.deleteGood(good)
+                    : this.create(good)
+                )}
+                className="container__item_button button"
+                type="button"
+              >
+                {buttonText}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default App;
