5852e8e6f4ce461c1a3d957713d03bbca791eeaa
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2022-02-17 14:14:26 +0200

    firstTry

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,78 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>No goods selected</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string;
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  clear = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  addWord = (word: string) => {
+    this.setState({ selectedGood: word });
+  };
+
+  clickHandler = (el: string, selectedGood: string) => (
+    selectedGood === el
+      ? this.clear()
+      : this.addWord(el)
+  );
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="app__title">
+          {selectedGood ? `${selectedGood} is selected`
+            : 'No goods selected'}
+          {'  '}
+          <button
+            type="button"
+            className="button"
+            onClick={this.clear}
+          >
+            X
+          </button>
+        </h1>
+
+        <ul className="list">
+          {goodsFromServer.map(el => (
+            <>
+              <li
+                key={el + Math.random()}
+                className={classNames(
+                  'list__item',
+                  {
+                    'list__item--selected':
+                    selectedGood === el,
+                  },
+                )}
+              >
+                {el}
+              </li>
+
+              <button
+                type="button"
+                className="list__button"
+                onClick={() => (
+                  this.clickHandler(el, selectedGood)
+                )}
+              >
+                {selectedGood === el
+                  ? 'delete'
+                  : 'add'}
+              </button>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
