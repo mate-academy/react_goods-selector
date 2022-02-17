bee26960963462f05f58c9bfd7956d2f232963c0
Alex <asultan4ik@gmail.com>
2022-02-17 14:20:17 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
-  'Ice cream',
+  'Ice-cream',
   'Apple',
   'Bread',
   'Fish',
@@ -14,11 +14,40 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>No goods selected: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: goodsFromServer,
+    name: 'Jam',
+  };
+
+  selected = (good: string) => {
+    this.setState({ name: good });
+  };
+
+  unselected = () => {
+    this.setState({ name: '' });
+  };
+
+  render(): React.ReactNode {
+    const { goods, name } = this.state;
+
+    return (
+      <div className="App">
+        <div className="display display__title">
+          <h1>{name ? `${name} is selected` : 'No goods selected'}</h1>
+          <button type="button" onClick={this.unselected} hidden={name === ''}>X</button>
+        </div>
+        <ul>
+          {goods.map(good => (
+            <div className="display">
+              <li id={(name === good) ? good : ''}>{good}</li>
+              <button type="button" onClick={() => this.selected(good)} hidden={name === good}>{`Select ${good}`}</button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
