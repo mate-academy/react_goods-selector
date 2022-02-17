d35a946941dcde523d371cea38f5bb47f48dbd28
DanyloLipar <danilo.lipar2016@gmail.com>
2022-02-17 10:00:06 +0200

    SSave

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const goodsFromServer: string[] = [
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Selected good: -</h1>
+    <h1>No selected goods</h1>
     {goodsFromServer.length}
   </div>
 );
