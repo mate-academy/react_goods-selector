fe3515d5e51f86d9c33f48288e68767b2cf52f3d
DanyloLipar <danilo.lipar2016@gmail.com>
2022-02-17 19:44:10 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,55 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>No selected goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selected: string
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selected: 'Jam',
+  };
+
+  remove = () => {
+    this.setState({ selected: '' });
+  };
+
+  render(): React.ReactNode {
+    const { selected } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__head">{selected ? `${selected} is selected` : 'No goods selected'}</h1>
+        <button
+          className="App__cancel"
+          type="button"
+          onClick={this.remove}
+        >
+          X
+        </button>
+        <ul className="App__list list">
+          {goodsFromServer.map((good) => (
+            <div className="list__goods">
+              <li key={good} className="list__goods-item">
+                {good}
+              </li>
+              <button
+                type="button"
+                className="list__button"
+                onClick={() => (
+                  selected === good
+                    ? this.remove()
+                    : this.setState({ selected: good })
+                )}
+              >
+                {selected === good ? 'Cancel' : 'Select'}
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
