422c71b881f6f39a0cb511926134152d7a9b7bd8
Artem Paskal <artem.yogi@gmail.com>
2022-02-17 15:54:46 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,46 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selected: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selected: [],
+  };
+
+  addItem = (item: string) => {
+    this.setState((state) => ({ selected: [...state.selected, item] }));
+  };
+
+  render() {
+    const { selected } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {' - '}
+          {selected.join(', ')}
+        </h1>
+
+        {goodsFromServer.map((item) => (
+          <div>
+            {item}
+            <button
+              type="button"
+              onClick={() => {
+                this.addItem(item);
+              }}
+            >
+              Add
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
