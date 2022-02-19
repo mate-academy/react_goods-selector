c1585d18ca2ccbaa6cd1ee123b29e4c7cb9c1828
Artem Paskal <artem.yogi@gmail.com>
2022-02-19 12:21:21 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -15,42 +16,79 @@ const goodsFromServer: string[] = [
 ];
 
 type State = {
-  selected: string[];
+  selected: string;
 };
 
 class App extends React.Component<{}, State> {
-  state = {
-    selected: [],
+  state: State = {
+    selected: 'Jam',
   };
 
-  addItem = (item: string) => {
-    this.setState((state) => ({ selected: [...state.selected, item] }));
+  deleteItem = () => {
+    this.setState({ selected: '' });
   };
+
+  addItem(item: string) {
+    this.setState({ selected: item });
+  }
 
   render() {
     const { selected } = this.state;
 
     return (
       <div className="App">
-        <h1>
-          Selected good:
-          {' - '}
-          {selected.join(', ')}
-        </h1>
-
-        {goodsFromServer.map((item) => (
-          <div>
-            {item}
-            <button
-              type="button"
-              onClick={() => {
-                this.addItem(item);
-              }}
+        {selected === ''
+          ? (
+            <>
+              <div className="title">
+                <h1>No selected Goods</h1>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="title">
+                <h1>{`Selected good: ${selected}`}</h1>
+                <button
+                  className="button"
+                  type="button"
+                  onClick={this.deleteItem}
+                >
+                  Clear
+                </button>
+              </div>
+            </>
+          )}
+        <ul className="goodList">
+          {goodsFromServer.map((item) => (
+            <li
+              key={item}
+              className={classNames('goodStyle', {
+                'goodStyle--selected': this.state.selected === item,
+              })}
             >
-              Add
-            </button>
-          </div>
-        ))}
+              <p>
+                {item}
+              </p>
+              {item === selected ? (
+                <button
+                  className="button"
+                  type="button"
+                  onClick={this.deleteItem}
+                >
+                  Delete
+                </button>
+              ) : (
+                <button
+                  className="button"
+                  type="button"
+                  onClick={() => this.addItem(item)}
+                >
+                  Add
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
