c51ea974f21b731c3c5554c3f642d5dcf080615d
Illia Shukh <ilyashuh0@gmail.com>
2022-02-21 14:16:21 +0200

    sol3(

--- src/App.tsx
+++ src/App.tsx
@@ -16,127 +16,74 @@ const goodsFromServer: string[] = [
 ];
 
 type State = {
-  selectedGoods: string[],
+  selectedGood: string,
 };
 
 class App extends React.Component<{}, State> {
   state = {
-    selectedGoods: ['Jam'],
+    selectedGood: 'Jam',
   };
 
-  toggleSelection = (
-    event: React.MouseEvent,
-    selectedGood: string,
-  ) => {
-    const button = event.target as HTMLElement;
-    const buttonText = button.textContent;
-    const { addGood, removeGood } = this;
+  selectGood(good: string) {
+    this.setState({ selectedGood: good });
+  }
 
-    switch (buttonText) {
-      case 'Add':
-        addGood(selectedGood);
-        button.textContent = 'Remove';
-        break;
-
-      case 'Remove':
-        removeGood(selectedGood);
-        button.textContent = 'Add';
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  getStringOfSelected = (goods: string[]) => {
-    const lastGood = goods[goods.length - 1];
-    const goodsExceptOfLast = goods.slice(0, -1).join(', ');
-
-    switch (goods.length) {
-      case 0:
-        return 'No goods selected';
-      case 1:
-        return `${goods[0]} is selected`;
-      default:
-        return `${goodsExceptOfLast} and ${lastGood} are selected`;
-    }
-  };
-
-  removeAllGoods = () => {
-    this.setState({
-      selectedGoods: [],
-    });
-  };
-
-  addGood = (selectedGood: string) => {
-    this.setState(state => ({
-      selectedGoods: [...state.selectedGoods, selectedGood],
-    }));
-  };
-
-  removeGood = (selectedGood: string) => {
-    this.setState(state => ({
-      selectedGoods: state.selectedGoods.filter(
-        good => selectedGood !== good,
-      ),
-    }));
-  };
+  removeGood() {
+    this.setState({ selectedGood: '' });
+  }
 
   render() {
-    const {
-      toggleSelection,
-      getStringOfSelected,
-      removeAllGoods,
-      state,
-    } = this;
-    const { selectedGoods } = state;
-    const isAnyGoodSelected = selectedGoods.length > 0;
+    const { selectedGood } = this.state;
+    const visible = selectedGood === '';
 
     return (
       <div className="App">
-        <h1 className="App__title">
-          {getStringOfSelected(selectedGoods)}
-          {isAnyGoodSelected && (
-            <button type="button" onClick={removeAllGoods}>
+        <h1>
+          {!visible
+          && (
+            <button
+              type="button"
+              onClick={() => {
+                this.removeGood();
+              }}
+            >
               X
             </button>
           )}
+
+          {(!selectedGood)
+            ? 'No goods selected'
+            : `${selectedGood} is selected`}
         </h1>
+        <div className="container">
+          <ul className="container__list">
+            {goodsFromServer.map(good => (
+              <li
+                key={good}
+                className={
+                  classNames('container__item', {
+                    'container__item--selected': good === selectedGood,
+                  })
+                }
+              >
+                {good}
 
-        <ul className="App__list">
-          {goodsFromServer.map((good) => {
-            const isSelected = selectedGoods.includes(good);
-            const buttonText = isSelected
-              ? 'Remove'
-              : 'Add';
-
-            return (
-              <li className="App__item" key={good}>
-                <p
-                  className={
-                    classNames(
-                      'App__item-title',
-                      {
-                        selected: isSelected,
-                      },
-                    )
-                  }
-                >
-                  {good}
-                </p>
-
-                <button
-                  type="button"
-                  onClick={(event) => {
-                    toggleSelection(event, good);
-                  }}
-                >
-                  {buttonText}
-                </button>
+                {(good !== selectedGood)
+                && (
+                  <button
+                    className="waves-effect waves-light btn"
+                    type="button"
+                    onClick={() => {
+                      this.selectGood(good);
+                    }}
+                  >
+                    Select
+                  </button>
+                )}
               </li>
-            );
-          })}
-        </ul>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
