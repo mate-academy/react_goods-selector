e756e66ff51d3078629b8678d35e59bb52a0d4d4
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-10 19:40:51 +0200

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 
+import classNames from 'classnames';
+
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -14,11 +16,85 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Prop = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, Prop> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  toggle = (good: string) => {
+    if (this.state.selectedGoods.includes(good)) {
+      this.setState(state => (
+        { selectedGoods: [...state.selectedGoods.filter(item => item !== good)] }
+      ));
+    } else {
+      this.setState(state => (
+        { selectedGoods: [...state.selectedGoods, good] }
+      ));
+    }
+  };
+
+  createTitle = (goods : string[]) => {
+    return (
+      goods.length
+        ? `${goods.length > 1
+          ? `${goods.slice(0, -1).join(', ')} and ${goods[goods.length - 1]} are selected`
+          : `${goods[0]} is selected`}`
+        : 'No goods selected'
+    );
+  };
+
+  cancelSelected = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Selected goods:</h1>
+        <p className="App__data-info">{this.createTitle(selectedGoods)}</p>
+        <ul className="App__list">
+          <button
+            type="button"
+            className="App__clear"
+            onClick={() => {
+              this.cancelSelected();
+            }}
+          >
+            Cancel Selected Goods
+          </button>
+          {goodsFromServer.map(good => {
+            return (
+              <li
+                key={good}
+                className={classNames(
+                  'App__item',
+                  { 'App__item--on': selectedGoods.includes(good) },
+                  { 'App__item--off': !selectedGoods.includes(good) },
+                )}
+              >
+                <h3>{ good }</h3>
+                <button
+                  className="App__btn"
+                  type="button"
+                  onClick={() => {
+                    this.toggle(good);
+                  }}
+                >
+                  {selectedGoods.includes(good) ? '-' : '+'}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
