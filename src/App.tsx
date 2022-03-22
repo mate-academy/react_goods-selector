d468c8d6ee42e5eeaab08ca9d7280674e9c57836
Dina Samoilova <s13.dina@gmail.com>
2022-03-22 15:27:22 +0200

    solution react goods selector without optional

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,62 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {`Selected good: ${selectedGood ? `${selectedGood} is selected` : 'No goods selected'}`}
+
+          <button
+            type="button"
+            className={classNames(
+              'clear-button',
+              { hidden: selectedGood === '' },
+            )}
+            onClick={() => {
+              this.setState({ selectedGood: '' });
+            }}
+          >
+            X
+          </button>
+        </h1>
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={classNames(
+                { selected: good === selectedGood },
+              )}
+            >
+              {good}
+
+              <button
+                type="button"
+                className={classNames(
+                  'item-button',
+                  { hidden: good === selectedGood },
+                )}
+                onClick={() => {
+                  this.setState({ selectedGood: good });
+                }}
+              >
+                Select
+              </button>
+            </li>
+
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
