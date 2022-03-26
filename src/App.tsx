81427f09cef5f167f717fd8aa1673f792fcc3756
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-03-26 15:15:30 +0200

    List of goods(main task)

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,57 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <div className="container">
+          <section className="App__header header">
+            <h1>{selectedGood.length ? `${selectedGood} is selected` : 'No goods selected'}</h1>
+            {selectedGood && (
+              <button
+                className="button button--close"
+                type="button"
+                onClick={() => (
+                  this.setState({ selectedGood: '' })
+                )}
+              >
+                X
+              </button>
+            )}
+          </section>
+
+          <ul className="list">
+            {goodsFromServer.map((good) => (
+              <li key={good} className="list__item">
+                <div className={selectedGood === good ? 'list__item_active' : ''}>
+                  {good}
+                </div>
+
+                {selectedGood !== good && (
+                  <button
+                    className="button"
+                    type="button"
+                    onClick={() => {
+                      this.setState({ selectedGood: good });
+                    }}
+                  >
+                    Select
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
