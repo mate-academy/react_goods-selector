cc7fc2821fedadd4400e113ec552f872e60cb4c8
Test <test@iMac-IMac.local>
2022-03-28 11:09:54 +0300

    last saved 28.03

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,64 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: '',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good: -&nbsp;
+          {selectedGood.length !== 0
+            ? `${selectedGood} is selected  `
+            : 'No goods selected  '}
+            &nbsp;
+          <button
+            className="btn-cancel"
+            type="button"
+            onClick={() => this.setState({ selectedGood: '' })}
+          >
+            X
+          </button>
+        </h1>
+
+        <ol className="goods">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className="good"
+            >
+              <p className="good__name">
+                {good}
+              </p>
+              <button
+                type="button"
+                className={classNames(
+                  'good__btn',
+                  {
+                    checked: selectedGood === good,
+                  },
+                )}
+                onClick={() => this.setState({
+                  selectedGood: good,
+                })}
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default App;
