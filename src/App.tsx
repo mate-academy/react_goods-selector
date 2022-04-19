58900577b8905b0026525b48c7026747d6648cf3
Roma <roma@MacBook-Pro-Roman.local>
2022-04-19 14:56:38 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
 import './App.scss';
 
-const goodsFromServer: string[] = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+class App extends React.Component {
+  state = {
+    goodsFromServer: [
+      'Dumplings',
+      'Carrot',
+      'Eggs',
+      'Ice cream',
+      'Apple',
+      'Bread',
+      'Fish',
+      'Honey',
+      'Jam',
+      'Garlic',
+    ],
+    selectedGood: 'Jam',
+  };
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+  render() {
+    const { goodsFromServer, selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {` ${selectedGood} is selected`}
+          { selectedGood !== '' && (
+            <button
+              type="button"
+              onClick={() => {
+                const elements = document.querySelectorAll('li');
+
+                // eslint-disable-next-line no-plusplus
+                for (let i = 0; i < elements.length; i++) {
+                  elements[i].classList.remove('selected');
+                }
+
+                this.setState({ selectedGood: '' });
+              }}
+            >
+              X
+            </button>
+          )}
+        </h1>
+        {goodsFromServer.length}
+        <ul>
+          {goodsFromServer.map(product => (
+            <li key={product} id={product}>
+              {product}
+              { ' - ' }
+              { selectedGood !== product && (
+                <button
+                  type="button"
+                  onClick={() => {
+                    const elements = document.querySelectorAll('li');
+
+                    // eslint-disable-next-line no-plusplus
+                    for (let i = 0; i < elements.length; i++) {
+                      elements[i].classList.remove('selected');
+                    }
+
+                    this.setState({ selectedGood: product });
+                    const elem = document.getElementById(product);
+
+                    elem?.classList.add('selected');
+                  }}
+                >
+                  Select
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
