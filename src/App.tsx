29588699389a04d585566aae9fd3ed63dd0a394d
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-04-20 19:25:27 +0300

    The draft of solution was implemented (without optional task)

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,110 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    initialHeader: 'No goods selected',
+    // selectedGood: 'Jam',
+  };
+
+  previousSelectedButton: HTMLElement | null
+  = document.querySelector('.buttonSelected');
+
+  previousSelectedGood: HTMLElement | null
+  = document.querySelector('.selected');
+
+  render() {
+    return (
+      <div className="App">
+        {goodsFromServer.map(good => (
+          <>
+            <div
+              className="goodContainer"
+              key={good}
+            >
+              <div className="goodName">
+                {good}
+              </div>
+              <button
+                type="button"
+                onClick={(event) => {
+                  this.setState({
+                    // selectedGood: good,
+                    initialHeader: `Selected good is: - ${good}`,
+                  });
+                  const selectedButton = event.target as HTMLElement;
+                  const selectedElement
+                  = selectedButton.previousElementSibling as HTMLElement;
+
+                  const clearSelection
+                    = document.querySelector('.deleteSelection');
+
+                  if (clearSelection) {
+                    clearSelection.className = 'deleteSelection';
+                  }
+
+                  if (selectedElement) {
+                    selectedElement.className = 'goodName selected';
+                  }
+
+                  selectedButton.className = 'buttonSelected hidden';
+                  if (this.previousSelectedButton) {
+                    this.previousSelectedButton.className = '';
+                  }
+
+                  if (this.previousSelectedGood) {
+                    this.previousSelectedGood.className = 'goodName';
+                  }
+
+                  this.previousSelectedButton = selectedButton;
+
+                  if (selectedElement) {
+                    this.previousSelectedGood = selectedElement;
+                  }
+                }}
+              >
+                Select
+              </button>
+            </div>
+          </>
+        ))}
+        <div className="headerContainer">
+          <h1>
+            {this.state.initialHeader}
+          </h1>
+          <button
+            type="button"
+            className="deleteSelection hidden"
+            onClick={() => {
+              this.setState({
+                initialHeader: 'Selected good is: - ',
+              });
+
+              const selectedButton = document.querySelector('.buttonSelected');
+              const goodSelected = document.querySelector('.selected');
+
+              if (selectedButton) {
+                selectedButton.className = '';
+              }
+
+              if (goodSelected) {
+                goodSelected.className = 'goodName';
+              }
+
+              const clearSelection = document.querySelector('.deleteSelection');
+
+              if (clearSelection) {
+                clearSelection.className = 'deleteSelection hidden';
+              }
+            }}
+          >
+            x
+          </button>
+        </div>
+        {goodsFromServer.length}
+      </div>
+    );
+  }
+}
 
 export default App;
