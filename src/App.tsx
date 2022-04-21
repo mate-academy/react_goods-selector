1515c93166d7882f85861b12b7827aec7e803709
Roma <roma@MacBook-Pro-Roman.local>
2022-04-21 10:54:10 +0300

    add classnames library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 class App extends React.Component {
@@ -30,13 +31,6 @@ class App extends React.Component {
             <button
               type="button"
               onClick={() => {
-                const elements = document.querySelectorAll('li');
-
-                // eslint-disable-next-line no-plusplus
-                for (let i = 0; i < elements.length; i++) {
-                  elements[i].classList.remove('selected');
-                }
-
                 this.setState({ selectedGood: '' });
               }}
             >
@@ -47,24 +41,18 @@ class App extends React.Component {
         {goodsFromServer.length}
         <ul>
           {goodsFromServer.map(product => (
-            <li key={product} id={product}>
+            <li
+              key={product}
+              id={product}
+              className={classNames({ selected: selectedGood === product })}
+            >
               {product}
               { ' - ' }
               { selectedGood !== product && (
                 <button
                   type="button"
                   onClick={() => {
-                    const elements = document.querySelectorAll('li');
-
-                    // eslint-disable-next-line no-plusplus
-                    for (let i = 0; i < elements.length; i++) {
-                      elements[i].classList.remove('selected');
-                    }
-
                     this.setState({ selectedGood: product });
-                    const elem = document.getElementById(product);
-
-                    elem?.classList.add('selected');
                   }}
                 >
                   Select
