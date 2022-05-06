de613e1e8a9273ce632a59d3b31a85a5c8187493
Oleg Lementarev <lementarev@gmail.com>
2022-05-06 21:01:52 +0300

    goods selector created

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,103 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  addGoods = (item: string) => {
+    this.setState(({ selectedGoods }) => {
+      const addedGoods = (selectedGoods.includes(item))
+        ? selectedGoods.filter((el) => el !== item)
+        : [...selectedGoods, item];
+
+      return {
+        selectedGoods: addedGoods,
+      };
+    });
+  };
+
+  makeSelectedGoodsString = () => {
+    const { selectedGoods } = this.state;
+
+    return (selectedGoods.length > 2)
+      ? [
+        selectedGoods.slice(0, -1).join(', '),
+        selectedGoods.slice(-1),
+      ].join(' and ')
+      : selectedGoods.join(' and ');
+  };
+
+  clearSelectedGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="Title">
+          {
+            (selectedGoods.length === 0)
+              ? 'No goods'
+              : this.makeSelectedGoodsString()
+          }
+          &nbsp;selected
+
+          {(selectedGoods.length > 1) && (
+            <button
+              type="button"
+              className="ClearButton"
+              onClick={() => {
+                this.clearSelectedGoods();
+              }}
+            >
+              CLEAR
+            </button>
+          )}
+        </h1>
+
+        <ul className="GoodsList">
+          {goodsFromServer.map((item) => (
+            <li
+              className={`GoodsList__item ${
+                selectedGoods.includes(item)
+                  ? 'GoodsList__selectedItem'
+                  : ''
+              }`}
+              key={item}
+            >
+              <span className="GoodsList__itemName">
+                {item}
+              </span>
+
+              <button
+                type="button"
+                className={`GoodsList__button ${
+                  selectedGoods.includes(item)
+                    ? 'GoodsList__selectedButton'
+                    : ''
+                }`}
+                onClick={() => {
+                  this.addGoods(item);
+                }}
+              >
+                {
+                  (selectedGoods.includes(item))
+                    ? 'REMOVE'
+                    : 'SELECT'
+                }
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
