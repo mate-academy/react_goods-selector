c31445f73cfb101bb1783637e52438e0aff8cad1
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-05-08 11:36:41 +0200

    react_goods_s PR

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,114 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  selectedGoodsList: string[],
+}
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoodsList: ['Jam'],
+  };
+
+  selectGoods = (item: string) => {
+    this.setState((state: State) => {
+      state.selectedGoodsList.push(item);
+
+      return { selectedGoodsList: state.selectedGoodsList };
+    });
+  };
+
+  removeGood = (item: string) => {
+    this.setState((state: State) => {
+      state.selectedGoodsList.splice(state.selectedGoodsList.indexOf(item), 1);
+
+      return { selectedGoodsList: state.selectedGoodsList };
+    });
+  };
+
+  getGoodsString = (): string => {
+    const { selectedGoodsList } = this.state;
+    let resultString = '';
+
+    switch (true) {
+      case (selectedGoodsList.length === 0):
+        resultString = 'No goods selected. Choose one or more:';
+        break;
+      case (selectedGoodsList.length === 1):
+        resultString = `${selectedGoodsList[0]} is selected`;
+        break;
+      case (selectedGoodsList.length === 2):
+        resultString = `${selectedGoodsList.join(' and ')} are selected`;
+        break;
+      case (selectedGoodsList.length === 10):
+        resultString = `${[selectedGoodsList.slice(0, -1).join(', '),
+          selectedGoodsList.slice(-1),
+        ].join(' and ')} are selected. Nothing else to choose`;
+        break;
+      case (selectedGoodsList.length > 2):
+        resultString = `${[selectedGoodsList.slice(0, -1).join(', '),
+          selectedGoodsList.slice(-1),
+        ].join(' and ')} are selected`;
+        break;
+      default:
+        throw new Error('Ups... something wrong!');
+    }
+
+    return resultString;
+  };
+
+  clearSelectedGoodsList = () => {
+    this.setState({ selectedGoodsList: [] });
+  };
+
+  render() {
+    const { selectedGoodsList } = this.state;
+
+    return (
+      <div className="Page">
+        <div className="App">
+          <h1>{this.getGoodsString()}</h1>
+          {
+            (selectedGoodsList.length > 0) && (
+              <button
+                type="button"
+                className="App__button"
+                onClick={() => {
+                  this.clearSelectedGoodsList();
+                }}
+              >
+                X
+              </button>
+            )
+          }
+        </div>
+
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={`App__list-element ${
+                selectedGoodsList.includes(good)
+                  ? 'isActiv'
+                  : ''
+              }`}
+            >
+              {good}
+              <button
+                type="button"
+                className="Button"
+                onClick={
+                  selectedGoodsList.includes(good)
+                    ? () => this.removeGood(good)
+                    : () => this.selectGoods(good)
+                }
+              >
+                {selectedGoodsList.includes(good) ? '-' : '+'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
