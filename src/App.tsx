d1d2262b66bc305846c7b7da3fca66ee31f0eec7
Alexandr Nikytenko <nika200382@gmail.com>
2022-05-08 14:28:12 +0300

    bascet

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,106 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: ['Jam'],
+  };
+
+  addGood = (good: string) => {
+    this.setState(
+      (prevState) => ({ selectedGood: [...prevState.selectedGood, good] }),
+    );
+  };
+
+  removeGood = (good: string) => {
+    this.setState(
+      (prevState) => (
+        { selectedGood: prevState.selectedGood.filter(elem => elem !== good) }
+      ),
+    );
+  };
+
+  clearGood = () => {
+    this.setState({ selectedGood: [] });
+  };
+
+  createTitle = () => {
+    const { selectedGood } = this.state;
+
+    switch (selectedGood.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `In bascet ${selectedGood[0]} is selected`;
+
+      case 2:
+        return `In bascet ${selectedGood.join(' and ')} are selected`;
+
+      default:
+        return `In bascet ${selectedGood.slice(0, -1).join(', ')} and ${selectedGood[selectedGood.length - 1]} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="products-title">
+          <div className="products-title__bascet">
+            {this.createTitle()}
+          </div>
+          <button
+            className="products-title__button"
+            type="button"
+            disabled={selectedGood.length === 0}
+            onClick={this.clearGood}
+          >
+            Clear basket
+          </button>
+        </h1>
+
+        <ul className="goods-list">
+          {goodsFromServer.map(good => (
+            <>
+              <li key={good} className="goods-list__good">
+                <button
+                  className={`goods-list__button ${
+                    selectedGood.includes(good)
+                      ? 'goods-list__button-selected'
+                      : ''
+                  }`}
+                  type="button"
+                  onClick={() => {
+                    if (selectedGood.includes(good)) {
+                      return this.removeGood(good);
+                    }
+
+                    return this.addGood(good);
+                  }}
+                >
+                  {selectedGood.includes(good) ? 'Remove' : 'Select'}
+                </button>
+                <div className={`goods-list__good ${
+                  selectedGood.includes(good)
+                    ? 'goods-list__good-selected'
+                    : ''
+                }`}
+                >
+                  {good}
+                </div>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
