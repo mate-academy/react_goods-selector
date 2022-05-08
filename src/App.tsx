fb87d9f922229d0155a30aa9338f8455ebd8b6b8
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-08 16:54:53 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,113 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  selectedGoods: string[],
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  selectedGood = this.state.selectedGoods[0];
+
+  componentDidMount() {
+    const highlighted = document.getElementById(this.selectedGood);
+
+    highlighted?.classList.add('goods__item--selected');
+  }
+
+  componentDidUpdate(_prevProps: {}, prevState: State) {
+    this.state.selectedGoods.forEach((good) => {
+      if (!prevState.selectedGoods.includes(good)) {
+        const elementToSetHighlight = document.getElementById(good);
+
+        elementToSetHighlight?.classList.add('goods__item--selected');
+      }
+    });
+
+    prevState.selectedGoods.forEach((good) => {
+      if (!this.state.selectedGoods.includes(good)) {
+        const elementToRemoveHighlight = document.getElementById(good);
+
+        elementToRemoveHighlight?.classList.remove('goods__item--selected');
+      }
+    });
+  }
+
+  selectHandler = (good: string) => {
+    this.setState((prevState) => {
+      if (prevState.selectedGoods.includes(good)) {
+        const newGoods = prevState.selectedGoods
+          .filter((item) => item !== good);
+
+        return {
+          selectedGoods: newGoods,
+        };
+      }
+
+      const newGoods = [...prevState.selectedGoods, good];
+
+      return {
+        selectedGoods: newGoods,
+      };
+    });
+  };
+
+  render() {
+    const {
+      selectedGoods,
+    } = this.state;
+
+    let title = '';
+
+    switch (selectedGoods.length) {
+      case 0:
+        title = 'No goods selected';
+        break;
+
+      case 1:
+        title = `${selectedGoods[0]} is selected`;
+        break;
+
+      default: {
+        const strPart1 = selectedGoods.slice(0, -1).join(',\n');
+        const strPart2 = selectedGoods.slice(-1);
+
+        title = `${strPart1} and ${strPart2} are selected`;
+      }
+    }
+
+    return (
+      <div className="app">
+        <h1 className="app__title">
+          {title}
+        </h1>
+        <ul className="goods">
+          {
+            goodsFromServer.map((good) => (
+              <li key={good} id={good} className="goods__item">
+                <span>{good}</span>
+                {'\n'}
+                <button
+                  type="button"
+                  className="goods__button"
+                  onClick={() => this.selectHandler(good)}
+                >
+                  {
+                    selectedGoods.includes(good)
+                      ? 'Remove'
+                      : 'Select'
+                  }
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
