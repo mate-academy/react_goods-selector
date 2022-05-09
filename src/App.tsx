9d67ddaacd9a1960a2b3295ad64ed9318c16484b
Dima Kotovich <kotovih950@gmail.com>
2022-05-10 02:14:54 +0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,113 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  switchGood = (goodName: string): void => {
+    this.setState(({ selectedGoods }) => {
+      const newGoods = (selectedGoods.includes(goodName))
+        ? selectedGoods.filter((good) => good !== goodName)
+        : [...selectedGoods, goodName];
+
+      return {
+        selectedGoods: newGoods,
+      };
+    });
+  };
+
+  clearGoods = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  getGoods = (): string => {
+    const { selectedGoods } = this.state;
+
+    return (selectedGoods.length > 2)
+      ? [
+        selectedGoods.slice(0, -1).join(', '),
+        selectedGoods.slice(-1),
+      ].join('and')
+      : selectedGoods.join(' and ');
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="app__title">
+          {
+            (selectedGoods.length === 0)
+              ? 'No goods'
+              : this.getGoods()
+          }
+
+          {
+            (selectedGoods.length > 0) && (
+              <>
+                <button
+                  className="app__button"
+                  type="button"
+                  onClick={() => {
+                    this.clearGoods();
+                  }}
+                >
+                  Clear
+                </button>
+              </>
+            )
+          }
+        </h1>
+        <ul className="goodsList">
+          {goodsFromServer.map(good => (
+            <li
+              className={`goodsList__item ${
+                selectedGoods.includes(good)
+                  ? 'goodsList__selected'
+                  : ''
+              }`}
+              key={good}
+            >
+              <span className="goodsList__goods">
+                {good}
+              </span>
+              <button
+                type="button"
+                className={`goodsList__button ${
+                  selectedGoods.includes(good)
+                    ? 'goodsList__selectedButton'
+                    : ''
+                }`}
+                onClick={() => {
+                  this.switchGood(good);
+                }}
+              >
+                {
+                  selectedGoods.includes(good)
+                    ? 'Remove'
+                    : 'Select'
+                }
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+// const App: React.FC = () => (
+//   <div className="App">
+//     <h1>Selected good: -</h1>
+//     {goodsFromServer.length}
+//   </div>
+// );
 
 export default App;
