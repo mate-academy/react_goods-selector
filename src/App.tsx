a80d14c1d5346c1fd4b9ff85ad5bf9c9ab0200d5
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2022-05-21 23:50:19 +0300

    solution v.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import className from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,115 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoodslist: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state:State = {
+    selectedGoodslist: [],
+  };
+
+  componentDidMount() {
+    this.setState({ selectedGoodslist: ['Jam'] });
+  }
+
+  addSelectedGood = (good: string) => {
+    this.setState(({ selectedGoodslist }) => (
+      { selectedGoodslist: [...selectedGoodslist, good] }
+    ));
+  };
+
+  removeSelectedGood = (good: string) => {
+    this.setState(({ selectedGoodslist }) => (
+      { selectedGoodslist: selectedGoodslist.filter(item => item !== good) }
+    ));
+  };
+
+  showSelectedGood = () => {
+    const { selectedGoodslist } = this.state;
+
+    if (!selectedGoodslist.length) {
+      return 'No goods selected';
+    }
+
+    if (selectedGoodslist.length === 1) {
+      return selectedGoodslist[0];
+    }
+
+    return selectedGoodslist.map((good, index) => {
+      if (index === selectedGoodslist.length - 1) {
+        return good;
+      }
+
+      if (index === selectedGoodslist.length - 2) {
+        return `${good} and `;
+      }
+
+      return `${good}, `;
+    }).join('');
+  };
+
+  buttonAddRemoveHandler = (isSelected: boolean, good: string) => {
+    return isSelected
+      ? this.removeSelectedGood(good)
+      : this.addSelectedGood(good);
+  };
+
+  buttonClearHandler = () => {
+    this.setState({ selectedGoodslist: [] });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <section className="title">
+          <h1 className="title__h1">
+            Selected good:
+          </h1>
+          <p className="title__selected-goods">{this.showSelectedGood()}</p>
+          {this.state.selectedGoodslist.length
+            ? (
+              <button
+                type="button"
+                className="button button__clear"
+                onClick={() => this.buttonClearHandler()}
+              >
+                Clear
+              </button>
+            )
+            : null}
+        </section>
+        <ul className="goods-list">
+          {goodsFromServer.map(good => {
+            const isSelected = this.state.selectedGoodslist.includes(good);
+
+            const buttonText = isSelected ? 'Remove' : 'Select';
+
+            return (
+              <li
+                className={
+                  className('goods-list__item', {
+                    'goods-list__item--selected': isSelected,
+                  })
+                }
+                key={good}
+              >
+                {good}
+                <button
+                  type="button"
+                  className="button"
+                  onClick={() => this.buttonAddRemoveHandler(isSelected, good)}
+                >
+                  {buttonText}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
