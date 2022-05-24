2267063850576d0f9f30eda4771c4572bdadbb91
Andriy Fesych <andriyhimself@gmail.com>
2022-05-24 22:22:55 +0300

    Save_1

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,81 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type State = {
+  selectedGoods : string[],
+};
+
+class App extends React.Component <Props, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  displayGoods = (goods: string[]) => {
+    const res = goods.join(', ').split('');
+
+    res[res.lastIndexOf(',')] = ' and';
+
+    return res.join('');
+  };
+
+  basketTransfer = (item: string) => {
+    const basket = this.state.selectedGoods;
+
+    if (basket.includes(item)) {
+      // eslint-disable-next-line no-console
+      console.log(`${item} removed`);
+
+      this.setState((prevState) => ({
+        selectedGoods:
+          prevState.selectedGoods.filter((good) => (good !== item)),
+      }));
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`${item} added to basket`);
+
+      this.setState((prevState) => ({
+        selectedGoods: [...prevState.selectedGoods, item],
+      }));
+    }
+  };
+
+  displayItem = (item: string) => {
+    return (
+      <div key={item}>
+        <br />
+        {item}
+        &#160;&#160;
+        <button
+          type="button"
+          onClick={() => (this.basketTransfer(item))}
+        >
+          add/remove
+        </button>
+      </div>
+    );
+  };
+
+  render() {
+    const goods = this.state.selectedGoods;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good
+          {(goods.length > 1) ? 's' : null}
+          :&#160;
+          {(goods.length > 1)
+            ? this.displayGoods(goods)
+            : goods}
+        </h1>
+        {goodsFromServer.map((good) => (
+          this.displayItem(good)
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
