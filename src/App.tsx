9afea438081f3c57568bcd1877818f36b92a0363
Roman Pavlenko <proman9973@gmail.com>
2022-05-31 19:29:26 +0300

    solution of main task

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,77 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {(this.state.selectedGood)
+            ? `${this.state.selectedGood} is selected`
+            : 'No goods selected'}
+        </h1>
+        <div className="goods">
+          <ul className="goods__list">
+            {goodsFromServer.map(el => (
+              <li
+                className="goods__list-item"
+              >
+                {(el === this.state.selectedGood)
+                  ? (
+                    <>
+                      <p className="goods__list-item-text-selected">
+                        {el}
+                      </p>
+                      <button
+                        className="goods__list-button"
+                        type="button"
+                        onClick={() => {
+                          this.setState({ selectedGood: false });
+                        }}
+                      >
+                        remove
+                      </button>
+                    </>
+                  )
+                  : (
+                    <>
+                      <p className="goods__list-item-text">
+                        {el}
+                      </p>
+                      <button
+                        className="goods__list-button"
+                        type="button"
+                        onClick={() => {
+                          this.setState({ selectedGood: el });
+                        }}
+                      >
+                        choose me
+                      </button>
+                    </>
+                  )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        {((this.state.selectedGood) || (this.state.selectedGood !== '')
+          ? (
+            <button
+              type="button"
+              onClick={() => {
+                this.setState({ selectedGood: '' });
+              }}
+            >
+              CLEAR
+            </button>
+          )
+          : '')}
+      </div>
+    );
+  }
+}
 
 export default App;
