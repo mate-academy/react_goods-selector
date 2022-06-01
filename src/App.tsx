6e9140697ce94da083d1793ed575d8889ca1daf9
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-06-01 14:49:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,66 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGoods: goodsFromServer[8],
+  };
+
+  getSelectedRemove(good: string) {
+    if (this.state.selectedGoods !== good) {
+      this.setState({ selectedGoods: good });
+    } else {
+      this.setState({ selectedGoods: '' });
+    }
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App m-4">
+        <h1 className="title is-3">
+          {selectedGoods
+            ? `${selectedGoods} is selected`
+            : 'No goods selected'}
+        </h1>
+        <button
+          type="button"
+          className="button is-black is-outlined"
+          onClick={() => this.setState({ selectedGoods: '' })}
+          disabled={!selectedGoods}
+        >
+          Clear
+        </button>
+        <ul>
+          {goodsFromServer.map(good => (
+            <>
+              <li
+                className={selectedGoods === good
+                  ? 'has-background-warning-light'
+                  : ''}
+              >
+                {good}
+              </li>
+              <button
+                type="button"
+                onClick={() => this.getSelectedRemove(good)}
+                className="button is-warning is-rounded is-small"
+              >
+                {selectedGoods === good
+                  ? 'Remove'
+                  : 'Selected'}
+              </button>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
