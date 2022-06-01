0d7c0f05cb7e9d9d50606189b96c42ea3c4025b7
Alexandra Protyanova <protyanova22@gmail.com>
2022-06-01 19:46:17 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,86 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: [goodsFromServer[8]],
+  };
+
+  selectGood = (good: string) => {
+    this.state.selectedGood.includes(good)
+      ? this.setState((state) => ({
+        selectedGood: state.selectedGood.filter(foddie => foddie !== good),
+      }))
+      : this.setState((state) => ({
+        selectedGood: [...state.selectedGood, good],
+      }));
+  };
+
+  switchCase = (param: string[] | []) => {
+    switch (param.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `${param[0]} is selected`;
+
+      default:
+        return `${param.slice(0, -1).join(', ')} and ${param[param.length - 1]} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          {this.switchCase(selectedGood)}
+        </h1>
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+
+            <div className="App__item" key={good}>
+              <li>
+                {good}
+              </li>
+              <button
+                type="button"
+                className={
+                  selectedGood.includes(good)
+                    ? 'button is-danger is-primary is-outlined'
+                    : 'button is-link is-outlined'
+                }
+                onClick={() => {
+                  this.selectGood(good);
+                }}
+              >
+                {selectedGood.includes(good) ? 'Remove' : 'Select'}
+              </button>
+            </div>
+
+          ))}
+        </ul>
+
+        {selectedGood.length !== 0
+          && (
+            <button
+              type="button"
+              className="button is-link is-outlined button-big"
+              onClick={() => {
+                this.setState({ selectedGood: [] });
+              }}
+            >
+              Clear
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
