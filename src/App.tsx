4878b67655d0d03e3d5ed1e4d2395f431e19ff86
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-06-02 19:22:24 +0200

    made goods selector v.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.scss';
+// import 'bulma/css/bulma.min.css';
 
-const goodsFromServer: string[] = [
+const goodsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,11 +15,75 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string;
+};
 
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  remover = (good: string) => {
+    if (this.state.selectedGood === good) {
+      return 'Romove';
+    }
+
+    return 'Select';
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div className="content">
+        <h1 className="content__title">
+          {(this.state.selectedGood)
+            ? `Selected goods: ${this.state.selectedGood}`
+            : 'No goods selected'}
+        </h1>
+        {(this.state.selectedGood)
+          ? (
+            <button
+              type="button"
+              onClick={() => {
+                this.setState({ selectedGood: '' });
+              }}
+              className="button button__remove"
+            >
+              Clear
+            </button>
+          )
+          : ''}
+        <ul className="">
+          {goodsFromServer.map(good => (
+            <React.Fragment key={good}>
+              <div className="content__item">
+                <li
+                  className={(good === this.state.selectedGood)
+                    ? 'isSelected'
+                    : ''}
+                >
+                  {good}
+                </li>
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.setState((state) => ({
+                      selectedGood: state.selectedGood === good ? '' : good,
+                    }));
+                  }}
+                  className={this.state.selectedGood === good
+                    ? 'button button__remove'
+                    : 'button'}
+                >
+                  {this.remover(good)}
+                </button>
+              </div>
+            </React.Fragment>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 export default App;
