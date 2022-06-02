0343bc32e53cb48e8846d9e0edd7ba3040c52a1d
Maryna <savna023@gmail.com>
2022-06-02 11:13:21 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,95 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGood: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: ['Jam'],
+  };
+
+  handleClick = (good: string) => {
+    if (this.state.selectedGood.includes(good)) {
+      this.setState({
+        // eslint-disable-next-line react/no-access-state-in-setstate
+        selectedGood: this.state.selectedGood
+          .filter(el => el !== good),
+      });
+    } else {
+      this.setState({
+        // eslint-disable-next-line react/no-access-state-in-setstate
+        selectedGood: [...this.state.selectedGood, good],
+      });
+      // eslint-disable-next-line no-console
+      console.log(this.state.selectedGood);
+    }
+  };
+
+  clearGoods = () => {
+    this.setState({
+      selectedGood: [],
+    });
+  };
+
+  checkGoods = (goods: string[]) => {
+    switch (goods.length) {
+      case 0:
+        return 'No selected good';
+      case 1:
+        return `${goods[0]} is selected`;
+      case 2:
+        return `${goods.join(' and ')} are selected`;
+      default:
+        return `${goods.slice(0, -1).join(', ')} and ${goods[goods.length - 1]} are selected`;
+    }
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="subtitle is-1">
+          Selected good:
+          <p className="subtitle is-2">{this.checkGoods(selectedGood)}</p>
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={this.clearGoods}
+          >
+            Clear
+          </button>
+        </h1>
+        <ul>
+          {goodsFromServer.map((good) => (
+            <li className="box subtitle is-4" key={good}>
+              <button
+                type="button"
+                className={classNames(
+                  'button',
+                  { 'is-warning is-light': selectedGood.includes(good) },
+                )}
+                onClick={() => {
+                  this.handleClick(good);
+                }}
+              >
+                {selectedGood.includes(good) ? 'Remove' : 'Select'}
+              </button>
+              <span className={classNames(
+                'active',
+                { active__selected: selectedGood.includes(good) },
+              )}
+              >
+                {good}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
