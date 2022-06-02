c6553f5983fedabbd282793bb39df5264a4fc2d1
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-06-02 23:55:40 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,85 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+interface State {
+  selectedGoods: string[];
+}
+
+class App extends React.Component<Props, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  showMessage = (listOfGoods: string[]):string => {
+    if (listOfGoods.length === 0) {
+      return 'No goods selected';
+    }
+
+    if (listOfGoods.length === 1) {
+      return `${listOfGoods[0]} is selected`;
+    }
+
+    const lastIndex = listOfGoods.length - 1;
+
+    return `${listOfGoods.slice(0, lastIndex).join(', ')} and ${listOfGoods[lastIndex]} are selected`;
+  };
+
+  stateToggler = (product: string) => {
+    const stateGoodsCopy = this.state.selectedGoods.slice();
+
+    if (stateGoodsCopy.includes(product)) {
+      const index = stateGoodsCopy.indexOf(product);
+
+      stateGoodsCopy.splice(index, 1);
+    } else {
+      stateGoodsCopy.push(product);
+    }
+
+    this.setState({ selectedGoods: stateGoodsCopy });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="content">
+        <h1>{this.showMessage(selectedGoods)}</h1>
+        {selectedGoods.length > 0 && (
+          <button
+            className="button is-danger"
+            type="button"
+            onClick={() => this.setState({ selectedGoods: [] })}
+          >
+            Clear
+          </button>
+        )}
+        <ul className="mt-4">
+          {goodsFromServer.map(product => (
+            <li
+              className="is-flex is-justify-content-space-between"
+              key={product}
+            >
+              <p className={
+                cn({ 'has-background-info': selectedGoods.includes(product) })
+              }
+              >
+                {product}
+              </p>
+              <button
+                type="button"
+                className="button is-success is-small"
+                onClick={() => this.stateToggler(product)}
+              >
+                {selectedGoods.includes(product) ? 'Remove' : 'Select'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
