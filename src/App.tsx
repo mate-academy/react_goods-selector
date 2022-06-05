286a4dc641d0f51420983fdfe8e3eb88a25c3691
Ihor Yeremenko <igoree86@gmail.com>
2022-06-05 23:00:36 +0300

    Without_Optional

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import './App.scss';
 
@@ -14,11 +15,79 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedItem: string;
+};
 
+type Props = {
+
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    selectedItem: 'Jam',
+  };
+
+  render() {
+    const { selectedItem } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected good:
+          {selectedItem ? (` ${selectedItem} is selected `) : (' No goods selected')}
+
+        </h1>
+        {selectedItem && (
+          <button
+            type="button"
+            onClick={() => this.setState({ selectedItem: '' })}
+          >
+            Clear
+          </button>
+        )}
+        <table>
+          <tr>
+            <th>Goods</th>
+            <th>Add goods</th>
+          </tr>
+
+          {goodsFromServer.map(element => {
+            const selectedElem = selectedItem === element;
+
+            return (
+              <tr key={element}>
+                <td className={selectedElem
+                  ? 'app__listItem--selected'
+                  : 'app__listItem'}
+                >
+                  {element}
+                </td>
+
+                <td>
+                  {!selectedElem ? (
+                    <button
+                      type="button"
+                      onClick={() => this.setState({ selectedItem: element })}
+                    >
+                      select
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      onClick={() => this.setState({ selectedItem: '' })}
+                    >
+                      remove
+                    </button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+
+      </div>
+    );
+  }
+}
 export default App;
