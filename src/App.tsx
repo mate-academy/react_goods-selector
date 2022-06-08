94d22d64696d8ba190824f52d8b92f91ed9b61b2
ShapovalDenys <denys.igorevich@gmail.com>
2022-06-08 13:47:46 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,84 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+    isClicked: false,
+  };
+
+  onSelected = (item: string) => {
+    this.setState({
+      isClicked: true,
+      selectedGood: item,
+    });
+  };
+
+  removeSelected = () => {
+    this.setState({
+      isClicked: false,
+      selectedGood: 'No goods selected',
+    });
+  };
+
+  render() {
+    const { selectedGood, isClicked } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          {!isClicked
+            ? 'No goods selected'
+            : (
+              <>
+                {selectedGood}
+                {' '}
+                is selected
+                <button
+                  type="button"
+                  className="list__button"
+                  onClick={() => this.removeSelected()}
+                >
+                  Clear
+                </button>
+              </>
+            )}
+        </h1>
+        <ul>
+          {goodsFromServer.map(item => (
+            <li
+              key={item}
+              className={
+                classNames('list__item',
+                  { list__active: item === selectedGood && isClicked })
+              }
+            >
+              {item}
+              {item === selectedGood && isClicked
+                ? (
+                  <button
+                    type="button"
+                    className="list__button"
+                    onClick={() => this.removeSelected()}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="list__button"
+                    onClick={() => this.onSelected(item)}
+                  >
+                    Select
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
