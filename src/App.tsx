cece9ec77ce2e32ac216708d95051c30e194b187
Lebogang Mogane <mogane420@gmail.com>
2022-06-08 08:25:05 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,91 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  goodItem: string;
+  isGoodSelected: boolean;
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    goodItem: 'No goods selected',
+    isGoodSelected: false,
+  };
+
+  render() {
+    const { goodItem, isGoodSelected } = this.state;
+
+    const h1Content = isGoodSelected
+      ? `${goodItem} is selected`
+      : `Selected good: - ${goodItem}`;
+
+    let id = 0;
+
+    return (
+      <div className="App">
+        <h1>
+          {goodItem && h1Content}
+
+          <br />
+
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({
+                goodItem: '',
+                isGoodSelected: false,
+              });
+            }}
+          >
+            Clear
+          </button>
+        </h1>
+        {goodsFromServer.length}
+
+        <ul className="list_of_goods">
+          {goodsFromServer.map(item => {
+            id += 1;
+
+            return (
+              <li
+                className={goodItem === item ? 'good is-active' : 'good'}
+                key={id}
+              >
+                {item}
+                {goodItem === item
+                  ? (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        this.setState({
+                          goodItem: 'No goods selected',
+                          isGoodSelected: false,
+                        });
+                      }}
+                    >
+                      Remove
+                    </button>
+                  )
+                  : (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        this.setState({
+                          goodItem: item,
+                          isGoodSelected: true,
+                        });
+                      }}
+                    >
+                      Select
+                    </button>
+                  )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
