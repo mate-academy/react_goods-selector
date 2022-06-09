cff3810e5fb7e475ce2da6bf25a08eb8e8844f48
Dima <d.maik2178@gmail.com>
2022-06-09 22:15:10 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,78 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    select: ['Jam'],
+  };
+
+  selected(good: string) {
+    this.setState({
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      select: [...this.state.select, good],
+    });
+  }
+
+  remove(good: string) {
+    this.setState({
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      select: this.state.select.filter((el) => {
+        return el !== good;
+      }),
+    });
+  }
+
+  render() {
+    const { select } = this.state;
+
+    return (
+      <div className="App">
+        <div className="Goods-container">
+          <div className="Goods-container__selected Selected">
+            <h1 className="Selected__header">
+              {select.length ? 'Selected good' : 'No goods selected'}
+            </h1>
+            <h2 className="Selected__goods">{select.join(', ')}</h2>
+          </div>
+          <ul className="Goods-container__list list">
+            {goodsFromServer.map((good) => {
+              return (
+                <li
+                  className={select.includes(good)
+                    ? 'Active list__item' : 'list__item'}
+                  key={good}
+                >
+                  <h2>{good}</h2>
+                  <button
+                    className="Button"
+                    type="button"
+                    onClick={() => {
+                      if (select.includes(good)) {
+                        this.remove(good);
+                      } else {
+                        this.selected(good);
+                      }
+                    }}
+                  >
+                    {select.includes(good) ? 'Remove' : 'Select'}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          <button
+            className={select.length ? 'Button' : 'Button none'}
+            type="button"
+            onClick={() => {
+              this.setState({ select: [] });
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
