a44e6ae69f70b6c05bf30f868890c51285c7d1dc
Lilia Mazur <liliamazur2508@gmail.com>
2022-06-14 16:03:00 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,87 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  items: string[],
+}
+
+class App extends React.Component<{}, State> {
+  state:State = {
+    items: ['Jam'],
+  };
+
+  selectItem = (good: string) => {
+    this.setState((prevState) => ({ items: [...prevState.items, good] }));
+  };
+
+  removeItem = (good : string) => {
+    this.setState((prevState) => ({
+      items: prevState.items.filter(remaining => remaining !== good),
+    }));
+  };
+
+  buttonSelection = (isSelected: boolean, good: string) => {
+    return isSelected
+      ? this.removeItem(good)
+      : this.selectItem(good);
+  };
+
+  createMessage = () => {
+    const { items } = this.state;
+
+    switch (items.length) {
+      case 0:
+        return 'No goods selected!';
+      case 1:
+        return `${items[0]} is selected`;
+      case 2:
+        return `${items[0]} and ${items[1]} are selected`;
+      default:
+        return `${items.slice(0, -1).join(', ')} and ${items.slice(-1)} are selected`;
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">
+          {this.createMessage()}
+        </h1>
+
+        <ul className="list">
+          {goodsFromServer.map((good) => {
+            const isSelected = this.state.items.includes(good);
+            const textButton = isSelected ? 'Remove' : 'Select';
+
+            return (
+              <li key={good} className={`list__item ${isSelected ? 'selected' : ''}`}>
+                {good}
+                <button
+                  type="button"
+                  onClick={() => this.buttonSelection(isSelected, good)}
+                  className={`button ${isSelected ? 'button-remove' : 'button-selected'}`}
+                >
+                  {textButton}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+
+        {!!this.state.items.length && (
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ items: [] });
+            }}
+            className="button button-clear"
+          >
+            Clear goods!
+          </button>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
