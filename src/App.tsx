9993e7da48bf777f0fe7c0bb8d43b3ca54079f9e
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-07-02 16:59:24 +0300

    List of goods added

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,96 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  selecedGoods: string[],
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selecedGoods: ['Jam'],
+  };
+
+  selectGood = (good: string) => {
+    this.setState((prevState) => (
+      { selecedGoods: [...prevState.selecedGoods, good] }
+    ));
+  };
+
+  removeGood = (good: string) => {
+    this.setState((prevState) => (
+      {
+        selecedGoods: prevState.selecedGoods
+          .filter(remaining => remaining !== good),
+      }
+    ));
+  };
+
+  buttonAction = (isSelected: boolean, good: string) => {
+    return isSelected ? this.removeGood(good) : this.selectGood(good);
+  };
+
+  createMessage = () => {
+    const { selecedGoods } = this.state;
+
+    switch (selecedGoods.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `${selecedGoods[0]} is selected`;
+
+      case 2:
+        return `${selecedGoods[0]} and ${selecedGoods[1]} are selected`;
+
+      default:
+        return `${selecedGoods.slice(0, -1).join(', ')} and ${selecedGoods.slice(-1).join('')} are selected`;
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>
+          {this.createMessage()}
+        </h1>
+
+        <ul className="list-group">
+          {goodsFromServer.map(good => {
+            const isSelected = this.state.selecedGoods.includes(good);
+            const buttonText = isSelected ? 'Remove' : 'Select';
+
+            return (
+              <li key={good} className={`list-group-item ${isSelected ? 'selectedGood' : ''}`}>
+                <span>
+                  {good}
+                </span>
+
+                <button
+                  type="button"
+                  onClick={() => this.buttonAction(isSelected, good)}
+                  className={`${isSelected ? 'btn btn-danger' : 'btn btn-success'}`}
+                >
+                  {buttonText}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+
+        {!!this.state.selecedGoods.length && (
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ selecedGoods: [] });
+            }}
+            className="btn btn-secondary"
+          >
+            Clear selected goods
+          </button>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
