a5bc69999780e7e3c3b3c4f10da4435c2b33f749
Alina Komashchuk <akomashchuk24@gmail.com>
2022-07-11 22:34:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,87 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+// const App: React.FC = () => (
+//   <div className="App">
+//     <h1>Selected good: No goods selected</h1>
+//     {goodsFromServer.length}
+//     <ul>
+//       {goodsFromServer.map(good => (
+//         <li>
+//           {good}
+//           {' '}
+//           <button
+//             type="button"
+//             // onClick={}
+//           >
+//             Select
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
 
-export default App;
+// export default App;
+
+type State = {
+  selectedGood: string,
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  onClick = (good: string) => {
+    if (this.state.selectedGood !== good) {
+      this.setState({ selectedGood: good });
+    } else {
+      this.setState({ selectedGood: '' });
+    }
+  };
+
+  getTitle = () => {
+    const { selectedGood } = this.state;
+    const selectedTitle = `${selectedGood} is selected`;
+    const emptyTitle = 'No goods selected';
+
+    return selectedGood ? selectedTitle : emptyTitle;
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="goods">
+          {`Selected good: ${this.getTitle()}`}
+          {' '}
+          {selectedGood && (
+            <button
+              type="button"
+              onClick={() => this.setState({ selectedGood: '' })}
+            >
+              Clear
+            </button>
+          )}
+        </h1>
+        {goodsFromServer.length}
+        <ul className="goods__list">
+          {goodsFromServer.map(good => (
+            <li className={`goods__item ${good === selectedGood ? 'goods__item--selected' : ''}`}>
+              {good}
+              {' '}
+              <button
+                type="button"
+                onClick={() => this.onClick(good)}
+              >
+                {good === selectedGood ? 'Remove' : 'Select'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
