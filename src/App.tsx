580c7712192afc848f11192018f83e9cb1638ae9
Olya Yevsieienko <olya.evsnko@gmail.com>
2022-07-12 10:53:21 +0300

    Created Good Selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Component } from 'react';
+import icon from './icon.svg';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,102 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[],
+};
+
+class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGoods: ['Jam'],
+  };
+
+  getCorrectTitle() {
+    if (!this.state.selectedGoods.length) {
+      return 'No goods selected';
+    }
+
+    if (this.state.selectedGoods.length === 1) {
+      return `${this.state.selectedGoods} is selected`;
+    }
+
+    return `${this.state.selectedGoods.slice(0, -1).join(', ')} and ${this.state.selectedGoods.slice(-1)} are selected`;
+  }
+
+  addedGoods = (good: string) => {
+    this.setState((prevState) => ({
+      selectedGoods: [...prevState.selectedGoods, good],
+    }));
+  };
+
+  removedGoods = (good: string) => {
+    this.setState((prevState) => ({
+      selectedGoods: prevState.selectedGoods
+        .filter(prevGood => prevGood !== good),
+    }));
+  };
+
+  clearedBin = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__container">
+          <div className="App__head">
+            <img className="App__icon" src={icon} alt="" />
+            <h1 className="App__title">
+              {this.getCorrectTitle()}
+            </h1>
+
+            <button
+              className="button is-white"
+              type="button"
+              onClick={this.clearedBin}
+            >
+              Clear
+            </button>
+          </div>
+
+          <ul className="App__list">
+            {goodsFromServer.map(good => (
+              <div>
+                <li className="App__item box" key={good}>
+                  <p>{good}</p>
+
+                  {!selectedGoods.includes(good)
+                    ? (
+                      <button
+                        className="button is-small is-success is-light"
+                        type="button"
+                        onClick={() => {
+                          this.addedGoods(good);
+                        }}
+                      >
+                        Select
+                      </button>
+                    )
+                    : (
+                      <button
+                        className="button is-small is-danger is-light"
+                        type="button"
+                        onClick={() => {
+                          this.removedGoods(good);
+                        }}
+                      >
+                        Remove
+                      </button>
+                    )}
+                </li>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
