91d364d2611b1fa2139ff597c11db76a616f51d3
Khrystyna Sabat <satttinka@gmail.com>
2022-07-13 22:03:29 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,93 @@
+import classNames from 'classnames';
 import React from 'react';
 import './App.scss';
+import goods from './goods';
 
-// import goodsFromServer from './goods';
-
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGood: string,
 };
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    selectedGood: 'Jam',
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            {selectedGood}
+            {' '}
+            is selected
+          </h1>
+
+          {selectedGood !== 'Nothing' && (
+            <button
+              type="button"
+              onClick={() => {
+                this.setState({
+                  selectedGood: 'Nothing',
+                });
+              }}
+              className="App__clear"
+            >
+              Clear
+            </button>
+          )}
+        </header>
+
+        <table>
+          <thead>
+            <caption> </caption>
+          </thead>
+          <tbody>
+            {goods.map(good => (
+              <tr key={good}>
+                <td>
+                  {good === selectedGood ? (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        this.setState({
+                          selectedGood: 'Nothing',
+                        });
+                      }}
+                      className="Good__remove"
+                    >
+                      Remove
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        this.setState({
+                          selectedGood: good,
+                        });
+                      }}
+                      className="Good__select"
+                    >
+                      Select
+                    </button>
+                  )}
+                </td>
+                <td
+                  className={classNames(
+                    'Good',
+                    {
+                      'Good--active': good === selectedGood,
+                    },
+                  )}
+                >
+                  {good}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
