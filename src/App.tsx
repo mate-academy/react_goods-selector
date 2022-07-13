0abbdb7be1dbd1f94548e6c835bd51487fe1c3a1
Julia Makarenko <julys.makarenko@gmail.com>
2022-07-13 19:20:48 +0300

    Add goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,98 @@
+/* eslint-disable no-console */
 import React from 'react';
 import './App.scss';
+import goodsFromServer from './goods';
 
-// import goodsFromServer from './goods';
+console.log(goodsFromServer);
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
+type State = {
+  selectedGood: string[],
+};
+
+function setTitle(selectedGoods: string[]): string {
+  if (!selectedGoods.length) {
+    return 'No goods selected';
+  }
+
+  if (selectedGoods.length === 1) {
+    return `${selectedGoods[0]} is selected`;
+  }
+
+  return `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods.slice(-1)} are selected`;
+}
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: ['Jam'],
+  };
+
+  addGood = (goods: string) => {
+    this.setState((state) => ({
+      selectedGood: [...state.selectedGood, goods],
+    }));
+  };
+
+  removeGood = (goods: string) => {
+    this.setState((state) => ({
+      selectedGood: state.selectedGood
+        .filter(selectGood => (selectGood !== goods)),
+    }));
+  };
+
+  clearGood = () => {
+    this.setState({ selectedGood: [] });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <div className="App">
         <h1 className="App__title">
-          Jam is selected
+          {setTitle(selectedGood)}
         </h1>
-
         <button
           type="button"
-          className="App__clear"
+          className={selectedGood.length
+            ? 'App__clear button is-warning is-focused'
+            : 'App__clear--hidden'}
+          onClick={() => {
+            this.clearGood();
+          }}
         >
           Clear
         </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
-};
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+            <li className="App__element" key={good}>
+              <p>{good}</p>
+              {!selectedGood.includes(good)
+                ? (
+                  <button
+                    type="button"
+                    className="App__element button is-success"
+                    onClick={() => {
+                      this.addGood(good);
+                    }}
+                  >
+                    Select
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="App__element button is-danger"
+                    onClick={() => {
+                      this.removeGood(good);
+                    }}
+                  >
+                    Remove
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
