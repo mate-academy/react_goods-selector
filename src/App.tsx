f85697fbef1582ac4222956c390e49eca3c90685
Andrew Bilous <bilous.a.work@gmail.com>
2022-07-13 23:08:09 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,80 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  selectedGoods: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGoods: ['Jam'],
+  };
+
+  reset = () => {
+    this.setState({ selectedGoods: [] });
+  };
+
+  clickHandler = (good: string) => {
+    const { selectedGoods: selectedGood } = this.state;
+
+    this.setState({
+      selectedGoods: selectedGood.includes(good)
+        ? selectedGood.filter(choosenGod => choosenGod !== good)
+        : [...selectedGood, good],
+    });
+  };
+
+  render() {
+    const { selectedGoods: selectedGood } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          {selectedGood.length
+            ? `${selectedGood.join(', ')} is selected`
+            : 'No goods selected'}
+        </h1>
+
+        <button
+          type="button"
+          className="App__button button"
+          onClick={this.reset}
+        >
+          Clear
+        </button>
+
+        <ul className="goods">
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={classNames(
+                'goods__good',
+                {
+                  'goods__good--selected': selectedGood
+                    .find(goods => goods === good),
+                },
+              )}
+            >
+              <p>{good}</p>
+
+              <button
+                type="button"
+                className={classNames(
+                  'goods__button',
+                  { 'goods__button--select': true },
+                )}
+                onClick={() => this.clickHandler(good)}
+              >
+                {selectedGood.find(goods => goods === good)
+                  ? 'Remove'
+                  : 'Select'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
