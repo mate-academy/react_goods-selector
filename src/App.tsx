2939f2546d89427da54729388eeea3f63f2efee6
Viacheslav Tsymbal <slava.tsymbal@ukr.net>
2022-07-13 12:30:48 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import classNames from 'classnames';
+import { Component } from 'react';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,84 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  status: string,
+};
+
+class App extends Component<{}, State> {
+  state = {
+    status: 'Jam',
+  };
+
+  render() {
+    const { status } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__header">
+          {status !== ''
+            ? (
+              <>
+                {status}
+                {' '}
+                is selected
+
+                <button
+                  className="App__header-button"
+                  type="button"
+                  onClick={() => {
+                    this.setState({ status: '' });
+                  }}
+                >
+                  Clear
+                </button>
+              </>
+            )
+            : 'No goods selected'}
+        </h1>
+
+        <ul className="App__list">
+          {goodsFromServer.map(good => (
+            <>
+              <li
+                className={classNames(
+                  'App__list-item',
+                  {
+                    'App__list-item--active': status === good,
+                  },
+                )}
+              >
+                {good}
+                {status !== good
+                  ? (
+                    <button
+                      className="App__list-button"
+                      type="button"
+                      onClick={() => {
+                        this.setState({ status: good });
+                      }}
+                    >
+                      Select
+                    </button>
+                  )
+                  : (
+                    <button
+                      className="App__list-button"
+                      type="button"
+                      onClick={() => {
+                        this.setState({ status: '' });
+                      }}
+                    >
+                      Remove
+                    </button>
+                  )}
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
