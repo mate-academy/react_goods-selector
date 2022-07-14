8c2c73c4d4e32b0655d3040bf241749f6909b898
Denys Politov <dpolitov2001@gmail.com>
2022-07-14 15:49:41 +0300

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,33 +14,76 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-class App extends React.Component {
+type State = {
+  selectedGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
   state = {
     selectedGoods: ['Jam'],
+  };
+
+  renderTitle = (goods: string[]) => {
+    switch (goods.length) {
+      case (0):
+        return 'No goods selected.';
+      case (1):
+        return (
+          <>
+            {`${goods} is selected.`}
+            <button
+              type="button"
+              className="App__clearButton"
+              onClick={() => this.removeAllGoods()}
+            >
+              Clear
+            </button>
+          </>
+        );
+      default:
+        return (
+          <>
+            {`${goods.slice(0, goods.length - 1).join(', ')} and ${goods[goods.length - 1]} are selected.`}
+            <button
+              type="button"
+              className="App__clearButton"
+              onClick={() => this.removeAllGoods()}
+            >
+              Clear
+            </button>
+          </>
+        );
+    }
+  };
+
+  selectGood = (good: string) => {
+    this.setState((state) => (
+      { selectedGoods: [...state.selectedGoods, good] }
+    ));
+  };
+
+  removeGood = (goodToRemove: string) => {
+    this.setState((state) => ({
+      selectedGoods: state.selectedGoods.filter(
+        good => good !== goodToRemove,
+      ),
+    }
+    ));
+  };
+
+  removeAllGoods = () => {
+    this.setState({ selectedGoods: [] });
   };
 
   render() {
     const { selectedGoods } = this.state;
 
+    const { selectGood, removeGood, renderTitle } = this;
+
     return (
       <div className="App">
         <h1 className="App__title">
-          {selectedGoods.length === 0
-            ? 'No goods selected'
-            : (
-              <>
-                {`Selected Goods: ${selectedGoods.join(', ')}`}
-                <button
-                  type="button"
-                  className="App__clearButton"
-                  onClick={() => {
-                    this.setState({ selectedGoods: [] });
-                  }}
-                >
-                  Clear
-                </button>
-              </>
-            )}
+          {renderTitle(selectedGoods)}
         </h1>
         <ul className="App__list">
           {goodsFromServer.map(good => {
@@ -55,10 +98,7 @@ class App extends React.Component {
                       App__listItemButton
                       App__listItemButton--toRemove
                     "
-                    onClick={() => {
-                      selectedGoods.splice(selectedGoods.indexOf(good), 1);
-                      this.setState({ selectedGoods });
-                    }}
+                    onClick={() => removeGood(good)}
                   >
                     Remove
                   </button>
@@ -74,10 +114,7 @@ class App extends React.Component {
                       App__listItemButton
                       App__listItemButton--toSelect
                     "
-                    onClick={() => {
-                      selectedGoods.push(good);
-                      this.setState({ selectedGoods });
-                    }}
+                    onClick={() => selectGood(good)}
                   >
                     Select
                   </button>
