331a7957b4f6e7b8068b9619374fff3158b963cd
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-07-14 08:51:12 +0300

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,128 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.scss';
+import { Button } from './components/Button/Button';
 
-// import goodsFromServer from './goods';
+interface Good {
+  id: string;
+  value: string;
+}
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
+const goodsFromServer: Good[] = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
+].map(good => ({
+  id: uuidv4(),
+  value: good,
+}));
+
+export type Props = {};
+
+interface State {
+  selectedGoods: string[];
+}
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    selectedGoods: ['Jam'],
+  };
+
+  clearSelection = () => this.setState({ selectedGoods: [] });
+
+  addGood = (good: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({ selectedGoods: [...selectedGoods, good] });
+  };
+
+  removeGood = (selectedGood: string) => {
+    const { selectedGoods } = this.state;
+
+    this.setState({
+      selectedGoods: selectedGoods.filter(
+        good => good !== selectedGood,
+      ),
+    });
+  };
+
+  formatTitle = () => {
+    const { selectedGoods } = this.state;
+
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selected';
+
+      case 1:
+        return `${selectedGoods[0]} is selected`;
+
+      default:
+        return `${selectedGoods.slice(0, -1).join(', ')}
+         and ${selectedGoods[selectedGoods.length - 1]}`;
+    }
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="title app__title">
+          {`Selected good: ${this.formatTitle()}`}
         </h1>
 
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
+        <ul className="goods app__list">
+          {
+            goodsFromServer.map(
+              good => {
+                const isGoodSelected = selectedGoods.includes(good.value);
 
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
+                const buttonCallback = isGoodSelected
+                  ? () => this.removeGood(good.value)
+                  : () => this.addGood(good.value);
 
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
+                const buttonTitle = isGoodSelected
+                  ? 'Remove good'
+                  : 'Add good';
 
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
-};
+                const listClassName = `${isGoodSelected ? 'active' : ''} goods__good`;
+
+                return (
+                  <li
+                    className={listClassName}
+                    key={good.id}
+                  >
+                    {good.value}
+                    <Button
+                      clickCallback={buttonCallback}
+                      title={buttonTitle}
+                    />
+                  </li>
+                );
+              },
+            )
+          }
+        </ul>
+
+        {(selectedGoods.length > 0) && (
+          <button
+            type="button"
+            onClick={this.clearSelection}
+            className="button button__clear-all app__button"
+          >
+            Clear all
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
