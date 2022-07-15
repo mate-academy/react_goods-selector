35fafc09a3f7bd5ff0b83aaf47a6945fff0c58e1
Batick3737 <antonandriy@ukr.net>
2022-07-15 11:33:18 +0300

    write code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,69 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected good: -</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: goodsFromServer,
+    selectedGoods: ['Jam'],
+  };
+
+  render() {
+    const { goods } = this.state;
+    let { selectedGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{selectedGoods.length > 0 ? `${selectedGoods.join(', ')} ${selectedGoods.length === 1 ? 'is' : 'are'} selected` : 'No goods selected'}</h1>
+        {(selectedGoods.length > 0 && (
+          <button
+            type="button"
+            className="button button-1"
+            onClick={() => {
+              this.setState(() => {
+                return {
+                  selectedGoods: [],
+                };
+              });
+            }}
+          >
+            Clear
+          </button>
+        )
+        )}
+        <ul>
+          {goods.map(good => (
+            <li
+              key={good}
+              className={
+                classNames('li', { active: selectedGoods.includes(good) })
+              }
+            >
+              {good}
+              <button
+                type="button"
+                className="button"
+                onClick={(event) => {
+                  if (event.currentTarget.textContent === 'Remove') {
+                    selectedGoods = selectedGoods.filter(x => x !== good);
+                  } else {
+                    selectedGoods.push(good);
+                  }
+
+                  this.setState(() => {
+                    return {
+                      selectedGoods,
+                    };
+                  });
+                }}
+              >
+                { selectedGoods.includes(good) ? 'Remove' : 'Select' }
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
