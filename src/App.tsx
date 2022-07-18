45acea680796254e58b9f0f84b2530f38b6b3773
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-07-18 15:50:41 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,83 @@
 import React from 'react';
 import './App.scss';
 
-// import goodsFromServer from './goods';
+import goodsFromServer from './goods';
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGood: string;
 };
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGood: '',
+  };
+
+  handlerClear = () => {
+    document.querySelector('.Good--active')?.classList.remove('Good--active');
+    this.setState({ selectedGood: '' });
+  };
+
+  handlerRemove = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.currentTarget.parentElement?.classList.remove('Good--active');
+    this.setState({ selectedGood: '' });
+  };
+
+  select = (event: React.MouseEvent<HTMLButtonElement>, good: string) => {
+    document.querySelector('.Good--active')?.classList.remove('Good--active');
+    event.currentTarget.parentElement?.classList.add('Good--active');
+    this.setState({ selectedGood: good });
+  };
+
+  render() {
+    return (
+      <main className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            {this.state.selectedGood
+              ? `${this.state.selectedGood} is selected`
+              : 'No goods selected'}
+          </h1>
+
+          {this.state.selectedGood && (
+            <button
+              type="button"
+              className="App__clear"
+              onClick={this.handlerClear}
+            >
+              Clear
+            </button>
+          )}
+        </header>
+
+        <ul>
+          {goodsFromServer.map(good => (
+            <li className="Good">
+              {good}
+              {this.state.selectedGood === good && (
+                <button
+                  type="button"
+                  className="Good__remove"
+                  onClick={this.handlerRemove}
+                >
+                  Remove
+                </button>
+              )}
+
+              {this.state.selectedGood !== good && (
+                <button
+                  type="button"
+                  className="Good__select"
+                  onClick={(event) => {
+                    this.select(event, good);
+                  }}
+                >
+                  Select
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
