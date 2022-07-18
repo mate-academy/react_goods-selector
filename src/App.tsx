047d6933d6efabdd4adbf8ab43e7119e15e311f0
Bohdana Usenko <usenkobogdana19@gmail.com>
2022-07-18 13:41:31 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
-// import goodsFromServer from './goods';
+const productsgoodsFromServer: string[] = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
+];
 
-export const App: React.FC = () => {
+const App: React.FC = () => {
+  const [selectedGoods, setSelectedGoods] = useState([' Jam']);
+
+  const add = (newGood: string) => {
+    setSelectedGoods(prev => [...prev, newGood]);
+  };
+
+  const remove = (newGoods: string) => {
+    setSelectedGoods(prev => prev.filter(good => good !== newGoods));
+  };
+
+  const getTitle = () => {
+    switch (selectedGoods.length) {
+      case 0:
+        return 'No goods selceted';
+      case 1:
+        return `${selectedGoods} is selected`;
+      case 2:
+        return `${selectedGoods[0]} and ${selectedGoods[1]} are selected`;
+      default:
+        return `${selectedGoods} are selected`;
+    }
+  };
+
   return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
+    <div className="App">
+      <ul className="App__list">
+        {productsgoodsFromServer.map((product) => {
+          return (
+            <li key={product} className={selectedGoods.includes(` ${product}`) ? 'App__item--active' : 'App__item'}>
+              <span className="App__span">
+                {product}
+              </span>
+              {selectedGoods.includes(` ${product}`)
+                ? (
+                  <button
+                    type="button"
+                    className="App__button button"
+                    onClick={() => remove(` ${product}`)}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="App__button button"
+                    onClick={() => add(` ${product}`)}
+                  >
+                    Select
+                  </button>
+                )}
+            </li>
+          );
+        })}
       </ul>
 
-      {/* Put required buttons into each Good */}
       <button
         type="button"
-        className="Good__remove"
+        className="App__button--clear button"
+        onClick={() => setSelectedGoods([])}
       >
-        Remove
+        Clear
       </button>
 
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
+      <h1 className="App__title">
+        {getTitle()}
+      </h1>
+    </div>
   );
 };
+
+export default App;
