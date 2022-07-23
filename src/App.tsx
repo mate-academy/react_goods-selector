2742d61b479facc57d7cc08539378659ca88d875
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-07-23 20:56:53 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,6 @@ export class App extends Component<{}, State> {
   handleClick = (el: string, event:React.MouseEvent<HTMLButtonElement>) => {
     const element = event.currentTarget.parentElement;
 
-    // console.log(element.parentElement, el);
-
     this.removeActive();
     if (element) {
       element.className = 'Good--active';
@@ -105,56 +103,7 @@ export class App extends Component<{}, State> {
             </li>
           ))}
         </ul>
-
-        {/* Put required buttons into each Good */}
       </main>
     );
   }
 }
-
-// export const App1: React.FC = () => {
-//   return (
-//     <main className="App">
-//       <header className="App__header">
-//         <h1 className="App__title">
-//           Jam is selected
-//         </h1>
-
-//         <button
-//           type="button"
-//           className="App__clear"
-//         >
-//           Clear
-//         </button>
-//       </header>
-
-//       <ul>
-//         <li className="Good">Dumplings</li>
-//         <li className="Good">Carrot</li>
-//         <li className="Good">Eggs</li>
-//         <li className="Good">Ice cream</li>
-//         <li className="Good">Apple</li>
-//         <li className="Good">Bread</li>
-//         <li className="Good">Fish</li>
-//         <li className="Good">Honey</li>
-//         <li className="Good Good--active">Jam</li>
-//         <li className="Good">Garlic</li>
-//       </ul>
-
-//       {/* Put required buttons into each Good */}
-//       <button
-//         type="button"
-//         className="Good__remove"
-//       >
-//         Remove
-//       </button>
-
-//       <button
-//         type="button"
-//         className="Good__select"
-//       >
-//         Select
-//       </button>
-//     </main>
-//   );
-// };
