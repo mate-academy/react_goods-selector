63726acbd18abcb719ffad55c465728dabfaa901
fledith <barateon023@gmail.com>
2022-08-01 16:27:34 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,127 @@
 import React from 'react';
+import classNames from 'classnames';
+import { v4 as uuidv4 } from 'uuid';
 import './App.scss';
 
-// import goodsFromServer from './goods';
+import goodsFromServer from './goods';
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
+const uniqueKeys: string[] = [];
 
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
+for (let i = 0; i < goodsFromServer.length; i += 1) {
+  uniqueKeys[i] = uuidv4();
+}
 
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGoods: string[];
+  title: string;
 };
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGoods: ['Jam'],
+    title: '',
+  };
+
+  componentDidMount() {
+    this.setTitle();
+  }
+
+  setTitle = () => {
+    const { selectedGoods } = this.state;
+    const { length } = selectedGoods;
+
+    if (length === 0) {
+      this.setState({ title: 'No goods selected' });
+    }
+
+    if (length === 1) {
+      this.setState({ title: `${selectedGoods[0]} is selected` });
+    }
+
+    if (length > 1) {
+      this.setState({ title: `${selectedGoods.slice(0, length - 1).join(', ')} and ${selectedGoods[length - 1]} are selected` });
+    }
+  };
+
+  handlingRemove = (good: string, goods: string[]) => {
+    this.setState(
+      { selectedGoods: goods.filter(item => item !== good) },
+      this.setTitle,
+    );
+  };
+
+  handlingSelect = (good: string, goods: string[]) => {
+    this.setState({ selectedGoods: [...goods, good] }, this.setTitle);
+  };
+
+  handlingClear = () => {
+    this.setState({ selectedGoods: [] }, this.setTitle);
+  };
+
+  render() {
+    const { selectedGoods, title } = this.state;
+    const { length } = selectedGoods;
+
+    return (
+      <main className="App is-light">
+        <header className="App__header">
+          <h1 className="App__title">
+            {title}
+          </h1>
+
+          {length
+            ? (
+              <button
+                type="button"
+                className="App__clear button is-dark"
+                onClick={this.handlingClear}
+              >
+                Clear
+              </button>
+            )
+            : '' }
+        </header>
+
+        <ul className="Goods">
+          {goodsFromServer.map((good, index) => (
+            <li
+              key={uniqueKeys[index]}
+              className={classNames(
+                'Good level-item',
+                {
+                  'Good--active': selectedGoods.includes(good),
+                },
+              )}
+            >
+              <h1>{good}</h1>
+
+              {selectedGoods.includes(good)
+                ? (
+                  <button
+                    type="button"
+                    className="Good__remove button is-dark"
+                    onClick={() => {
+                      this.handlingRemove(good, selectedGoods);
+                    }}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="Good__select button is-dark"
+                    onClick={() => {
+                      this.handlingSelect(good, selectedGoods);
+                    }}
+                  >
+                    Select
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
