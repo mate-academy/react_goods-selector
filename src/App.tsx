b878d30d6a2f4111b9a05051a2361665d88c2543
Mykhailo Parfeniuk <mypa@brighta.biz>
2022-08-02 02:06:44 +0300

    Task completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './App.scss';
+import 'bulma/css/bulma.min.css';
 
-// import goodsFromServer from './goods';
+import goodsFromServer from './goods';
 
 export const App: React.FC = () => {
+  const [selectedGood, setSelectedGood] = useState(['Jam']);
+
+  function deleteAllProducts(): void {
+    setSelectedGood([]);
+  }
+
+  function deleteProduct(product: string): void {
+    setSelectedGood((products: string[]) => {
+      return products.filter(el => el !== product);
+    });
+  }
+
+  function selectProduct(product: string): void {
+    setSelectedGood((products: string[]) => {
+      return [...products, product];
+    });
+  }
+
   return (
-    <main className="App">
-      <header className="App__header">
+    <main className="App main p-3 box">
+      <header className="App__header title is-1 is-flex is-align-items-center">
         <h1 className="App__title">
-          Jam is selected
+          {
+            selectedGood.length
+              ? `
+                ${selectedGood.map(el => ` ${el}`)}
+                ${selectedGood.length > 1 ? 'are' : 'is'} selected
+              `
+              : 'No goods selected'
+          }
         </h1>
 
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
+        {selectedGood.length > 0 && (
+          <button
+            type="button"
+            className="App__clear button is-warning"
+            onClick={deleteAllProducts}
+          >
+            Clear
+          </button>
+        )}
       </header>
 
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
+      <ul className="is-three-quarters panel-list">
+        {goodsFromServer.map((product: string) => (
+          <li
+            className={
+              cn(
+                'Good subtitle is-flex is-align-items-center p-1 ',
+                { 'Good--active': selectedGood.find(el => el === product) },
+              )
+            }
+            key={product}
+          >
+            {product}
+
+            {selectedGood.find(el => el === product) && (
+              <button
+                type="button"
+                className="Good__remove button is-warning"
+                onClick={() => deleteProduct(product)}
+              >
+                Remove
+              </button>
+            )}
+
+            {!selectedGood.find(el => el === product) && (
+              <button
+                type="button"
+                className="Good__select button is-primary"
+                onClick={() => selectProduct(product)}
+              >
+                Select
+              </button>
+            )}
+          </li>
+        ))}
       </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
     </main>
   );
 };
