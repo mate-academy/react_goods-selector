ddcd21725b8e7ca1a3753901888a30c2d871e6d4
Olha Stakhova <olha.stakhova@gmail.com>
2022-08-07 01:08:36 +0300

    added  select multiple goods

--- src/App.tsx
+++ src/App.tsx
@@ -9,34 +9,93 @@ import classNames from 'classnames';
 import goodsFromServer from './goods';
 
 type State = {
-  selectedGood: string;
+  selectedGood: string[];
+  isSelectedDefault: boolean;
 };
 
 export class App extends Component<{}, State> {
   state: Readonly<State> = {
-    selectedGood: 'Jam',
+    selectedGood: ['Jam'],
+    isSelectedDefault: true,
   };
 
   goods: string[] = [...goodsFromServer];
+
+  isActive = (good: string) => {
+    return !!(this.state.selectedGood.find(item => item === good));
+  };
+
+  removeItem = (good: string) => (
+    this.setState((prevState) => {
+      const { selectedGood } = prevState;
+      const indexFinde = selectedGood
+        .findIndex(item => item === good);
+
+      if (indexFinde !== -1) {
+        selectedGood.splice(indexFinde, 1);
+      }
+
+      return {
+        selectedGood: [...selectedGood],
+      };
+    })
+  );
+
+  clearState = () => {
+    this.setState({
+      selectedGood: [],
+    });
+  };
+
+  setHeader = (): string => {
+    const lengthSelected = this.state.selectedGood.length;
+
+    if (!(lengthSelected)) {
+      return 'No goods selected';
+    }
+
+    if (lengthSelected === 1) {
+      return `${this.state.selectedGood[0]} is selected`;
+    }
+
+    const selectedWithoutLastElement = [...this.state.selectedGood];
+
+    const lastSelected = selectedWithoutLastElement.pop();
+
+    return `${selectedWithoutLastElement.join(', ')} and ${lastSelected} are selected`;
+  };
+
+  addeItem = (good: string): void => {
+    if (this.state.isSelectedDefault) {
+      this.clearState();
+      this.setState(() => {
+        return {
+          isSelectedDefault: false,
+        };
+      });
+    }
+
+    this.setState((prevState) => {
+      const { selectedGood } = prevState;
+
+      return {
+        selectedGood: [...selectedGood, good],
+      };
+    });
+  };
 
   render() {
     return (
       <main className="App">
         <header className="App__header">
           <h1 className="App__title">
-            { this.state.selectedGood
-              ? `${this.state.selectedGood} is selected`
-              : 'No goods selected'}
+            { this.setHeader() }
           </h1>
-          {this.state.selectedGood && (
+          {!!(this.state.selectedGood.length) && (
             <button
               type="button"
               className="App__clear button is-danger is-light"
-              onClick={() => {
-                this.setState({
-                  selectedGood: '',
-                });
-              }}
+              onClick={() => this.clearState()}
             >
               Clear
             </button>
@@ -47,23 +106,20 @@ export class App extends Component<{}, State> {
           {this.goods.map(good => (
             <li
               key={uniqid()}
-              // className="Good"
               className={classNames(
                 'Good',
                 'level',
-                { 'Good--active': this.state.selectedGood === good },
+                { 'Good--active': this.isActive(good) },
               )}
             >
               {good}
-              { good === this.state.selectedGood
+              { this.isActive(good)
                 ? (
                   <button
                     type="button"
                     className="Good__remove button is-warning is-light"
                     onClick={() => {
-                      this.setState({
-                        selectedGood: '',
-                      });
+                      this.removeItem(good);
                     }}
                   >
                     Remove
@@ -74,9 +130,7 @@ export class App extends Component<{}, State> {
                     type="button"
                     className="Good__select button is-success is-light"
                     onClick={() => {
-                      this.setState({
-                        selectedGood: good,
-                      });
+                      this.addeItem(good);
                     }}
                   >
                     Select
