0a89a1775af3cd6d974295430e848ddde1b07bd7
Yaroslav Lizogub <lizogub.yar@gmail.com>
2022-08-07 08:26:47 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,95 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
-// import goodsFromServer from './goods';
+import goodsFromServer from './goods';
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGood: string;
 };
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGood: 'Jam',
+  };
+
+  selectGood = (selectedGood: string) => {
+    this.setState({ selectedGood });
+  };
+
+  removeSelectedGood = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="App has-background-light">
+        <div className="App__wrapper has-background-success">
+          <header className="App__header">
+            <h1 className="App__title has-text-info-light">
+              {
+                selectedGood
+                  ? `${selectedGood} is selected`
+                  : 'No goods selected'
+              }
+            </h1>
+
+            {selectedGood && (
+              <button
+                type="button"
+                className="App__clear button is-danger"
+                onClick={this.removeSelectedGood}
+              >
+                Clear
+              </button>
+            )}
+
+          </header>
+
+          <ul className="App__list">
+            {
+              goodsFromServer.map(good => {
+                const isSelected = selectedGood === good;
+
+                return (
+                  <li
+                    key={good}
+                    className={classNames(
+                      'Good has-text-warning-light',
+                      { 'Good--active': isSelected },
+                    )}
+                  >
+                    <span className="App__list-item-text">
+                      {good}
+                    </span>
+
+                    { isSelected
+                      ? (
+                        <button
+                          type="button"
+                          className="Good__remove button is-danger"
+                          onClick={this.removeSelectedGood}
+                        >
+                          Remove
+                        </button>
+                      ) : (
+                        <button
+                          type="button"
+                          className="Good__select button is-info"
+                          onClick={() => this.selectGood(good)}
+                        >
+                          Select
+                        </button>
+                      )}
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+      </main>
+    );
+  }
+}
