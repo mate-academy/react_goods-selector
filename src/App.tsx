c936a3913f6a59e7d5f1832c3c9ee1ac8f6f4653
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-08-09 00:37:14 +0300

    made a selectable goods list

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,147 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
-// import goodsFromServer from './goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
+import goodsFromServer from './goods';
 
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
+function takeGoodsFromServer(goods: string[]): Good[] {
+  return goods.map((el: string) => ({ name: el }));
+}
 
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
+interface State {
+  goods: Good[],
+  selectedGoods: string[],
+}
 
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
+export class App extends React.Component<{}, State> {
+  state = {
+    goods: takeGoodsFromServer(goodsFromServer),
+    selectedGoods: ['Jam'],
+  };
 
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
-};
+  handleRemove = (name: string): void => {
+    this.setState((prevState) => {
+      const previousGood = [...prevState.selectedGoods];
+      const index = previousGood.indexOf(name);
+
+      if (index !== -1) {
+        previousGood.splice(index, 1);
+      }
+
+      return { selectedGoods: previousGood };
+    });
+  };
+
+  handleSelect = (name: string): void => {
+    this.setState((prevState) => {
+      const previousSelectedGoods = [...prevState.selectedGoods];
+
+      if (!previousSelectedGoods.includes(name)) {
+        previousSelectedGoods.push(name);
+      }
+
+      return { selectedGoods: previousSelectedGoods };
+    });
+  };
+
+  getSelectedGoods = (selected: string[]): string => {
+    switch (true) {
+      case selected.length > 3:
+        return `${selected[0]}, ${selected[1]} and ${selected.length - 2} others are selected`;
+
+      case selected.length === 3:
+        return `${selected[0]}, ${selected[1]} and ${selected[2]} are selected`;
+
+      case selected.length === 2:
+        return `${selected[0]} and ${selected[1]} are selected`;
+
+      case selected.length === 1:
+        return `${selected[0]} is selected`;
+
+      case selected.length === 0:
+        break;
+
+      default:
+        throw new Error('error in getSelectedGoods() function');
+    }
+
+    return 'No goods selected';
+  };
+
+  render() {
+    const { goods, selectedGoods } = this.state;
+
+    return (
+      <main className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            {this.getSelectedGoods(selectedGoods)}
+          </h1>
+
+          {Boolean(selectedGoods.length) && (
+            <button
+              type="button"
+              className={classNames(
+                'App__clear',
+                {
+                  'Display--none': !selectedGoods.length,
+                },
+              )}
+              onClick={() => this.setState({ selectedGoods: [] })}
+            >
+              Clear
+            </button>
+          )}
+        </header>
+
+        <ul>
+          {goods.map(good => (
+            <li
+              key={good.name}
+              className={classNames(
+                'Good',
+                {
+                  'Good--active': selectedGoods.includes(good.name),
+                },
+              )}
+            >
+              {good.name}
+
+              {!selectedGoods.includes(good.name) && (
+                <button
+                  type="button"
+                  className={classNames(
+                    'Good__select',
+                    {
+                      'Display--none': selectedGoods.includes(good.name),
+                    },
+                  )}
+                  onClick={() => this.handleSelect(good.name)}
+                >
+                  Select
+                </button>
+              )}
+
+              {selectedGoods.includes(good.name) && (
+                <button
+                  type="button"
+                  className={classNames(
+                    'Good__remove',
+                    {
+                      'Display--none': !selectedGoods.includes(good.name),
+                    },
+                  )}
+                  onClick={() => this.handleRemove(good.name)}
+                >
+                  Remove
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
