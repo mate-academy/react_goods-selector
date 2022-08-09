a72144ecc18664d2bec891a50067ecd773c93f40
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-08-09 18:24:27 +0300

    create goods selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,90 @@
+/* eslint-disable react/no-access-state-in-setstate */
 import React from 'react';
 import './App.scss';
+import 'bulma/css/bulma.min.css';
 
-// import goodsFromServer from './goods';
+import goodsFromServer from './goods';
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGoods: string[]
 };
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGoods: ['Jam'],
+  };
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <main className="App">
+        <header className="App__header block breadcrumb is-centered">
+          <h1 className="App__title">
+            {!selectedGoods.length && 'No goods selected'}
+            {selectedGoods.length === 1 && `${selectedGoods[0]} is selected`}
+            {selectedGoods.length === 2 && `${selectedGoods.join(', ')} are selected`}
+            {selectedGoods.length > 2 && `${selectedGoods.slice(0, -1).join(', ')} and ${selectedGoods[selectedGoods.length - 1]} are selected`}
+          </h1>
+
+          {selectedGoods.length > 0 && (
+            <button
+              type="button"
+              className="App__clear button is-danger is-rounded"
+              onClick={() => {
+                this.setState({ selectedGoods: [] });
+              }}
+            >
+              Clear
+            </button>
+          )}
+        </header>
+
+        <ul className="GoodsList">
+          {goodsFromServer.map(good => {
+            return (
+              <li
+                key={good}
+                className={
+                  !selectedGoods.includes(good)
+                    ? 'Good block'
+                    : 'Good--active block'
+                }
+              >
+                {good}
+                {selectedGoods.includes(good) ? (
+                  <button
+                    type="button"
+                    className="Good--delete button is-danger is-rounded"
+                    onClick={() => {
+                      this.setState({
+                        selectedGoods: this.state.selectedGoods
+                          .filter(el => el !== good),
+                      });
+                    }}
+                  >
+                    Delete
+                  </button>
+                )
+
+                  : (
+                    <button
+                      type="button"
+                      className="Good--select button is-success is-rounded"
+                      onClick={() => {
+                        this.setState({
+                          selectedGoods: [...selectedGoods, good],
+                        });
+                      }}
+                    >
+                      Select
+                    </button>
+                  )}
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    );
+  }
+}
