39646a193b8697560db4cf97f8e5cfe76c263ae4
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-08-13 13:09:55 +0300

    secondCommit

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,59 @@ import './App.scss';
 import goodsFromServer from './goods';
 
 type State = {
-  selectedItem: string
+  selectedItem: string[]
 };
 
 export class App extends React.Component<{}, State > {
   state: Readonly<State> = {
-    selectedItem: 'Jam',
+    selectedItem: ['Jam'],
+  };
+
+  selectButton = (item: string) => {
+    this.setState((state) => ({
+      selectedItem: [
+        ...state.selectedItem,
+        item,
+      ],
+    }));
+  };
+
+  removeButton = (element: React.MouseEvent) => {
+    const goodToRemove
+    = element.currentTarget.parentElement?.innerText.split('\n')
+      .slice(0, 1).toString();
+
+    this.setState((state) => ({
+      selectedItem: [
+        ...state.selectedItem,
+      ].filter(x => x !== goodToRemove),
+    }));
+  };
+
+  titleText = () => {
+    const { selectedItem } = this.state;
+
+    switch (selectedItem.length) {
+      default:
+        return `${selectedItem.map((item, i, arr) => {
+          if (i === arr.length - 2) {
+            return item;
+          }
+
+          if (i === arr.length - 1) {
+            return `and ${item}`;
+          }
+
+          return `${item},`;
+        }).join(' ')} are selected`;
+      case 0:
+        return 'No goods selected';
+      case 1:
+        return `${selectedItem.toString()} is selected`;
+      case 2:
+        return `${selectedItem.map((item, i, arr) => (
+          i === arr.length - 1 ? `and ${item}` : item)).join(' ')} are selected`;
+    }
   };
 
   render() {
@@ -22,24 +69,19 @@ export class App extends React.Component<{}, State > {
           <header className="App__header">
             <h1 className="App__title">
               {
-                selectedItem.length
-                  ? `${selectedItem} is selected`
-                  : 'No goods selected'
-
+                this.titleText()
               }
 
-              {selectedItem
-           && (
-             <button
-               type="button"
-               className="App__clear"
-               onClick={() => {
-                 this.setState({ selectedItem: '' });
-               }}
-             >
-               Clear
-             </button>
-           )}
+              <button
+                type="button"
+                className={classNames('App__clear',
+                  { 'App__clear--active': !selectedItem.length })}
+                onClick={() => {
+                  this.setState({ selectedItem: [] });
+                }}
+              >
+                Clear
+              </button>
             </h1>
 
           </header>
@@ -51,34 +93,26 @@ export class App extends React.Component<{}, State > {
                 className={classNames('Good',
                   { 'Good--active': selectedItem.includes(item) })}
               >
-                <p>
-                  {item}
-                </p>
 
-                <div className="Good__button-wrapper">
-                  <button
-                    type="button"
-                    className={classNames('Good__select',
-                      { 'Good__select--active': selectedItem.includes(item) })}
-                    onClick={() => {
-                      this.setState({ selectedItem: item });
-                    }}
-                  >
-                    Select
-                  </button>
+                {item}
 
-                  <button
-                    type="button"
-                    className={classNames('Good__remove',
-                      { 'Good__remove--active': !selectedItem.includes(item) })}
-                    onClick={() => {
-                      this.setState({ selectedItem: '' });
-                    }}
-                  >
-                    Remove
-                  </button>
+                <button
+                  type="button"
+                  className={classNames('Good__select',
+                    { 'Good__select--active': selectedItem.includes(item) })}
+                  onClick={() => this.selectButton(item)}
+                >
+                  Select
+                </button>
 
-                </div>
+                <button
+                  type="button"
+                  className={classNames('Good__remove',
+                    { 'Good__remove--active': !selectedItem.includes(item) })}
+                  onClick={(e) => this.removeButton(e)}
+                >
+                  Remove
+                </button>
 
               </li>
             ))}
