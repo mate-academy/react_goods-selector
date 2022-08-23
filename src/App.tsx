2a62406c0cc2bd35b5f1a84e37589d63323ede65
Victor Hrygorash <viktor12041991@i.ua>
2022-08-23 10:47:32 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,66 @@
-import React from 'react';
 import classNames from 'classnames';
+import React from 'react';
 import './App.scss';
+
 import goodsFromServer from './goods';
 
-type State = {
-  selectedGood: string,
-};
-
-export class App extends React.Component<{}, State> {
-  state: Readonly<State> = {
+export class App extends React.Component {
+  state = {
     selectedGood: 'Jam',
-  };
-
-  selectGood = (good: string) => {
-    this.setState({ selectedGood: good });
-  };
-
-  removeGood = (good: string) => {
-    if (good === this.state.selectedGood) {
-      this.setState({ selectedGood: '' });
-    }
   };
 
   render() {
     const { selectedGood } = this.state;
 
-    function isGoodSelected(good: string): boolean {
-      return good === selectedGood;
-    }
-
     return (
-      <main className="App panel is-info">
-        <header className="App__header panel-heading">
+      <main className="App">
+        <header className="App__header">
           <h1 className="App__title">
-            {selectedGood
-              ? `${selectedGood} is selected`
-              : 'No goods selected'}
+            { selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
           </h1>
-        </header>
-
-        <ul className="Good__list">
-          {goodsFromServer.map(good => {
-            return (
-              <li
-                key={good}
-                className={classNames(
-                  'Good',
-                  'panel-block',
-                  {
-                    'Good Good--active': isGoodSelected(good),
-                  },
-                )}
-              >
-                {good}
-
-                {isGoodSelected(good)
-                  ? (
-                    <button
-                      type="button"
-                      className="Good__remove button is-danger"
-                      onClick={() => {
-                        this.removeGood(good);
-                      }}
-                    >
-                      Remove
-                    </button>
-                  )
-                  : (
-                    <button
-                      type="button"
-                      className="Good__select button is-success"
-                      onClick={() => {
-                        this.selectGood(good);
-                      }}
-                    >
-                      Select
-                    </button>
-                  )}
-              </li>
-            );
-          })}
-        </ul>
-        {selectedGood
-          ? (
-            <>
+          {selectedGood
+            && (
               <button
                 type="button"
-                className="App__clear button is-danger is-light"
-                onClick={() => {
-                  this.setState({ selectedGood: '' });
-                }}
+                className="App__clear"
+                onClick={() => this.setState({ selectedGood: '' })}
               >
                 Clear
               </button>
-            </>
-          )
-          : null}
+            )}
+        </header>
+        <ul>
+          {goodsFromServer.map(good => (
+            <li
+              key={good}
+              className={classNames(
+                'Good',
+                { 'Good--active': selectedGood === good },
+              )}
+            >
+              {good}
+              {selectedGood === good
+                ? (
+                  <button
+                    type="button"
+                    className="Good__remove"
+                    onClick={() => this.setState({ selectedGood: '' })}
+                  >
+                    Remove
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="Good__select"
+                    onClick={() => this.setState({ selectedGood: good })}
+                  >
+                    Select
+                  </button>
+                )}
+            </li>
+          ))}
+        </ul>
       </main>
     );
   }
