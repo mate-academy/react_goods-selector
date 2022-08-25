64cb142f795deb7a0cad40922670e4fc02282278
Max Shylenko <maxshylenko@gmail.com>
2022-08-25 21:11:21 +0200

    react goods selector done

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,108 @@
-import React from 'react';
+import { Component, ReactNode } from 'react';
 import './App.scss';
 
-// import goodsFromServer from './goods';
+import goodsFromServer from './goods';
 
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
+const goods = [...goodsFromServer];
 
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGood: string,
+  selected: boolean,
 };
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGood: '',
+    selected: false,
+  };
+
+  clear = () => {
+    const selectedItem = document.querySelector('.Good--active');
+
+    if (selectedItem !== null) {
+      selectedItem.classList.remove('Good--active');
+      const selectedButton = selectedItem.querySelector('.Good__select');
+
+      if (selectedButton) {
+        selectedButton.removeAttribute('hidden');
+      }
+
+      this.setState({
+        selectedGood: '',
+        selected: false,
+      });
+    }
+  };
+
+  handleButtonSelect = (event: React.MouseEvent, good: string) => {
+    if (this.state.selected) {
+      this.clear();
+    }
+
+    const listItem: Element | null = event.currentTarget.parentElement;
+    const selectButton = event.currentTarget;
+
+    if (listItem !== null) {
+      listItem.classList.add('Good--active');
+      selectButton.setAttribute('hidden', 'on');
+    }
+
+    this.setState({
+      selectedGood: good,
+      selected: true,
+    });
+  };
+
+  render(): ReactNode {
+    return (
+      <main className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            {!this.state.selected
+              ? 'No goods selected'
+              : `${this.state.selectedGood} is selected`}
+          </h1>
+
+          <button
+            type="button"
+            className="App__clear"
+            onClick={this.clear}
+            hidden={!this.state.selected}
+          >
+            Clear
+          </button>
+        </header>
+
+        <ul>
+          {goods.map(good => {
+            return (
+              <li className="Good" key={good}>
+                {good}
+                <button
+                  type="button"
+                  className="Good__remove"
+                  onClick={this.clear}
+                >
+                  Remove
+                </button>
+
+                <button
+                  type="button"
+                  className="Good__select"
+                  onClick={(event) => {
+                    this.handleButtonSelect(
+                      event,
+                      good,
+                    );
+                  }}
+                >
+                  Select
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    );
+  }
+}
