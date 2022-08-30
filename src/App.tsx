0acd430c3b4ed784b98d218100b48d1b7519cafa
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-08-30 22:22:18 +0800

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,11 @@ export class App extends React.Component<{}, State> {
             is selected
 
             {/* {selectedGood}
-             ? {selectedGood} 'is selected'
-             : 'No goods selected'} */}
+            ?
+            {`${selectedGood}
+            is selected
+            :
+            No goods selected`} */}
           </h1>
 
           <button
@@ -49,45 +52,37 @@ export class App extends React.Component<{}, State> {
 
         <ul>
           {goodsFromServer.map(good => (
-            <li>
+            <li
+              className="Good"
+              // type="button"
+              key={good}
+            >
+              {good}
+
               <button
-                className="Good"
                 type="button"
-                key={good}
+                className="Good__select"
                 onClick={() => {
                   this.setState({
                     selectedGood: good,
-                    // className='Good--active',
                     // event.classList.add('.Good--active')
                   });
                 }}
               >
-                {good}
+                Select
+              </button>
+              <button
+                type="button"
+                className="Good__remove"
+                onClick={() => {
+                  // event.classList.remove('.Good--active');
+                }}
+              >
+                Remove
               </button>
             </li>
           ))}
         </ul>
-
-        {/* Put required buttons into each Good */}
-        <button
-          type="button"
-          className="Good__remove"
-          onClick={() => {
-            // event.classList.remove('.Good--active');
-          }}
-        >
-          Remove
-        </button>
-
-        <button
-          type="button"
-          className="Good__select"
-          // onClick={() => {
-          //   this.setState({className.add('Good--active')});
-          // }}
-        >
-          Select
-        </button>
       </main>
     );
   }
@@ -95,14 +90,3 @@ export class App extends React.Component<{}, State> {
 
 /* {selectedGood ? {selectedGood} 'is selected'
             : 'No goods selected' } */
-
-/* <li className="Good">Dumplings</li>
-<li className="Good">Carrot</li>
-<li className="Good">Eggs</li>
-<li className="Good">Ice cream</li>
-<li className="Good">Apple</li>
-<li className="Good">Bread</li>
-<li className="Good">Fish</li>
-<li className="Good">Honey</li>
-<li className="Good Good--active">Jam</li>
-<li className="Good">Garlic</li> */
