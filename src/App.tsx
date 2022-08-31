b3e78caf28310adcf7302da777045c88ea2541a5
Koba_ <kovbasinskyv@gmail.com>
2022-08-31 15:43:36 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,113 @@
+import classNames from 'classnames';
 import React from 'react';
 import './App.scss';
+import goodsFromServer from './goods';
 
-// import goodsFromServer from './goods';
-
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type Goods = {
+  name?: string,
+  id?: number,
 };
+
+const goodswithid = (FromServer: string[]) => {
+  const newarr: Goods[] = [];
+
+  // eslint-disable-next-line array-callback-return
+  FromServer.map((good: string, index: number) => {
+    const obj: Goods = {};
+
+    obj.name = good;
+    obj.id = index;
+
+    newarr.push(obj);
+  });
+
+  return newarr;
+};
+
+const newgoods = goodswithid(goodsFromServer);
+
+export class App extends React.Component {
+  state = {
+    selectedName: 'Jam',
+    selectedId: newgoods.length - 2,
+  };
+
+  render() {
+    return (
+      <main className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            {this.state.selectedName ? (
+              `${this.state.selectedName} is selected`
+            ) : (
+              'No good is selected'
+            )}
+          </h1>
+
+          {this.state.selectedName !== null && (
+            <button
+              type="button"
+              onClick={() => {
+                this.setState(
+                  {
+                    selectedId: null,
+                    selectedName: null,
+                  },
+                );
+              }}
+            >
+              Clear
+            </button>
+          )}
+        </header>
+
+        <ul>
+          {newgoods.map(good => (
+            <li
+              className={
+                classNames(
+                  'Good',
+                  {
+                    'Good--active': good.id === this.state.selectedId,
+                  },
+                )
+              }
+              key={good.id}
+            >
+              {good.name}
+              {good.id === this.state.selectedId ? (
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.setState(
+                      {
+                        selectedId: null,
+                        selectedName: null,
+                      },
+                    );
+                  }}
+                >
+                  Clear
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.setState(
+                      {
+                        selectedId: good.id,
+                        selectedName: good.name,
+                      },
+                    );
+                  }}
+                >
+                  Select
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
