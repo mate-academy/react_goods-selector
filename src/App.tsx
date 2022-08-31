a1c4d56af4ce7853ea5b7eadb96d59599070587c
Alyona Pisotska <pisotska.o@gmail.com>
2022-08-31 19:55:18 +0300

    React - goods selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,97 @@
+import { Button, List, ListItem } from '@mui/material';
+import cn from 'classnames';
 import React from 'react';
 import './App.scss';
+import goodsFromServer from './goods';
 
-// import goodsFromServer from './goods';
-
-export const App: React.FC = () => {
-  return (
-    <main className="App">
-      <header className="App__header">
-        <h1 className="App__title">
-          Jam is selected
-        </h1>
-
-        <button
-          type="button"
-          className="App__clear"
-        >
-          Clear
-        </button>
-      </header>
-
-      <ul>
-        <li className="Good">Dumplings</li>
-        <li className="Good">Carrot</li>
-        <li className="Good">Eggs</li>
-        <li className="Good">Ice cream</li>
-        <li className="Good">Apple</li>
-        <li className="Good">Bread</li>
-        <li className="Good">Fish</li>
-        <li className="Good">Honey</li>
-        <li className="Good Good--active">Jam</li>
-        <li className="Good">Garlic</li>
-      </ul>
-
-      {/* Put required buttons into each Good */}
-      <button
-        type="button"
-        className="Good__remove"
-      >
-        Remove
-      </button>
-
-      <button
-        type="button"
-        className="Good__select"
-      >
-        Select
-      </button>
-    </main>
-  );
+type State = {
+  selectedGood: string;
 };
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  handleSelectClear = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  handleSelectGood = (good: string) => {
+    this.setState({
+      selectedGood: good,
+    });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            {selectedGood
+              ? `${selectedGood} is selected`
+              : 'No goods selected'}
+          </h1>
+
+          {selectedGood && (
+            <Button
+              variant="outlined"
+              sx={{ color: '#6A718F' }}
+              type="button"
+              className="App__clear"
+              onClick={this.handleSelectClear}
+            >
+              Clear
+            </Button>
+          )}
+        </header>
+
+        <List
+          className="block"
+          component="nav"
+          aria-label="mailbox folders"
+        >
+          {goodsFromServer.map(good => {
+            const isSelectedGood = good === selectedGood;
+
+            return (
+              <ListItem
+                key={good}
+                sx={{ display: 'flex', justifyContent: 'space-between' }}
+                className={cn('Good', {
+                  'Good--active': isSelectedGood,
+                })}
+              >
+                {good}
+                {isSelectedGood && (
+                  <Button
+                    variant="outlined"
+                    sx={{ width: 90, color: '#6A718F' }}
+                    type="button"
+                    className="button Good__remove"
+                    onClick={this.handleSelectClear}
+                  >
+                    Remove
+                  </Button>
+                )}
+                {isSelectedGood || (
+                  <Button
+                    variant="outlined"
+                    sx={{ width: 90, color: '#6A718F' }}
+                    type="button"
+                    className="button Good__select"
+                    onClick={() => this.handleSelectGood(good)}
+                  >
+                    Select
+                  </Button>
+                )}
+              </ListItem>
+            );
+          })}
+        </List>
+      </main>
+    );
+  }
+}
