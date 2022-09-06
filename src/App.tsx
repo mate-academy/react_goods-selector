cbd0e094c56746d405c7fc304a9db1a50bcbe95f
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-09-06 19:22:57 +0300

    change syntax for arrow functions return and add keys on mapped elements

--- src/App.tsx
+++ src/App.tsx
@@ -59,49 +59,48 @@ export class App extends Component<{}, State> {
 
         <table className="table">
           <tbody>
-            {goods.map(good => {
-              return (
-                <tr
-                  data-cy="Good"
-                  className={
-                    classNames(
-                      { 'has-background-success-light': good === selectedGood },
-                    )
-                  }
-                >
-                  <td>
-                    {good === selectedGood
-                      ? (
-                        <button
-                          data-cy="RemoveButton"
-                          type="button"
-                          className="button is-info"
-                          onClick={() => {
-                            this.toggleSelection(good);
-                          }}
-                        >
-                          -
-                        </button>
-                      ) : (
-                        <button
-                          data-cy="AddButton"
-                          type="button"
-                          className="button"
-                          onClick={() => {
-                            this.toggleSelection(good);
-                          }}
-                        >
-                          +
-                        </button>
-                      )}
-                  </td>
+            {goods.map(good => (
+              <tr
+                key={good}
+                data-cy="Good"
+                className={
+                  classNames(
+                    { 'has-background-success-light': good === selectedGood },
+                  )
+                }
+              >
+                <td>
+                  {good === selectedGood
+                    ? (
+                      <button
+                        data-cy="RemoveButton"
+                        type="button"
+                        className="button is-info"
+                        onClick={() => {
+                          this.toggleSelection(good);
+                        }}
+                      >
+                        -
+                      </button>
+                    ) : (
+                      <button
+                        data-cy="AddButton"
+                        type="button"
+                        className="button"
+                        onClick={() => {
+                          this.toggleSelection(good);
+                        }}
+                      >
+                        +
+                      </button>
+                    )}
+                </td>
 
-                  <td data-cy="GoodTitle" className="is-vcentered">
-                    {good}
-                  </td>
-                </tr>
-              );
-            })}
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </main>
