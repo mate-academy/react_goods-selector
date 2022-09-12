827f0b0ec550b022ed152cb5a8e7888cc6a00bd0
NataliyaAn <otchet333@gmail.com>
2022-09-12 20:00:06 +0300

    Goods-selector solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -15,71 +16,118 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+type State = {
+  selectedGood: string;
+  activeGood: boolean;
+};
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+type Props = {
+  goods: string[];
+};
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+export class App extends React.Component< Props, State> {
+  state: Readonly<State> = {
+    selectedGood: 'Jam',
+    activeGood: false,
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
+  indexGood = goods.indexOf('Jam');
+
+  handleClick = (good: string) => {
+    this.indexGood = this.props.goods.indexOf(good);
+    this.setState({
+      selectedGood: good,
+      activeGood: true,
+    });
+  };
+
+  handleClickClear = () => {
+    this.indexGood = -1;
+    this.setState({
+      selectedGood: '',
+      activeGood: false,
+    });
+  };
+
+  render() {
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const { selectedGood, activeGood } = this.state;
+
+    return (
+      <main className="section container">
+        {(activeGood === true) && (
+          <h1 className="title is-flex is-align-items-center">
+            `
+            {selectedGood}
+            {' '}
+            is selected`
+
             <button
-              data-cy="AddButton"
+              aria-label="button"
+              data-cy="ClearButton"
               type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+              className="delete ml-3"
+              onClick={this.handleClickClear}
+            />
+          </h1>
+        )}
+        {(activeGood === false) && (
+          <h1 className="title">
+            No goods selected
+          </h1>
+        )}
+        <table className="table">
+          <tbody>
+            {this.props.goods.map(good => (
+              <tr
+                data-cy="Good"
+                className={classNames({
+                  'has-background-success-light':
+                    this.props.goods.indexOf(good) === this.indexGood,
+                })}
+              >
+                <td>
+                  {(this.indexGood !== this.props.goods.indexOf(good)) && (
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => {
+                        this.handleClick(good);
+                      }}
+                    >
+                      +
+                    </button>
+                  )}
+                  {(this.indexGood === this.props.goods.indexOf(good)) && (
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={() => {
+                        this.handleClickClear();
+                      }}
+                    >
+                      -
+                    </button>
+                  )}
+                </td>
+                <td
+                  data-cy="GoodTitle"
+                  key={good}
+                  className="is-vcentered"
+                >
+                  {good}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
+function selectedGood(selectedGood: any) {
+  throw new Error('Function not implemented.');
+}
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
