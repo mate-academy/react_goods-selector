052ad703be3a73802a54cd601baffcd97268b609
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-09-13 13:14:56 +0300

    Fix issues after review

--- src/App.tsx
+++ src/App.tsx
@@ -38,34 +38,39 @@ export class App extends Component<{}, State> {
 
     return (
       <main className="section container">
-        {selectedGood ? (
-          <h1 className="title is-flex is-align-items-center">
-            {`${selectedGood} is selected`}
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={this.clearSelected}
-            />
-          </h1>
-        ) : (
-          <h1 className="title">No goods selected</h1>
-        )}
+        <h1
+          className={classNames(
+            'title',
+            { 'is-flex is-align-items-center': selectedGood },
+          )}
+        >
+          {selectedGood ? (
+            <>
+              {`${selectedGood} is selected`}
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={this.clearSelected}
+              />
+            </>
+          ) : (
+            'No goods selected'
+          )}
+        </h1>
 
         <table className="table">
           <tbody>
             {goods.map(good => {
               const isSelected = selectedGood === good;
-              const selectedBackground = classNames(
-                { 'has-background-success-light': isSelected },
-              );
 
               return (
                 <tr
                   data-cy="Good"
-                  className={selectedBackground}
+                  className={classNames(
+                    { 'has-background-success-light': isSelected },
+                  )}
                   key={Math.random()}
                 >
 
