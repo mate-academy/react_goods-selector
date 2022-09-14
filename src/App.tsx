a49dd7b5eb476145732415fa78208dd338751bc9
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-09-14 15:57:49 +0300

    add classname ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -31,6 +32,12 @@ export class App extends Component<{}, State> {
     });
   };
 
+  chengSelecktor = (element: string) => (
+    this.state.curentSelector === element
+      ? this.setState({ curentSelector: '' })
+      : this.setState({ curentSelector: element })
+  );
+
   render() {
     const {
       curentSelector,
@@ -56,45 +63,44 @@ export class App extends Component<{}, State> {
 
         <table className="table">
           <tbody>
-            {goods.map(element => (
-              <tr
-                data-cy="Good"
-                className={
-                  (curentSelector !== element)
-                    ? ''
-                    : 'has-background-success-light'
-                }
-              >
-                <td key={element}>
-                  <button
-                    data-cy={(curentSelector !== element)
-                      ? 'AddButton'
-                      : 'RemoveButton'}
-                    type="button"
-                    className={
-                      (curentSelector !== element)
-                        ? 'button'
-                        : 'button is-info'
-                    }
-                    onClick={(curentSelector !== element)
-                      ? () => {
-                        this.setState({
-                          curentSelector: element,
-                        });
-                      }
-                      : this.removeSelecktor}
-                  >
-                    {curentSelector !== element
-                      ? '+'
-                      : '-'}
-                  </button>
-                </td>
+            {goods.map(element => {
+              const valueSelector = curentSelector === element;
 
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  { element }
-                </td>
-              </tr>
-            ))}
+              return (
+                <tr
+                  key={element}
+                  data-cy="Good"
+                  className={classNames(
+                    '',
+                    { 'has-background-success-light': valueSelector },
+                  )}
+                >
+                  <td>
+                    <button
+                      data-cy={valueSelector
+                        ? 'RemoveButton'
+                        : 'AddButton'}
+                      type="button"
+                      className={classNames(
+                        'button',
+                        { 'button is-info': valueSelector },
+                      )}
+                      onClick={() => {
+                        this.chengSelecktor(element);
+                      }}
+                    >
+                      {valueSelector
+                        ? '-'
+                        : '+'}
+                    </button>
+                  </td>
+
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    { element }
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </main>
