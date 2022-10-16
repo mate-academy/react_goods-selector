fb340fdb70b8e969a14c5efe2e63de5917abe9fd
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-10-16 19:15:53 +0300

    solution with readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import "bulma/css/bulma.css";
-import "./App.scss";
+import React from 'react';
+import 'bulma/css/bulma.css';
+import './App.scss';
 
 export const goods = [
-  { id: 1, name: "Dumplings" },
-  { id: 2, name: "Carrot" },
-  { id: 3, name: "Eggs" },
-  { id: 4, name: "Ice cream" },
-  { id: 5, name: "Apple" },
-  { id: 6, name: "Bread" },
-  { id: 7, name: "Fish" },
-  { id: 8, name: "Honey" },
-  { id: 9, name: "Jam" },
-  { id: 10, name: "Garlic" },
+  { id: 1, name: 'Dumplings' },
+  { id: 2, name: 'Carrot' },
+  { id: 3, name: 'Eggs' },
+  { id: 4, name: 'Ice cream' },
+  { id: 5, name: 'Apple' },
+  { id: 6, name: 'Bread' },
+  { id: 7, name: 'Fish' },
+  { id: 8, name: 'Honey' },
+  { id: 9, name: 'Jam' },
+  { id: 10, name: 'Garlic' },
 ];
 
 type State = {
@@ -24,7 +24,7 @@ type Props = {};
 export class App extends React.Component<Props, State> {
   state: Readonly<State> = {
     // eslint-disable-next-line react/no-unused-state
-    selectedGood: "",
+    selectedGood: '',
   };
 
   render(): React.ReactNode {
@@ -33,13 +33,13 @@ export class App extends React.Component<Props, State> {
         <h1 className="title is-flex is-align-items-center">
           {this.state.selectedGood
             ? `${this.state.selectedGood} is selected`
-            : "No goods selected"}
+            : 'No goods selected'}
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           <button
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => this.setState({ selectedGood: "" })}
+            onClick={() => this.setState({ selectedGood: '' })}
           />
         </h1>
 
@@ -51,8 +51,8 @@ export class App extends React.Component<Props, State> {
                 key={item.id}
                 className={`${
                   item.name === this.state.selectedGood
-                    ? "has-background-success-light"
-                    : ""
+                    ? 'has-background-success-light'
+                    : ''
                 }`}
               >
                 <td>
@@ -60,19 +60,19 @@ export class App extends React.Component<Props, State> {
                     data-cy="AddButton"
                     type="button"
                     className={`button ${
-                      item.name === this.state.selectedGood ? "is-info" : ""
+                      item.name === this.state.selectedGood ? 'is-info' : ''
                     }`}
                     onClick={() => {
                       // eslint-disable-next-line react/no-unused-state
                       this.setState(
                         // eslint-disable-next-line react/no-access-state-in-setstate
                         this.state.selectedGood === item.name
-                          ? { selectedGood: "" }
-                          : { selectedGood: item.name }
+                          ? { selectedGood: '' }
+                          : { selectedGood: item.name },
                       );
                     }}
                   >
-                    {item.name === this.state.selectedGood ? "-" : "+"}
+                    {item.name === this.state.selectedGood ? '-' : '+'}
                   </button>
                 </td>
 
