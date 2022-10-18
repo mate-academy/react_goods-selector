9775046456705287bcc8ba7a6d19ceae87ba2fa1
Artem Musii <musiy.artem@gmail.com>
2022-10-18 16:32:13 +0300

    uuid keys added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 import classNames from 'classnames';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid';
 
 export const goods = [
   'Dumplings',
@@ -34,49 +36,53 @@ export class App extends Component<{}, State> {
   };
 
   render() {
+    const { selectedGood } = this.state;
+
     return (
       <main className="section container">
-        {this.state.selectedGood
-          ? (
-            <h1 className="title is-flex is-align-items-center">
-              {`${this.state.selectedGood} is selected`}
-              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-              <button
-                data-cy="ClearButton"
-                type="button"
-                className="delete ml-3"
-                onClick={() => this.removeFromSelected()}
-              />
-            </h1>
-          )
-          : (
-            <h1 className="title">
-              No goods selected
-            </h1>
-          )}
+        <h1 className={classNames(
+          'title',
+          { 'is-flex is-align-items-center': selectedGood },
+        )}
+        >
+          {selectedGood
+            ? (
+              <>
+                {`${selectedGood} is selected`}
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  data-cy="ClearButton"
+                  type="button"
+                  className="delete ml-3"
+                  onClick={this.removeFromSelected}
+                />
+              </>
+            )
+            : 'No goods selected'}
+        </h1>
 
         <table className="table">
           <tbody>
             {goods.map(good => (
               <tr
                 data-cy="Good"
-                key={good}
+                key={uuidv4()}
                 className={classNames(
                   {
                     'has-background-success-light':
-                    good === this.state.selectedGood,
+                    good === selectedGood,
                   },
                 )}
               >
                 <td>
                   {
-                    this.state.selectedGood === good
+                    selectedGood === good
                       ? (
                         <button
                           data-cy="RemoveButton"
                           type="button"
                           className="button is-info"
-                          onClick={() => this.removeFromSelected()}
+                          onClick={this.removeFromSelected}
                         >
                           -
                         </button>
