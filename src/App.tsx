442a014cbba904e84abee90c14c0ea8065b67d1a
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-18 14:56:15 +0300

    fixed id and repeating h1 tag problem

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,46 @@ import './App.scss';
 import classNames from 'classnames';
 
 export const goods = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
+  {
+    name: 'Dumplings',
+    id: 1,
+  },
+  {
+    name: 'Carrot',
+    id: 2,
+  },
+  {
+    name: 'Eggs',
+    id: 3,
+  },
+  {
+    name: 'Ice cream',
+    id: 4,
+  },
+  {
+    name: 'Apple',
+    id: 5,
+  },
+  {
+    name: 'Bread',
+    id: 6,
+  },
+  {
+    name: 'Fish',
+    id: 7,
+  },
+  {
+    name: 'Honey',
+    id: 8,
+  },
+  {
+    name: 'Jam',
+    id: 9,
+  },
+  {
+    name: 'Garlic',
+    id: 10,
+  },
 ];
 
 type State = {
@@ -33,25 +63,29 @@ export class App extends Component<{}, State> {
 
     return (
       <main className="section container">
-        {selectedGood === ''
-          ? <h1 className="title">No goods selected</h1>
-          : null}
-        {selectedGood && (
-          <h1 className="title is-flex is-align-items-center">
-            {selectedGood && `${selectedGood} is selected`}
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => {
-                this.setState({
-                  selectedGood: '',
-                });
-              }}
-            />
-          </h1>
-        )}
+        <h1 className={!selectedGood
+          ? 'title'
+          : 'title is-flex is-align-items-center'}
+        >
+          {selectedGood === ''
+            ? 'No goods selected'
+            : (
+              <>
+                {`${selectedGood} is selected`}
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  data-cy="ClearButton"
+                  type="button"
+                  className="delete ml-3"
+                  onClick={() => {
+                    this.setState({
+                      selectedGood: '',
+                    });
+                  }}
+                />
+              </>
+            )}
+        </h1>
 
         <table className="table">
           <tbody>
@@ -60,36 +94,39 @@ export class App extends Component<{}, State> {
                 data-cy="Good"
                 className={
                   classNames(
-                    { 'has-background-success-light': selectedGood === good },
+                    {
+                      'has-background-success-light':
+                    selectedGood === good.name,
+                    },
                   )
                 }
-                key={good}
+                key={good.id}
               >
                 <td>
                   <button
-                    data-cy={selectedGood === good
+                    data-cy={selectedGood === good.name
                       ? 'RemoveButton'
                       : 'AddButton'}
                     type="button"
                     className={
                       classNames('button',
-                        { 'is-info': selectedGood === good })
+                        { 'is-info': selectedGood === good.name })
                     }
                     onClick={() => {
-                      if (selectedGood === good) {
+                      if (selectedGood === good.name) {
                         this.setState({ selectedGood: '' });
                       } else {
-                        this.setState({ selectedGood: good });
+                        this.setState({ selectedGood: good.name });
                       }
                     }}
                   >
-                    {selectedGood === good
+                    {selectedGood === good.name
                       ? '-'
                       : '+'}
                   </button>
                 </td>
                 <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
+                  {good.name}
                 </td>
               </tr>
             ))}
