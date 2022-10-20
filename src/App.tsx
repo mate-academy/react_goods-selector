81153dd3a890941dd4fd3912e37a7a1ca624b075
Kamil Witek <kamilprogramuje@gmail.com>
2022-10-20 23:42:09 +0200

    add good removed - used only once, readibility improved

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,8 @@ export class App extends React.Component {
     selectedGood: 'Jam',
   };
 
-  cleanGoods = () => this.setState(
+  clearGoods = () => this.setState(
     { selectedGood: '' },
-  );
-
-  addGood = (good: string) => this.setState(
-    { selectedGood: good },
   );
 
   render() {
@@ -44,7 +40,7 @@ export class App extends React.Component {
                 data-cy="ClearButton"
                 type="button"
                 className="delete ml-3"
-                onClick={this.cleanGoods}
+                onClick={this.clearGoods}
               />
             </h1>
           )
@@ -56,66 +52,64 @@ export class App extends React.Component {
 
         <table className="table">
           <tbody>
-            {goods.map(good => {
-              const isSelected = good === selectedGood;
-
-              return (
-                <>
-                  {
-                    isSelected
-                      ? (
-                        <tr
-                          data-cy="Good"
-                          key={good}
-                          className={`${
-                            'has-background-success-light'
-                          }`}
-                        >
-                          <td>
-                            <button
-                              data-cy="RemoveButton"
-                              type="button"
-                              className="button is-info"
-                              onClick={this.cleanGoods}
-                            >
-                              -
-                            </button>
-                          </td>
-                          <td
-                            data-cy="GoodTitle"
-                            className="is-vcentered"
+            {goods.map(good => (
+              <>
+                {
+                  good === selectedGood
+                    ? (
+                      <tr
+                        data-cy="Good"
+                        key={good}
+                        className={`${
+                          'has-background-success-light'
+                        }`}
+                      >
+                        <td>
+                          <button
+                            data-cy="RemoveButton"
+                            type="button"
+                            className="button is-info"
+                            onClick={this.clearGoods}
                           >
-                            {good}
-                          </td>
-                        </tr>
-                      )
-                      : (
-                        <tr
-                          data-cy="Good"
-                          key={good}
+                            -
+                          </button>
+                        </td>
+                        <td
+                          data-cy="GoodTitle"
+                          className="is-vcentered"
                         >
-                          <td>
-                            <button
-                              data-cy="AddButton"
-                              type="button"
-                              className="button"
-                              onClick={() => this.addGood(good)}
-                            >
-                              +
-                            </button>
-                          </td>
-                          <td
-                            data-cy="GoodTitle"
-                            className="is-vcentered"
+                          {good}
+                        </td>
+                      </tr>
+                    )
+                    : (
+                      <tr
+                        data-cy="Good"
+                        key={good}
+                      >
+                        <td>
+                          <button
+                            data-cy="AddButton"
+                            type="button"
+                            className="button"
+                            onClick={() => (
+                              this.setState({ selectedGood: good })
+                            )}
                           >
-                            {good}
-                          </td>
-                        </tr>
-                      )
-                  }
-                </>
-              );
-            })}
+                            +
+                          </button>
+                        </td>
+                        <td
+                          data-cy="GoodTitle"
+                          className="is-vcentered"
+                        >
+                          {good}
+                        </td>
+                      </tr>
+                    )
+                }
+              </>
+            ))}
           </tbody>
         </table>
       </main>
