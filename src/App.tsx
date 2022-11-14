cdbdb9db33c816ba17a9dd2694ac9e396187de10
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-11-14 17:57:10 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -15,7 +16,101 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
+type State = {
+  selectedProduct: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedProduct: 'Jam',
+  };
+
+  render() {
+    return (
+      <main className="section container">
+        <h1 className="title is-flex is-align-items-center">
+          {
+            this.state.selectedProduct
+              ? `${this.state.selectedProduct} is selected`
+              : 'No goods selected'
+          }
+
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          {
+            this.state.selectedProduct && (
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={() => {
+                  this.setState({ selectedProduct: '' });
+                }}
+              >
+                {}
+              </button>
+            )
+          }
+        </h1>
+
+        <table className="table">
+          <tbody>
+            {
+              goods.map(product => (
+                <tr
+                  data-cy="Good"
+                  className={
+                    classNames({
+                      'has-background-success-light':
+                      this.state.selectedProduct === product,
+                    })
+                  }
+                >
+                  <td>
+                    {
+                      this.state.selectedProduct !== product
+                        ? (
+                          <button
+                            data-cy="AddButton"
+                            type="button"
+                            className="button"
+                            onClick={() => {
+                              this.setState({ selectedProduct: product });
+                            }}
+                          >
+                            +
+                          </button>
+                        )
+                        : (
+                          <button
+                            data-cy="RemoveButton"
+                            type="button"
+                            className="button is-info"
+                            onClick={() => {
+                              this.setState({ selectedProduct: '' });
+                            }}
+                          >
+                            -
+                          </button>
+                        )
+                    }
+
+                  </td>
+
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {product}
+                  </td>
+                </tr>
+              ))
+            }
+
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
+
+(
   <main className="section container">
     <h1 className="title">No goods selected</h1>
 
@@ -61,22 +156,6 @@ export const App: React.FC = () => (
 
           <td data-cy="GoodTitle" className="is-vcentered">
             Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
           </td>
         </tr>
       </tbody>
