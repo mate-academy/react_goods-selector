cb72f1c214ced1fbf60914f80250ed7c6223aa50
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-11-16 17:16:11 +0200

    Fixed2

--- src/App.tsx
+++ src/App.tsx
@@ -26,28 +26,25 @@ export class App extends React.Component<{}, State> {
   };
 
   render() {
-    const goodsSelectedMessage = `${this.state.selectedProduct} is selected`;
+    const goodsSelectedMessage = this.state.selectedProduct
+      ? `${this.state.selectedProduct} is selected`
+      : 'No goods selected';
 
     return (
       <main className="section container">
         <h1 className="title is-flex is-align-items-center">
-          {
-            this.state.selectedProduct
-              ? goodsSelectedMessage
-              : 'No goods selected'
-          }
+          {goodsSelectedMessage}
           {
             this.state.selectedProduct && (
               <button
                 data-cy="ClearButton"
+                aria-label="ClearButton"
                 type="button"
                 className="delete ml-3"
                 onClick={() => {
                   this.setState({ selectedProduct: '' });
                 }}
-              >
-                {}
-              </button>
+              />
             )
           }
         </h1>
@@ -55,53 +52,58 @@ export class App extends React.Component<{}, State> {
         <table className="table">
           <tbody>
             {
-              goods.map(product => (
-                <tr
-                  data-cy="Good"
-                  className={
-                    classNames({
-                      'has-background-success-light':
-                      this.state.selectedProduct === product,
-                    })
-                  }
-                >
-                  <td>
-                    <button
-                      data-cy={
-                        this.state.selectedProduct !== product
-                          ? 'AddButton'
-                          : 'RemoveButton'
-                      }
-                      type="button"
-                      className={
-                        classNames(
-                          'button',
-                          { 'is-info': this.state.selectedProduct === product },
-                        )
-                      }
-                      onClick={
-                        this.state.selectedProduct !== product
-                          ? () => {
-                            this.setState({ selectedProduct: product });
-                          }
-                          : () => {
-                            this.setState({ selectedProduct: '' });
-                          }
-                      }
-                    >
-                      {
-                        this.state.selectedProduct !== product
-                          ? '+'
-                          : '-'
-                      }
-                    </button>
-                  </td>
+              goods.map(product => {
+                const goodsData = this.state.selectedProduct !== product
+                  ? 'AddButton'
+                  : 'RemoveButton';
 
-                  <td data-cy="GoodTitle" className="is-vcentered">
-                    {product}
-                  </td>
-                </tr>
-              ))
+                const clickEvent = this.state.selectedProduct !== product
+                  ? () => {
+                    this.setState({ selectedProduct: product });
+                  }
+                  : () => {
+                    this.setState({ selectedProduct: '' });
+                  };
+
+                const buttonContent = this.state.selectedProduct !== product
+                  ? '+'
+                  : '-';
+
+                return (
+                  <tr
+                    data-cy="Good"
+                    className={
+                      classNames({
+                        'has-background-success-light':
+                          this.state.selectedProduct === product,
+                      })
+                    }
+                  >
+                    <td>
+                      <button
+                        data-cy={goodsData}
+                        type="button"
+                        className={
+                          classNames(
+                            'button',
+                            {
+                              'is-info':
+                                this.state.selectedProduct === product,
+                            },
+                          )
+                        }
+                        onClick={clickEvent}
+                      >
+                        {buttonContent}
+                      </button>
+                    </td>
+
+                    <td data-cy="GoodTitle" className="is-vcentered">
+                      {product}
+                    </td>
+                  </tr>
+                );
+              })
             }
           </tbody>
         </table>
