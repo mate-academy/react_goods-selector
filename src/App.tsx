a9608d256bc1723025ea393b87eddc96b0505950
Svitlana Yudina <osbka31@gmail.com>
2022-11-22 14:06:46 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -37,22 +37,25 @@ export class App extends React.Component<{}, State> {
 
     return (
       <main className="section container">
-        <h1 className="title is-flex is-align-items-center">
-          {selectedGood
-            ? `${selectedGood} is selected`
-            : 'No goods selected'}
+        {selectedGood
+          ? (
+            <h1 className="title is-flex is-align-items-center">
+              {`${selectedGood} is selected`}
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={this.clearSelection}
+              />
+            </h1>
+          )
 
-          {selectedGood
-          && (
-          /* eslint-disable-next-line jsx-a11y/control-has-associated-label */
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={this.clearSelection}
-            />
+          : (
+            <h1 className="title">
+              No goods selected
+            </h1>
           )}
-        </h1>
 
         <table className="table">
           <tbody>
