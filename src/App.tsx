bb3ad80a75e99b4aec1591eea165dd5fc27be54b
bokorzita <104830913+bokorzita@users.noreply.github.com>
2022-11-27 22:15:15 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,73 @@
-import React from 'react';
+/* eslint-disable */
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
-export const goods = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+export const goods = ['Dumplings', 'Carrot', 'Eggs',
+  'Ice cream', 'Apple', 'Bread', 'Fish', 'Honey', 'Jam', 'Garlic'];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+export const App: React.FC = () => {
+  const [selectedGoods, setSelectedGoods] = useState<string[]>([]);
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  const clearAll = () => {
+    return setSelectedGoods([]);
+  };
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+  const handleSelectGood = (good: string) => {
+    setSelectedGoods(
+      (selectedGoods: string[]) => (selectedGoods.includes(good)
+        ? selectedGoods.filter((selectedGood) => selectedGood !== good)
+        : [...selectedGoods, good]),
+    );
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
+  return (
+    <main className="section container">
+      {selectedGoods.length < 1
+        ? <h1 className="title">No goods selected</h1>
+        : (
+          <h1 className="title is-flex is-align-items-center">
+            {`${selectedGoods.join(', ')} is selected`}
             <button
-              data-cy="AddButton"
+              onClick={clearAll}
+              className="delete ml-3"
               type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+              data-cy="ClearButton"
+            />
+          </h1>
+        )}
+      <table className="table">
+        <tbody>
+          {goods.map((good) => {
+            const isSelected = selectedGoods.includes(good);
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+            return (
+              <tr
+                key={good}
+                className={isSelected ? 'has-background-success-light' : ''}
+                data-cy="Good"
+              >
+                <td>
+                  <button
+                    id={good}
+                    onClick={() => handleSelectGood(good)}
+                    className="button"
+                    data-cy="AddButton"
+                  >
+                    {isSelected ? '-' : '+'}
+                  </button>
+                </td>
+                <td
+                  className="is-vcentered"
+                  data-cy="GoodTitle"
+                >
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
+};
