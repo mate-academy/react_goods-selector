dfcb67704ab6fc7b42c69749fb6b2805f89ab8ea
Lefteris Loizides <loizides.lef@gmail.com>
2022-12-30 00:38:26 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 
-import { Component, FC, ReactNode } from 'react';
+import { Component, ReactNode } from 'react';
 import cn from 'classnames';
 
 import 'bulma/css/bulma.css';
@@ -20,115 +20,96 @@ const goods = [
 ];
 
 type State = { selectedGood: string };
-type GoodProps = { good: string };
-type ButtonProps = {
-  isSelected: boolean,
-  setSelected: () => void
-};
 
 export class App extends Component<{}, State> {
   state = { selectedGood: 'Jam' };
 
-  Good: FC<GoodProps> = ({ good }) => {
-    const { selectedGood } = this.state;
-    const isSelected = good === selectedGood;
+  isSelected = (item: string) => (
+    item === this.state.selectedGood
+  );
 
-    const setSelected = () => (
-      this.setState({
-        selectedGood: isSelected
-          ? ''
-          : good,
-      })
-    );
+  clearSelected = () => (
+    this.setState({ selectedGood: '' })
+  );
+
+  setSelected = (item: string) => (
+    this.setState({
+      selectedGood: this.isSelected(item)
+        ? ''
+        : item,
+    })
+  );
+
+  render = (): ReactNode => {
+    const { selectedGood } = this.state;
 
     return (
-      <tr data-cy="Good">
-        <td>
-          <this.Button
-            isSelected={isSelected}
-            setSelected={setSelected}
-          />
-        </td>
-
-        <td
-          data-cy="GoodTitle"
+      <main className="section container">
+        <h1
           className={cn(
-            'is-vcentered', {
-              'has-background-success-light': isSelected,
+            'title', {
+              'is-flex is-align-items-center': selectedGood,
             },
           )}
         >
-          {good}
-        </td>
-      </tr>
+          {selectedGood
+            ? (
+              <>
+                {`${selectedGood} is selected`}
+
+                <button
+                  data-cy="ClearButton"
+                  type="button"
+                  className="delete ml-3"
+                  onClick={this.clearSelected}
+                />
+              </>
+            )
+            : 'No goods selected'}
+        </h1>
+
+        <table className="table">
+          <tbody>
+            {goods.map(good => {
+              const isSelected = this.isSelected(good);
+              const setSelected = () => this.setSelected(good);
+
+              return (
+                <tr data-cy="Good">
+                  <td>
+                    <button
+                      type="button"
+                      data-cy={isSelected
+                        ? 'RemoveButton'
+                        : 'AddButton'}
+                      className={cn(
+                        'button',
+                        { 'is-info': isSelected },
+                      )}
+                      onClick={setSelected}
+                    >
+                      {isSelected
+                        ? '-'
+                        : '+'}
+                    </button>
+                  </td>
+
+                  <td
+                    data-cy="GoodTitle"
+                    className={cn(
+                      'is-vcentered', {
+                        'has-background-success-light': isSelected,
+                      },
+                    )}
+                  >
+                    {good}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
     );
   };
-
-  Button: FC<ButtonProps> = ({ isSelected, setSelected }) => (
-    <button
-      type="button"
-      data-cy={isSelected
-        ? 'RemoveButton'
-        : 'AddButton'}
-      className={cn(
-        'button',
-        { 'is-info': isSelected },
-      )}
-      onClick={setSelected}
-    >
-      {isSelected ? '-' : '+'}
-    </button>
-  );
-
-  GoodList: FC<{}> = () => (
-    <table className="table">
-      <tbody>
-        {goods.map(good => (
-          <this.Good
-            key={good}
-            good={good}
-          />
-        ))}
-      </tbody>
-    </table>
-  );
-
-  Title: FC<{}> = () => {
-    const { selectedGood } = this.state;
-    const clearSelected = () => (
-      this.setState({ selectedGood: '' })
-    );
-
-    return (
-      <h1
-        className={cn(
-          'title', {
-            'is-flex is-align-items-center': selectedGood,
-          },
-        )}
-      >
-        {selectedGood
-          ? (
-            <>
-              {`${selectedGood} is selected`}
-
-              <button
-                data-cy="ClearButton"
-                type="button"
-                className="delete ml-3"
-                onClick={clearSelected}
-              />
-            </>
-          )
-          : 'No goods selected'}
-      </h1>
-    );
-  };
-
-  render = (): ReactNode => (
-    <main className="section container">
-      <this.Title />
-      <this.GoodList />
-    </main>
-  );
 }
