69d2934d5a7fec1b3b8e6beba412117c7f41dafc
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-30 11:48:18 +0200

    Some excess code was removed

--- src/App.tsx
+++ src/App.tsx
@@ -42,21 +42,21 @@ export class App extends React.Component <{}, State> {
 
     return (
       <main className="section container">
-        {selectedGood === '' ? (
-          <h1 className="title">No goods selected</h1>
-        ) : (
-          <h1 className="title is-flex is-align-items-center">
-            {`${selectedGood} is selected`}
+        {!selectedGood
+          ? <h1 className="title">No goods selected</h1>
+          : (
+            <h1 className="title is-flex is-align-items-center">
+              {`${selectedGood} is selected`}
 
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button
-              onClick={this.remove}
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-            />
-          </h1>
-        )}
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                onClick={this.remove}
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+              />
+            </h1>
+          )}
 
         <table className="table">
           <tbody>
