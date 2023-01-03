9fa21f3c7f67036da4d01cdbe5cab1280f0b2909
OLEXANDRA KOROL <sandrakorol3@gmail.com>
2023-01-03 23:30:43 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -47,18 +47,18 @@ export class App extends React.Component<{}, State> {
             <h1 className="title is-flex is-align-items-center">
               {`${selectedGood} is selected`}
 
-               {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
               <button
                 data-cy="ClearButton"
                 type="button"
                 className="delete ml-3"
-                onClick={() => this.removeClick()}
+                onClick={this.removeClick}
               />
             </h1>
           )
           : (
             <h1 className="title">
-              {`No goods selected`}
+              No goods selected
             </h1>
           )}
 
@@ -92,7 +92,7 @@ export class App extends React.Component<{}, State> {
                         data-cy="RemoveButton"
                         type="button"
                         className="button is-info"
-                        onClick={() => this.removeClick()}
+                        onClick={this.removeClick}
                       >
                         -
                       </button>
@@ -111,70 +111,3 @@ export class App extends React.Component<{}, State> {
   }
 }
 
-// {/* <main className="section container">
-//   <h1 className="title">No goods selected</h1>
-
-//   <h1 className="title is-flex is-align-items-center">
-//     Jam is selected
-
-//     {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */
-//   <button
-//     data-cy="ClearButton"
-//     type="button"
-//     className="delete ml-3"
-//   />
-//   </h1 >
-
-//     <table className="table">
-//       <tbody>
-//         <tr data-cy="Good">
-//           <td>
-//             <button
-//               data-cy="AddButton"
-//               type="button"
-//               className="button"
-//             >
-//               +
-//             </button>
-//           </td>
-
-//           <td data-cy="GoodTitle" className="is-vcentered">
-//             Dumplings
-//           </td>
-//         </tr>
-
-//         <tr data-cy="Good" className="has-background-success-light">
-//           <td>
-//             <button
-//               data-cy="RemoveButton"
-//               type="button"
-//               className="button is-info"
-//             >
-//               -
-//             </button>
-//           </td>
-
-//           <td data-cy="GoodTitle" className="is-vcentered">
-//             Jam
-//           </td>
-//         </tr>
-
-//         <tr data-cy="Good">
-//           <td>
-//             <button
-//               data-cy="AddButton"
-//               type="button"
-//               className="button"
-//             >
-//               +
-//             </button>
-//           </td>
-
-//           <td data-cy="GoodTitle" className="is-vcentered">
-//             Garlic
-//           </td>
-//         </tr>
-//       </tbody>
-//     </table>
-// </main > * /} */
-// // }
