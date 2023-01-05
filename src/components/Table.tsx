ff156ea9456c8da93afa46ee47e7ae84bbce0c37
Kenneth Delgado <kenvastolord@gmail.com>
2023-01-05 16:46:16 +0100

    solution

---
+++ src/components/Table.tsx
@@ -0,0 +1,55 @@
+export const Table = () => {
+  return (
+    <table className="table">
+      <tbody>
+        <tr data-cy="Good">
+          <td>
+            <button
+              data-cy="AddButton"
+              type="button"
+              className="button"
+            >
+              +
+            </button>
+          </td>
+
+          <td data-cy="GoodTitle" className="is-vcentered">
+            Dumplings
+          </td>
+        </tr>
+
+        <tr data-cy="Good" className="has-background-success-light">
+          <td>
+            <button
+              data-cy="RemoveButton"
+              type="button"
+              className="button is-info"
+            >
+              -
+            </button>
+          </td>
+
+          <td data-cy="GoodTitle" className="is-vcentered">
+            Jam
+          </td>
+        </tr>
+
+        <tr data-cy="Good">
+          <td>
+            <button
+              data-cy="AddButton"
+              type="button"
+              className="button"
+            >
+              +
+            </button>
+          </td>
+
+          <td data-cy="GoodTitle" className="is-vcentered">
+            Garlic
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
