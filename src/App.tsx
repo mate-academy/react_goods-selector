a8c5e0f424993e0b94d6f09fc48feb0987685509
Anastasiia <anastasianauk44@gmail.com>
2023-01-09 17:05:58 +0200

    arrow function in onClick was deleted

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ export class App extends React.Component<{}, State> {
                         data-cy="RemoveButton"
                         type="button"
                         className="button is-info"
-                        onClick={() => this.handleClickRemoved()}
+                        onClick={this.handleClickRemoved}
                       >
                         -
                       </button>
