e2492f5d26214c3399cee5861c5f0bd239acd421
maasiimka <mrozisalovec@gmail.com>
2023-01-31 10:27:44 +0200

    Fixed bag

--- src/App.tsx
+++ src/App.tsx
@@ -42,21 +42,18 @@ export class App extends Component<{}, State> {
           selectedGood === ''
             ? <h1 className="title">No goods selected</h1>
             : (
-              <div className="is-flex is-align-items-center">
-                <h1 className="title mb-0">{`${selectedGood} is selected`}</h1>
+              <h1 className="title is-flex is-align-items-center">
+                {`${selectedGood} is selected`}
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                 <button
                   data-cy="ClearButton"
                   type="button"
                   className="delete ml-3"
                   onClick={clearGood}
-                >
-                  {}
-                </button>
-              </div>
+                />
+              </h1>
             )
         }
-
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
 
         <table className="table">
           <tbody>
