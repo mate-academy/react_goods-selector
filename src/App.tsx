c2eea0ad4b3a3ea32f8412ebfae22948a09ebff5
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-25 22:54:07 +0200

    did the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,71 +15,109 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+type State = {
+  selectedGoods: string,
+};
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+function removeClass(
+  collection: NodeListOf<HTMLButtonElement | HTMLTableRowElement>,
+  className: string,
+) {
+  collection.forEach(element => {
+    element.classList.remove(className);
+  });
+}
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGoods: 'Jam',
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+  unsetCells = () => {
+    const cells = document.querySelectorAll('tr');
+    const buttons = document.querySelectorAll('button');
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+    buttons.forEach((b: HTMLButtonElement) => {
+      // eslint-disable-next-line no-param-reassign
+      b.innerText = '+';
+    });
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+    removeClass(buttons, 'is-info');
+    removeClass(cells, 'has-background-success-light');
+  };
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+  setCell = (event: React.SyntheticEvent) => {
+    const button = event.currentTarget as HTMLElement;
+    const cell = button.parentElement?.parentElement;
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+    cell?.classList.add('has-background-success-light');
+    button.classList.add('is-info');
+    button.innerText = '-';
+  };
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+  toggleCell(event: React.SyntheticEvent) {
+    const currentGood
+      = event.currentTarget.parentElement?.nextSibling as HTMLElement;
+
+    if (this.state.selectedGoods === currentGood.innerText) {
+      this.unsetCells();
+      this.setState({ selectedGoods: '' });
+    } else {
+      this.unsetCells();
+      this.setState({ selectedGoods: currentGood?.innerText || '' });
+      this.setCell(event);
+    }
+  }
+
+  render() {
+    const { selectedGoods } = this.state;
+
+    return (
+      <main className="section container">
+        {selectedGoods
+          ? (
+            <h1 className="title is-flex is-align-items-center">
+              {`${selectedGoods} is selected`}
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={() => {
+                  this.unsetCells();
+                  this.setState({ selectedGoods: '' });
+                }}
+              />
+            </h1>
+          )
+          : (<h1 className="title">No goods selected</h1>)}
+
+        <table className="table">
+          <tbody>
+            {goods.map(good => (
+              <tr key={good} data-cy="Good">
+                <td>
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={(event) => {
+                      this.toggleCell(event);
+                    }}
+                  >
+                    +
+                  </button>
+                </td>
+
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
