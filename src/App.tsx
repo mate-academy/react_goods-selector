ac9db493dd5ca5099b541eb9b313c4c0b6b7de7b
Nadiia Kotliar <eknakaron@gmail.com>
2023-03-02 13:34:40 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ type State = {
 
 export class App extends Component<{}, State> {
   state = {
-    selectedGood: 'Jam',
+    selectedGood: goods[goods.indexOf('Jam')] || goods[0],
   };
 
   clearHendler = () => {
