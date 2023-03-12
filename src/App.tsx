7165c3d6031b904637b2a17e4ffe9cc01303ae6e
timoxa2t <alex2t172@gmail.com>
2023-03-12 12:38:05 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -15,71 +16,122 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+type BtnProps = {
+  dataCy: string,
+  content: string,
+  classes?: string[],
+  handler: () => void
+};
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
-
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
+const GoodsButton: React.FC<BtnProps> = (
+  {
+    dataCy,
+    content,
+    handler,
+    classes = [],
+  },
+) => (
+  <button
+    data-cy={dataCy}
+    type="button"
+    className={classNames(...classes)}
+    onClick={() => handler()}
+  >
+    {content}
+  </button>
 );
+
+GoodsButton.defaultProps = {
+  classes: ['button'],
+};
+
+type State = {
+  selectedGood: string,
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGood: 'Jam',
+  };
+
+  handleAdd = (good: string) => {
+    this.setState(
+      {
+        selectedGood: good,
+      },
+    );
+  };
+
+  handleRemove = () => {
+    this.setState(
+      {
+        selectedGood: '',
+      },
+    );
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="section container">
+        {
+          selectedGood
+            ? (
+              <h1 className="title is-flex is-align-items-center">
+                {`${selectedGood} is selected`}
+                <GoodsButton
+                  dataCy="ClearButton"
+                  content=""
+                  classes={['delete', 'ml-3']}
+                  handler={() => this.handleRemove()}
+                />
+              </h1>
+            )
+            : (<h1 className="title">No goods selected</h1>)
+        }
+
+        <table className="table">
+          <tbody>
+            {goods.map(good => (
+              <tr
+                key={good}
+                data-cy="Good"
+                className={classNames(
+                  {
+                    'has-background-success-light': good === selectedGood,
+                  },
+                )}
+              >
+                <td>
+                  {
+                    good === selectedGood
+                      ? (
+                        <GoodsButton
+                          dataCy="RemoveButton"
+                          content="-"
+                          classes={['button', 'is-info']}
+                          handler={() => this.handleRemove()}
+                        />
+                      )
+                      : (
+                        <GoodsButton
+                          dataCy="AddButton"
+                          content="+"
+                          handler={() => this.handleAdd(good)}
+                        />
+                      )
+                  }
+                </td>
+
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
