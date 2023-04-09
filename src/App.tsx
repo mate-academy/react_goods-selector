470576c44d256ed9b09c71e7cf6447f632626bc3
andbilous <andbilous@gmail.com>
2023-04-09 21:22:28 +0200

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -22,64 +22,79 @@ interface GoodProps {
 }
 
 const Good: React.FC<GoodProps> = ({ selectedGood, setSelectedGood, name }) => (
-  <tr data-cy="Good" className={selectedGood === name ? 'has-background-success-light' : ''}>
+  <tr
+    data-cy="Good"
+    className={selectedGood === name
+      ? 'has-background-success-light'
+      : ''}
+  >
     <td>
-      {selectedGood === name ? <button
-        data-cy="RemoveButton"
-        type="button"
-        className="button is-info"
-        onClick={() => setSelectedGood('')}
-      >
-        -
-      </button>
-        :
+      {selectedGood === name ? (
         <button
-          data-cy="AddButton"
+          aria-label="Remove"
+          data-cy="RemoveButton"
           type="button"
-          className="button"
-          onClick={() => setSelectedGood(name)}
+          className="button is-info"
+          onClick={() => setSelectedGood('')}
         >
-          +
+          -
         </button>
-      }
+      )
+        : (
+          <button
+            aria-label="Add"
+            data-cy="AddButton"
+            type="button"
+            className="button"
+            onClick={() => setSelectedGood(name)}
+          >
+            +
+          </button>
+        )}
     </td>
 
     <td data-cy="GoodTitle" className="is-vcentered">
       {name}
     </td>
   </tr>
-)
+);
 
 export const App: React.FC = () => {
   const [selectedGood, setSelectedGood] = React.useState('Jam');
+
   return (
     <main className="section container">
-
-      <h1 className="title is-flex is-align-items-center has-background-success-light">
-
+      <h1
+        className="
+          title is-flex
+          is-align-items-center
+          has-background-success-light"
+      >
         {selectedGood.length > 0 ? `${selectedGood} is selected` : 'No goods selected'}
-
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        {selectedGood && <button
-          data-cy="ClearButton"
-          type="button"
-          className="delete ml-3"
-          onClick={() => setSelectedGood('')}
-        />}
+        {selectedGood && (
+          <button
+            aria-label="Clear"
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setSelectedGood('')}
+          />
+        )}
       </h1>
 
       <table className="table">
         <tbody>
-          {goods.map(item => <Good
-            key={item}
-            name={item}
-            selectedGood={selectedGood}
-            setSelectedGood={setSelectedGood}
-          />
-          )}
+          {goods.map(item => (
+            <Good
+              key={item}
+              name={item}
+              selectedGood={selectedGood}
+              setSelectedGood={setSelectedGood}
+            />
+          ))}
         </tbody>
       </table>
     </main>
   );
-}
-
+};
