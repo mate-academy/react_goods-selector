b028f5b696eb1aab626973e9a2875b99d1804bec
andbilous <andbilous@gmail.com>
2023-04-10 21:18:32 +0200

    add sep file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { Good } from './components/Good';
 
 export const goods = [
   'Dumplings',
@@ -15,49 +16,6 @@ export const goods = [
   'Garlic',
 ];
 
-interface GoodProps {
-  name: string;
-  selectedGood: string;
-  setSelectedGood: (value: string) => void;
-}
-
-const Good: React.FC<GoodProps> = ({ selectedGood, setSelectedGood, name }) => (
-  <tr
-    data-cy="Good"
-    className={selectedGood === name
-      ? 'has-background-success-light'
-      : ''}
-  >
-    <td>
-      {selectedGood === name ? (
-        <button
-          aria-label="Remove"
-          data-cy="RemoveButton"
-          type="button"
-          className="button is-info"
-          onClick={() => setSelectedGood('')}
-        >
-          -
-        </button>
-      )
-        : (
-          <button
-            aria-label="Add"
-            data-cy="AddButton"
-            type="button"
-            className="button"
-            onClick={() => setSelectedGood(name)}
-          >
-            +
-          </button>
-        )}
-    </td>
-
-    <td data-cy="GoodTitle" className="is-vcentered">
-      {name}
-    </td>
-  </tr>
-);
 
 export const App: React.FC = () => {
   const [selectedGood, setSelectedGood] = React.useState('Jam');
---
+++ src/components/Good.tsx
@@ -0,0 +1,43 @@
+interface GoodProps {
+  name: string;
+  selectedGood: string;
+  setSelectedGood: (value: string) => void;
+}
+
+export const Good: React.FC<GoodProps> = ({ selectedGood, setSelectedGood, name }) => (
+  <tr
+    data-cy="Good"
+    className={selectedGood === name
+      ? 'has-background-success-light'
+      : ''}
+  >
+    <td>
+      {selectedGood === name ? (
+        <button
+          aria-label="Remove"
+          data-cy="RemoveButton"
+          type="button"
+          className="button is-info"
+          onClick={() => setSelectedGood('')}
+        >
+          -
+        </button>
+      )
+        : (
+          <button
+            aria-label="Add"
+            data-cy="AddButton"
+            type="button"
+            className="button"
+            onClick={() => setSelectedGood(name)}
+          >
+            +
+          </button>
+        )}
+    </td>
+
+    <td data-cy="GoodTitle" className="is-vcentered">
+      {name}
+    </td>
+  </tr>
+);
