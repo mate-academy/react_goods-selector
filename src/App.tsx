ef959e655195379a56e1c09b201ecadf03f55447
yana-kotsulym <yanak7170@gmail.com>
2023-04-11 23:22:29 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+// import { flowRight } from 'cypress/types/lodash';
+// import { log } from 'console';
 
 export const goods = [
   'Dumplings',
@@ -15,71 +17,101 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+type State = {
+  selectedValue: string,
+};
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedValue: 'Jam',
+  };
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+  clearValue = () => {
+    this.setState({ selectedValue: '' });
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+    document.querySelectorAll('tr').forEach(el => {
+      el.classList.remove('has-background-success-light');
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+      const button = el.querySelector('button');
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+      if (button) {
+        button.innerText = '+';
+        button.classList.remove('is-info');
+        button.dataset.cy = 'AddButton';
+      }
+    });
+  };
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+  selectValue = (currentTarget: HTMLButtonElement, value: string) => {
+    if (currentTarget.dataset.cy === 'AddButton') {
+      this.clearValue();
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+      const target = currentTarget;
+      const row = target.closest('tr');
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+      target.innerHTML = '-';
+      target.classList.add('is-info');
+
+      if (row) {
+        row.classList.toggle(
+          'has-background-success-light',
+        );
+      }
+
+      target.dataset.cy = 'RemoveButton';
+      this.setState({ selectedValue: value });
+    } else {
+      this.clearValue();
+    }
+  };
+
+  render() {
+    return (
+      <main className="section container">
+        {this.state.selectedValue
+          ? (
+            <h1 className="title is-flex is-align-items-center">
+              {`${this.state.selectedValue} is selected`}
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={this.clearValue}
+              />
+            </h1>
+          )
+          : <h1 className="title">No goods selected</h1>}
+
+        <table className="table">
+          <tbody>
+            {goods.map(item => (
+              <tr
+                data-cy="Good"
+              >
+                <td>
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={(event) => {
+                      this.selectValue(event.currentTarget, item);
+                    }}
+                  >
+                    +
+                  </button>
+                </td>
+
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {item}
+                </td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
