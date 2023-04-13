cd3130ceb7e34f6fa22807cd6e4849fbdfd41812
andbilous <andbilous@gmail.com>
2023-04-13 12:15:32 +0200

    refactor

--- src/components/Good.tsx
+++ src/components/Good.tsx
@@ -5,40 +5,43 @@ interface GoodProps {
 }
 
 export const Good: React.FC<GoodProps>
-  = ({ selectedGood, setSelectedGood, name }) => (
-    <tr
-      data-cy="Good"
-      className={selectedGood === name
-        ? 'has-background-success-light'
-        : ''}
-    >
-      <td>
-        {selectedGood === name ? (
-          <button
-            aria-label="Remove"
-            data-cy="RemoveButton"
-            type="button"
-            className="button is-info"
-            onClick={() => setSelectedGood('')}
-          >
-            -
-          </button>
-        )
-          : (
+  = ({ selectedGood, setSelectedGood, name }) => {
+    const handleButtonClick = (value = '') => setSelectedGood(value);
+    return (
+      <tr
+        data-cy="Good"
+        className={selectedGood === name
+          ? 'has-background-success-light'
+          : ''}
+      >
+        <td>
+          {selectedGood === name ? (
             <button
-              aria-label="Add"
-              data-cy="AddButton"
+              aria-label="Remove"
+              data-cy="RemoveButton"
               type="button"
-              className="button"
-              onClick={() => setSelectedGood(name)}
+              className="button is-info"
+              onClick={() => handleButtonClick('')}
             >
-              +
+              -
             </button>
-          )}
-      </td>
+          )
+            : (
+              <button
+                aria-label="Add"
+                data-cy="AddButton"
+                type="button"
+                className="button"
+                onClick={() => handleButtonClick(name)}
+              >
+                +
+              </button>
+            )}
+        </td>
 
-      <td data-cy="GoodTitle" className="is-vcentered">
-        {name}
-      </td>
-    </tr>
-  );
+        <td data-cy="GoodTitle" className="is-vcentered">
+          {name}
+        </td>
+      </tr>
+    );
+  }
