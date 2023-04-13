75525eb8ee85a48c71ef72c80f59e25bae7bc219
Ihor <ikruzhynskyy@gmail.com>
2023-04-13 11:03:03 +0300

    created react_goods-selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import 'bulma/css/bulma.css';
 
@@ -17,61 +17,76 @@ export const goods = [
   'Garlic',
 ];
 
-type Product = string | null;
+interface State {
+  product: string | null
+}
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    product: 'Jam',
+  };
 
-export const App: React.FC = () => {
-  const [product, setProduct] = useState<Product>('Jam');
+  render() {
+    const { product } = this.state;
 
-  return (
-    <main className="section container">
-      <h1 className="title is-flex is-align-items-center">
-        {product ? `${product} is selected` : 'No goods selected'}
+    return (
+      <main className="section container">
+        <h1 className="title is-flex is-align-items-center">
+          {product ? `${product} is selected` : 'No goods selected'}
 
-        {product && (
-          // eslint-disable-next-line jsx-a11y/control-has-associated-label
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={() => setProduct(null)}
-          />
-        )}
-      </h1>
+          {product && (
+            // eslint-disable-next-line jsx-a11y/control-has-associated-label
+            <button
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+              onClick={() => this.setState({ product: null })}
+            />
+          )}
+        </h1>
 
-      <table className="table">
-        <tbody>
-          {goods.map(productName => (
-            <tr
-              key={uuidv4()}
-              data-cy="Good"
-              className={productName === product
-                ? 'has-background-success-light'
-                : ''}
-            >
-              <td>
-                <button
-                  data-cy={productName === product
-                    ? 'RemoveButton'
-                    : 'AddButton'}
-                  type="button"
-                  className={productName === product
-                    ? 'button is-info'
-                    : 'button'}
-                  onClick={() => setProduct(productName === product
-                    ? null
-                    : productName)}
+        <table className="table">
+          <tbody>
+            {goods.map(productName => {
+              const selectedProduct = productName === product;
+
+              return (
+                <tr
+                  key={uuidv4()}
+                  data-cy="Good"
+                  className={selectedProduct
+                    ? 'has-background-success-light'
+                    : ''}
                 >
-                  {productName === product ? '-' : '+'}
-                </button>
-              </td>
+                  <td>
+                    <button
+                      data-cy={selectedProduct
+                        ? 'RemoveButton'
+                        : 'AddButton'}
+                      type="button"
+                      className={selectedProduct
+                        ? 'button is-info'
+                        : 'button'}
+                      onClick={() => {
+                        this.setState({
+                          product: selectedProduct
+                            ? null
+                            : productName,
+                        });
+                      }}
+                    >
+                      {selectedProduct ? '-' : '+'}
+                    </button>
+                  </td>
 
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {productName}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </main>
-  );
-};
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {productName}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
