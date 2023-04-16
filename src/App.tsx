a64864f636528737f615d6e5c1ac536257f4120a
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-04-16 19:52:13 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
@@ -15,71 +16,92 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+export class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  render() {
+    const { selectedGood } = this.state;
+    const goodLength = selectedGood.length;
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+    return (
+      <main className="section container">
+        <h1 className="title is-flex is-align-items-center">
+          { (goodLength > 0)
+            ? (
+              `${selectedGood} is selected`
+            )
+            : 'No goods selected' }
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={(e) => {
+              this.setState({ selectedGood: '' });
+              e.currentTarget.remove();
+            }}
+          />
+        </h1>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+        <table className="table">
+          <tbody>
+            { goods.map(el => {
+              const isSelected = selectedGood === el;
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+              return (
+                <tr
+                  data-cy="Good"
+                  key={el}
+                  className={isSelected
+                    ? 'has-background-success-light'
+                    : ''}
+                >
+                  <td>
+                    <button
+                      data-cy={isSelected ? 'RemoveButton' : 'AddButton'}
+                      type="button"
+                      className={isSelected ? 'button is-info' : 'button'}
+                      style={{ display: 'flex' }}
+                      onClick={() => {
+                        this.setState({
+                          selectedGood: (isSelected) ? '' : el,
+                        });
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+                        const clearButton
+                        = document.querySelector('.ml-3');
+                        const title = document.querySelector('.title');
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+                        if (isSelected && clearButton) {
+                          clearButton.remove();
+                        }
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+                        if (!isSelected && !clearButton) {
+                          const newClearButton
+                          = document.createElement('button');
+
+                          newClearButton.className = 'delete ml-3';
+                          newClearButton.style.display = 'flex';
+                          title?.append(newClearButton);
+                        }
+                      }}
+                    >
+                      {isSelected
+                        ? '-'
+                        : '+'}
+                    </button>
+                  </td>
+                  <td data-cy="GoodTitle" className="is-vcentered" id={el}>
+                    {el}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
