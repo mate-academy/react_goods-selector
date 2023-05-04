144202e8c38060b595907ce58820e258ec9997d8
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-05-04 13:00:31 +0300

    added  goods[goods.indexOf(Jam)] || goods[0]

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ type Props = {};
 
 export class App extends Component<Props, State> {
   state: Readonly<State> = {
-    selectedGood: 'Jam',
+    selectedGood: goods[goods.indexOf('Jam')] || goods[0],
   };
 
   selectGood = (good: string) => {
