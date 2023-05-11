b7af1da78e7d335023b47326c1aa0b11a938e7e2
catgode <105614630+catgode@users.noreply.github.com>
2023-05-11 22:46:56 +0200

    Header checks for whether a good is selecter or not

--- src/App.tsx
+++ src/App.tsx
@@ -30,28 +30,33 @@ export class App extends React.Component<{}, State> {
   };
 
   render() {
+    const { selectedGood } = this.state;
+
     return (
 
       <main className="section container">
-        <h1 className="title">No goods selected</h1>
 
-        <h1 className="title is-flex is-align-items-center">
-          {`${this.state.selectedGood} is selected`}
+        {selectedGood
+          ? (
+            <h1 className="title is-flex is-align-items-center">
+              {`${selectedGood} is selected`}
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+              />
+            </h1>
 
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-          />
-        </h1>
+          )
+          : <h1 className="title">No goods selected</h1>}
 
         <table className="table">
           <tbody>
             {goods.map(good => {
               let isSelected = false;
 
-              if (good === this.state.selectedGood) {
+              if (good === selectedGood) {
                 isSelected = true;
               }
 
