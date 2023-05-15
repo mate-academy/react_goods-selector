7b255eb04550fb6131d93f12bda4f088c9051e8d
anton_chornobai <antonchornobajj@gmail.com>
2023-05-15 21:32:00 +0200

    lint fixed

--- src/App.tsx
+++ src/App.tsx
@@ -24,76 +24,72 @@ export class App extends React.Component<{}, State> {
     selectedGood: 'Jam',
   };
 
-  onClickHandler = (item: string | null) =>
-    this.setState({ selectedGood: item 
-  })
-
+  onClickHandler = (item: string | null) => {
+    this.setState({
+      selectedGood: item,
+    });
+  };
 
   render() {
     const { selectedGood } = this.state;
 
     return (
-      <main className="section container" >
-
-        {selectedGood
-          ? (
-            <h1 className="title is-flex is-align-items-center">
-              {selectedGood} is selected
-              <button
-                data-cy="ClearButton"
-                type="button"
-                className="delete ml-3"
-                title="button"
-                onClick={() => this.onClickHandler(null)}
-              />
-            </h1>
-          )
-          : (
-            <h1 className="title is-flex is-align-items-center">
-              No goods selected
-            </h1>
-          )
-        }
-
+      <main className="section container">
+        {selectedGood ? (
+          <h1 className="title is-flex is-align-items-center">
+            {`${selectedGood} is selected`}
+            <button
+              type="button"
+              data-cy="ClearButton"
+              className="delete ml-3"
+              onClick={() => this.onClickHandler(null)}
+            >
+              {' '}
+            </button>
+          </h1>
+        ) : (
+          <h1 className="title is-flex is-align-items-center">
+            No goods selected
+          </h1>
+        )}
 
         <table className="table">
           <tbody>
-            {goods.map((item, index) => {
+            {goods.map((item) => {
               return (
-                <tr data-cy="Good" key={index}  className={selectedGood === item
-                  ? 'has-background-success-light'
-                  : ''}>
+                <tr
+                  data-cy="Good"
+                  className={
+                    selectedGood === item ? 'has-background-success-light' : ''
+                  }
+                >
                   <td>
-                    {selectedGood === item
-                      ? (
-                        <button
-                          data-cy="RemoveButton"
-                          type="button"
-                          className="button is-info"
-                          onClick={() => this.onClickHandler(null)}
-                        >
-                          -
-                        </button>
-                      )
-                      : (
-                        <button
-                          data-cy="AddButton"
-                          type="button"
-                          className="button"
-                          onClick={() => this.onClickHandler(item)}
-                        >
-                          +
-                        </button>
-                      )}
+                    {selectedGood === item ? (
+                      <button
+                        data-cy="RemoveButton"
+                        type="button"
+                        className="button is-info"
+                        onClick={() => this.onClickHandler(null)}
+                      >
+                        -
+                      </button>
+                    ) : (
+                      <button
+                        data-cy="AddButton"
+                        type="button"
+                        className="button"
+                        onClick={() => this.onClickHandler(item)}
+                      >
+                        +
+                      </button>
+                    )}
                   </td>
-
                   <td data-cy="GoodTitle" className="is-vcentered">
                     {item}
                   </td>
                 </tr>
-              )
-            }
-            )}
+              );
+            })}
           </tbody>
         </table>
       </main>
