5af108e02e2fbb12d77ce9fe22ac2e65ee0c0cac
Emil Sukiennik <ewilemil@gmail.com>
2023-06-12 23:46:50 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,71 +15,119 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+type State = {
+  selectedGood: string,
+};
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+  handleClickOnGood = (good: string) => {
+    this.setState({ selectedGood: good });
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
+  handleClickOnClearButton = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  // Below is alternative Goods Button implementation
+
+  // button = (isSelected: boolean, good: string) => {
+  //   if (isSelected) {
+  //     return (
+  //       <button
+  //         data-cy="RemoveButton"
+  //         type="button"
+  //         className="button is-info"
+  //         onClick={this.handleClickOnClearButton}
+  //       >
+  //         -
+  //       </button>
+  //     );
+  //   }
+
+  //   return (
+  //     <button
+  //       data-cy="AddButton"
+  //       type="button"
+  //       className="button"
+  //       onClick={() => this.handleClickOnGood(good)}
+  //     >
+  //       +
+  //     </button>
+  //   );
+  // };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="section container">
+        <h1 className="title is-flex is-align-items-center">
+          {
+            selectedGood
+              ? `${selectedGood} is selected`
+              : 'No goods selected'
+          }
+
+          {selectedGood && (
+            // eslint-disable-next-line
             <button
-              data-cy="AddButton"
+              data-cy="ClearButton"
               type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+              className="delete ml-3"
+              onClick={this.handleClickOnClearButton}
+            />
+          )}
+        </h1>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+        <table className="table">
+          <tbody>
+            {goods.map(good => {
+              const isSelected = selectedGood === good;
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+              return (
+                <tr
+                  data-cy="Good"
+                  className={
+                    isSelected
+                      ? 'has-background-success-light'
+                      : ''
+                  }
+                >
+                  <td>
+                    {/* Below is alternative Goods Button implementation */}
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+                    {/* {this.button(isSelected, good)} */}
+                    <button
+                      data-cy={
+                        isSelected
+                          ? 'RemoveButton'
+                          : 'AddButton'
+                      }
+                      type="button"
+                      className={`button ${isSelected ? 'is-info' : ''}`}
+                      onClick={
+                        isSelected
+                          ? this.handleClickOnClearButton
+                          : () => this.handleClickOnGood(good)
+                      }
+                    >
+                      {isSelected ? '-' : '+'}
+                    </button>
+                  </td>
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
