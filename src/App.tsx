38c15344d62154a38d4be942c42545f45f37db1a
Ivan Khoroshylov <host513@gmail.com>
2023-06-14 21:24:04 +0300

    refactor solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import cn from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -39,63 +40,65 @@ export class App extends React.Component<{}, State> {
 
     return (
       <main className="section container">
-        <h1 className="title is-flex is-align-items-center">
-          {selectedGood
-            ? (
-              <>
-                { `${selectedGood} is selected`}
-                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                <button
-                  data-cy="ClearButton"
-                  type="button"
-                  className="delete ml-3"
-                  onClick={this.clearSelectedGood}
-                />
-              </>
-            )
-            : 'No goods selected'}
-        </h1>
+        {selectedGood
+          ? (
+            <h1 className="title is-flex is-align-items-center">
+              { `${selectedGood} is selected`}
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={this.clearSelectedGood}
+              />
+            </h1>
+          )
+          : <h1 className="title">No goods selected</h1>}
 
         <table className="table">
           <tbody>
-            {goods.map(good => (
-              <tr
-                data-cy="Good"
-                key={good}
-                className={selectedGood === good
-                  ? 'has-background-success-light'
-                  : ''}
-              >
-                <td>
-                  {selectedGood === good
-                    ? (
-                      <button
-                        data-cy="RemoveButton"
-                        type="button"
-                        className="button is-info"
-                        onClick={this.clearSelectedGood}
-                      >
-                        -
-                      </button>
-                    )
-                    : (
-                      <button
-                        data-cy="AddButton"
-                        type="button"
-                        className="button"
-                        onClick={() => this.setSelectedGood(good)}
-                      >
-                        +
-                      </button>
-                    )}
+            {goods.map(good => {
+              const isSelected = selectedGood === good;
 
-                </td>
+              return (
+                <tr
+                  data-cy="Good"
+                  key={good}
+                  className={cn({
+                    'has-background-success-light': isSelected,
+                  })}
+                >
+                  <td>
+                    {isSelected
+                      ? (
+                        <button
+                          data-cy="RemoveButton"
+                          type="button"
+                          className="button is-info"
+                          onClick={this.clearSelectedGood}
+                        >
+                          -
+                        </button>
+                      )
+                      : (
+                        <button
+                          data-cy="AddButton"
+                          type="button"
+                          className="button"
+                          onClick={() => this.setSelectedGood(good)}
+                        >
+                          +
+                        </button>
+                      )}
 
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>
-            ))}
+                  </td>
+
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </main>
