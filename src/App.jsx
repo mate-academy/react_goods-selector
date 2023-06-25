b3ebcaf29b7b1f27f0df3e053947afc3b2e6e1da
Nazar Hotko <nazar.hotko@outlook.com>
2023-06-25 21:16:40 +0200

    task solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,70 +15,142 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [food, setFood] = useState('');
 
+  function addFood(typeOfFood) {
+    return () => {
+      setFood(typeOfFood);
+    };
+  }
+
+  function resetFood() {
+    setFood('');
+  }
+
+  const noFoodResponse = (
     <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+      No goods selected
+    </h1>
+  );
 
+  const foodResponse = (
+    <h1 className="title is-flex is-align-items-center">
+      {`${food} is selected`}
       <button
         data-cy="ClearButton"
         type="button"
         className="delete ml-3"
+        onClick={resetFood}
       />
     </h1>
+  );
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+  return (
+    <main className="section container">
+      {food ? foodResponse : noFoodResponse}
+      <table className="table">
+        <tbody>
+          <tr
+            data-cy="Good"
+            className={food === 'Dumplings' && 'has-background-success-light'}
+          >
+            <td>
+              {food === 'Dumplings'
+                ? (
+                  <button
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                    onClick={resetFood}
+                  >
+                    -
+                  </button>
+                )
+                : (
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={addFood('Dumplings')}
+                  >
+                    +
+                  </button>
+                )}
+            </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Dumplings
+            </td>
+          </tr>
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+          <tr
+            data-cy="Good"
+            className={food === 'Jam' && 'has-background-success-light'}
+          >
+            <td>
+              {food === 'Jam'
+                ? (
+                  <button
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                    onClick={resetFood}
+                  >
+                    -
+                  </button>
+                )
+                : (
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={addFood('Jam')}
+                  >
+                    +
+                  </button>
+                )}
+            </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Jam
+            </td>
+          </tr>
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+          <tr
+            data-cy="Good"
+            className={food === 'Garlic' && 'has-background-success-light'}
+          >
+            <td>
+              {food === 'Garlic'
+                ? (
+                  <button
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                    onClick={resetFood}
+                  >
+                    -
+                  </button>
+                )
+                : (
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={addFood('Garlic')}
+                  >
+                    +
+                  </button>
+                )}
+            </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Garlic
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
+};
