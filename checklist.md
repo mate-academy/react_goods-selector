c0f7b34c29152113042518fcc2d726258ebd4e72
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-06-26 11:18:13 +0300

    update

--- checklist.md
+++ checklist.md
@@ -1,2 +1,103 @@
-1. Method's name should start with a verb (so you could easily tell what your method actually do)
-2. DON'T use index as a key.
+1. [CODE STYLE] - Method's name should start with a verb (so you could easily tell what your method actually do)
+
+BAD EXAMPLE:
+```jsx
+clickHandler = () => {
+ console.log('Hello, world');
+}
+```
+
+GOOD EXAMPLE:
+```jsx
+handleClick = () => {
+ console.log('Hello, world');
+}
+```
+
+2. [CODE KNOWLEDGE] - Create separate methods instead of creating inline event handlers (if we do so - we don't create new arrow function after component re-rendering)
+
+BAD EXAMPLE:
+```jsx
+<button 
+  type="button"
+  onClick={() => {}}
+>
+ Click me
+</button>
+```
+
+GOOD EXAMPLE:
+```jsx
+handleClick = () => {};
+
+return (
+  <button 
+    type="button"
+    onClick={handleClick}
+  >
+   Click me
+</button>
+)
+```
+
+3. [CODE KNOWLEDGE] - If your method get `event` as argument  or don't get any argument, you don't need to create new arrow function.
+Just pass your method.
+
+BAD EXAMPLE:
+```jsx
+handleClick = () => console.log(333);
+
+return (
+  <button 
+    type="button"
+    onClick={() => this.handleClick()}
+  >
+   Click me
+</button>
+)
+```
+
+
+GOOD EXAMPLE:
+```jsx
+handleClick = () => console.log(333);
+
+return (
+  <button 
+    type="button"
+    onClick={this.handleClick}
+  >
+   Click me
+</button>
+)
+```
+
+BAD EXAMPLE:
+```jsx
+handleClick = (event) => console.log(event);
+
+return (
+  <button 
+    type="button"
+    onClick={(event) => this.handleClick(event)}
+  >
+   Click me
+</button>
+)
+```
+
+GOOD EXAMPLE:
+```jsx
+handleClick = (event) => console.log(event);
+
+return (
+  <button 
+    type="button"
+    onClick={this.handleClick}
+  >
+   Click me
+</button>
+)
+```
+
+4. [CODE KNOWLEDGE] - NEVER EVER EVER use array index as a key prop
---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import './App.scss';
+
+const goods = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
+];
+
+type State = {
+  selectedGood: string | undefined;
+};
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    selectedGood: goods[goods.indexOf('Jam')] || goods[0],
+  };
+
+  handleButtonClick = (clickedGood: string) => {
+    const { selectedGood } = this.state;
+
+    this.setState({
+      selectedGood: selectedGood !== clickedGood ? clickedGood : undefined,
+    });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="section container">
+        {selectedGood ? (
+          <h1 className="title is-flex is-align-items-center">
+            {`${selectedGood} is selected`}
+            <button
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+              onClick={() => this.setState({ selectedGood: undefined })}
+            >
+              <span className="visually-hidden">Clear selected good</span>
+            </button>
+          </h1>
+        ) : (
+          <h1 className="title">No goods selected</h1>
+        )}
+
+        <table className="table">
+          <tbody>
+            {goods.map((good) => {
+              const isSelected = selectedGood === good;
+              const trClassName = isSelected
+                ? 'has-background-success-light' : '';
+              const buttonClassName = `button ${isSelected ? 'is-info' : ''}`;
+
+              return (
+                <tr key={good} data-cy="Good" className={trClassName}>
+                  <td>
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className={buttonClassName}
+                      onClick={() => this.handleButtonClick(good)}
+                    >
+                      {isSelected ? '-' : '+'}
+                    </button>
+                  </td>
+
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
