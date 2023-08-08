c8bc17117a6ca4e49a2ab4b98b179728695379b3
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-08 11:38:54 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable no-return-assign */
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
@@ -15,71 +20,124 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+export class App extends React.Component {
+  state = {
+    selectedGood: 'Jam',
+  };
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  reset = () => {
+    goods.forEach(good => {
+      const element:HTMLElement | null = document.getElementById(`${good}`);
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+      if (element) {
+        element.classList.remove('has-background-success-light');
+        const button:HTMLElement | null = element.querySelector('.button');
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
+        if (button && button.classList.contains('is-info')) {
+          button.classList.remove('is-info');
+          button.setAttribute('data-cy', 'AddButton');
+          button.textContent = '+';
+        }
+      }
+    });
+
+    this.setState({ selectedGood: null });
+  };
+
+  handleClick = (good: string) => {
+    const element:HTMLElement | null = document.getElementById(`${good}`);
+
+    if (element) {
+      if (element.classList.contains('has-background-success-light')) {
+        this.reset();
+      } else {
+        this.reset();
+
+        if (element) {
+          element.classList.add('has-background-success-light');
+          const button:HTMLElement | null = element.querySelector('.button');
+
+          if (button) {
+            goods.forEach(good => {
+              const overButton:HTMLElement | null = document.getElementById(`${good}`);
+
+              if (overButton) {
+                const otherButton:HTMLElement | null
+                = overButton.querySelector('.button');
+
+                if (otherButton) {
+                  otherButton.classList.remove('is-info');
+                  otherButton.setAttribute('data-cy', 'AddButton');
+                  otherButton.textContent = '+';
+                }
+              }
+            });
+
+            button.setAttribute('data-cy', 'RemoveButton');
+            button.classList.add('is-info');
+            button.textContent = '-';
+          }
+        }
+
+        this.setState({ selectedGood: good });
+      }
+    }
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="section container">
+        {selectedGood ? (
+          <h1 className="title is-flex is-align-items-center">
+            {selectedGood}
+            {' '}
+            is selected
+
             <button
-              data-cy="AddButton"
+              data-cy="ClearButton"
               type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+              className="delete ml-3"
+              onClick={this.reset}
+            />
+          </h1>
+        ) : (
+          <h1 className="title">
+            No goods selected
+          </h1>
+        )}
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+        <table className="table">
+          <tbody>
+            {goods.map(good => (
+              <tr
+                data-cy="Good"
+                id={good}
+                key={good}
+                className={good === 'Jam'
+                  ? 'has-background-success-light'
+                  : ''}
+              >
+                <td>
+                  <button
+                    data-cy={good === 'Jam' ? 'RemoveButton' : 'AddButton'}
+                    type="button"
+                    className={good === 'Jam' ? 'button is-info' : 'button'}
+                    onClick={() => this.handleClick(good)}
+                  >
+                    {good === 'Jam' ? '-' : '+'}
+                  </button>
+                </td>
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
