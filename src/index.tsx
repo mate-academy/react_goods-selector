fb9ba7d6b34419b12ddafc2856f2dc2fb9b22ff4
domkab <dominykas.bobinas@gmail.com>
2023-08-08 20:35:22 +0300

    rendered good list

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { App2 } from './App2';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <div>My app</div>
+    <App2 />
+  </div>,
   document.getElementById('root'),
 );
