239dcfa7320e5bce0f8c65b6994c90b3b0f93479
domkab <dominykas.bobinas@gmail.com>
2023-08-09 12:28:52 +0300

    1.0.0 initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { App2 } from './App2';
 
 ReactDOM.render(
-  <div>
-    <App />
-    <div>My app</div>
-    <App2 />
-  </div>,
+  <App />,
   document.getElementById('root'),
 );
