fbb6f165cbdd53cb57653cc3b394624b7e03efa8
Piotr <piotrkrasucki@gmail.com>
2023-08-29 16:04:04 +0200

    just testing some test solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,76 +15,86 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
-  const [selectedGood, setSelectedGood] = useState('Jam');
-
-  const handleGoodSelect = (good: string) => {
-    setSelectedGood(good);
-  };
-
-  const handleClearSelection = () => {
-    setSelectedGood('');
-  };
-
-  return (
-    <main className="section container">
-      <h1 className="title">
-        {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
-      </h1>
-
-      {selectedGood && (
-        <h1 className="title is-flex is-align-items-center">
-          {selectedGood}
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={handleClearSelection}
-          />
-        </h1>
-      )}
-
-      <table className="table">
-        <tbody>
-          {goods.map((good) => (
-            <tr
-              key={good}
-              data-cy="Good"
-              className={selectedGood === good
-                ? 'has-background-success-light'
-                : ''}
-            >
-              <td>
-                {!selectedGood && (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => handleGoodSelect(good)}
-                  >
-                    +
-                  </button>
-                )}
-                {selectedGood === good && (
-                  <button
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                    onClick={handleClearSelection}
-                  >
-                    -
-                  </button>
-                )}
-              </td>
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </main>
-  );
+type State = {
+  selectedGood: string;
 };
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  handleGoodSelect = (good: string) => {
+    this.setState({ selectedGood: good });
+  };
+
+  handleClearSelection = () => {
+    this.setState({ selectedGood: '' });
+  };
+
+  render() {
+    const { selectedGood } = this.state;
+
+    return (
+      <main className="section container">
+        <h1 className="title">
+          {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
+        </h1>
+
+        {selectedGood && (
+          <h1 className="title is-flex is-align-items-center">
+            {selectedGood}
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+              onClick={this.handleClearSelection}
+            />
+          </h1>
+        )}
+
+        <table className="table">
+          <tbody>
+            {goods.map((good) => (
+              <tr
+                key={good}
+                data-cy="Good"
+                className={selectedGood === good
+                  ? 'has-background-success-light'
+                  : ''}
+              >
+                <td>
+                  {!selectedGood && (
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => this.handleGoodSelect(good)}
+                    >
+                      +
+                    </button>
+                  )}
+                  {selectedGood === good && (
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={this.handleClearSelection}
+                    >
+                      -
+                    </button>
+                  )}
+                </td>
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
