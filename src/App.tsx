33b7c7c57f6c1706bc83edc78b5c2b0e857a8ca2
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-12-01 00:45:09 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,90 @@ export const goods = [
   'Garlic',
 ];
 
+type State = {
+  selectedGood: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedGood: 'Jam',
+  };
+
+  addGoodHandler = (element: string) => {
+    this.setState({ selectedGood: element });
+  }
+
+  removeGoodHandler = () => {
+    this.setState({ selectedGood: '' });
+  }
+
+  render() {
+    return (
+      <main className="section container">
+        {this.state.selectedGood.length > 0
+          ? (
+            <h1 className="title is-flex is-align-items-center">
+              {`${this.state.selectedGood} is selected`}
+
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={this.removeGoodHandler}
+              />
+            </h1>
+          )
+          : (<h1 className="title">No goods selected</h1>) }
+
+        <table className="table">
+          <tbody>
+            {goods.map((element => (
+              <tr
+                data-cy="Good"
+                key={element}
+                className={this.state.selectedGood === element
+                  ? ('has-background-success-light') : ('')}
+              >
+                <td>
+                  {this.state.selectedGood === element
+                    ? (
+                      <button
+                        data-cy="RemoveButton"
+                        type="button"
+                        className="button is-info"
+                        onClick={this.removeGoodHandler}
+                      >
+                        -
+                      </button>
+                    )
+                    : (
+                      <button
+                        data-cy="AddButton"
+                        type="button"
+                        className="button"
+                        onClick={
+                          () => this.addGoodHandler(element)
+                        }
+                      >
+                        +
+                      </button>
+                    ) }
+                </td>
+
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {element}
+                </td>
+              </tr>
+            )))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
+
+/*
 export const App: React.FC = () => (
   <main className="section container">
     <h1 className="title">No goods selected</h1>
@@ -22,7 +106,7 @@ export const App: React.FC = () => (
     <h1 className="title is-flex is-align-items-center">
       Jam is selected
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      //{ eslint-disable-next-line jsx-a11y/control-has-associated-label }
       <button
         data-cy="ClearButton"
         type="button"
@@ -83,3 +167,4 @@ export const App: React.FC = () => (
     </table>
   </main>
 );
+*/
