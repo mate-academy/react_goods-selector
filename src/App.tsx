2ae7d155dd1e0bf628fc1ca17d96ebbf1295588f
kate-prakofyeva <catherine.smiff@gmail.com>
2023-12-02 18:40:44 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
+import { useState } from 'react';
 import './App.scss';
 
 export const goods = [
@@ -15,71 +15,78 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+export const App = () => {
+  const [selectedGood, setSelectedGood] = useState<string>('Jam');
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  const handleClearSelected = () => {
+    setSelectedGood('');
+  };
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+  const handleSelectGood = (newGood:string) => {
+    setSelectedGood(selectedGood === newGood ? '' : newGood);
+  };
+
+  type Props = {
+    isSelected: boolean,
+    good: string,
+  };
+
+  const CustomButton = (props:Props) => {
+    const { isSelected, good } = props;
+
+    return (
       <button
-        data-cy="ClearButton"
+        data-cy={isSelected ? 'RemoveButton' : 'AddButton'}
         type="button"
-        className="delete ml-3"
-      />
-    </h1>
+        className={`button ${isSelected ? 'is-info' : ''}`}
+        onClick={() => handleSelectGood(good)}
+      >
+        {isSelected ? '-' : '+'}
+      </button>
+    );
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
+  return (
+    <main className="section container">
+      {!selectedGood
+        ? <h1 className="title">No goods selected</h1>
+        : (
+          <h1 className="title is-flex is-align-items-centper">
+            {`${selectedGood} is selected`}
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
-              data-cy="AddButton"
+              data-cy="ClearButton"
               type="button"
-              className="button"
+              className="delete ml-3"
+              onClick={handleClearSelected}
+            />
+          </h1>
+        )}
+      <table className="table">
+        <tbody>
+          {goods.map(good => (
+            <tr
+              data-cy="Good"
+              key={good}
+              className={
+                selectedGood === good ? 'has-background-success-light' : ''
+              }
             >
-              +
-            </button>
-          </td>
+              <td aria-label={good}>
+                <CustomButton
+                  isSelected={selectedGood === good}
+                  good={good}
+                />
+              </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {good}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
