a69f0ff2042c075728745a4d0655e43c3abd7596
Tommaso Giacopini <giacopini@bluewin.ch>
2023-12-04 16:02:31 +0100

    task solved

--- src/App.tsx
+++ src/App.tsx
@@ -15,71 +15,86 @@ export const goods = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <main className="section container">
-    <h1 className="title">No goods selected</h1>
+interface State {
+  goodSelected: string | null;
+}
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goodSelected: 'Jam',
+  };
 
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+  clearSelected = () => {
+    this.setState({ goodSelected: null });
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
+  onSelect = (selectedGood: string) => {
+    this.setState({ goodSelected: selectedGood });
+  };
+
+  render(): JSX.Element {
+    const { goodSelected } = this.state;
+
+    return (
+      <main className="section container">
+        {goodSelected ? (
+          <h1 className="title is-flex is-align-items-center">
+            {`${goodSelected} is selected`}
             <button
-              data-cy="AddButton"
+              data-cy="ClearButton"
+              aria-label="remove"
               type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+              className="delete ml-3"
+              onClick={this.clearSelected}
+            />
+          </h1>
+        ) : (
+          <h1 className="title">No goods selected</h1>
+        )}
+        <table className="table">
+          <tbody>
+            {goods.map((good) => (
+              goodSelected === good ? (
+                <tr
+                  data-cy="Good"
+                  className="has-background-success-light"
+                  key={good}
+                >
+                  <td>
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={this.clearSelected}
+                    >
+                      -
+                    </button>
+                  </td>
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              ) : (
+                <tr data-cy="Good" key={good}>
+                  <td>
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => this.onSelect(good)}
+                    >
+                      +
+                    </button>
+                  </td>
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              )
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
