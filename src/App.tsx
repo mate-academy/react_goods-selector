d96ba642c8d7a826a91128e2abf4f35f7f5ef2c6
Maryna Karpova <mari.artarea@gmail.com>
2024-03-29 11:14:56 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export const App: React.FC = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => clearSelection}
+            onClick={clearSelection}
           ></button>
         )}
       </h1>
