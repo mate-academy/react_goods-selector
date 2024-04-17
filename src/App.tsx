8f6cd2c49ad848e03cd21bd7ee2f20429165b9a7
spencerspe <spencerspe@gmail.com>
2024-04-17 14:57:40 +0100

    lint changes corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,79 @@
 import React, { useState } from 'react';
-  import 'bulma/css/bulma.css';
-  import './App.scss';
+import 'bulma/css/bulma.css';
+import './App.scss';
 
-  export const goods = [
-    'Dumplings',
-    'Carrot',
-    'Eggs',
-    'Ice cream',
-    'Apple',
-    'Bread',
-    'Fish',
-    'Honey',
-    'Jam',
-    'Garlic',
-  ];
+export const goods = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
+];
 
+export const App: React.FC = () => {
+  const [selectedGood, setSelectedGood] = useState('Jam');
 
-  export const App: React.FC = () => {
-
-    const [selectedGood, setSelectedGood] = useState('Jam');
-
-    const handleChange = (good: string) => {
-      good === selectedGood ? setSelectedGood('') : setSelectedGood(good);
+  const handleChange = (good: string) => {
+    if (good === selectedGood) {
+      setSelectedGood('');
+    } else {
+      setSelectedGood(good);
     }
+  };
 
-    return (
-      <>
-          <main className="section container">
+  return (
+    <>
+      <main className="section container">
 
-      <h1 className="title is-flex is-align-items-center">
-        {`${selectedGood !== '' ? `${selectedGood} is selected` : 'No goods selected'}`}
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            {selectedGood !== '' && (
-                <button
-                data-cy="ClearButton"
-                type="button"
-                className="delete ml-3"
-                onClick={() => setSelectedGood('')}
-              />
-            )
-            }
+        <h1 className="title is-flex is-align-items-center">
+          {`${selectedGood !== '' ? `${selectedGood} is selected` : 'No goods selected'}`}
+          {selectedGood !== '' && (
+            <button
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+              onClick={() => setSelectedGood('')}
+            >
+              clear
+            </button>
+          )}
 
-      </h1>
+        </h1>
 
-      <table className="table">
-            <tbody>
-                {goods.map((good, index) => (
-                  <tr
-                    data-cy="Good"
-                    className={`${good === selectedGood && ('has-background-success-light')}`}
+        <table className="table">
+          <tbody>
+            {goods.map((good) => (
+              <tr
+                data-cy="Good"
+                className={`${good === selectedGood && ('has-background-success-light')}`}
+              >
+                <td>
+                  <button
+                    key={good}
+                    value={good}
+                    onClick={() => handleChange(good)}
+                    data-cy={`${good === selectedGood ? 'RemoveButton' : 'AddButton'}`}
+                    type="button"
+                    className={`${good === selectedGood && ('is-info')} button`}
                   >
-                          <td>
-                      <button
-                        key={index}
-                        value={good}
-                        onClick={() => handleChange(good)}
-                        data-cy={`${good === selectedGood ? 'RemoveButton' : 'AddButton'}`}
-                        type="button"
-                        className={`${good === selectedGood && ('is-info')} button`}>
-                              {good === selectedGood ? '-' : '+'}
-                            </button>
-                          </td>
+                    {good === selectedGood ? '-' : '+'}
+                  </button>
+                </td>
 
-                          <td data-cy="GoodTitle" className="is-vcentered">
-                            {good}
-                          </td>
-                        </tr>
-                ))}
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
 
-        </tbody>
-      </table>
-    </main>
-      </>
-
-    )
-
-    };
+          </tbody>
+        </table>
+      </main>
+    </>
+  );
+};
