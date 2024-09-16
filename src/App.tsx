598909a1eeed968b0a4f200862f3f69d86375d94
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-16 14:38:20 +0200

    feat: alternate with h1 content

--- src/App.tsx
+++ src/App.tsx
@@ -25,20 +25,22 @@ export class App extends React.Component {
 
     return (
       <main className="section container">
-        <h1 className="title">No goods selected</h1>
-
-        <h1 className="title is-flex is-align-items-center">
-          {selectedGood} is selected
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          {selectedGood !== '' && (
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => this.setState({ selectedGood: '' })}
-            />
-          )}
-        </h1>
+        {selectedGood === '' ? (
+          <h1 className="title">No goods selected</h1>
+        ) : (
+          <h1 className="title is-flex is-align-items-center">
+            {selectedGood} is selected
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            {selectedGood !== '' && (
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={() => this.setState({ selectedGood: '' })}
+              />
+            )}
+          </h1>
+        )}
 
         <table className="table">
           <tbody>
